316321428864668033dd2f21d5268aa094662485
Ger Hean <gerhean@live.com>
2020-01-17 19:48:49 +0800

    Level-1
    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,55 @@
+import java.util.*;
+
 public class Duke {
+    private void run() {
+        Scanner sc = new Scanner(System.in);
+        String logo = "*******   **     ** **   ** ********\n"
+                + "/**////** /**    /**/**  ** /**/////\n"
+                + "/**    /**/**    /**/** **  /**\n"
+                + "/**    /**/**    /**/****   /*******\n"
+                + "/**    /**/**    /**/**/**  /**////\n"
+                + "/**    ** /**    /**/**//** /**\n"
+                + "/*******  //******* /** //**/********\n"
+                + "///////    ///////  //   // //////// \n";
+        System.out.println("\nHi hi I'm \n" + logo);
+        String[] text = {"Master! I'm so glad you used me!", "What will you do to me today?"};
+        dukeSays(text);
+        boolean running = true;
+        while(running){
+            System.out.print("\nMaster: ");
+            String inp = sc.nextLine();
+            System.out.println("");
+            switch (inp){
+                case "bye":
+                    running = false;
+                    break;
+                default:
+                    dukeSays(inp);
+            }
+        }
+        text = new String[] {"Are you leaving already?", "Please come back and play with Duke soon..."};
+        dukeSays(text);
+    }
+
+    private void dukeSays(String[] text) {
+        boolean first = true;
+        for(String line : text) {
+            if(first){
+                System.out.print("Duke: ");
+                first = false;
+            } else{
+                System.out.print("      ");
+            }
+            System.out.println(line);
+        }
+    }
+
+    private void dukeSays(String text) {
+        System.out.println("Duke: " + text);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke newDuke = new Duke();
+        newDuke.run();
     }
 }
