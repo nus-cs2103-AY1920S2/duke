e4cfe14172a429ac20f98a7d882513e60e094428
joel-lim <joellimaojun@hotmail.com>
2020-01-17 19:57:35 +0800

    [Feat] - Add ability to store texts entered by user and display when requested

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,27 +1,44 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Duke {
     public static void main(String[] args) {
         System.out.print(createGreeting());
         Scanner sc = new Scanner(System.in);
+        List<String> texts = new ArrayList<String>();
         while (true) {
             String cmd = sc.nextLine();
             if (cmd.equalsIgnoreCase("bye")) {
                 System.out.print(formatReply("Bye. Hope to see you again soon!"));
                 sc.close();
                 return;
+            } else if (cmd.equalsIgnoreCase("list")) {
+                System.out.print(formatReply(list(texts)));
+            } else {
+                texts.add(cmd);
+                System.out.print(formatReply("added: " + cmd));
             }
-            System.out.print(formatReply(cmd));
         }
+    }
+
+    private static String list(List<String> texts) {
+        int counter = 1;
+        StringBuilder sb = new StringBuilder();
+        for (String text : texts) {
+            sb.append(counter);
+            sb.append(". ");
+            sb.append(text);
+            sb.append("\n");
+            counter += 1;
+        }
+        return sb.toString();
     }
 
     private static String createGreeting() {
         StringBuilder sb = new StringBuilder();
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = " ____        _        \n" + "|  _ \\ _   _| | _____ \n" + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n" + "|____/ \\__,_|_|\\_\\___|\n";
         sb.append(logo);
         sb.append("\n");
         sb.append(formatReply("Hello! I'm Duke\nWhat can I do for you?"));
