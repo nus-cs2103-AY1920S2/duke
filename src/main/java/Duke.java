01670e76c835e8ac83aa498d7269be5205b11d07
Leslie Harland <leslieharland@outlook.com>
2020-01-18 00:31:48 +0800

    Enhancement: Chat UI

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,23 +11,44 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello I'm Duke\nWhat can I do for you?");
         System.out.println("____________________________________________________________");
-        String input = "";
-        while(!input.equals("bye")) {
-            input = sc.next();
-            switch (input) {
-                case "bye":
+        String[] input = new String[100];
+        int i = 0;
+        String current = "";
+        while(!(current = sc.nextLine()).equals("bye")) {
+            switch (current) {
+                case "read book":
                     System.out.println("    ____________________________________________________________");
-                    System.out.println("    Bye. Hope to see you again soon!");
+                    System.out.println("    added: " + current);
+                    System.out.println("    ____________________________________________________________");
+                    break;
+                case "return book":
+                    System.out.println("    ____________________________________________________________");
+                    System.out.println("    added: " + current);
+                    System.out.println("    ____________________________________________________________");
+                    break;
+                case "list":
+                    System.out.println("    ____________________________________________________________");
+                    for(int count = 0; count < i; count++){
+                        System.out.println("    " + (count+1) + ". " + input[count]);
+                    }
+
                     System.out.println("    ____________________________________________________________");
                     break;
                 default:
                     System.out.println("    ____________________________________________________________");
-                    System.out.println("    " + input);
+                    System.out.println("    " + current);
                     System.out.println("    ____________________________________________________________");
                     break;
 
 
             }
+            input[i] = current;
+            i++;
+
         }
+
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Bye. Hope to see you again soon!");
+        System.out.println("    ____________________________________________________________");
     }
 }
