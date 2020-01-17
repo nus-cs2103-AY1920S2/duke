904630889c0c7d37e7deb82a5574cf05deb2c6e8
Ger Hean <gerhean@live.com>
2020-01-17 20:42:03 +0800

    1.02
    add gradle and checkstyle

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
-import java.util.*;
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     private void run() {
@@ -17,20 +18,20 @@ public class Duke {
 
         System.out.println("\nHi hi I'm \n" + logo);
         dukeSays(new String[] {"Master! I'm so glad you used me!", "What will you do to me today?"});
-        while(running){
+        while (running) {
             System.out.print("\nMaster: ");
             String inp = sc.nextLine();
             System.out.println("");
-            switch (inp){
-                case "bye":
-                    running = false;
-                    break;
-                case "list":
-                    dukeSays("Master already forgotten what Master wanted to do?!");
-                    break;
-                default:
-                    dukeSays("So Master wants to " + inp + "...");
-                    tasks.add(inp);
+            switch (inp) {
+            case "bye":
+                running = false;
+                break;
+            case "list":
+                dukeSays("Master already forgotten what Master wanted to do?!");
+                break;
+            default:
+                dukeSays("So Master wants to " + inp + "...");
+                tasks.add(inp);
             }
         }
         dukeSays(new String[] {"Are you leaving already?", "Please come back and play with Duke soon..."});
@@ -38,11 +39,11 @@ public class Duke {
 
     private void dukeSays(String[] text) {
         boolean first = true;
-        for(String line : text) {
-            if(first){
+        for (String line : text) {
+            if (first) {
                 System.out.print("Duke: ");
                 first = false;
-            } else{
+            } else {
                 System.out.print("      ");
             }
             System.out.println(line);
