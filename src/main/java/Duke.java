4ab41cac55a1b350e93acf73020f9a4b6681c57e
joel-lim <joellimaojun@hotmail.com>
2020-01-17 17:03:04 +0800

    [Refact] - Create new method for greeting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
 public class Duke {
     public static void main(String[] args) {
+        System.out.println(createGreeting());
+    }
+
+    private static String createGreeting() {
+        StringBuilder sb = new StringBuilder();
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String lineBreak = "____________________________________________________________\n";
+        sb.append("Hello from\n");
+        sb.append(logo);
+        sb.append(lineBreak);
+        sb.append("Hello! I'm Duke\nWhat can I do for you?\n");
+        sb.append(lineBreak);
+        return sb.toString();
     }
 }
