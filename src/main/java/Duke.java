ce0ef311391c46800c9cc64a8aaef96c05d2a98e
joel-lim <joellimaojun@hotmail.com>
2020-01-17 17:15:30 +0800

    [Feat] - Add feature to echo user's commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         System.out.println(createGreeting());
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String cmd = sc.nextLine();
+            if (cmd.equalsIgnoreCase("bye")) {
+                System.out.println(wrapLineBreak("Bye. Hope to see you again soon!"));
+                sc.close();
+                return;
+            }
+            System.out.println(wrapLineBreak(cmd));
+        }
     }
 
     private static String createGreeting() {
@@ -10,12 +22,16 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        String lineBreak = "____________________________________________________________\n";
         sb.append("Hello from\n");
         sb.append(logo);
-        sb.append(lineBreak);
-        sb.append("Hello! I'm Duke\nWhat can I do for you?\n");
-        sb.append(lineBreak);
+        sb.append(wrapLineBreak("Hello! I'm Duke\nWhat can I do for you?"));
         return sb.toString();
+    }
+
+    private static String wrapLineBreak(String str) {
+        return "____________________________________________________________\n"
+            + str
+            + "\n"
+            + "____________________________________________________________\n";
     }
 }
