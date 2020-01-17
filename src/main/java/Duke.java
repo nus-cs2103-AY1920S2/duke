a53b2a28f849959b19a5832442c5909d5b45c24e
Joshua Lay <joshualay@u.nus.edu>
2020-01-17 22:22:51 +0800

    Added Level 1
    Signed-off-by: Joshua <x3tvaddict@hotmail.com>

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,31 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+
+        // Welcome message for the user
+        String welcome_message = "____________________________________________________________\n" +
+                "Hello! I'm Duke\n" + "What can I do for you today?\n"+
+                "____________________________________________________________";
+
+        String lines = "        ____________________________________________________________";
+
+        System.out.println(welcome_message);
+
+        while(sc.hasNext()) {
+            String input = sc.next();
+            if(input.equals("bye")) {
+                System.out.println(lines);
+                System.out.println("        Bye. Hope to see you again soon");
+                System.out.println(lines);
+                break;
+            }
+            // Print out the input
+            System.out.println(lines);
+            System.out.println("        "+input);
+            System.out.println(lines);
+
+        }
     }
 }
