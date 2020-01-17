431ea0384a63afd1e2ab87146bbbd9d2d6d0cc79
Joshua Lay <x3tvaddict@hotmail.com>
2020-01-17 22:47:16 +0800

    Added Level 2
    Signed-off-by: Joshua <x3tvaddict@hotmail.com>

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        ArrayList<String> stored_list = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
 
         // Welcome message for the user
@@ -9,21 +11,32 @@ public class Duke {
                 "Hello! I'm Duke\n" + "What can I do for you today?\n"+
                 "____________________________________________________________";
 
+        // Lines are for in between the two words
         String lines = "        ____________________________________________________________";
+        String space = "        ";
 
         System.out.println(welcome_message);
 
         while(sc.hasNext()) {
-            String input = sc.next();
+            String input = sc.nextLine();
             if(input.equals("bye")) {
                 System.out.println(lines);
                 System.out.println("        Bye. Hope to see you again soon");
                 System.out.println(lines);
+                stored_list.clear();
                 break;
+            } else if (input.equals("list")) {
+                System.out.println(lines);
+                for(int i=0; i< stored_list.size(); i++) {
+                    System.out.println(space + (i+1) + ". " + stored_list.get(i));
+                }
+                System.out.println(lines);
+                continue;
             }
+            stored_list.add(input);
             // Print out the input
             System.out.println(lines);
-            System.out.println("        "+input);
+            System.out.println("         added: "+input);
             System.out.println(lines);
 
         }
