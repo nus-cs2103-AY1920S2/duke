ca0074027a244b72edfbc8c354ff7629741a7ae8
Daniel Hoe <danielhoe97@gmail.com>
2020-01-18 18:38:31 +0800

    Level-2: Add, List
    Updated the bot as per the Level 2 requirements, and created a separate class for DukeBot.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,32 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
 
-
-
+    /**
+     * Main method of Duke. It initialises the bot and reads user input.
+     * @param args No args applicable.
+     */
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("I'm your personal chat bot assistant! How may I be of service today?");
+        DukeBot duke = new DukeBot();
+        duke.dukeHi();
 
         Scanner sc = new Scanner(System.in);
 
-        boolean isActive = true;
-        while(isActive) {
-            String command = sc.next();
-
-            switch(command) {
-                case "bye":
-                    System.out.println("Bye. Hope to see you again soon!");
-                    isActive = false;
-                    break;
-                default:
-                    System.out.println(command);
-            }
+        while(duke.isActive()) {
+            String command = sc.nextLine();
+            duke.processCommand(command);
         }
     }
 }
