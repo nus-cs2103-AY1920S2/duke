49b6231a1f3747f04429ffb591e3bce31394a96a
ignatiusong <e0324134@u.nus.edu>
2020-01-18 17:26:44 +0800

    add goodbye function and ability to repeat given strings

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,36 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+
         System.out.println("Hello from\n" + logo);
+        System.out.println("What can I do for you?");
+
+        while(true) {
+            String command = sc.next();
+            boolean wantToQuit = false;
+            switch (command) {
+                case "bye":
+                    wantToQuit = true;
+                    System.out.println("Goodbye!! See you some time soon.");
+
+            }
+
+            if(wantToQuit) {
+                break;
+            }
+
+            System.out.println((command));
+        }
+
+
+
+
     }
 }
