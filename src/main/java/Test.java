b0649466433f8db58ec556f3461287ca4498b329
Ray-97 <rc97@u.nus.edu>
2020-01-18 22:13:44 +0800

    Level-3

--- src/main/java/Test.java
+++
@@ -1,9 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        String strArr = "done 1";
-        for (String s : strArr.split(" ")) {
-            System.out.println(s);
-        }
-        System.out.println(strArr.split(" ")[0].equals("done"));
-    }
-}
