71effa40f41ca943ef6796042fc4f3e92816bded
Ger Hean <gerhean@live.com>
2020-01-18 12:07:42 +0800

    Level-3
    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,49 +1,76 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.lang.StringBuilder;
 
 public class Duke {
+    private static final String logo = "*******   **     ** **   ** ********\n"
+            + "/**////** /**    /**/**  ** /**/////\n"
+            + "/**    /**/**    /**/** **  /**\n"
+            + "/**    /**/**    /**/****   /*******\n"
+            + "/**    /**/**    /**/**/**  /**////\n"
+            + "/**    ** /**    /**/**//** /**\n"
+            + "/*******  //******* /** //**/********\n"
+            + "///////    ///////  //   // //////// \n";
     private boolean sayFirst = true;
-    private boolean sayNumbered = false;
 
     private void run() {
-        String logo = "*******   **     ** **   ** ********\n"
-                + "/**////** /**    /**/**  ** /**/////\n"
-                + "/**    /**/**    /**/** **  /**\n"
-                + "/**    /**/**    /**/****   /*******\n"
-                + "/**    /**/**    /**/**/**  /**////\n"
-                + "/**    ** /**    /**/**//** /**\n"
-                + "/*******  //******* /** //**/********\n"
-                + "///////    ///////  //   // //////// \n";
 
         Scanner sc = new Scanner(System.in);
         boolean running = true;
-        ArrayList<String> tasks = new ArrayList<String>();
+        ArrayList<Task> tasks = new ArrayList<>();
 
         System.out.println("\nHi hi I'm \n" + logo);
-        dukeSays(new String[] {"Master! I'm so glad you used me!", "What will you do to me today?"});
+        dukeSays(new String[] {"Master! I'm so glad you used me!", "What will you do today?"});
         while (running) {
             System.out.println("\nMaster: ");
             String inp = sc.nextLine();
-            System.out.println("");
-            switch (inp) {
+            System.out.println();
+            String[] inpArr = inp.split(" ");
+            switch (inpArr[0]) {
+            case "":
+                dukeSays("I can't hear anything Master... Is Master all right?");
+                break;
             case "bye":
                 running = false;
                 break;
             case "list":
-                dukeSays(new String[] {"Master already forgotten what Master wanted to do?!"
-                    ,"Duke has no choice but to remind Master then!"});
-                if (tasks.size()==0) {
+                dukeSays(new String[] {"Master already forgotten what Master wanted to do?!",
+                    "Duke has no choice but to remind Master then!"});
+                sayFirst = false;
+                if (tasks.size() == 0) {
                     dukeSays("Huh there are no tasks! Master is so forgetful...");
                 } else {
                     dukeSays("These are the tasks which Master forgot:");
-                    this.sayFirst = false;
-                    this.sayNumbered = true;
-                    dukeSays(tasks.toArray(new String[tasks.size()]));
+                    sayTasks(tasks);
+                }
+                break;
+            case "done":
+                if (inpArr.length == 1) {
+                    dukeSays("Duke doesn't think Master has done anything yet...");
+                } else {
+                    try {
+                        int taskInd = Integer.parseInt(inpArr[1]) - 1;
+                        if (taskInd >= tasks.size() || taskInd < 0) {
+                            dukeSays("Duke can't seem to recall that item...");
+                        } else {
+                            Task task = tasks.get(taskInd);
+                            if (task.getDone()) {
+                                dukeSays("Didn't Master already do that?");
+                            } else {
+                                dukeSays(new String[] {"So master finally completed " + task + "?",
+                                        "Duke's really proud of Master!"
+                                });
+                                task.setDone();
+                            }
+                        }
+                    } catch (NumberFormatException e) {
+                        dukeSays("Stop teasing Duke... Even Duke knows that isn't a number...");
+                    }
                 }
                 break;
             default:
                 dukeSays("So Master wants to " + inp + "...");
-                tasks.add(inp);
+                tasks.add(new Task(inp));
             }
         }
         dukeSays(new String[] {"Are you leaving already?", "Please come back and play with Duke soon..."});
@@ -51,12 +78,8 @@ public class Duke {
 
     private void dukeSays(String[] text) {
         boolean first = this.sayFirst;
-        int i = 1;
         for (String line : text) {
-            if (this.sayNumbered) {
-                System.out.print("      " + Integer.toString(i) + ". ");
-                i += 1;
-            } else if (first) {
+            if (first) {
                 System.out.print("Duke: ");
                 first = false;
             } else {
@@ -65,11 +88,22 @@ public class Duke {
             System.out.println(line);
         }
         this.sayFirst = true;
-        this.sayNumbered = false;
     }
 
     private void dukeSays(String line) {
         dukeSays(new String[] {line});
+    }
+
+    private void sayTasks(ArrayList<Task> tasks) {
+        int i = 1;
+        for (Task task : tasks) {
+            System.out.println("      "
+                + i + ". "
+                + task
+                + (task.getDone() ? " (Done!)" : "")
+            );
+            i += 1;
+        }
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/Task.java
@@ -0,0 +1,28 @@
+public class Task {
+    protected String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void setDone() {
+        this.isDone = true;
+    }
+
+    public boolean getDone() {
+        return this.isDone;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+
+    @Override
+    public String toString() {
+        return (description);
+    }
+
+    //...
+}
