3970184ae64d0e67a83ff878122e6f38b09e778c
Ray-97 <rc97@u.nus.edu>
2020-01-18 11:58:28 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,23 +1,38 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        List<String> taskList = new ArrayList<>(100);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        String query = "What can the Duke do for you today?";
+        String prompt = "Anything Duke can do for you?";
         String exitInfo = "Enter bye to exit!";
-        String command = "";
-        while (!command.equals("bye")) {
-            System.out.println(query);
-            System.out.println(exitInfo);
-            command = scanner.nextLine().toLowerCase();
-            System.out.println(command);
+        String exitMessage = "Goodbye, see you again!";
+        String instruction = "";
+        System.out.println(exitInfo);
+        while (true) {
+            System.out.println(prompt);
+            instruction = scanner.nextLine().toLowerCase();
+            if (instruction.equals("bye")) {
+                break;
+            } else if (instruction.equals("list")) {
+                for (int i = 0; i < taskList.size(); i++) {
+                    System.out.format("%s." + taskList.get(i) + '\n', String.valueOf(i + 1));
+                }
+            } else {
+                taskList.add(instruction);
+                System.out.print("Added: ");
+                System.out.println(instruction);
+            }
         }
+        System.out.println(exitMessage);
         scanner.close();
     }
 }
