68cfb48c2ce6bb714d74a783492dfa4b15eb4bd6
aliciatxl <example@mail.com>
2020-01-19 12:51:38 +0800

    Implemented Add, List in level 2,  which is to store user input and display them back to user when requested.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Duke {
     public static void main(String[] args) {
@@ -9,8 +11,8 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
 
+        System.out.println("Hello from\n" + logo);
         System.out.println(
                 "    ____________________________________________________________\n" +
                 "     Hey friend! I'm Duke V2.0.0 at your service\n" +
@@ -20,28 +22,49 @@ public class Duke {
 
         BufferedReader bufferedReader = new BufferedReader (new InputStreamReader(System.in));
         String input;
+        List<String> list = new ArrayList<>();
 
+        // Parse each command by user
         while (true) {
+            // Get command entered by user
             try {
                  input = bufferedReader.readLine();
             } catch (IOException e) {
                 System.out.println("Error in reading input");
                 break;
             }
-            if (input.equals("bye")) {
-                System.out.println(
-                        "    _____________________________DUKE___________________________\n" +
-                        "     Sorry to see you go. Hope to see you again soon! (＾▽＾)／\n" +
-                        "    _________★゜・。。・゜゜・。。・゜☆゜・。。・゜゜・。。・゜★_______\n"
-                );
-                break;
-            } else {
-                System.out.println(
-                        "    _____________________________DUKE___________________________\n" +
-                        "     " + input + "\n" +
-                        "    _________★゜・。。・゜゜・。。・゜☆゜・。。・゜゜・。。・゜★_______\n"
-                );
+
+            switch (input) {
+                case "bye":
+                    // Exit program
+                    prettyPrint("Sorry to see you go. Hope to see you again soon! (＾▽＾)／");
+                    break;
+                case "list":
+                    String listItems = "";
+                    Integer listSize = list.size();
+
+                    // Print out all items in list
+                    for(int i = 0; i < listSize; i++) {
+                        listItems += (i + 1) + ". " + list.get(i);
+                        if (i != listSize - 1) {
+                            listItems += "\n     ";
+                        }
+                    }
+                    listItems = listItems.equals("") ? "There is nothing on your list." : listItems;
+                    prettyPrint(listItems);
+                    break;
+                default:
+                    list.add(input);
+                    prettyPrint("You have added " + input);
             }
         }
+    }
+
+    public static void prettyPrint(String line) {
+        System.out.println(
+                "    _____________________________DUKE___________________________\n" +
+                "     " + line + "\n" +
+                "    _________★゜・。。・゜゜・。。・゜☆゜・。。・゜゜・。。・゜★_______\n"
+        );
     }
 }
