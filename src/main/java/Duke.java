8301079d8667de467580dfd19eabc4eb6ef60529
zixinn <ng.zixin99@gmail.com>
2020-01-19 15:07:50 +0800

    Add add, list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,12 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Duke {
     private static final String indent = "    ";
     private static final String horizontal_line = "____________________________________________________________";
+
+    private static List<String> list = new ArrayList<>();
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -16,7 +20,12 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         while (!input.equals("bye")) {
-            print(input);
+            if (input.equals("list")) {
+                printList();
+            } else {
+                list.add(input);
+                print("added: " + input);
+            }
             input = sc.nextLine();
         }
         print("Bye. Hope to see you again soon!");
@@ -28,6 +37,14 @@ public class Duke {
         System.out.println(indent + horizontal_line);
         for (String line : lines) {
             System.out.println(indent + line);
+        }
+        System.out.println(indent + horizontal_line + "\n");
+    }
+
+    private static void printList() {
+        System.out.println(indent + horizontal_line);
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(indent + (i + 1) + ". " + list.get(i));
         }
         System.out.println(indent + horizontal_line + "\n");
     }
