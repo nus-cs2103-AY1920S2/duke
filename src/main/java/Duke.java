62d64a9bea6d5e5c9b706660ac92bf1af05db81f
zixinn <ng.zixin99@gmail.com>
2020-01-19 14:54:05 +0800

    Add greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,34 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String indent = "    ";
+    private static final String horizontal_line = "____________________________________________________________";
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        print(logo + "\nHello! I'm Duke\nWhat can I do for you?");
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        while (!input.equals("bye")) {
+            print(input);
+            input = sc.nextLine();
+        }
+        print("Bye. Hope to see you again soon!");
+        sc.close();
+    }
+
+    private static void print(String text) {
+        String[] lines = text.split("\n");
+        System.out.println(indent + horizontal_line);
+        for (String line : lines) {
+            System.out.println(indent + line);
+        }
+        System.out.println(indent + horizontal_line + "\n");
     }
 }
