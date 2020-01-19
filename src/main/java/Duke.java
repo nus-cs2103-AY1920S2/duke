eff74eb8bac1279dae77bba33137ec73c076be5d
alushingg <ang.lu.shing99@gmail.com>
2020-01-19 16:57:38 +0800

    add todos, events, deadlines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,7 +9,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        output("Hello! I'm Duke\n     What can I do for you?");
+        output(new String[]{"Hello! I'm Duke\n     What can I do for you?"});
         Scanner sc = new Scanner(System.in);
         String op = sc.nextLine();
         ArrayList<Task> list = new ArrayList<>();
@@ -17,26 +17,50 @@ public class Duke {
             if (op.equals("list")) {
                 show_list(list);
                 op = sc.nextLine();
-            } else if (op.indexOf("done") == 0) {
-                String[] temp = op.split(" ", 2);
-                Task task = list.get(Integer.parseInt(temp[1]) - 1);
-                task.markAsDone();
-                output("Nice! I've marked this task as done:\n      " +
-                            task.toString());
-                op = sc.nextLine();
             } else {
-                output("added: " + op);
-                list.add(new Task(op));
+                String[] temp = op.split(" ", 2);
+                String cmd = temp[0];
+                String body = temp[1];
+                if (cmd.equals("done")) {
+                    Task task = list.get(Integer.parseInt(body) - 1);
+                    task.markAsDone();
+                    output(new String[]{"Nice! I've marked this task as done:",
+                            task.toString()});
+                } else if (cmd.equals("todo")) {
+                    Task task = new ToDo(body);
+                    int size = list.size();
+                    output(new String[]{"Got it. I've added this task:", " " + task.toString(),
+                                "Now you have " + (size+1) + " tasks in the list."});
+                    list.add(task);
+                } else if (cmd.equals("deadline")) {
+                    String[] task_date = body.split(" /by ");
+                    Task task = new Deadline(task_date[0], task_date[1]);
+                    int size = list.size();
+                    output(new String[]{"Got it. I've added this task:", " " + task.toString(),
+                            "Now you have " + (size+1) + " tasks in the list."});
+                    list.add(task);
+                } else {
+                    String[] task_date = body.split(" /at ");
+                    Task task = new Event(task_date[0], task_date[1]);
+                    int size = list.size();
+                    output(new String[]{"Got it. I've added this task:", " " + task.toString(),
+                            "Now you have " + (size+1) + " tasks in the list."});
+                    list.add(task);
+
+                }
                 op = sc.nextLine();
+
             }
         }
-        output("Bye. Hope to see you again soon!");
+        output(new String[]{"Bye. Hope to see you again soon!"});
     }
 
-    private static void output(String op) {
+    private static void output(String[] op) {
         String divider = "    -------------------------------------------------------";
         System.out.println(divider);
-        System.out.println("     " + op);;
+        for (String s: op) {
+            System.out.println("     " + s);
+        }
         System.out.println(divider);
     }
     private static void show_list(ArrayList<Task> list) {
