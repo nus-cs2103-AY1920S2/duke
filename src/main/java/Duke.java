5d2c12b638db499458ddcbbafdfa6346d5219fb1
CheyanneSim <Cheyanne@u.nus.edu>
2020-01-19 15:09:39 +0800

    add suport for tracking todos, deadlines and events

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,12 +3,12 @@ import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
-        /*String logo = " ____        _        \n"
+        String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);*/
+        System.out.println("Hello from\n" + logo);
         ArrayList<Task> tasks = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
         System.out.println("\n    ––––––––––––––––––––––––––––––––––––––––––––––––––––––––");
@@ -20,6 +20,7 @@ public class Duke {
         while (!command[0].equals("bye")) {
             System.out.println("\n    ––––––––––––––––––––––––––––––––––––––––––––––––––––––––");
             if (command[0].equals("list")) {
+                System.out.println("     Here are the tasks in your list:");
                 for (int i = 0; i < tasks.size(); i++) {
                     System.out.println("     " + (i + 1) + ". " + tasks.get(i));
                 }
@@ -29,9 +30,20 @@ public class Duke {
                 System.out.println("     Nice! I've marked this task as done:");
                 System.out.println("       " + task);
             } else {
-                Task task = new Task(command[0] + " " + command[1]);
-                System.out.println("     added: " + command[0] + " " + command[1]);
+                Task task = null;
+                if (command[0].equals("todo")) {
+                    task = new Todo(command[1]);
+                } else if (command[0].equals("deadline")) {
+                    String[] info = command[1].split(" /by ");
+                    task = new Deadline(info[0], info[1]);
+                } else if (command[0].equals("event")) {
+                    String[] info = command[1].split(" /at ");
+                    task = new Event(info[0], info[1]);
+                }
+                System.out.println("     Got it. I've added this task:");
+                System.out.println("       " + task);
                 tasks.add(task);
+                System.out.println("     Now you have " + tasks.size() + " tasks in the list.");
             }
             System.out.println("    ––––––––––––––––––––––––––––––––––––––––––––––––––––––––\n");
             command = sc.nextLine().split(" ", 2);
