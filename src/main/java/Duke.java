eb09d118263271062cc29b18bdf253c477fefe93
aliciatxl <example@mail.com>
2020-01-19 12:29:15 +0800

    Implemented Greet, Echo, Exit in level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +10,38 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println(
+                "    ____________________________________________________________\n" +
+                "     Hey friend! I'm Duke V2.0.0 at your service\n" +
+                "     What can I do for you today?\n" +
+                "    ____________________________________________________________\n"
+        );
+
+        BufferedReader bufferedReader = new BufferedReader (new InputStreamReader(System.in));
+        String input;
+
+        while (true) {
+            try {
+                 input = bufferedReader.readLine();
+            } catch (IOException e) {
+                System.out.println("Error in reading input");
+                break;
+            }
+            if (input.equals("bye")) {
+                System.out.println(
+                        "    _____________________________DUKE___________________________\n" +
+                        "     Sorry to see you go. Hope to see you again soon! (＾▽＾)／\n" +
+                        "    _________★゜・。。・゜゜・。。・゜☆゜・。。・゜゜・。。・゜★_______\n"
+                );
+                break;
+            } else {
+                System.out.println(
+                        "    _____________________________DUKE___________________________\n" +
+                        "     " + input + "\n" +
+                        "    _________★゜・。。・゜゜・。。・゜☆゜・。。・゜゜・。。・゜★_______\n"
+                );
+            }
+        }
     }
 }
