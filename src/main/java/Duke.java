aefcff64a64ca43b5860628a557908e9c4dc334b
alushingg <ang.lu.shing99@gmail.com>
2020-01-19 15:11:16 +0800

    add task.java, mark as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,14 +12,21 @@ public class Duke {
         output("Hello! I'm Duke\n     What can I do for you?");
         Scanner sc = new Scanner(System.in);
         String op = sc.nextLine();
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Task> list = new ArrayList<>();
         while(!op.equals("bye")) {
             if (op.equals("list")) {
                 show_list(list);
                 op = sc.nextLine();
+            } else if (op.indexOf("done") == 0) {
+                String[] temp = op.split(" ", 2);
+                Task task = list.get(Integer.parseInt(temp[1]) - 1);
+                task.markAsDone();
+                output("Nice! I've marked this task as done:\n      " +
+                            task.toString());
+                op = sc.nextLine();
             } else {
                 output("added: " + op);
-                list.add(op);
+                list.add(new Task(op));
                 op = sc.nextLine();
             }
         }
@@ -32,12 +39,13 @@ public class Duke {
         System.out.println("     " + op);;
         System.out.println(divider);
     }
-    private static void show_list(ArrayList<String> list) {
+    private static void show_list(ArrayList<Task> list) {
         String divider = "    -------------------------------------------------------";
         System.out.println(divider);
+        System.out.println("     Here are the tasks in your list:");
         int size = list.size();
         for (int i = 0; i < size; i++) {
-            System.out.println("     " + (i+1) + ". " + list.get(i));
+            System.out.println("     " + (i+1) + ". " + list.get(i).toString());
         }
         System.out.println(divider);
     }
