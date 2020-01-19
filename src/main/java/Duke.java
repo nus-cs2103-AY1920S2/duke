3502f6133a0e4ad98ab17cf7a8c81e5e774ed22d
XinPei-ng <elainengxp@gmail.com>
2020-01-19 23:54:22 +0800

    Add Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+       Duke duke = new Duke();
+       duke.frontDesk();
+    }
+
+    public void frontDesk() {
+     try {
+         String greetings = "____________________________________________________________ \n" +
+                            "Hello! I'm Chu Chu \n" +
+                            "What can I do for you ? \n" +
+                            "____________________________________________________________\n";
+         System.out.println(greetings);
+
+         InputStreamReader rd = new InputStreamReader(System.in);
+         BufferedReader br = new BufferedReader(rd);
+         String command = null;
+
+         while(!(command = br.readLine()).equals("bye")) {
+
+             String echo = "____________________________________________________________ \n" +
+                            command + "\n" +
+                           "____________________________________________________________ \n";
+             System.out.println(echo);
+
+         }
+
+         System.out.println("Bye. I hope you liked the service and hope to see you soon ! \n");
+
+     } catch(IOException e) {
+         System.out.print(e);
+     }
+
     }
 }
