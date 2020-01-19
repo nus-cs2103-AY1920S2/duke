b0c95228aac1ab84ac98c1629d5e6bdfd28b06f3
alushingg <ang.lu.shing99@gmail.com>
2020-01-19 14:33:09 +0800

    greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,20 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        output("Hello! I'm Duke\n     What can I do for you?");
+        Scanner sc = new Scanner(System.in);
+        String op = sc.nextLine();
+        while(!op.equals("bye")) {
+            output(op);
+            op = sc.nextLine();
+        }
+        output("Bye. Hope to see you again soon!");
+    }
+
+    private static void output(String op) {
+        String divider = "    -------------------------------------------------------";
+        System.out.println(divider);
+        System.out.println("     " + op);;
+        System.out.println(divider);
     }
 }
