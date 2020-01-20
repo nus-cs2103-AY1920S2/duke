5b593cc4d4d8d8e040157900ac7b1c705a966d92
ongch00 <ongchoonhan@gmail.com>
2020-01-20 23:57:13 +0800

    Implement Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,11 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
         boolean isRunning  = true;
+        List<Task> tasks = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         System.out.println("____________________________________________________________\n" +
                 " Hello! I'm Duke\n" +
@@ -16,10 +19,18 @@ public class Duke {
                         " Bye. Hope to see you again soon!\n" +
                         "____________________________________________________________");
                 isRunning = false;
+            } else if (input.toLowerCase().equals("list")){
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.printf(" %d. %s\n", (i + 1), tasks.get(i));
+                }
+                System.out.println("____________________________________________________________");
             } else {
-                System.out.println("____________________________________________________________\n" +
-                        " " + input + "\n" +
-                        "____________________________________________________________");
+                Task newTask = new Task(input);
+                tasks.add(newTask);
+                System.out.println("____________________________________________________________");
+                System.out.printf(" added: %s\n", newTask);
+                System.out.println("____________________________________________________________");
             }
         }
     }
