bda75c41c1f9b2bb15577e256e217efe96d0c47f
ChesterSim <chestersimdy@gmail.com>
2020-01-20 23:37:38 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,32 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+
+//        System.out.println("Hello from\n" + logo);
+
+        printFormattedOutput("Hello! I'm Duke\n    What can I do for you?");
+
+        // Chat logic
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        while(!input.equals("bye")) {
+            printFormattedOutput(input);
+            input = sc.nextLine();
+        }
+
+        printFormattedOutput("Bye. Hope to see you again soon!");
+
+    }
+
+    public static void printFormattedOutput(String output) {
+        String bar = "    __________________________________________________\n";
+
+        System.out.println(bar + "    " + output + "\n" + bar);
     }
 }
