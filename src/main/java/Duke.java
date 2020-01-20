c365b730865fb1407706a32a8dca3a4c5672ae4f
XinPei-ng <elainengxp@gmail.com>
2020-01-20 19:47:45 +0800

    Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,40 +1,58 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Duke {
+
+    ArrayList<String> textArr = new ArrayList<String>();
+
     public static void main(String[] args) {
 
-       Duke duke = new Duke();
-       duke.frontDesk();
+        Duke duke = new Duke();
+        duke.frontDesk();
     }
 
     public void frontDesk() {
-     try {
-         String greetings = "____________________________________________________________ \n" +
-                            "Hello! I'm Chu Chu \n" +
-                            "What can I do for you ? \n" +
-                            "____________________________________________________________\n";
-         System.out.println(greetings);
+        try {
+            String greetings = "____________________________________________________________ \n" +
+                    "Hello! I'm Chu Chu \n" +
+                    "What can I do for you ? \n" +
+                    "____________________________________________________________\n";
+            System.out.println(greetings);
 
-         InputStreamReader rd = new InputStreamReader(System.in);
-         BufferedReader br = new BufferedReader(rd);
-         String command = null;
+            InputStreamReader rd = new InputStreamReader(System.in);
+            BufferedReader br = new BufferedReader(rd);
+            String command = null;
 
-         while(!(command = br.readLine()).equals("bye")) {
+            while (!(command = br.readLine()).equals("bye")) {
 
-             String echo = "____________________________________________________________ \n" +
-                            command + "\n" +
-                           "____________________________________________________________ \n";
-             System.out.println(echo);
+                if (command.equals("list")) {
+                    String header = "____________________________________________________________ ";
+                    System.out.println(header);
+                    String text = null;
 
-         }
+                    for(int i = 0; i < textArr.size(); i++) {
+                      text = (i + 1) + "." + " " + textArr.get(i);
+                      System.out.println(text);
+                    }
 
-         System.out.println("Bye. I hope you liked the service and hope to see you soon ! \n");
+                    String foot = "____________________________________________________________ ";
+                    System.out.println(foot);
+                } else {
+                    textArr.add(command);
+                    String text = "____________________________________________________________ \n" +
+                            "added: " + command + "\n" +
+                            "____________________________________________________________ \n";
+                    System.out.println(text);
+                }
+            }
 
-     } catch(IOException e) {
-         System.out.print(e);
-     }
+            System.out.println("Bye. I hope you liked the service and hope to see you soon ! \n");
+
+        } catch (IOException e) {
+            System.out.print(e);
+        }
 
     }
 }
