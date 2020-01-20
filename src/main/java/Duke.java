ea0c59af99b147eb25ec10a7ed1bf72964412f2c
ivanteejj <58815457+ivanteejj@users.noreply.github.com>
2020-01-20 10:29:18 +0800

    Set up for gradle complete

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,8 @@
 public class Duke {
+    // Main Function of Duke
     public static void main(String[] args) {
+
+        //Prints DUKE logo
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
