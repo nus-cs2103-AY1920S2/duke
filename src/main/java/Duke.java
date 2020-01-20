8eb2d2394a5b054f8972e03034d972e101ed444a
NNpanpan <dpnhat.nguyen@gmail.com>
2020-01-20 22:06:07 +0800

    Finished Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,17 +2,12 @@ public class Duke {
     public static String bot_name = "Duke";
     private String padding = "       ";
 
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+    public Duke() {
+        // Place-holder constructor, may need to extend later
     }
 
     public void greet() {
-        System.out.println(padding + "Greetings! This is Duke, and I am your friend!");
+        System.out.println(padding + "Greetings! This is " + bot_name + ", and I am your friend!");
         System.out.println(padding + "You don't have to be formal. Relax and tell me how I can help you");
     }
 
