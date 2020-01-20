7c8bb7c5773f374ff44aa8590ec4ed4a7c056660
FeliciaTay <E0323765@u.nus.edu>
2020-01-20 14:22:51 +0800

    change structure of Duke to greet, echo and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hi! I am Duke! What would you like to tell me today?:)");
+
+        String command = sc.nextLine();
+        String exit = "bye";
+        while (!command.equals(exit)) {
+            System.out.println(command);
+            command = sc.nextLine();
+        }
+        System.out.println("Okay then! Goodbye!");
     }
 }
