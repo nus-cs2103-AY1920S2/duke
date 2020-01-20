35444b920da7dbd775b96427bc8ca3a68ba7c151
NNpanpan <dpnhat.nguyen@gmail.com>
2020-01-20 21:51:48 +0800

    Created Main

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,7 @@
 public class Duke {
+    public static String bot_name = "Duke";
+    private String padding = "       ";
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +9,15 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+    }
+
+    public void greet() {
+        System.out.println(padding + "Greetings! This is Duke, and I am your friend!");
+        System.out.println(padding + "You don't have to be formal. Relax and tell me how I can help you");
+    }
+
+
+    public void echo(String str) {
+        System.out.println(padding + str);
     }
 }
---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    private final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        
+    }
+}
