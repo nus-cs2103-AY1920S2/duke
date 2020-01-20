194db6cfd53569725c01a711e2298e32eaf3b354
CranstonYeo <cranston_d_yeo@hotmail.com>
2020-01-21 00:30:22 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,25 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from\n" + logo + "\nWhat can I do for you?");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        String[] items = new String[100];
+        int i = 0;
+
+        while (!input.equals("bye")) {
+            if (input.equals("list")) {
+                for (int j = 0; j < i; j++) {
+                    System.out.println(j + 1 + ". " + items[j]);
+                }
+            } else {
+                items[i] = input;
+                i++;
+                System.out.println("added: " + input);
+            }
+            input = sc.nextLine();
+        }
+
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
