cc26f7dd9fd03186ddeb72d572d31669728b2b71
Guofeng-Tang <tgf.tang.guofeng@gmail.com>
2020-01-21 04:47:26 +0800

    Level 1 initial commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        String input;
+
+        String greetingMessage = "Hello! I'm Duke\n" +
+                "What can I do for you?";
+        String endingCommand = "bye";
+
+
+        System.out.println(greetingMessage);
+
+        while (true) {
+            input = sc.nextLine();
+            if (input.equals(endingCommand)) {
+                break;
+            }
+            System.out.println(input);
+        }
+
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
