730a56a42233240fd9585c1ee5ad2a08225a48a6
hanchingyong <hanchingyong@gmail.com>
2020-01-20 16:00:35 +0800

    initial commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Greetings, I am\n" + logo);
+        System.out.println("How may I be of assistance today?");
+        String command = sc.nextLine();
+
+        while (!command.equals("bye")) {
+            System.out.println(command);
+            command = sc.nextLine();
+        }
+
+        System.out.println("It was my pleasure to help you.\n");
+
     }
 }
