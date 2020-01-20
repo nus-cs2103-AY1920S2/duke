a1cca393bb1dad4efc63c98e45c7dba688b6f9bd
ongch00 <ongchoonhan@gmail.com>
2020-01-20 23:43:07 +0800

    Add Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,15 @@
+public class Task {
+    private String description;
+
+    public Task (String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String toString() {
+        return String.format("%s", description);
+    }
+}
