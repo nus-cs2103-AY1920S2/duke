35dc397e63d3d72a272aa1447e4615ec30a8031a
Dban1 <huggavamp@gmail.com>
2020-01-20 16:22:12 +0800

    -implemented Level 5 -added error handling for unknown commands -added error handling for wrong number of parameters given for commands -added error handling for "done" if number is not given in parameter [1]

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -23,57 +23,80 @@ public class Duke {
         while(true) {
             String input = scan.nextLine(); //reads in user input
             String[] command = input.split(" ");
-            switch(command[0]) {
-                case "bye":
-                    divider("Aww okay, see you next time!");
-                    return;
-                case "list":
-                    if (storedTasks.size() < 1) {
-                        divider("no tasks");
+            try {
+                switch (command[0]) {
+                    case "bye":
+                        divider("Aww okay, see you next time!");
+                        return;
+                    case "list":
+                        if (storedTasks.size() < 1) {
+                            divider("no tasks");
+                            break;
+                        }
+                        divider("");
+                        System.out.println("Here are the tasks in your list:");
+                        int index = 1;
+                        String output = "";
+                        for (Task t : storedTasks) {
+                            output = index + ". " + t.toString();
+                            System.out.println(output);
+                            index++;
+                        }
+                        divider("");
+                        break;
+                    case "done":
+                        if (command.length <= 1) {throw new DukeException("OOPS boi. Need moar arguments!!");}
+                        try {Integer.parseInt(command[1]);} catch(Exception e) {throw new DukeException("Input a number boi.");}
+                        if (Integer.parseInt(command[1]) > storedTasks.size()) {
+                            throw new DukeException("Boi. You don't have that many tasks boi! :)");}
+
+                        int taskNum = Integer.parseInt(command[1]);
+                        storedTasks.get(taskNum - 1).setDone();
+                        divider("");
+                        System.out.println("Nice! I've marked this task as done:\n" +
+                                storedTasks.get(taskNum - 1).toString());
+                        divider("");
+                        break;
+
+                    case "todo":
+                        if (command.length <= 1) {throw new DukeException("OOPS boi. Need moar arguments!!");}
+                        Task task = new Todo(input.substring(5)); //stores input to storedText List
+                        insertTask(task);
+                        break;
+
+                    case "deadline": {
+                        String[] elements = input.split("/by");
+                        if (elements.length <= 1) {
+                            throw new DukeException("OOPS boi. Need moar arguments!!");
+                        }
+                        String deadlineDescription = elements[0].substring(9);
+                        String by = elements[1].substring(1);
+                        Task deadline = new Deadline(deadlineDescription, by);
+                        insertTask(deadline);
                         break;
                     }
-                    divider("");
-                    System.out.println("Here are the tasks in your list:");
-                    int index = 1;
-                    String output = "";
-                    for (Task t: storedTasks) {
-                        output = index + ". " + t.toString();
-                        System.out.println(output);
-                        index++;
+
+                    case "event": {
+                        String[] elements = input.split("/at");
+                        if (elements.length <= 1) {
+                            throw new DukeException("OOPS boi. Need moar arguments!!");
+                        }
+                        String eventDescription = elements[0].substring(6);
+                        String at = elements[1].substring(1);
+                        Task event = new Event(eventDescription, at);
+                        insertTask(event);
+                        break;
                     }
-                    divider("");
-                    break;
-                case "done":
-                    int taskNum = Integer.parseInt(command[1]);
-                    storedTasks.get(taskNum - 1).setDone();
-                    divider("");
-                    System.out.println("Nice! I've marked this task as done:\n" +
-                                        storedTasks.get(taskNum - 1).toString());
-                    divider("");
-                    break;
 
-                case "todo":
-                    Task task = new Todo(input.substring(5)); //stores input to storedText List
-                    insertTask(task);
-                    break;
-
-                case "deadline":
-                    String deadlineDescription = input.split("/by")[0].substring(9);
-                    String by = input.split("/by")[1].substring(1);
-                    Task deadline = new Deadline(deadlineDescription, by);
-                    insertTask(deadline);
-                    break;
-
-                case "event":
-                    String eventDescription = input.split("/at")[0].substring(6);
-                    String at = input.split("/at")[1].substring(1);
-                    Task event = new Event(eventDescription, at);
-                    insertTask(event);
-                    break;
-
-                default:
-                    storedTasks.add(new Task(input)); //stores input to storedText List
-                    divider("added " + input);
+                    default:
+                        throw new DukeException("unknown command!!!");
+//                    storedTasks.add(new Task(input)); //stores input to storedText List
+//                    divider("added " + input);
+                }
+            } catch(DukeException d) {
+                divider(d.getMessage());
+            } catch(Exception e) {
+                divider("General error caught boi: " + e.toString());
             }
         }
     }
