a76bf9ca7f1f22091d4609897445025d7990f3e7
Joshua <joshuaftw000@gmail.com>
2020-01-20 23:58:58 +0800

    Level 1 Completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,18 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Hello, Luke! I'm Your Father \nWhat can I do for you today?");
+        String word = sc.nextLine();
+
+        while(!word.equals("bye")){
+            System.out.println(word);
+            word = sc.nextLine();
+        }
+        System.out.println("Noooo Join the Dark Side, Luke, don't leave me");
+
     }
 }
