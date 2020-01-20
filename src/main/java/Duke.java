ffdac2db7061c7ce24f99f9d9e77cc3ea6fa1cb8
FeliciaTay <E0323765@u.nus.edu>
2020-01-20 14:49:00 +0800

    add ability to store text and display back to user

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,23 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> arr = new ArrayList<>();
         System.out.println("Hi! I am Duke! What would you like to tell me today?:)");
 
         String command = sc.nextLine();
         String exit = "bye";
         while (!command.equals(exit)) {
-            System.out.println(command);
+            if (command.equals("list")) {
+                for (int i = 1; i <= arr.size(); i++) {
+                    System.out.println(i + ". " + arr.get(i - 1));
+                }
+            } else {
+                arr.add(command);
+                System.out.println("I have taken note: " + command);
+            }
             command = sc.nextLine();
         }
         System.out.println("Okay then! Goodbye!");
