b474b9f0d3469eb55b3b002138264f1daa2257bd
kusnim <minschoi97@gmail.com>
2020-01-20 22:15:05 +0800

    Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        String greeting = "Hello! I'm Duke\nWhat can I do for you?";
+        System.out.println(greeting);
+
+        String userInput = sc.nextLine();
+        while (!userInput.equals("bye")) {
+            System.out.println(userInput);
+            userInput = sc.nextLine();
+        }
+        System.out.print("Bye. Hope to see you again soon!");
     }
 }
