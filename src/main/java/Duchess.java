77f6badc968f8d865710eac0d6ab689a85aef840
Hanming Zhu <hanming@huawen.com>
2020-01-20 14:27:54 +0800

    feat: build level-1 features - greet, echo, exit
    Changed the name to Duchess and separated the functions into introduction, echo and goodbye functions of the Duchess class. The main function now initialises a Duchess object before using it to execute actions.
    
    To do: perhaps abstract the whole running process into a function of Duchess as well.

---
+++ src/main/java/Duchess.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Duchess {
+
+    private void introduction() {
+        String logo = " _____             _                   \n"
+                + "|  __ \\           | |                  \n"
+                + "| |  | |_   _  ___| |__   ___  ___ ___ \n"
+                + "| |  | | | | |/ __| '_ \\ / _ \\/ __/ __|\n"
+                + "| |__| | |_| | (__| | | |  __/\\__ \\__ \\\n"
+                + "|_____/ \\__,_|\\___|_| |_|\\___||___/___/";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("My name is Duchess, as you can see above.");
+        System.out.println("How may I help you?");
+    }
+
+    private void echo(String input) {
+        System.out.println("Oh? You said \"" + input + "\"? Interesting.");
+        System.out.println("Anything else?");
+    }
+
+    private void goodbye() {
+        System.out.println("Bye, is it? Shoo shoo then.");
+
+    }
+
+    public static void main(String[] args) {
+        Duchess duchess = new Duchess();
+        duchess.introduction();
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        while (!input.toLowerCase().equals("bye")) {
+            duchess.echo(input);
+            input = scanner.nextLine();
+        }
+        duchess.goodbye();
+    }
+}
