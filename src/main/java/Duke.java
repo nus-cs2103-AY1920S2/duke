60e98f964507acfdb0f1439d776e53413499aef6
jumpcutfindo <danielhoe97@gmail.com>
2020-01-20 16:14:43 +0800

    Revert "Level-1: Greet, Echo, Exit"
    This reverts commit 25fbfa08e2a8ca8c870a124b317146c41832dbff.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,4 @@
-import java.util.Scanner;
-
 public class Duke {
-
-
-
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -11,22 +6,5 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println("I'm your personal chat bot assistant! How may I be of service today?");
-
-        Scanner sc = new Scanner(System.in);
-
-        boolean isActive = true;
-        while(isActive) {
-            String command = sc.next();
-
-            switch(command) {
-                case "bye":
-                    System.out.println("Bye. Hope to see you again soon!");
-                    isActive = false;
-                    break;
-                default:
-                    System.out.println(command);
-            }
-        }
     }
 }
