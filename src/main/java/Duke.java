e9735c16d58876336cd99f352792b5c4715cdc94
hanchingyong <hanchingyong@gmail.com>
2020-01-20 17:04:45 +0800

    Level-3: added Task class and done function.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,12 @@
+import main.java.Task;
+import java.util.StringTokenizer;
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList dukeList = new ArrayList<>();
+        ArrayList<Task> dukeList = new ArrayList<Task>();
 
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -17,13 +19,24 @@ public class Duke {
         String command = sc.nextLine();
         while (!command.equals("bye")) {
             if (command.equals("list")) {
+                System.out.println("These items are in your list: \n");
                 for (int i = 0; i < dukeList.size(); i++){
-                    System.out.println(i + 1 + ". " + dukeList.get(i));
+                    System.out.println(i + 1 + ". " + dukeList.get(i).getStatusIcon() + " "
+                            + dukeList.get(i).getDescription());
                 }
                 command = sc.nextLine();
                 continue;
             }
-            dukeList.add(command);
+            if (command.contains("done")){
+                String[] splitCommand = command.split(" ");
+                int index = Integer.parseInt(splitCommand[1]);
+                dukeList.get(index - 1).markAsDone();
+                System.out.println("You have completed this task.\n");
+                System.out.println(dukeList.get(index - 1));
+                command = sc.nextLine();
+                continue;
+            }
+            dukeList.add(new Task(command));
             System.out.println("added: " + command);
             command = sc.nextLine();
         }
