99caf3801503b03b607244e8978b73614ba9c4c0
Clouddoggo <jel.lim99@gmail.com>
2020-01-21 18:35:30 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class Duke {
+    static String separator = "____________________________________________________________";
+
+    /**
+     * Displays Duke logo.
+     * @param args input
+     */
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(separator + "\nHello! I'm Duke\nWhat can I do for you?\n" + separator);
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        while (!input.equals("bye")) {
+            System.out.printf("%s\n%s\n%s\n", separator, input, separator);
+            input = sc.next();
+        }
+
+        if (input.equals("bye")) {
+            System.out.println(separator + "\nBye. Hope to see you again soon!\n" + separator);
+        }
     }
 }
