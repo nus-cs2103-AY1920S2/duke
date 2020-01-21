9f819de4549165c988a071ba9dd06d12597de33a
KwanXinJie <xjkwan1998@gmail.com>
2020-01-21 14:59:14 +0800

    Lvl3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,6 +10,7 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("What can I do for you?");
+
         input();
         System.out.println("Bye. Hope to see you again soon!");
     }
@@ -17,15 +18,17 @@ public class Duke {
     private static void input() {
         Scanner sc;
         String statement;
-        ArrayList<String> arrList = new ArrayList<>();
+        ArrayList<Task> arrList = new ArrayList<>();
 
         sc = new Scanner(System.in);
         statement = sc.nextLine();
 
         while (!statement.equals("bye")) {
             if (statement.equals("list") && arrList.size() != 0) {
+                System.out.println("These are the tasks in your list:");
                 for (int i=1; i <= arrList.size(); i++) {
-                    System.out.println(i +". " + arrList.get(i-1));
+                    Task t = arrList.get(i-1);
+                    System.out.println(i + ". [" +t.getStatusIcon()+ "] " +t.description);
                 }
                 System.out.println();
             }
@@ -33,8 +36,16 @@ public class Duke {
                 System.out.println("Empty");
                 System.out.println();
             }
+            else if ((statement.split(" ")[0]).equals("done")) {
+                int a = Integer.parseInt(statement.split(" ")[1])-1;
+                arrList.get(a).markAsDone();
+                System.out.println("Nice! I've marked this task as done: ");
+                System.out.println("  [" +arrList.get(a).getStatusIcon()+ "] " +arrList.get(a).description);
+                System.out.println();
+            }
             else {
-                arrList.add(statement);
+                Task t = new Task(statement);
+                arrList.add(t);
                 System.out.println("added: " + statement);
                 System.out.println();
             }
