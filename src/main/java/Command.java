c5b1243d962b210b095eb2851d71030d2edfb81c
chifa3 <foochifa@gmail.com>
2020-01-21 12:32:55 +0800

    modify lookup method to throw InvalidCommandException

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -1,7 +1,11 @@
 public enum Command {
     LIST, DONE, TODO, DEADLINE, EVENT;
 
-    public static Command lookUp(String str) {
-        return Command.valueOf(str.toUpperCase());
+    public static Command lookUp(String str) throws InvalidCommandException {
+        try {
+            return Command.valueOf(str.toUpperCase());
+        } catch (IllegalArgumentException e){
+            throw new InvalidCommandException(str + " is an invalid command");
+        }
     }
 }
