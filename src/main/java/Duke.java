d07dcebd0680cc607bcaa7d846153b823cfdc27e
Eugene Tan <eugenetanyc@gmail.com>
2020-01-21 17:53:26 +0800

    Completed: Level 1. Greet, Echo, Exit added personality features for chatbot Duke for better audience engagement

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        // Welcomes the user
+        WelcomeGoodbye wm = new WelcomeGoodbye();
+        wm.welcome();
+
+        // Begins accepting input commands
+        TaskHandler taskHandler = new TaskHandler();
+        taskHandler.serveUser();
+
+        // User is done
+        wm.goodbye();
+
     }
+
 }
