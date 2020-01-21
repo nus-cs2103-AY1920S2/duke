78514250faa0a3eca1791d4b5cc3cae814ba01d1
Permas-Teo <permasteo@gmail.com>
2020-01-21 20:08:55 +0800

    feat: add greet, echo, exit functions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    /**
+     * Formats a string to an output produced by Duke.
+     * @param s String to be formatted.
+     * @return Formatted string ready to be printed.
+     */
+    public static String dukeFormat(String s) {
+        String fill = "----------------------------------------";
+        String indent = "    ";
+        return indent + fill + "\n" + indent + s + "\n" + indent + fill;
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        String getInput = null;
+
+        System.out.println(dukeFormat("Hello I'm your mum. What can I do for you?"));
+
+        getInput = sc.next();
+        while (!getInput.equals("bye")) {
+            System.out.println(dukeFormat(getInput));
+            getInput = sc.next();
+        }
+
+        System.out.println(dukeFormat("Bye, have a good day!"));
     }
 }
