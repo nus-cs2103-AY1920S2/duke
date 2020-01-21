6a953cffd40d0d3289aa7369a704170dd5ebdb92
harrychengly <harrychengly@gmail.com>
2020-01-21 23:58:22 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,23 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from\n" + logo + "\n I am at your service\n");
+        String botReplyLine = "----------------------------------------------";
+
+
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()){
+            String userInput = sc.nextLine();
+            if (userInput.equalsIgnoreCase("bye")){
+                System.out.println(botReplyLine +"\n Duke: I'll say goodnight now \n" +botReplyLine);
+                break;
+            }
+            else{
+                System.out.println(botReplyLine +"\n Duke: "+ userInput + "\n" + botReplyLine);
+            }
+        }
+
     }
 }
+
