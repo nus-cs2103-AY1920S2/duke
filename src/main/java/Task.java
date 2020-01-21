ade757e4a2a61f561310ea77d30200a0d112bfc9
chifa3 <foochifa@gmail.com>
2020-01-21 10:49:06 +0800

    Implemented access modifiers and get/set methods

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,7 +1,20 @@
 public class Task {
-    int id;
-    String task;
-    boolean done;
+    protected int id;
+    protected String task;
+    protected boolean done;
+
+    public int getId() {
+        return id;
+    }
+    public String getTask() {
+        return task;
+    }
+    public boolean getDone() {
+        return done;
+    }
+    public void setDone(boolean bool) {
+        done = bool;
+    }
 
     public Task (int id, String task) {
         this.id = id;
