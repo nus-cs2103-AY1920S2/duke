a5d4039531c67ea6437b252d8d5223f4137292b9
Arthur Lee <ykarthurlee@gmail.com>
2020-01-21 22:46:29 +0800

    Implemented base functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        String line = "-----------------------------------";
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println(line);
         System.out.println("Hello from\n" + logo);
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println(line);
+            System.out.println(input);
+            System.out.println(line);
+            input = scanner.nextLine();
+        }
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
     }
 }
