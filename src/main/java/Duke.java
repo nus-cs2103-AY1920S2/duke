50f63b6ed50b8c0432510723ff4dd2602a3296ce
muhd97 <zain.sma.mz@gmail.com>
2020-01-21 23:43:08 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -18,22 +18,28 @@ public class Duke {
 
     private static void run() {
         Scanner sc = new Scanner(System.in);
-        List<String> tasks = new ArrayList<>();
+        List<Task> tasks = new ArrayList<>();
         String command = sc.nextLine();
-
         while (!command.equals("bye")) {
-            if (command.equals("list") && tasks.size() != 0) {
-                for (int i = 1; i <= tasks.size(); i++) {
-                    System.out.println(i +". " + tasks.get(i - 1));
+            if (command.equals("list")) {
+                if (tasks.size() != 0) {
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 1; i <= tasks.size(); i++) {
+                        System.out.println(i + "." + tasks.get(i - 1));
+                    }
+                    System.out.println("____________________________________________________________");
+                } else {
+                    System.out.println("There are no remaining tasks");
+                    System.out.println("____________________________________________________________");
                 }
+            } else if ((command.split(" ")[0]).equals("done")) {
+                int i = Integer.parseInt(command.split(" ")[1]) - 1;
+                tasks.get(i).markAsDone();
+                System.out.println("Nice! I've marked this task as done: ");
+                System.out.println(tasks.get(i));
                 System.out.println("____________________________________________________________");
-            }
-            else if (command.equals("list") && tasks.size() == 0) {
-                System.out.println("There are no tasks left");
-                System.out.println("____________________________________________________________");
-            }
-            else {
-                tasks.add(command);
+            } else {
+                tasks.add(new Task(command));
                 System.out.println("added: " + command);
                 System.out.println("____________________________________________________________");
             }
@@ -42,5 +48,6 @@ public class Duke {
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
+
 }
 
