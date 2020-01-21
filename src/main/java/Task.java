681a7c6ae7d858d951dad160183cbe2cd82f3778
J-Dan23 <janiceongyw@gmail.com>
2020-01-21 16:58:41 +0800

    feat: changed Task from general class to abstract class

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,22 +1,30 @@
-public class Task {
+public abstract class Task {
     protected String description;
+    protected String type;
+    protected String date;
     protected boolean isDone;
-
-    public Task(String description) {
-        this.description = description;
-        this.isDone = false;
-    }
 
     public String getStatusIcon() {
         return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+
+    //Type should only be T(odo), D(eadline), or E(vent).
+    public String getType() {
+        return this.type;
     }
 
     public String getDescription() {
         return this.description;
     }
 
+    public String getDate() {
+        return this.date;
+    }
+
     public void markAsDone() {
         this.isDone = true;
     }
+
+    public abstract String printTaskDetails();
 
 }
