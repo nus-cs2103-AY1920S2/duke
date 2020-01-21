4293d7ceb954ed30151eb1a92d4bd8157ce0ccad
ongch00 <ongchoonhan@gmail.com>
2020-01-21 21:59:12 +0800

    Implement Inheritance

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,21 +1,26 @@
 public class Task {
-    private String description;
-    private boolean isDone;
+    private String description = "";
+    private boolean isDone = false;
     
     public Task (String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    public void markDone() {
+        isDone = true;
+    }
+
     public String getDescription() {
         return description;
+    }
+
+    public boolean getIsDone() {
+        return isDone;
     }
 
     public String toString() {
         return String.format("[%s] %s", (isDone ? "\u2713" : "\u2718"), description);
     }
 
-    public void markDone() {
-        isDone = true;
-    }
 }
