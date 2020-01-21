b98df276ccb04575a0269413edb907d8f2fb4b4d
alam8064 <59945273+alam8064@users.noreply.github.com>
2020-01-21 15:56:43 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,14 +10,20 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         Scanner sc = new Scanner(System.in);
+        String[] data = new String[100];
+        int slot = 0;
 
         while (sc.hasNext()) {
             String input = sc.nextLine();
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (input.equals("list")){
+                for (int i = 0; i < slot; i++) {System.out.println(i+1 + ". " + data[i]);}
             } else {
-                System.out.println(input);
+                data[slot] = input;
+                System.out.println("added: " + input);
+                slot++;
             }
         }
 
