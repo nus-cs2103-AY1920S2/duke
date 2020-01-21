21a22f3bfdf86c5016336ec99d76ae6970c34a28
Liao Meng E0148811 <lyhlm5@hotmail.com>
2020-01-22 01:03:23 +0800

    Extract methods

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -21,23 +21,34 @@ class Duke {
         while (!(instruction = sc.nextLine()).equals("bye")) {
             String[] instructionByWord = instruction.split(" ");
             if (instruction.equals("list")) {
-                System.out.println("Here is the task list:");
-                for (int i = 1; i <= list.size(); i++) {
-                    System.out.println(i + ". " + list.get(i - 1).toString());
-                }
+                printList(list);
             } else if (instructionByWord.length == 2 && instructionByWord[0].equals("done")) {
-                Task taskToBeCompleted = list.get(Integer.parseInt(instructionByWord[1]) - 1);
-                taskToBeCompleted.setDone();
-                System.out.println("Noted, the following task is marked done:");
-                System.out.println(taskToBeCompleted.getTask());
+                markATaskDone(list.get(Integer.parseInt(instructionByWord[1]) - 1));
             } else {
-                Task t = new Task(instruction);
-                list.add(t);
-                System.out.println("Noted, the following pending task is stored:");
-                System.out.println(instruction);
+                addAPendingTask(list, instruction);
             }
         }
 
         System.out.println("Goodbye. See you next time!");
+    }
+
+    static void addAPendingTask(List<Task> list, String instruction) {
+        Task t = new Task(instruction);
+        list.add(t);
+        System.out.println("Noted, the following pending task is stored:");
+        System.out.println(instruction);
+    }
+
+    static void markATaskDone(Task taskToBeCompleted) {
+        taskToBeCompleted.setDone();
+        System.out.println("Noted, the following task is marked done:");
+        System.out.println(taskToBeCompleted.getTask());
+    }
+
+    static void printList(List<Task> list) {
+        System.out.println("Here is the task list:");
+        for (int i = 1; i <= list.size(); i++) {
+            System.out.println(i + ". " + list.get(i - 1).toString());
+        }
     }
 }
