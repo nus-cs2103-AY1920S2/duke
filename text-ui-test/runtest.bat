89c389c33f7668248ed13f776d07defc4ac34ef6
Joash Chin <joashchin@gmail.com>
2020-01-21 15:15:59 +0800

    Implement A-TextUITesting

---
+++ text-ui-test/runtest.bat
@@ -0,0 +1,15 @@
+@ECHO OFF
+
+REM create bin directory if it doesn't exist
+if not exist ..\bin mkdir ..\bin
+
+REM delete output from previous run
+del ACTUAL.TXT
+
+REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
+cd ..
+gradlew run --console=plain --quiet < text-ui-test\input.txt > text-ui-test\ACTUAL.TXT
+cd text-ui-test
+
+REM compare the output to the expected output
+FC ACTUAL.TXT EXPECTED.TXT
