d60c4e43bcd16d24e1825add95cf433e9f43515b
Zhang Yuanyu <e0322803@u.nus.edu>
2020-01-22 02:23:44 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,20 +9,32 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        String line = "    ____________________________________________________________" +  "\n";
-        System.out.println(line + "     Hello! I'm Duke\n" + "     Whatcha wanna do?\n" + line);
-        String fourSpace = "     ";
+        String line = "    ____________________________________________________________" + "\n";
+        String fiveSpaces = "      ";
+        System.out.println(line + fiveSpaces + "Hello! I'm Duke\n" + fiveSpaces + "Whatcha wanna do?\n" + line);
+        int counter = 0;
+        String[] tasks = new String[101];
         while(scanner.hasNextLine()){
             String word = scanner.nextLine();
-            if(word.equals("bye")){
-                System.out.println(line + fourSpace + "See ya later alligator!\n"+ line);
+            if(word.equals("list")){
+                System.out.print(line);
+                for(int i = 1; i < counter + 1; i++){
+                    System.out.println(fiveSpaces + Integer.toString(i) + ". " + tasks[i]);
+                }
+                System.out.println(line);
+            }
+            else if(word.equals("bye")){
+                System.out.println(line + fiveSpaces + "See ya later alligator!\n"+ line);
                 break;
             }
-            else {
-                System.out.println(line + fourSpace + word + "\n" + line);
+            else{
+                String add = fiveSpaces + "added: ";
+                counter++;
+                tasks[counter] = word;
+                System.out.println(line + add + word + "\n" + line);
             }
         }
     }
-
 }
+
 
