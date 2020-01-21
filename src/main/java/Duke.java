eff907f1cd43535d8cbc104d966228ce0e6427b9
Zhang Yuanyu <e0322803@u.nus.edu>
2020-01-22 01:16:29 +0800

    Level 1 done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        String line = "    ____________________________________________________________" +  "\n";
+        System.out.println(line + "     Hello! I'm Duke\n" + "     Whatcha wanna do?\n" + line);
+        String fourSpace = "     ";
+        while(scanner.hasNextLine()){
+            String word = scanner.nextLine();
+            if(word.equals("bye")){
+                System.out.println(line + fourSpace + "See ya later alligator!\n"+ line);
+                break;
+            }
+            else {
+                System.out.println(line + fourSpace + word + "\n" + line);
+            }
+        }
     }
+
 }
+
