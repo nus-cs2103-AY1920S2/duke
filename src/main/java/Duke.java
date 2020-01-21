f9e753ffd4bef33b1ab678c2f1cae8ebb71d9b1e
Willis Tay <27534078+tzihiang@users.noreply.github.com>
2020-01-21 14:47:15 +0800

    gradleTutorial pt1
    Did everything till checkstyle, continue from Shadow section

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,13 @@
+/**
+ * Javadoc Comment.
+ */
+
 public class Duke {
+    /**
+     * This is a javadoc comment.
+     */
     public static void main(String[] args) {
+        // Main method.
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
