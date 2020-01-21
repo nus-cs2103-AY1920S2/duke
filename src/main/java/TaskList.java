31d2ecada22ab28f043e45df1027c153560d3458
Thng Yao Jie <yaoz1337@gmail.com>
2020-01-21 17:56:19 +0800

    Revert "Revert "Level-4""
    This reverts commit 63711e97a49c27d7b6961f757968daafcecc32c6.

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -4,16 +4,29 @@ class TaskList {
     // int representing number of tasks in the list
     private int nTasks = 0;
 
-    public String add(String s) {
-        lTasks[nTasks] = new Task(s);
-        nTasks++;
-        return "added: " + s;
+    public String[] addTodo(String s) {
+        return add(new Todo(s));
     }
     
+    public String[] addDeadline(String s, String by) {
+        return add(new Deadline(s, by));
+    }
+    
+    public String[] addEvent(String s, String at) {
+        return add(new Event(s, at));
+    }
+
+    private String[] add(Task s) {
+        lTasks[nTasks] = s;
+        nTasks++;
+        return new String[]{"Got it. I've added this task:", "  " + s.toString(), String.format("Now you have %d tasks in the list.", nTasks) };
+    }
+
     public String[] list() {
-        String[] tasks = new String[nTasks];
-        for (int i = 0; i < nTasks; i++) {
-            tasks[i] = String.format("%d.%s", i + 1, lTasks[i].toString());
+        String[] tasks = new String[nTasks + 1];
+        tasks[0] = nTasks == 0 ? "You have no tasks in your list." : "Here are the tasks in your list:";
+        for (int i = 1; i <= nTasks; i++) {
+            tasks[i] = String.format("%d.%s", i, lTasks[i - 1].toString());
         }
         return tasks;
     }
