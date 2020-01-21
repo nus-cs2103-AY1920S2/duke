e71f0cdeff9e4a5084fa9d97b2f0589366ca3ffb
bscrow <boshen@live.com>
2020-01-21 22:42:33 +0800

    Added cathulhu exit logo

--- src/main/java/Cathulhu.java
+++ src/main/java/Cathulhu.java
@@ -5,11 +5,29 @@ public class Cathulhu {
     private void interact() {
         Scanner sc = new Scanner(System.in);
         String cmd = "";
+        String catLeaves = "......................................................................\n"
+        +".......................................Iä! Iä! .......................\n"
+        +"...........................................Cthul......................\n"
+        +"............................................hu fh.....................\n"
+        +".........','...............................tagn!......................\n"
+        +"........'Ph'nglui mglw'nfah Cthulhu R'lyeh wgah.......................\n"
+        +".......'nagl fhatgn! Iä! Iä! Cthulhu fhtagn! P........................\n"
+        +".......h'nglui mglw'nfah Cthulhu R'lyeh wgah'n........................\n"
+        +"........agl fhtagn! Iä! Iä! Cthulhu fhtagn! P.........................\n"
+        +"...............h'nglui mglw'nfah Cthulhu R'ly;........................\n"
+        +"................yeh wgah'nagl fhtagn! Iä! Iä!,........................\n"
+        +"...............Cthul...hufht.....agn!Ph'nglui ........................\n"
+        +"..............mglw.....'nfah.....Cthu.....lhu.........................\n"
+        +".............R'ly........ehw....gah'.......nag........................\n"
+        +".............lf...........ht....ag..........n!........................\n"
+        +"......................................................................\n";
+
         while (true) {
             System.out.println("\t-------------C-A-T-H-U-L-H-U-------------");
             cmd = sc.nextLine();
             if (cmd.equalsIgnoreCase("bye")) {
-                System.out.println("\tBe gone then, mortal.");
+                System.out.println("\tBe gone then, mortal.\n");
+                System.out.println(catLeaves);
                 break;
             }
             System.out.println("\t" + cmd);
