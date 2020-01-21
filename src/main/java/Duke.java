fad6a43b60abfc977db18748361533fdd68caf7a
Thng Yao Jie <yaoz1337@gmail.com>
2020-01-21 13:18:53 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,27 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        // System.out.println("Hello from\n" + logo);
+        System.out.println(addBorders("Hello! I'm Duke\n  What can I do for you?"));
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {
+            String next = in.next();
+            if (next.equals("bye")) {
+                System.out.println(addBorders("Bye. Hope to see you again soon!"));
+                break;
+            } else {
+                System.out.println(addBorders(echo(next)));
+            }
+        }
+        in.close();
+    }
+
+    private static String addBorders(String s) {
+        String border = "____________________________________________________________";
+        return String.format("    %s\n     %s\n    %s", border, s, border);
+    }
+
+    public static String echo(String s) {
+        return s;
     }
 }
