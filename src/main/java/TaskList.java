815e4c1d2b47cc4fdefdaf03433154f646ac696f
Thng Yao Jie <yaoz1337@gmail.com>
2020-01-21 17:54:53 +0800

    Revert "Revert "A-Classes""
    This reverts commit f2b50bed4590642b69ecdb174c84b715dcf2228f.

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,13 +1,11 @@
 class TaskList {
-    // List of tasks represented as a String array; assume no more than 100 tasks.
-    private String[] lTasks = new String[100];
-    // boolean representing state of each task
-    private boolean[] lDone = new boolean[100];
+    // List of tasks; assume no more than 100 tasks.
+    private Task[] lTasks = new Task[100];
     // int representing number of tasks in the list
     private int nTasks = 0;
 
     public String add(String s) {
-        lTasks[nTasks] = s;
+        lTasks[nTasks] = new Task(s);
         nTasks++;
         return "added: " + s;
     }
@@ -15,13 +13,12 @@ class TaskList {
     public String[] list() {
         String[] tasks = new String[nTasks];
         for (int i = 0; i < nTasks; i++) {
-            tasks[i] = String.format("%d.%s %s", i + 1, lDone[i] ? "[✓]" : "[✗]", lTasks[i]);
+            tasks[i] = String.format("%d.%s", i + 1, lTasks[i].toString());
         }
         return tasks;
     }
 
     public String[] done(int i) {
-        lDone[i - 1] = true;
-        return new String[]{ "Nice! I've marked this task as done:", String.format("[✓] %s", lTasks[i - 1]) };
+        return new String[]{ "Nice! I've marked this task as done:", lTasks[i - 1].done() };
     }
 }
