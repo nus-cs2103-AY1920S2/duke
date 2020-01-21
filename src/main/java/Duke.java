c2ca6b8cc7dc7af3710f0df1ca55701c72f4dac5
KwanXinJie <xjkwan1998@gmail.com>
2020-01-21 14:57:43 +0800

    Lvl2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,8 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        String statement;
-        Scanner sc;
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -11,15 +10,36 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("What can I do for you?");
+        input();
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    private static void input() {
+        Scanner sc;
+        String statement;
+        ArrayList<String> arrList = new ArrayList<>();
+
         sc = new Scanner(System.in);
         statement = sc.nextLine();
 
         while (!statement.equals("bye")) {
-            System.out.println(statement);
-            System.out.println();
+            if (statement.equals("list") && arrList.size() != 0) {
+                for (int i=1; i <= arrList.size(); i++) {
+                    System.out.println(i +". " + arrList.get(i-1));
+                }
+                System.out.println();
+            }
+            else if (statement.equals("list")) {
+                System.out.println("Empty");
+                System.out.println();
+            }
+            else {
+                arrList.add(statement);
+                System.out.println("added: " + statement);
+                System.out.println();
+            }
             sc = new Scanner(System.in);
             statement = sc.nextLine();
         }
-        System.out.println("Bye. Hope to see you again soon!");
     }
 }
