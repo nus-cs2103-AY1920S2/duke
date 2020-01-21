facff977825642588c8b2c15b16a33fe034c6122
alam8064 <59945273+alam8064@users.noreply.github.com>
2020-01-21 16:52:29 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,18 +10,28 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         Scanner sc = new Scanner(System.in);
-        String[] data = new String[100];
+        Task[] data = new Task[100];
         int slot = 0;
 
         while (sc.hasNext()) {
             String input = sc.nextLine();
+            String[] inputArr = input.split("\\s+");
+            String action = inputArr[0];
+
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
-            } else if (input.equals("list")){
-                for (int i = 0; i < slot; i++) {System.out.println(i+1 + ". " + data[i]);}
+            } else if (input.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < slot; i++) {
+                    System.out.println(i + 1 + ".[" + data[i].getStatusIcon() + "] " + data[i].task);
+                }
+            } else if (action.equals("done")) {
+                int option = Integer.parseInt(inputArr[1]);
+                data[option-1].setDone();
+                System.out.println("Nice! I've marked this task as done:\n  [" + data[option-1].getStatusIcon() + "] " + data[option-1].task);
             } else {
-                data[slot] = input;
+                data[slot] = new Task(input);
                 System.out.println("added: " + input);
                 slot++;
             }
---
+++ src/main/java/Task.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Task {
+    protected String task;
+    protected boolean done; //✘
+
+    public Task(String task) {
+        this.task = task;
+        this.done = false;
+    }
+
+    public void setDone() {
+        this.done = true; //✓
+    }
+
+    public String getStatusIcon() {
+        return (done ? "\u2713" : "\u2718");  //return tick or X symbols
+    }
+}
