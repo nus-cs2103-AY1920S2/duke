f1acba9ddf4d20748bc48e2d06a8c506ec24429f
Jy47 <Reus1212>
2020-01-21 16:25:05 +0800

    Revert "Level 1 commit"
    This reverts commit 7d9d704304ad3d076c83e3d22f3d72a10a0f653c.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,3 @@
-import java.util.*;
-
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -8,26 +6,5 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        Scanner sc = new Scanner(System.in);
-        System.out.println("-----------");
-        System.out.println("Hello! I am Duke");
-        System.out.println("What can i do for you");
-        System.out.println("------------");
-        String mes = "Hello";
-
-        while(!mes.equalsIgnoreCase("bye")){
-            // in-case first case bye in entered ##
-            mes = sc.nextLine();
-            if(!mes.equalsIgnoreCase("bye")){
-                System.out.println("  ---------");
-                System.out.println("     "+mes+"   ");
-                System.out.println("  ---------");
-            }
-        }
-
-
-        System.out.println("  ---------");
-        System.out.println("     Bye. Hope to see you again");
-        System.out.println("  ---------");
     }
 }
