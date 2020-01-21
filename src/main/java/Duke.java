84edcb2905719994aefaaa2f42d40e526e1e5dcb
KwanXinJie <xjkwan1998@gmail.com>
2020-01-22 01:18:05 +0800

    Lvl5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -20,6 +20,7 @@ public class Duke {
         ArrayList<Task> arrList = new ArrayList<>();
         String taskType, task, date, word, statement;
         Task t;
+        String taskArray[] = new String[2];
 
         sc = new Scanner(System.in);
         statement = sc.nextLine();
@@ -34,40 +35,54 @@ public class Duke {
                 System.out.println();
             }
             else if (statement.equals("list")) {
-                System.out.println("Empty");
+                DukeException error = new DukeException();
+                System.out.println(error.empty());
                 System.out.println();
             }
             else {
-                taskType = statement.split(" ")[0];
-                if ((statement.split(" ")[0]).equals("done")) {
-                    int a = Integer.parseInt(statement.split(" ")[1]) - 1;
-                    arrList.get(a).markAsDone();
-                    System.out.println("Nice! I've marked this task as done: ");
-                    System.out.println("  [" + arrList.get(a).getStatusIcon() + "] " + arrList.get(a).description);
-                    System.out.println();
+                taskArray = statement.split(" ");
+                if (taskArray.length ==1) {
+                    DukeException error = new DukeException();
+                    System.out.println(error.errorMsg(taskArray[0]));
+                    System.out.println("");
                 }
                 else {
-                    statement = statement.substring(statement.indexOf(" "), statement.length());
-                    if (!taskType.equals("todo")) {
-                        task = statement.split("/")[0];
-                        date = statement.split("/")[1];
-                        word = date.substring(0, date.indexOf(" "));
-                        date = date.substring(date.indexOf(" ") + 1, date.length());
-                        if (task.equals("event")) {
-                            t = new Event(task, word, date);
+                    taskType = taskArray[0];
+                    if ((statement.split(" ")[0]).equals("done")) {
+                        int a = Integer.parseInt(statement.split(" ")[1]) - 1;
+                        if (a <= arrList.size() && a!=-1) {
+                            arrList.get(a).markAsDone();
+                            System.out.println("Nice! I've marked this task as done: ");
+                            System.out.println("  [" + arrList.get(a).getStatusIcon() + "] " + arrList.get(a).description);
+                            System.out.println();
                         }
                         else {
-                            t = new Deadline(task, word, date);
+                            DukeException error = new DukeException();
+                            System.out.println(error.outOfBound());
+                            System.out.println();
                         }
                     }
                     else {
-                        t = new Todo(statement);
+                        statement = statement.substring(statement.indexOf(" "), statement.length());
+                        if (!taskType.equals("todo")) {
+                            task = statement.split("/")[0];
+                            date = statement.split("/")[1];
+                            word = date.substring(0, date.indexOf(" "));
+                            date = date.substring(date.indexOf(" ") + 1, date.length());
+                            if (task.equals("event")) {
+                                t = new Event(task, word, date);
+                            } else {
+                                t = new Deadline(task, word, date);
+                            }
+                        } else {
+                            t = new Todo(statement);
+                        }
+                        arrList.add(t);
+                        System.out.println("Got it. I've added this task:");
+                        System.out.println("  [" + t.getType() + "][" + t.getStatusIcon() + "]" + t.getTask());
+                        System.out.println("Now you have " + arrList.size() + " tasks in the list.");
+                        System.out.println();
                     }
-                    arrList.add(t);
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println("  [" +t.getType()+ "][" +t.getStatusIcon()+ "]" +t.getTask());
-                    System.out.println("Now you have " +arrList.size()+ " tasks in the list.");
-                    System.out.println();
                 }
             }
             sc = new Scanner(System.in);
---
+++ src/main/java/DukeException.java
@@ -0,0 +1,20 @@
+public class DukeException {
+    protected String task;
+
+    public String errorMsg(String task) {
+        if (task.equals("todo") || task.equals("deadline") || task.equals("event")) {
+            return "☹ OOPS!!! The description of a " +task+ " cannot be empty.";
+        }
+        else {
+            return "☹ OOPS!!! I'm sorry, but I don't know what that means :-(";
+        }
+    }
+
+    public String empty() {
+        return "There is no task in the list.";
+    }
+
+    public String outOfBound() {
+        return "No such task exist.";
+    }
+}
