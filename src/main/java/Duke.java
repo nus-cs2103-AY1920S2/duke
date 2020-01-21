72d0e0d7460ab184374336cc277e8e3118e109e5
khsc96 <khsc96@gmail.com>
2020-01-22 00:29:14 +0800

    Level 1 completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,42 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    private final static String INDENT = "    ";
+    private final static String BORDER = "____________________________________________________________";
+    private final static String EXIT = "bye";
+    private final static String GOODBYE_MESSAGE = "Goodbye and have a beautiful time!";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = INDENT + "  _____  __    __  _____ \n"
+                + INDENT + " |  ___||  \\  /  ||  ___| \n"
+                + INDENT + " | |__   \\  \\/  / | |__\n"
+                + INDENT + " |  __|   |    |  |  __|\n"
+                + INDENT + " | |___  /  /\\  \\ | |___\n"
+                + INDENT + " |_____||__/  \\__\\|_____| \n";
+        System.out.println(INDENT + BORDER);
+        System.out.println(logo);
+        System.out.println(INDENT + "Hello! I'm EXE, I'll execute anything on your command! :)");
+        System.out.println(INDENT + "What do you want to exe?");
+        System.out.println(INDENT + BORDER);
+
+        Scanner scanner = new Scanner(System.in);
+
+        String message = scanner.nextLine();
+
+        while (!message.equals(EXIT)) {
+            System.out.println(formatReply(message));
+            message = scanner.nextLine();
+        }
+
+        System.out.println(formatReply(GOODBYE_MESSAGE));
+
+        scanner.close();
     }
+
+    public static String formatReply(String message) {
+        String reply = INDENT + BORDER + "\n" + INDENT + " " + message + "\n" + INDENT + BORDER;
+        return reply;
+    }
+
 }
