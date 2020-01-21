42be50fed37eaeb9a5666b66c9a1420fc49a16c8
Ern Tek Teoh <erntekteoh@gmail.com>
2020-01-21 13:39:05 +0800

    Completed Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,10 +7,22 @@ public class Duke {
         System.out.println("What can I do for you?");
         System.out.println("____________________________");
         String command = sc.nextLine();
+        String[] tasks = new String[100];
+        int size = 0;
         while(!command.equals("bye")) {
-            System.out.println("____________________________");
-            System.out.println(command);
-            System.out.println("____________________________");
+            if(command.equals("list")){
+                System.out.println("____________________________");
+                for(int i = 0; i < size; i++) {
+                    System.out.println(i + 1 + ". " + tasks[i]);
+                }
+                System.out.println("____________________________");
+            } else {
+                tasks[size] = command;
+                size++;
+                System.out.println("____________________________");
+                System.out.println("added: " + command);
+                System.out.println("____________________________");
+            }
             command = sc.nextLine();
         }
         System.out.println("____________________________");
