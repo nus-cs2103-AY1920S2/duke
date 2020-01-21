4345ae59e5f1457fabef2aa445580cc824e9508f
JingYuOng <jingyu.ong1212@gmail.com>
2020-01-21 17:20:27 +0800

    Level 2 Commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,19 +7,28 @@ public class Duke {
         System.out.println("Hello! I am Duke");
         System.out.println("What can i do for you");
         System.out.println("------------");
-        String mes = "Hello";
+        String mesInput = "";
+        String [] arr = new String[100];
+        int index = 0;
+        while(!mesInput.equalsIgnoreCase("bye")){
+            mesInput = sc.nextLine();
 
-        while(!mes.equalsIgnoreCase("bye")){
-            // in-case first case bye in entered ##
-            mes = sc.nextLine();
-            if(!mes.equalsIgnoreCase("bye")){
+            if(!mesInput.equalsIgnoreCase("bye") && !mesInput.equalsIgnoreCase("list")){
                 System.out.println("  ---------");
-                System.out.println("     "+mes+"   ");
-                System.out.println("  ---------");
+                System.out.println("   added: "  +mesInput);
+                System.out.println("   ---------");
+                arr[index] = mesInput ;
+                index = index + 1;
+            }
+
+            if (mesInput.equalsIgnoreCase("list") && arr[0] != ""){
+                System.out.println("  --------");
+                for(int i = 0 ; i < index ; i ++){
+                    System.out.println("   "+ (i+1)+ ". "+ arr[i]);
+                }
+                System.out.println("  --------");
             }
         }
-
-
         System.out.println("  ---------");
         System.out.println("     Bye. Hope to see you again");
         System.out.println("  ---------");
