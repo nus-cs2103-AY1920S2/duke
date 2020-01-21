57feb73056381892e63cc03e0cbd14058cf3e9a9
nelsontky <nelson@ccb.wtf>
2020-01-21 14:20:11 +0800

    Complete Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,48 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+
+        printHorizontalLine();
+        printIndented("Hello! I'm Duke");
+        printIndented("What can I do for you?");
+        printHorizontalLine();
+        System.out.println();
+
+        String input = sc.nextLine();
+
+        while (!input.equals("bye")) {
+            switch (input) {
+            case "list":
+                printHorizontalLine();
+                printIndented("list");
+                printHorizontalLine();
+                break;
+
+            case "blah":
+                printHorizontalLine();
+                printIndented("blah");
+                printHorizontalLine();
+                break;
+
+            default:
+                break;
+            }
+
+            input = sc.nextLine();
+        }
+
+        printHorizontalLine();
+        printIndented("Bye. Hope to see you again soon!");
+        printHorizontalLine();
+    }
+
+    public static void printIndented(String text) {
+        System.out.println("     " + text);
+    }
+
+    public static void printHorizontalLine() {
+        System.out.println("    ____________________________________________________________");
     }
 }
