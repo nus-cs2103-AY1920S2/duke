fd0265c059dbd7fc12f25480f0c2b5a82eae7bd2
Eugene Tan <eugenetanyc@gmail.com>
2020-01-21 20:12:13 +0800

    Completed Level 3: Mark as Done. Added more helper functions in Task.java and TaskHandler.java. Tasks now have a isDone attribute, which can be marked as true/false. The Task obtainStatusIcon String value depends on the value of isDone. Tasks can be marked as complete using doTask method in Task.java. Added more cases in TaskHandler's switch-case block.

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,19 +1,34 @@
 public class Task {
 
+    // whether or not user has completed the task
+    private boolean isDone;
+
     // main task that user wants to log
     private String command;
+
     // for utility
     private String horizontalLine = "____________________________________________________________";
 
     public Task(String command) {
         this.command = command;
+        this.isDone = false; // all tasks are created incomplete
     }
 
     // Prints the indication that the Task's command has been added
     public void taskAddedMessage() {
         printLine();
-        System.out.println("==> Added unique task: " + this);
+        print("==> Added unique task: " + this);
         printLine();
+    }
+
+    // Marks the Task as completed
+    public void doTask() {
+        this.isDone = true;
+    }
+
+    // Obtains tick or cross status icon dependent on completion
+    public String obtainStatusIcon() {
+        return (this.isDone) ? "\u2713" : "\u2718";
     }
 
     private void printLine() {
--- src/main/java/TaskHandler.java
+++ src/main/java/TaskHandler.java
@@ -16,9 +16,11 @@ public class TaskHandler {
 
         while (io.hasNextLine()) {
 
+            // obtain String command and split it to find the intention of the user (first word)
             String command = io.nextLine();
+            String[] commandWords = command.split("\\s");
 
-            switch (command) {
+            switch (commandWords[0]) {
 
             case "bye":
                 return;
@@ -27,22 +29,37 @@ public class TaskHandler {
                 printAllTasks();
                 break;
 
+            case "done":
+                // user inputs will be "done _____"
+                // Take only the very next token which must be an integer
+                Task currentTaskDone = allTasks.get(Integer.valueOf(commandWords[1]) - 1);
+                doTask(currentTaskDone);
+                break;
+
             default:
                 // This is the only part where we create new tasks
-                Task currentTask = new Task(command);
-                addTaskToStored(currentTask);
+                // Add task, then announce this addition
+                Task currentTaskDefault = new Task(command);
+                addTaskToStored(currentTaskDefault);
 
             }
 
         }
     }
 
-    // adds the Task to store in the ArrayList, and prints added message
+    // Adds the Task to store in the ArrayList, and prints added message
     private void addTaskToStored(Task t) {
         allTasks.add(t);
         t.taskAddedMessage();
     }
 
+    // Mark task as completed, and prints completion message
+    private void doTask(Task t) {
+        t.doTask();
+        printTaskComplete(t);
+    }
+
+    // Prints all tasks, their number order, and their completion
     private void printAllTasks() {
         printLine();
         for (int i = 0; i < allTasks.size(); i++) {
@@ -52,8 +69,15 @@ public class TaskHandler {
     }
 
     // for utility
+    private void printTaskComplete(Task t) {
+        print("Nice! The following task has been marked completed:\n"
+                + "[\u2713] " + t);
+    }
+
+    // for utility
     private void printTaskFromStored(int i) {
-        print(String.valueOf(i+1) + ". " + allTasks.get(i));
+        String tickOrCross = allTasks.get(i).obtainStatusIcon();
+        print(String.valueOf(i+1) + ". [" + tickOrCross + "]" + allTasks.get(i));
     }
 
     // for utility
