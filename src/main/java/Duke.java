cb6eb63c27b8f7e908786a8724723768523c57f1
Ern Tek Teoh <erntekteoh@gmail.com>
2020-01-21 13:31:21 +0800

    Completed Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________");
+        String command = sc.nextLine();
+        while(!command.equals("bye")) {
+            System.out.println("____________________________");
+            System.out.println(command);
+            System.out.println("____________________________");
+            command = sc.nextLine();
+        }
+        System.out.println("____________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________");
     }
 }
