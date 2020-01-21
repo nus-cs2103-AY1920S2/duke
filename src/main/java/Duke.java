fbef8f89f5a00f85394e416a3e93fbdd39c26d35
Vincent Chow <51158450+thetruevincentchow@users.noreply.github.com>
2020-01-21 23:03:37 +0800

    implement Level 1: Greet, Echo, Exit Greets the user, echos commands entered, and exits when "bye" is entered as the command. Horizontal header line and indents are printed.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,59 @@
+import java.util.Scanner;
+import java.io.PrintStream;
+
 public class Duke {
-    public static void main(String[] args) {
+    private static final int indentLevels = 4;
+    
+    private static String indentString(String s) {
+        //https://stackoverflow.com/questions/15888934/how-to-indent-a-multi-line-paragraph-being-written-to-the-console-in-java
+        return s.replaceAll("(?m)^", " ".repeat(indentLevels));
+    }
+    
+    private static void indentedPrintln(String s) {
+        System.out.println(indentString(s));
+    }
+    
+    private static void indentedPrintf(String format, Object... args) {
+        System.out.print(indentString(String.format(format, args)));
+    }
+    
+    private static void printHeaderLine() {
+        indentedPrintln("____________________________________________________________");
+    }
+    
+    private static void greet() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        indentedPrintln("Hello from\n" + logo);
+    }
+    
+    private static boolean runCommand(String command) {
+        if (command.equals("bye")) {
+            indentedPrintln("Goodbye");
+            return false;
+        } else {
+            indentedPrintln(command);
+        }
+        return true;
+    }
+    
+    public static void main(String[] args) {
+        printHeaderLine();
+        greet();
+        printHeaderLine();
+        
+        Scanner sc = new Scanner(System.in);
+        boolean running = true;
+        while (running) {
+            
+            String command = sc.nextLine();
+            
+            printHeaderLine();
+            running = runCommand(command);
+            printHeaderLine();
+        }
     }
 }
