3b0e23b46548ac71c527460fbcf7ab603c7a2109
beatricetay <beatricetayyt@gmail.com>
2020-01-22 00:24:51 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -9,27 +10,37 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         //System.out.println("Hello from\n" + logo);
 
-        String greet = "    ____________________________________________________________\n"
+        String borderDesign = "    ____________________________________________________________";
+        String greet = borderDesign + "\n"
                 + "     Hello! I'm Duke\n     What can I do for you?\n"
-                + "    ____________________________________________________________\n";
+                + borderDesign + "\n";
         System.out.println(greet);
 
-        String exit = "    ____________________________________________________________\n"
-                + "     Bye. Hope to see you again soon!\n"
-                + "    ____________________________________________________________\n";
+        String exit = "     Bye. Hope to see you again soon!\n"
+                + borderDesign;
 
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> lst = new ArrayList<String>();
+
         while (sc.hasNextLine()) {
             String line = sc.nextLine();
+
+            System.out.println(borderDesign);
             if (line.equals("bye")) {
-                System.out.print(exit);
+                System.out.println(exit);
                 System.exit(0);
+
+            } else if (line.equals("list")) {
+                for (int i = 1; i <= lst.size(); i++) {
+                    String item = "     " + i + ". " + lst.get(i - 1);
+                    System.out.println(item);
+                }
+
             } else {
-                String echo = "    ____________________________________________________________\n"
-                        + "    " + line + "\n"
-                        + "    ____________________________________________________________\n\n";
-                System.out.print(echo);
+                lst.add(line);
+                System.out.println("     added: " + line);
             }
+            System.out.println(borderDesign + "\n");
         }
     }
 }
