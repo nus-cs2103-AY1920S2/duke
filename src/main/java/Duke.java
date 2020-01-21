4966286c1cc1f61b2e90e299dfec631d8bb3daac
Eugene Tan <eugenetanyc@gmail.com>
2020-01-21 17:55:29 +0800

    Revert "Completed: Level 1. Greet, Echo, Exit"
    This reverts commit d07dcebd0680cc607bcaa7d846153b823cfdc27e.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,10 @@
-import java.util.Scanner;
-
 public class Duke {
     public static void main(String[] args) {
-
-        // Welcomes the user
-        WelcomeGoodbye wm = new WelcomeGoodbye();
-        wm.welcome();
-
-        // Begins accepting input commands
-        TaskHandler taskHandler = new TaskHandler();
-        taskHandler.serveUser();
-
-        // User is done
-        wm.goodbye();
-
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
     }
-
 }
