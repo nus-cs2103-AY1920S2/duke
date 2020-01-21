08a5792956c7115203e36018554e14bc670d827a
ongch00 <ongchoonhan@gmail.com>
2020-01-21 22:00:56 +0800

    Implement Level 4. ToDos, Events, Deadlines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,32 +14,50 @@ public class Duke {
 
         while (isRunning) {
             String input = scanner.nextLine();
-            String[] command = input.split(" ", 2);
-            if (command[0].equals("bye")) {
+            String[] commandLine = input.split(" ", 2);
+            String command = commandLine[0];
+            if (command.equals("bye")) {
                 System.out.println("____________________________________________________________\n" +
                         " Bye. Hope to see you again soon!\n" +
                         "____________________________________________________________");
                 isRunning = false;
-            } else if (command[0].equals("list")) {
+            } else if (command.equals("list")) {
                 System.out.println("____________________________________________________________\n" +
                         " Here are the tasks in your list:");
                 for (int i = 0; i < tasks.size(); i++) {
                     System.out.printf(" %d. %s\n", (i + 1), tasks.get(i));
                 }
                 System.out.println("____________________________________________________________");
-            } else if (command[0].equals("done")) {
-                int taskNum = Integer.parseInt(command[1]);
+            } else if (command.equals("done")) {
+                int taskNum = Integer.parseInt(commandLine[1]);
                 Task taskToMark = tasks.get(taskNum - 1);
                 taskToMark.markDone();
                 System.out.println("____________________________________________________________\n" +
                         " Nice! I've marked this task as done:");
                 System.out.printf(" %s\n", taskToMark);
                 System.out.println("____________________________________________________________");
-            } else {
-                Task newTask = new Task(input);
+            } else if (command.equals("todo")){
+                Task newTask = new Todo(commandLine[1]);
                 tasks.add(newTask);
                 System.out.println("____________________________________________________________");
-                System.out.printf(" added: %s\n", newTask.getDescription());
+                System.out.printf(" Got it. I've added this task: \n   %s\n", newTask);
+                System.out.printf(" Now you have %d tasks in the list.\n", tasks.size());
+                System.out.println("____________________________________________________________");
+            } else if (command.equals("deadline")){
+                String[] deadlineDescriptionDate = commandLine[1].split("/by ");
+                Task newTask = new Deadline(deadlineDescriptionDate[0], deadlineDescriptionDate[1]);
+                tasks.add(newTask);
+                System.out.println("____________________________________________________________");
+                System.out.printf(" Got it. I've added this task: \n   %s\n", newTask);
+                System.out.printf(" Now you have %d tasks in the list.\n", tasks.size());
+                System.out.println("____________________________________________________________");
+            } else if (command.equals("event")){
+                String[] eventDescriptionDate = commandLine[1].split("/at ");
+                Task newTask = new Event(eventDescriptionDate[0], eventDescriptionDate[1]);
+                tasks.add(newTask);
+                System.out.println("____________________________________________________________");
+                System.out.printf(" Got it. I've added this task: \n   %s\n", newTask);
+                System.out.printf(" Now you have %d tasks in the list.\n", tasks.size());
                 System.out.println("____________________________________________________________");
             }
         }
