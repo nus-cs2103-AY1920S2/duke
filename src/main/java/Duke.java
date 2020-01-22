bff1c42eaddbacb4743dcb1bfb7aa4a09a14786b
kschiew <kschiew98@gmail.com>
2020-01-23 06:49:13 +0800

    Improve IO style

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -28,7 +28,7 @@ public class Duke {
             case "done":
                 int targetIdx = sc.nextInt() - 1;
                 this.tasks.get(targetIdx).markDone();
-                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("Good job! I've marked this task as done:");
                 System.out.printf("%d. %s\n", targetIdx + 1, tasks.get(targetIdx).toString());
                 echo(new Scanner(System.in));
                 break;
@@ -37,7 +37,8 @@ public class Duke {
                 String userInput = sc.nextLine();
                 TodoTask todoTask = TodoTask.createTodoTask(userInput);
                 tasks.add(todoTask);
-                System.out.println("added " + todoTask.toString());
+                System.out.println("Added: " + todoTask.toString());
+                System.out.printf("Now you have %d task(s) on your list.\n", tasks.size());
                 System.out.println();
                 echo(new Scanner(System.in));
                 break;
@@ -47,7 +48,8 @@ public class Duke {
                 String userDue1 = sc.next();
                 DeadlineTask deadlineTask = DeadlineTask.createDeadlineTask(userInput1, userDue1);
                 tasks.add(deadlineTask);
-                System.out.println("added " + deadlineTask.toString());
+                System.out.println("Added: " + deadlineTask.toString());
+                System.out.printf("Now you have %d task(s) on your list.\n", tasks.size());
                 System.out.println();
                 echo(new Scanner(System.in));
                 break;
@@ -57,7 +59,8 @@ public class Duke {
                 String userDue2 = sc.next();
                 EventTask eventTask = EventTask.createEventTask(userInput2, userDue2);
                 tasks.add(eventTask);
-                System.out.println("added " + eventTask.toString());
+                System.out.println("Added: " + eventTask.toString());
+                System.out.printf("Now you have %d task(s) on your list.\n", tasks.size());
                 System.out.println();
                 echo(new Scanner(System.in));
                 break;
@@ -70,23 +73,24 @@ public class Duke {
         }
     }
 
-        private void printList() {
-            for (int i = 0; i < tasks.size(); ++i) {
-                Task task = tasks.get(i);
-                System.out.printf("%d. %s\n", i + 1, task.toString());
-            }
-        }
-
-        private static void printByeMsg() {
-            System.out.println("Bye. Hope to see you again soon! :)");
-        }
-
-        public static void main(String[] args) {
-
-            Duke duke  = new Duke();
-
-            Duke.greet();
-
-            duke.echo(new Scanner(System.in));
+    private void printList() {
+        System.out.println("Here is your list of tasks:");
+        for (int i = 0; i < tasks.size(); ++i) {
+            Task task = tasks.get(i);
+            System.out.printf("%d. %s\n", i + 1, task.toString());
         }
     }
+
+    private static void printByeMsg() {
+        System.out.println("Bye. Hope to see you again soon! :)");
+    }
+
+    public static void main(String[] args) {
+
+        Duke duke  = new Duke();
+
+        Duke.greet();
+
+        duke.echo(new Scanner(System.in));
+    }
+}
