4a85a981cd105741fdeb5b3e030065417a6b1ab4
amrl <amirul.ardy@gmail.com>
2020-01-22 23:48:40 +0800

    Separate greeting into own function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,17 @@
 public class Duke {
     public static void main(String[] args) {
+        printGreeting();
+    }
+
+    public static void printGreeting() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        System.out.println(logo);
+        System.out.println("Hi, I'm Duke!");
+        System.out.println("What can I do for you?\n");
     }
 }
