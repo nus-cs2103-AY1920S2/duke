21ffb5847f2ed14728a915db8b47aff46ff1926d
garysyndromes <garylimky@hotmail.com>
2020-01-22 23:34:47 +0800

    ip level 1 completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,25 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("What can I do for you today?");
+        System.out.println("Please tell me what to do!");
+        System.out.println("\n");
+        System.out.println("====================================================================================");
+        Scanner sc = new Scanner(System.in);
+        String exitCommand = "bye";
+        while (sc.hasNext()) {
+            String input = sc.next();
+            if (input.toLowerCase().equals(exitCommand)) {
+                break;he
+            }
+            System.out.println(input);
+
+
+        }
+
+        System.out.println("\n");
+        System.out.println("====================================================================================");
+        System.out.println("Bye bye! Thank you for using me! Hope to see you again soon.");
+
     }
 }
