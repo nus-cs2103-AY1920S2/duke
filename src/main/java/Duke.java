efcec96aef23e295f04c188473905f162cc1afbf
helloimhai <1503.hai.nguyen@gmail.com>
2020-01-23 00:41:09 +0800

    Level 1 commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,17 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("What can I do for you today homie?\n");
+        Scanner io = new Scanner(System.in);
+        String command = io.nextLine();
+        while(!command.equals("bye")) {
+            System.out.println("   -----");
+            System.out.println("   " + command);
+            System.out.println("   -----");
+            command = io.nextLine();
+        }
+        System.out.println("   -----");
+        System.out.println("   Bye bye friend!");
+        System.out.println("   -----");
     }
 }
