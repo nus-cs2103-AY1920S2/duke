a1a9ad22b769bce93998409f91e2a3eb5215913b
huutienvt98 <huutienvt98@gmail.com>
2020-01-22 23:00:51 +0800

    Level-1 of Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,6 +4,7 @@ public class Duke {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Greeting, traveler. My name is Andrew. What can I do for you?");
+
         while (scan.hasNext()) {
             String input = scan.nextLine();
             if (input.equals("bye")) {
