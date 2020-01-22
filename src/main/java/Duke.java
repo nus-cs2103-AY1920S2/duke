7fe16f9c141c8f51f89ab53575ee502242b1d0c0
Jade Tay <jadet.hx@gmail.com>
2020-01-23 03:31:05 +0800

    Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,14 @@
 import java.util.*;
 
 public class Duke {
-    public static final String line = "    ____________________________________________________________";
+    public static final String line = "    ____________________________________________________________\n";
     static Task[] tasks = new Task[100];
     public static int totalTasks = 0;
 
     public static String hello() {
         String s1 = "     Hello! i'm dUKE!";
         String s2 = "     How can I help you!";
-        return line + "\n" + s1 + "\n" + s2 + "\n" + line + "\n";
+        return line + s1 + "\n" + s2 + "\n" + line;
     }
 
     public static String list() {
@@ -22,15 +22,9 @@ public class Duke {
         return listContent.toString();
     }
 
-    public static String blah() {
-        return line + "\n" + "     blah blah" + "\n" + line + "\n";
-    }
-
     public static String bye() {
         return line + "\n" + "     Bye see you again（ｉДｉ）" + "\n" + line;
     }
-
-
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -38,35 +32,54 @@ public class Duke {
         System.out.println(hello());
 
         while (sc.hasNext()) {
-            String input = sc.nextLine();
-            if (input.equals("list")) {
-                System.out.println(list());
-            } else if (input.equals("blah")) {
-                System.out.println(blah());
-            } else if (input.equals("bye")) {
-                System.out.println(bye());
-                break;
-            } else if (input.substring(0,4).equals("done")) {
-                int index = Integer.parseInt(input.substring(5)) - 1;
-                Task comTask = tasks[index];
-                comTask.markAsDone();
-                System.out.println(line + "\n" + "     Wow you finally completed something: " + "\n"
-                            + "       " + comTask.toString() + "\n" + line + "\n");
-            } else {
-                String task = line + "\n" + "     Got it! i've added this task!:" + "\n";
-                if (input.contains("/by")) {
-                    String[] temp = input.split(" /by ");
-                    tasks[totalTasks] = new Deadline(temp[0].substring(9), temp[1]);
-                } else if (input.contains(" /at")) {
-                    String[] temp = input.split(" /at ");
-                    tasks[totalTasks] = new Event(temp[0].substring(6), temp[1]);
-                } else { //todoTask
-                    tasks[totalTasks] = new Todo(input.substring(5));
+            try {
+                String input = sc.nextLine();
+                String[] temp = input.split(" ");
+                String command = temp[0];
+
+                if (command.equals("list")) {
+                    System.out.println(list());
+                } else if (command.equals("bye")) {
+                    System.out.println(bye());
+                    break;
+                } else if (command.equals("done")) {
+                    int index = Integer.parseInt(input.substring(5)) - 1;
+                    Task comTask = tasks[index];
+                    comTask.markAsDone();
+                    System.out.println(line + "     Wow you finally completed something: " + "\n"
+                            + "       " + comTask.toString() + "\n" + line);
+                } else if (command.equals("todo") || command.equals("deadline") || command.equals("event")){
+                    String task = line + "     Got it! i've added this task!:" + "\n";
+                    if (command.equals("deadline")) {
+                        if (temp.length <= 1) {
+                            throw new DukeException(command);
+                        } else {
+                            String[] arr = input.split(" /by ");
+                            tasks[totalTasks] = new Deadline(arr[0].substring(9), temp[1]);
+                        }
+                    } else if (command.equals("event")) {
+                        if (temp.length <= 1) {
+                            throw new DukeException(command);
+                        } else {
+                            String[] arr = input.split(" /at ");
+                            tasks[totalTasks] = new Event(arr[0].substring(6), temp[1]);
+                        }
+                    } else { //todoTask
+                        if (temp.length <= 1) {
+                            throw new DukeException(command);
+                        } else {
+                            tasks[totalTasks] = new Todo(input.substring(5));
+                        }
+                    }
+                    totalTasks++;
+                    task += "       " + tasks[totalTasks - 1] + "\n";
+                    task += "     Now you have " + totalTasks + " tasks in the list." + "\n" + line;
+                    System.out.println(task);
+                } else {
+                    throw new DukeException("others");
                 }
-                totalTasks++;
-                task += "       " + tasks[totalTasks-1] + "\n";
-                task += "     Now you have " + totalTasks + " tasks in the list." + "\n" + line + "\n";
-                System.out.println(task);
+            } catch (Exception e) {
+                System.out.println(e);
             }
         }
 
---
+++ src/main/java/DukeException.java
@@ -0,0 +1,22 @@
+public class DukeException extends Exception {
+
+    public String msg;
+
+    public DukeException(String msg) {
+        super(msg);
+        this.msg = msg;
+    }
+
+    @Override
+    public String toString() {
+        if (!msg.equals("others")) {
+            return "    ____________________________________________________________\n" +
+                    "     " + msg + " does not tell me anything! What exactly do you want? \n" +
+                    "    ____________________________________________________________\n";
+        } else {
+            return "    ____________________________________________________________\n" +
+                    "     " + "I do not understand what you mean!!! :( \n" +
+                    "    ____________________________________________________________\n";
+        }
+    }
+}
