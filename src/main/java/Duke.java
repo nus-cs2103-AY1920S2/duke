a695eedd3062d9d7df05c053452a044f9526da79
johannagwan <johannagwan@gmail.com>
2020-01-23 01:21:20 +0800

    Add add, list for Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,31 +1,49 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
+    private static List<String> tasks = new ArrayList<>();
     public static void main(String[] args) {
         greet();
         Scanner sc = new Scanner(System.in);
         while (sc.hasNext()) {
-            String command = sc.nextLine();
+            String input = sc.nextLine();
+            String[] inputs = input.split(" ", 2);
+            String command = inputs[0];
             if (command.equals("bye")) {
                 sayBye();
                 break;
+            } else if (command.equals("read") || command.equals("return")) {
+                addToList(input);
+            } else if (command.equals("list")) {
+                printList();
             } else {
-                echoCommand(command);
+                sayInvalidInput();
             }
         }
     }
 
     public static void greet() {
-        String greeting = "Hi there, I'm Dodo!\nHow may I help you today?";
-        System.out.println(greeting);
-    }
-
-    public static void echoCommand(String command) {
-        System.out.println(command);
+        System.out.println("Hi there, I'm Dodo!\nHow may I help you today?");
     }
 
     public static void sayBye() {
-        String byeCommand = "Stop procrastinating. See you!";
-        System.out.println(byeCommand);
+        System.out.println("Stop procrastinating. See you!");
+    }
+
+    public static void addToList(String input) {
+        tasks.add(input);
+        System.out.println("added: " + input);
+    }
+
+    public static void printList() {
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.printf("%d. %s\n", i + 1, tasks.get(i));
+        }
+    }
+
+    public static void sayInvalidInput() {
+        System.out.println("Apologies, I'm too dumb to understand that!");
     }
 }
