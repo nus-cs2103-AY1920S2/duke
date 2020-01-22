4e076ddda21a6ebfc28101c5e140c56e0bd1875f
Willis Tay <27534078+tzihiang@users.noreply.github.com>
2020-01-22 12:08:46 +0800

    Finished level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,39 @@
-/**
- * Javadoc Comment.
- */
+import java.util.Scanner;
 
 public class Duke {
-    /**
-     * This is a javadoc comment.
-     */
-    public static void main(String[] args) {
-        // Main method.
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+    static Scanner sc = new Scanner(System.in);
+
+    static void printIntro() {
+        System.out.print(
+                "____________________________________________________________\n" +
+                        "Hello! I'm Duke\n" +
+                        "What can I do for you?\n" +
+                        "____________________________________________________________\n\n");
     }
+
+    static void printReply(String input) {
+        System.out.print(
+                "____________________________________________________________\n" +
+                input +
+                "\n____________________________________________________________\n");
+    }
+
+    static void printGoodbye() {
+        System.out.print(
+                "____________________________________________________________\n" +
+                        "Bye. Hope to see you again soon!" +
+                        "\n____________________________________________________________\n");
+    }
+    public static void main(String[] args) {
+        printIntro();
+        String input = sc.nextLine();
+        while (!input.toLowerCase().equals("bye")){
+            printReply(input);
+            input = sc.nextLine();
+        }
+        printGoodbye();
+    }
+
+
 }
