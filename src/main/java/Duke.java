a58780d7e225520fbbae8a9cb8a9d39b8ac0237a
BenFheng <benfheng@gmail.com>
2020-01-23 00:50:37 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,21 +1,33 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
-        String welcome = "OwO Hello! Watashi Duke desu! \n" + "What can I do for you senpai? *sweats profusely*";
+        Scanner input = new Scanner(System.in);
+        String welcome = "OwO Hello! I am your neckbeard chatbot! \n" + "What can I do for you senpai? *sweats profusely*";
         System.out.println(welcome);
 
-        Scanner input = new Scanner(System.in);
-        while(input.hasNextLine()) {
-            String echoInput = input.next();
+        ArrayList<String> storeList = new ArrayList<>();
 
-            if(echoInput.equals("bye")) {
+        while(input.hasNextLine()) {
+            String addition = input.nextLine();
+
+            if (addition.equals("bye")) {
 
                 System.out.println("UwU gone so fast? You're a fat bitch anyway.");
                 break;
+
+            } else if (addition.equals("list")) {
+
+                for (int i = 0; i < storeList.size(); i++) {
+                    int indexNumber = i + 1;
+                    System.out.println(indexNumber + ". " + storeList.get(i));
+                }
+
             } else {
 
-                System.out.println(echoInput);
+                storeList.add(addition);
+                System.out.println("added: " + addition);
 
             }
         }
