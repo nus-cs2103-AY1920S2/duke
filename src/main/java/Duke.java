c5e29bbba545154b4f7f57133343d408b2edab03
Jade Tay <jadet.hx@gmail.com>
2020-01-22 21:45:34 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,29 +2,31 @@ import java.util.*;
 
 public class Duke {
     public static final String line = "    ____________________________________________________________";
-    static ArrayList<String> newList = new ArrayList<>();
+    static ArrayList<Task> newList = new ArrayList<>();
 
     public static String hello() {
-        String s1 = "    hello! i'm dUKE!";
-        String s2 = "    what do you want from me (・∀・)";
+        String s1 = "     hello! i'm dUKE!";
+        String s2 = "     what do you want from me (・∀・)";
         return line + "\n" + s1 + "\n" + s2 + "\n" + line + "\n";
     }
 
     public static String list() {
         String listContent = line + "\n";
         for (int i = 1; i <= newList.size(); i++) {
-            listContent += "     " + i + ". " + newList.get(i-1) + "\n";
+            Task curTask = newList.get(i-1);
+            listContent += "     " + i + ".[" + curTask.getStatusIcon()
+                                + "] " + newList.get(i-1) + "\n";
         }
         listContent += line + "\n";
         return listContent;
     }
 
     public static String blah() {
-        return line + "\n" + "    blah blah" + "\n" + line + "\n";
+        return line + "\n" + "     blah blah" + "\n" + line + "\n";
     }
 
     public static String bye() {
-        return line + "\n" + "    bye see you again（ｉДｉ）" + "\n" + line + "\n";
+        return line + "\n" + "     bye see you again（ｉДｉ）" + "\n" + line + "\n";
     }
 
 
@@ -47,12 +49,20 @@ public class Duke {
                 System.out.println(list());
             } else if (input.equals("blah")) {
                 System.out.println(blah());
-            } else if (input.equals("bye")){
+            } else if (input.equals("bye")) {
                 System.out.println(bye());
                 break;
+            } else if (input.length() > 5) {
+                int index = Integer.parseInt(input.substring(5)) - 1;
+                Task comTask = newList.get(index);
+                comTask.markAsDone();
+                System.out.println(line + "\n" + "     wow you finally completed something: " + "\n"
+                            + "      [" + comTask.getStatusIcon() + "] " + comTask.toString()
+                            + "\n" + line + "\n");
             } else {
-                System.out.println(line + "\n" + "    adding into list: " + input+ "\n" + line + "\n");
-                newList.add(input);
+                System.out.println(line + "\n" + "     adding into list: " + input+ "\n" + line + "\n");
+                Task t = new Task(input);
+                newList.add(t);
             }
         }
 
