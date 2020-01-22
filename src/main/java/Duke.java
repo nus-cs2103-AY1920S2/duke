8a5d3875e65dbead85f5d145f36924d09631a664
souwmyaa <souwmyaa9@gmail.com>
2020-01-22 18:12:08 +0800

    Week 2 Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
+        /*String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from\n" + logo);*/
+        System.out.println("____________________________________________________________\n Hello! I'm Duke\n What can I do for you?\n____________________________________________________________");
+        Scanner sc = new Scanner(System.in);
+        boolean flag = true;
+
+        while (flag) {
+            String sentence = sc.nextLine();
+            if (sentence.equalsIgnoreCase("bye")) {
+                System.out.println("____________________________________________________________\n Bye. Hope to see you again soon!\n____________________________________________________________");
+                break;
+            } else {
+                System.out.println("____________________________________________________________\n" + sentence + "\n____________________________________________________________");
+            }
+        }
     }
 }
