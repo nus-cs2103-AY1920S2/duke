d9fbb918ff2f2ba2a6f504d1587b8c6d9f37db36
fatin99 <fatinnbs@gmail.com>
2020-01-22 17:00:27 +0800

    Level-3: Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,16 +12,29 @@ public class Duke {
         System.out.printf("Hello! I'm Duke\nWhat can I do for you?\n");
 
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> newList = new ArrayList<String>();
+        ArrayList<Task> newList = new ArrayList<Task>();
         String command = sc.nextLine();
 
         while (!command.equals("bye")) {
             if (command.equals("list")) {
-                for (int i = 0; i < newList.size(); i += 1) {
-                    System.out.printf("%d. %s\n", i+1, newList.get(i));
+                if (newList.size() == 0) {
+                    System.out.printf("You have no tasks in your list\n");
+                } else {
+                    System.out.printf("Here are the tasks in your list:\n");
+                    for (int i = 0; i < newList.size(); i += 1) {
+                        System.out.printf("%d. %s\n", i + 1, newList.get(i).toString());
+                    }
                 }
+            } else if (command.startsWith("done ")) {
+                String[] arr = command.split(" ");
+                int i = Integer.valueOf(arr[1]) - 1;
+                Task task = newList.get(i);
+                newList.get(i).markAsDone();
+                System.out.printf("Nice! I've marked this task as done: \n%s\n",
+                        newList.get(i).toString());
             } else {
-                newList.add(command);
+                Task newTask = new Task(command);
+                newList.add(newTask);
                 System.out.printf("added: %s\n", command);
             }
             command = sc.nextLine();
