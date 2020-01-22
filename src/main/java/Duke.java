011cabde696af544daa9506badb5bd09d51880ab
Zhang Yuanyu <e0322803@u.nus.edu>
2020-01-22 16:38:35 +0800

    Level 5 Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,80 +14,96 @@ public class Duke {
         String fiveSpaces = "      ";
         System.out.println(line + fiveSpaces + "Hello! I'm Duke\n" + fiveSpaces + "Whatcha wanna do?\n" + line);
         ArrayList<Task> tasks = new ArrayList<>();
-        while(scanner.hasNextLine()){
-            Scanner commandScanner = new Scanner(scanner.nextLine());
-            String word = commandScanner.next();
-            if(word.equals("bye")){
-                System.out.println(line + fiveSpaces + "See ya later alligator!\n"+ line);
-                break;
-            }
-            else {
-                String taskName = "";
-                switch (word) {
-                    case "list":
-                        System.out.print(line);
-                        System.out.println("     Here are the tasks in your list:");
-                        for (Task task : tasks) {
-                            System.out.println("      " + Integer.toString(task.thisTaskNum) + "." + task);
-                        }
-                        System.out.println(line);
+        boolean continueDoLoop = true;
+        do {
+            try {
+                while (scanner.hasNextLine()) {
+                    Scanner commandScanner = new Scanner(scanner.nextLine());
+                    String word = commandScanner.next();
+                    if (word.equals("bye")) {
+                        System.out.println(line + fiveSpaces + "See ya later alligator!\n" + line);
+                        continueDoLoop = false;
                         break;
+                    } else {
+                        String taskName = "";
+                        switch (word) {
+                            case "list":
+                                System.out.print(line);
+                                System.out.println("     Here are the tasks in your list:");
+                                for (Task task : tasks) {
+                                    System.out.println("      " + Integer.toString(task.thisTaskNum) + "." + task);
+                                }
+                                System.out.println(line);
+                                break;
 
-                    case "done":
-                        int taskNum = Integer.parseInt(commandScanner.next()) - 1;
-                        tasks.get(taskNum).isDone = true;
-                        System.out.print(line);
-                        System.out.println("     Nice! I've marked this task as done:");
-                        System.out.println("       " + tasks.get(taskNum).toString());
-                        System.out.println(line);
-                        break;
+                            case "done":
+                                int taskNum = Integer.parseInt(commandScanner.next()) - 1;
+                                tasks.get(taskNum).isDone = true;
+                                System.out.print(line);
+                                System.out.println("     Nice! I've marked this task as done:");
+                                System.out.println("       " + tasks.get(taskNum).toString());
+                                System.out.println(line);
+                                break;
 
-                    case "todo":
-                        while(commandScanner.hasNext()){
-                            taskName += commandScanner.next() + " ";
-                        }
-                        taskName = taskName.substring(0, taskName.length() - 1);
-                        Task todoTask = new Todo(taskName);
-                        tasks.add(todoTask);
-                        System.out.print(line);
-                        System.out.println("     Got it. I've added this task:");
-                        System.out.println("       " + todoTask.toString());
-                        System.out.println("     Now you have " + Integer.toString(tasks.size()) + " tasks in the list.");
-                        System.out.println(line);
-                        break;
+                            case "todo":
+                                if (!commandScanner.hasNext()) {
+                                    throw new DukeException(word);
+                                } else {
+                                    while (commandScanner.hasNext()) {
+                                        taskName += commandScanner.next() + " ";
+                                    }
+                                    taskName = taskName.substring(0, taskName.length() - 1);
+                                    Task todoTask = new Todo(taskName);
+                                    tasks.add(todoTask);
+                                    System.out.print(line);
+                                    System.out.println("     Got it. I've added this task:");
+                                    System.out.println("       " + todoTask.toString());
+                                    System.out.println("     Now you have " + Integer.toString(tasks.size()) + " tasks in the list.");
+                                    System.out.println(line);
+                                    break;
+                                }
 
-                    case "deadline":
-                        while(commandScanner.hasNext()){
-                            taskName += commandScanner.next() + " ";
-                        }
-                        taskName = taskName.substring(0, taskName.length() - 1);
-                        String deadlineTask[] = taskName.split("/by ");
-                        Task newDeadLineTask = new Deadline(deadlineTask[0], deadlineTask[1]);
-                        tasks.add(newDeadLineTask);
-                        System.out.print(line);
-                        System.out.println("     Got it. I've added this task:");
-                        System.out.println("       " + newDeadLineTask.toString());
-                        System.out.println("     Now you have " + Integer.toString(tasks.size()) + " tasks in the list.");
-                        System.out.println(line);
-                        break;
+                            case "deadline":
+                                while (commandScanner.hasNext()) {
+                                    taskName += commandScanner.next() + " ";
+                                }
+                                taskName = taskName.substring(0, taskName.length() - 1);
+                                String deadlineTask[] = taskName.split("/by ");
+                                Task newDeadLineTask = new Deadline(deadlineTask[0], deadlineTask[1]);
+                                tasks.add(newDeadLineTask);
+                                System.out.print(line);
+                                System.out.println("     Got it. I've added this task:");
+                                System.out.println("       " + newDeadLineTask.toString());
+                                System.out.println("     Now you have " + Integer.toString(tasks.size()) + " tasks in the list.");
+                                System.out.println(line);
+                                break;
 
-                    case "event":
-                        while(commandScanner.hasNext()){
-                            taskName += commandScanner.next() + " ";
+                            case "event":
+                                while (commandScanner.hasNext()) {
+                                    taskName += commandScanner.next() + " ";
+                                }
+                                taskName = taskName.substring(0, taskName.length() - 1);
+                                String eventTask[] = taskName.split("/at ");
+                                Task newEventTask = new Event(eventTask[0], eventTask[1]);
+                                tasks.add(newEventTask);
+                                System.out.print(line);
+                                System.out.println("     Got it. I've added this task:");
+                                System.out.println("       " + newEventTask.toString());
+                                System.out.println("     Now you have " + Integer.toString(tasks.size()) + " tasks in the list.");
+                                System.out.println(line);
+                                break;
+
+                            default:
+                                throw new DukeException("");
                         }
-                        taskName = taskName.substring(0, taskName.length() - 1);
-                        String eventTask[] = taskName.split("/at ");
-                        Task newEventTask = new Event(eventTask[0], eventTask[1]);
-                        tasks.add(newEventTask);
-                        System.out.print(line);
-                        System.out.println("     Got it. I've added this task:");
-                        System.out.println("       " + newEventTask.toString());
-                        System.out.println("     Now you have " + Integer.toString(tasks.size()) + " tasks in the list.");
-                        System.out.println(line);
-                        break;
+                    }
                 }
+            } catch (DukeException ex) {
+                System.err.print(line);
+                System.err.println(ex);
+                System.err.println(line);
             }
-        }
+        } while(continueDoLoop);
     }
 }
 
---
+++ src/main/java/DukeException.java
@@ -0,0 +1,16 @@
+public class DukeException extends Exception {
+    String exception;
+    public DukeException(String exception){
+        this.exception = exception;
+    }
+
+    @Override
+    public String toString(){
+        if (exception.equals("todo")){
+            return "      OOPS!!! The description of a todo cannot be empty.";
+        }
+        else{
+            return "      OOPS!!! I'm sorry, but I don't know what that means :-(";
+        }
+    }
+}
