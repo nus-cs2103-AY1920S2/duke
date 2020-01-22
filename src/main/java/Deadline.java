2289162540df4b3200704e593ee09ce1b1c99454
Catherine <catherineloy99@gmail.com>
2020-01-22 19:33:12 +0800

    Level-8

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,46 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task{
     protected String by;
+    protected LocalDate date;
+    protected LocalTime time;
 
     public Deadline(String description, String by) {
         super(description);
         this.by = by;
     }
 
+    public void setDateTime() {
+        String[] b = this.by.split(" ");
+        if (b.length > 1) {
+            this.date = LocalDate.parse(b[0]);
+            this.time = LocalTime.parse(b[1]);
+        } else {
+            this.date = LocalDate.parse(this.by);
+            this.time = null;
+        }
+    }
+
+    public String dateToString() {
+        setDateTime();
+        return this.date.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+    }
+
+    public String timeToString() {
+        setDateTime();
+        return this.time.format(DateTimeFormatter.ofPattern("h:mm a"));
+    }
+
     @Override
     public String toString() {
-        return "[D]" + super.toString() + "(by: " + by + ")";
+        //return "[D]" + super.toString() + "(by: " + by + ")";
+        String[] b = this.by.split(" ");
+        if (b.length > 1) {
+            return "[D]" + super.toString() + "(by: " + dateToString() + " " + timeToString()+ ")";
+        } else {
+            return "[D]" + super.toString() + "(by: " + dateToString() + ")";
+        }
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,13 +1,46 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
     protected String at;
+    protected LocalDate date;
+    protected LocalTime time;
 
     public Event(String description, String at) {
         super(description);
         this.at = at;
     }
 
+    public void setDateTime() {
+        String[] b = this.at.split(" ");
+        if (b.length > 1) {
+            this.date = LocalDate.parse(b[0]);
+            this.time = LocalTime.parse(b[1]);
+        } else {
+            this.date = LocalDate.parse(this.at);
+            this.time = null;
+        }
+    }
+
+    public String dateToString() {
+        setDateTime();
+        return this.date.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+    }
+
+    public String timeToString() {
+        setDateTime();
+        return this.time.format(DateTimeFormatter.ofPattern("h:mm a"));
+    }
+
     @Override
     public String toString() {
-        return "[E]" + super.toString() + "(at: " + at + ")";
+        //return "[E]" + super.toString() + "(at: " + at + ")";
+        String[] b = this.at.split(" ");
+        if (b.length > 1) {
+            return "[E]" + super.toString() + "(by: " + dateToString() + " " + timeToString()+ ")";
+        } else {
+            return "[E]" + super.toString() + "(by: " + dateToString() + ")";
+        }
     }
 }
