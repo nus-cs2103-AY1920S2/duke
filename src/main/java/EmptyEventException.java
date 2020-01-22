c5b16c6a90796fa0e9d53a8fb19c0ee713e55795
gohkh <gohkh2011@gmail.com>
2020-01-22 23:34:42 +0800

    Fix typo
    Edit class name.

--- src/main/java/EmptyEventException.java
+++ src/main/java/EmptyEventException.java
@@ -1,5 +1,5 @@
-class EmptyTodoException extends EmptyTaskException {
-    EmptyTodoException() {
+class EmptyEventException extends EmptyTaskException {
+    EmptyEventException() {
         super("Oops! The description of an event cannot be empty.");
     }
 }
