e683610146acbad6c104bb04751673e5eeec0636
tingalinga <itshuitingg@gmail.com>
2020-01-23 01:45:04 +0800

    A-TextUiTesting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,8 +9,8 @@ public class Duke {
     }
 
     public static void main(String[] args) {
-        String logo = "      ____        _        \n"
-                + "     |  _ \\ _   _| | _____ \n"
+        String logo = "      ____        _\n"
+                + "     |  _ \\ _   _| | _____\n"
                 + "     | | | | | | | |/ / _ \\\n"
                 + "     | |_| | |_| |   <  __/\n"
                 + "     |____/ \\__,_|_|\\_\\___|\n";
@@ -43,7 +43,7 @@ public class Duke {
                 int num = Integer.parseInt(split[1]) - 1;
                 list.get(num).markDone();
                 printLine();
-                System.out.println("     Nice! I've marked this task as done: ");
+                System.out.println("     Nice! I've marked this task as done:");
                 System.out.println("       " + list.get(num));
                 printLine();
             } else {
@@ -60,7 +60,7 @@ public class Duke {
                     list.add(new Event(split[1]));
                 }
                 printLine();
-                System.out.println("     Got it. I've added this task: ");
+                System.out.println("     Got it. I've added this task:");
                 System.out.println("       " + task);
                 System.out.println("     Now you have " + list.size() + " tasks in the list.");
                 printLine();
@@ -69,3 +69,77 @@ public class Duke {
     }
 }
 
+class Task {
+    String description;
+    boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markDone() {
+        this.isDone = true;
+    }
+
+    public String toString() {
+        if (this.isDone) {
+            return "[✓] " + this.description;
+        } else {
+            return "[✗] " + this.description;
+        }
+    }
+}
+
+class Todo extends Task {
+
+    public Todo(String description) {
+        super(description);
+    }
+
+    public String toString() {
+        if (this.isDone) {
+            return "[T][✓] " + this.description;
+        } else {
+            return "[T][✗] " + this.description;
+        }
+    }
+}
+
+class Deadline extends Task {
+    public String date;
+
+    public Deadline(String description) {
+        super(description);
+        String[] arr = description.split(" /by ");
+        this.description = arr[0];
+        this.date = arr[1];
+    }
+
+    public String toString() {
+        if (this.isDone) {
+            return "[D][✓] " + this.description + " (by: " + this.date + ")";
+        } else {
+            return "[D][✗] " + this.description + " (by: " + this.date + ")";
+        }
+    }
+}
+
+class Event extends Task {
+    public String date;
+
+    public Event(String description) {
+        super(description);
+        String[] arr = description.split(" /at ");
+        this.description = arr[0];
+        this.date = arr[1];
+    }
+
+    public String toString() {
+        if (this.isDone) {
+            return "[E][✓] " + this.description + " (at: " + this.date + ")";
+        } else {
+            return "[E][✗] " + this.description + " (at: " + this.date + ")";
+        }
+    }
+}
