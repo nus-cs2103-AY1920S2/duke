2f466b78fc449b4b7d86f5b2a961087846ccb4d7
TheFirstCouchPotato97 <39809238+TheFirstCouchPotato97@users.noreply.github.com>
2020-01-23 01:28:24 +0800

    Add basic commands and fix formatting
    New commands:
    greet - welcomes the user
    echo - repeats the user's command
    exit - closes the program

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
+
 public class Duke {
 
     /** The main method is where the chat-bot is created and executed. */
     public static void main(String[] args) {
         displayLogo();
+        greet();
+
+        Scanner sc = new Scanner(System.in);
+        inputCommands(sc);
+
+        exit();
     }
 
     /** Outputs the program's logo in the chat-bot. */
@@ -15,9 +23,40 @@ public class Duke {
         System.out.println(indent("Hello from\n" + logo));
     }
 
+    /**
+     * The main loop for handling user commands. Stops looping when the given
+     * exit command is the next input.
+     *
+     * @param sc the scanner accepting user input
+     */
+    public static void inputCommands(Scanner sc) {
+        // Terminates the chat-bot if true
+        boolean exit = false;
+
+        while (!exit && sc.hasNext()) {
+            String command = sc.next();
+            System.out.println();
+
+            // Handle different commands
+            switch (command) {
+            case "bye":
+                exit = true; // Terminate the program
+                break;
+            default:
+                echo(command); // Repeat the user's command
+                break;
+            }
+        }
+    }
+
     /** Adds a four-space indent to all lines of a given text. */
     private static String indent(String text) {
-        String indent = " ".repeat(4); // Change this number for different indent widths
+        return indent(text, 4);
+    }
+
+    /** Adds an indent to all lines of a given text. */
+    private static String indent(String text, int indentWidth) {
+        String indent = " ".repeat(indentWidth); // Change this number for different indent widths
 
         return text.replaceAll("(?m)^", indent);
     }
@@ -28,5 +67,35 @@ public class Duke {
         int lineWidth = 60;
 
         System.out.println(indent(lineSymbol.repeat(lineWidth)));
+    }
+
+    /**
+     * Outputs the given message into the chat-bot. The message will be
+     * sandwiched between two lines and indented.
+     *
+     * @param message the text to output into the chat-bot
+     */
+    public static void echo(String message) {
+        drawLine();
+
+        System.out.println(indent(message, 5));
+
+        drawLine();
+        System.out.println(); // New line below each chat-bot message
+    }
+
+    /** Welcomes the chat-bot user. */
+    public static void greet() {
+        String greeting = "Hey there! I'm Duke\n"
+                + "Your word is my command!";
+
+        echo(greeting);
+    }
+
+    /** Farewells the chat-bot user. */
+    public static void exit() {
+        String farewell = "Bye! Please give a review if you like this program!";
+
+        echo(farewell);
     }
 }
