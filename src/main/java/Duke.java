5df12a4d2d5c0b1b58fc38a8fd78407ea1f03bef
Dylan Lim <60003775+limdylan@users.noreply.github.com>
2020-01-23 02:27:28 +0800

    Add ability to store texts and display texts

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
 import java.util.Scanner;
 
 public class Duke {
@@ -8,14 +11,32 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         Scanner sc = new Scanner(System.in);
+        boolean isRunning = true;
+        List<String> texts = new ArrayList<>();
 
         System.out.println("Hello from\n" + logo);
-        while (sc.hasNext()) {
-            String input = sc.next();
-            if (input.equals("bye")) {
+        while (isRunning && sc.hasNextLine()) {
+            String input = sc.nextLine();
+            switch (input) {
+            case "list": {
+                StringBuilder output = new StringBuilder("List so far:\n");
+                ListIterator<String> iterator = texts.listIterator();
+                while (iterator.hasNext()) {
+                    int index = iterator.nextIndex() + 1;
+                    String text = iterator.next();
+                    output.append(index + ". " + text + "\n");
+                }
+                System.out.print(output);
                 break;
-            } else {
-                System.out.println(input);
+            }
+            case "bye": {
+                isRunning = false;
+                break;
+            }
+            default: {
+                texts.add(input);
+                System.out.println("Added: '" + input + "'");
+            }
             }
         }
         System.out.println("Goodbye!");
