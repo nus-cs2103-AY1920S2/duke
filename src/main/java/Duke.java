c043933d40b9bb05c153145b9eef640b2d610304
Jarrod-Bob <e0309003@u.nus.edu>
2020-01-22 15:38:05 +0800

    Done Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,14 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("My name is Jarvis!\nHow may I provide my services on this fine day?");
+
+        Scanner sc = new Scanner(System.in);
+        String nextLine = sc.nextLine();
+        while (!nextLine.equals("bye")) {
+            System.out.println(nextLine);
+            nextLine = sc.nextLine();
+        }
+        System.out.println("Hope my service has been of great help! See you again!");
     }
 }
