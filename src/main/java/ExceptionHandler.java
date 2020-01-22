d94837e609d2bd5df9684a55cd390174289f8bf5
J-Dan23 <janiceongyw@gmail.com>
2020-01-23 00:38:57 +0800

    A-Exceptions

--- src/main/java/ExceptionHandler.java
+++ src/main/java/ExceptionHandler.java
@@ -3,13 +3,11 @@ import java.util.Arrays;
 public class ExceptionHandler {
     public void checkEmptyField(String[] taskStringArr, String type) throws EmptyFieldException {
         if (type.equals("todo") || type.equals("deadline") || type.equals("event")) {
-            System.out.println(Arrays.toString(taskStringArr));
             if (taskStringArr.length != 2) {
                 throw new EmptyFieldException(
                         "You're usually not that taciturn, Red. Explain things to me.");
             } else if (!type.equals("todo")) {
                 String[] time = taskStringArr[1].split("/", 2);
-                System.out.println(Arrays.toString(time));
                 if (time.length != 2) {
                     throw new EmptyFieldException(
                             "We're running short of time, so make sure you note it down.");
@@ -18,6 +16,18 @@ public class ExceptionHandler {
                             "You're usually not that taciturn, Red. Explain things to me.");
                 }
             }
+        }
+    }
+
+    public void checkWrongCommand(String type) throws WrongCommandException{
+        if (!(type.equals("todo")
+                || type.equals("deadline")
+                || type.equals("event")
+                || type.equals("list")
+                || type.equals("done")
+                || type.equals("bye"))) {
+            throw new WrongCommandException(
+                    "Uhh... You're gonna have to say that again, Red.");
         }
     }
 }
