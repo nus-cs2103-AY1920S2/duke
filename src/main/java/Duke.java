9d6abc635a70bf59bd5d438770d18f51eddb34ce
Arthur Lee <ykarthurlee@gmail.com>
2020-01-22 13:52:30 +0800

    Implemented level 2 functions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+
 
 public class Duke {
+    static ArrayList<String> arr;
+
     public static void main(String[] args) {
         String line = "-----------------------------------";
         String logo = " ____        _        \n"
@@ -12,17 +16,42 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("What can I do for you?");
         System.out.println(line);
+        arr = new ArrayList<>();
 
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         while (!input.equals("bye")) {
-            System.out.println(line);
-            System.out.println(input);
-            System.out.println(line);
+            checkCommand(input);
             input = scanner.nextLine();
         }
+        printWithFormat("Bye. Hope to see you again soon!");
+    }
+
+    public static void checkCommand(String input) {
+        String line = "-----------------------------------";
+        switch (input) {
+            case "bye":
+                printWithFormat(("Bye. Hope to see you again soon!"));
+                break;
+            case "list":
+                System.out.println(line);
+                for (int i = 1; i <= arr.size(); i++) {
+                    String item = arr.get(i-1);
+                    System.out.println(i + ". " + input );
+                }
+                System.out.println(line);
+                break;
+            default:
+                arr.add(input);
+                printWithFormat("added: " + input);
+                break;
+        }
+    }
+
+    public static void printWithFormat(String message) {
+        String line = "-----------------------------------";
         System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(message);
         System.out.println(line);
     }
 }
