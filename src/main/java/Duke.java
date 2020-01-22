ee15127423e2e1b186f309d98cb3fed1c5696d17
seanlowcy77 <seanlowcy77@Seans-MacBook-Pro-4.local>
2020-01-23 00:53:02 +0800

    Add Greet, Echo and Exit commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+
+        System.out.println("Hello ! I'm Ashley\nOi What u want");
+
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()) {
+            String str = sc.nextLine();
+
+            if (str.equals("bye")) {
+                System.out.println("Bye! Hope to see you again soon!");
+                return;
+
+            } else {
+                System.out.println(str + "\n");
+            }
+        }
+
     }
 }
