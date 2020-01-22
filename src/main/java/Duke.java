53685701588cec739951ab7b2a6a25a2cdc46d45
amrl <amirul.ardy@gmail.com>
2020-01-23 00:04:21 +0800

    Add echo and exit functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        printGreeting();
+        runDuke();
     }
 
     public static void printGreeting() {
@@ -13,5 +15,25 @@ public class Duke {
         System.out.println(logo);
         System.out.println("Hi, I'm Duke!");
         System.out.println("What can I do for you?\n");
+    }
+
+    public static String getUserInput() {
+        System.out.print("> ");
+
+        Scanner sc = new Scanner(System.in);
+
+        return sc.nextLine();
+    }
+
+    public static void runDuke() {
+        printGreeting();
+
+        String input = getUserInput();
+        while (!input.equals("bye")) {
+            System.out.println(input + "\n");
+            input = getUserInput();
+        }
+
+        System.out.println("Have a nice day!");
     }
 }
