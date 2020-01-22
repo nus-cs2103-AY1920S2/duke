085582685b4e939a99689b2a3eca9f331d8a42c6
BenFheng <benfheng@gmail.com>
2020-01-23 00:23:06 +0800

    Finished Level 1, committing for reference

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String welcome = "OwO Hello! Watashi Duke desu! \n" + "What can I do for you senpai? *sweats profusely*";
+        System.out.println(welcome);
+
+        Scanner input = new Scanner(System.in);
+        while(input.hasNextLine()) {
+            String echoInput = input.next();
+
+            if(echoInput.equals("bye")) {
+
+                System.out.println("UwU gone so fast? You're a fat bitch anyway.");
+                break;
+            } else {
+
+                System.out.println(echoInput);
+
+            }
+        }
+
+
     }
 }
