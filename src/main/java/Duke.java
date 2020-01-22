439bf35e89f69824c3f5b486387e0a42c35a9e63
Willis Tay <27534078+tzihiang@users.noreply.github.com>
2020-01-22 20:14:49 +0800

    Finished level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,35 @@
 import java.util.*;
 
+class Task {
+
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718");
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+
+    public String printTask(){
+        return "[" + getStatusIcon() + "] " + getDescription();
+    }
+
+    public void markAsDone(){
+        this.isDone = true;
+    }
+}
+
 public class Duke {
 
-    static ArrayList<String> tasks = new ArrayList<String>();
+    static ArrayList<Task> tasks = new ArrayList<Task>();
     static Scanner sc = new Scanner(System.in);
 
     static void printIntro() {
@@ -27,22 +54,36 @@ public class Duke {
                         "\n____________________________________________________________\n");
     }
 
-    static void printList(ArrayList<String> tasks){
-        System.out.print("____________________________________________________________\n");
+    static void printList(ArrayList<Task> tasks){
+        System.out.print("____________________________________________________________\n" +
+                "Here are the tasks in your list:\n");
         for (int i =  0; i < tasks.size(); i++){
-            System.out.println(Integer.toString(i + 1) + ": " + tasks.get(i));
+            System.out.println(Integer.toString(i + 1) + ". " + tasks.get(i).printTask());
         }
         System.out.print("____________________________________________________________\n");
+    }
+
+    static void markTaskDone(Task task){
+        task.markAsDone();
+        System.out.println("____________________________________________________________\n"
+            + "Nice! I've marked this task as done:\n" + task.printTask()
+                + "\n____________________________________________________________\n");
+
     }
 
     public static void main(String[] args) {
         printIntro();
         String input = sc.nextLine();
-        while (!input.toLowerCase().equals("bye")){
-            if (input.toLowerCase().equals("list")){
+        while (!input.toLowerCase().equals("bye")) {
+            if (input.toLowerCase().equals("list")) {
                 printList(tasks);
-            } else {
-                tasks.add(input);
+            } else if (input.split(" ")[0].equals("done")) {
+                int taskNumber = Integer.parseInt(input.split(" ")[1]) - 1;
+                markTaskDone(tasks.get(taskNumber));
+            }
+            else {
+                Task task = new Task(input);
+                tasks.add(task);
                 printReply(input);
             }
             input = sc.nextLine();
