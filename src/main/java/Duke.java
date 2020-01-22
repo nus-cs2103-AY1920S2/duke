b3fa3f8857740c8bc259ee4f9c37320a25e51f71
FyonnOh <>
2020-01-22 16:42:57 +0800

    Level 2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -10,13 +11,20 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("Konnichiwa! Watashi no namaeha Duke desu! What can I do for you? (^.___.^)");
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> strArr = new ArrayList<>();
         while(sc.hasNext()) {
-            String input = sc.next();
-            if (!input.equals("bye")) {
-                System.out.println(input + "(=´∇｀=)");
-            } else {
+            String input = sc.nextLine();
+            if (input.equals("bye")){
                 System.out.println("Sayonara~ （＾・ω・＾✿）");
                 break;
+            } else if (input.equals("list")) {
+                for(int i=0; i< strArr.size(); i++) {
+                    String text = strArr.get(i);
+                    System.out.println(i+1 +". "+text);
+                }
+            } else {
+               strArr.add(input);
+               System.out.println("(=´∇｀=) added:" + input +"!");
             }
         }
     }
