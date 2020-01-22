44a1fd35e731a922911474cbe3d888100ea10d60
tingalinga <itshuitingg@gmail.com>
2020-01-23 02:40:10 +0800

    Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,7 +8,20 @@ public class Duke {
         System.out.println("    ____________________________________________________________");
     }
 
-    public static void main(String[] args) {
+    public static String nonEmpty(String tag) {
+        return "    ____________________________________________________________\n"
+                + "     ☹ OOPS!!! " + tag + " cannot be empty.\n"
+                + "    ____________________________________________________________";
+    }
+
+    public static String dontKnow() {
+        return "    ____________________________________________________________\n"
+                + "     ☹ OOPS!!! I'm sorry, but I don't know what that means :-(\n"
+                + "    ____________________________________________________________";
+    }
+
+    public static void main(String[] args) throws DukeException {
+        // greeting
         String logo = "      ____        _\n"
                 + "     |  _ \\ _   _| | _____\n"
                 + "     | | | | | | | |/ / _ \\\n"
@@ -23,48 +36,72 @@ public class Duke {
 
         Scanner scanner = new Scanner(System.in);
         List<Task> list = new ArrayList<>();
-        while (true) {
-            String token = scanner.nextLine();
-            // String[] words = token.split("/");
-            String[] split = token.split(" ", 2);
-            if (token.equals("bye")) {
-                printLine();
-                System.out.println("    Bye. Hope to see you again soon!");
-                printLine();
-                break;
-            } else if (token.equals("list")) {
-                printLine();
-                System.out.println("     Here are the tasks in your list:");
-                for (int i = 0; i < list.size(); i++) {
-                    System.out.println("     " + (i + 1) + ". " + list.get(i));
+        try {
+            while (true) {
+                String token = scanner.nextLine();
+                // split[0] => first word of the line
+                String[] split = token.split(" ", 2);
+                if (token.equals("bye")) { // greet goodbye
+                    printLine();
+                    System.out.println("    Bye. Hope to see you again soon!");
+                    printLine();
+                    break;
+                } else if (token.equals("list")) { // print all tasks in list
+                    printLine();
+                    System.out.println("     Here are the tasks in your list:");
+                    for (int i = 0; i < list.size(); i++) {
+                        System.out.println("     " + (i + 1) + ". " + list.get(i));
+                    }
+                    printLine();
+                } else if (split[0].equals("done")) { // mark task as complete
+                    int num = Integer.parseInt(split[1]) - 1;
+                    list.get(num).markDone();
+                    printLine();
+                    System.out.println("     Nice! I've marked this task as done:");
+                    System.out.println("       " + list.get(num));
+                    printLine();
+                } else { // creating new task
+                    // error: invalid task
+                    if (!split[0].equals("todo") && !split[0].equals("deadline") && !split[0].equals("event")) {
+                        throw new DukeException(dontKnow());
+                    }
+                    // error: missing description
+                    if (split.length <= 1) {
+                        throw new DukeException(nonEmpty("The description of a " + split[0]));
+                    }
+                    char tag = split[0].toUpperCase().charAt(0);
+                    Task task = new Task(token); // placeholder
+                    // create task according to type of task
+                    if (tag == 'T') {
+                        task = new Todo(split[1]);
+                        list.add(new Todo(split[1]));
+                    } else if (tag == 'D') {
+                        String[] arr = split[1].split(" /by ");
+                        // error: task is missing deadline
+                        if (arr.length <= 1) {
+                            throw new DukeException(nonEmpty("Deadline of a task"));
+                        }
+                        task = new Deadline(split[1]);
+                        list.add(new Deadline(split[1]));
+                    } else if (tag == 'E') {
+                        String[] arr = split[1].split(" /at ");
+                        // error: event is missing time
+                        if (arr.length <= 1) {
+                            throw new DukeException(nonEmpty("Time of an event"));
+                        }
+                        task = new Event(split[1]);
+                        list.add(new Event(split[1]));
+                    }
+                    printLine();
+                    System.out.println("     Got it. I've added this task:");
+                    System.out.println("       " + task);
+                    System.out.println("     Now you have " + list.size() + " tasks in the list.");
+                    printLine();
                 }
-                printLine();
-            } else if (split[0].equals("done")) {
-                int num = Integer.parseInt(split[1]) - 1;
-                list.get(num).markDone();
-                printLine();
-                System.out.println("     Nice! I've marked this task as done:");
-                System.out.println("       " + list.get(num));
-                printLine();
-            } else {
-                char tag = split[0].toUpperCase().charAt(0);
-                Task task = new Task(token);
-                if (tag == 'T') {
-                    task = new Todo(split[1]);
-                    list.add(new Todo(split[1]));
-                } else if (tag == 'D') {
-                    task = new Deadline(split[1]);
-                    list.add(new Deadline(split[1]));
-                } else if (tag == 'E') {
-                    task = new Event(split[1]);
-                    list.add(new Event(split[1]));
-                }
-                printLine();
-                System.out.println("     Got it. I've added this task:");
-                System.out.println("       " + task);
-                System.out.println("     Now you have " + list.size() + " tasks in the list.");
-                printLine();
             }
+        }
+        catch (DukeException ex) {
+            System.out.println(ex.getMessage());
         }
     }
 }
@@ -141,5 +178,11 @@ class Event extends Task {
         } else {
             return "[E][✗] " + this.description + " (at: " + this.date + ")";
         }
+    }
+}
+
+class DukeException extends Exception {
+    public DukeException(String message) {
+        super(message);
     }
 }
