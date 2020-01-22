93685cbd82e67f769a15495f9739bad9b055f874
wongchishan <wongchishan99@gmail.com>
2020-01-22 17:55:56 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,24 +1,46 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
         System.out.println("    ____________________________________________________________\n" +
-                "     Hello! I'm Duke\n" +
-                "     What can I do for you?\n" +
+                "     Hello! :) I'm Duke\n" +
+                "     How can I help you today?\n" +
                 "    ____________________________________________________________");
 
         Scanner sc = new Scanner(System.in);
+        ArrayList<Task> lst = new ArrayList<>();
+
         while (sc.hasNextLine()) {
             String input = sc.nextLine();
             if (input.compareTo("bye") != 0) {
-                System.out.println("    ____________________________________________________________\n"
-                        + "     " + input + "\n    ____________________________________________________________");
+                if (input.compareTo("list") == 0) {
+                    printList(lst);
+                } else {
+                    Task task = new Task(input);
+                    lst.add(task);
+                    System.out.println("    ____________________________________________________________");
+                    System.out.println("     added: " + task.getContent());
+                    System.out.println("    ____________________________________________________________");
+                }
             } else {
                 System.out.println("    ____________________________________________________________\n"
-                        + "     Bye. Hope to see you again soon!\n"
+                        + "     Goodbye. See you again soon!\n"
                         + "    ____________________________________________________________");
                 break;
             }
         }
+    }
+
+    public static void printList(ArrayList<Task> lst) {
+        System.out.println("    ____________________________________________________________");
+        if (lst.isEmpty()) {
+            System.out.println("     Sorry, your list is currently empty!");
+        }
+        for (int i = 0; i < lst.size(); i++) {
+            Task task = lst.get(i);
+            System.out.println("     " + (i+1) + ". " + task.getContent());
+        }
+        System.out.println("    ____________________________________________________________");
     }
 }
