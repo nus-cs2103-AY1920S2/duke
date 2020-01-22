dd35509b6d35a452be4184e3a71011c8028878b2
harrychengly <harrychengly@gmail.com>
2020-01-22 13:49:55 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,7 @@
+import java.lang.reflect.Array;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Duke {
     public static void main(String[] args) {
@@ -12,15 +15,24 @@ public class Duke {
 
 
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> arr = new ArrayList<>();
 
-        while (sc.hasNext()){
+
+        while (sc.hasNext()) {
             String userInput = sc.nextLine();
-            if (userInput.equalsIgnoreCase("bye")){
-                System.out.println(botReplyLine +"\n Duke: I'll say goodnight now \n" +botReplyLine);
+            if (userInput.equalsIgnoreCase("bye")) {
+                System.out.println(botReplyLine + "\n Duke: I'll say goodnight now \n" + botReplyLine);
                 break;
-            }
-            else{
-                System.out.println(botReplyLine +"\n Duke: "+ userInput + "\n" + botReplyLine);
+            } else if (userInput.equalsIgnoreCase("list")) {
+                System.out.println(botReplyLine);
+                for (int i=0; i<arr.size(); i++) {
+                    System.out.println(i +". "+ arr.get(i));
+                }
+                System.out.println(botReplyLine);
+
+            } else {
+                arr.add(userInput);
+                System.out.println(botReplyLine + "\n Duke: added " + userInput + "\n" + botReplyLine);
             }
         }
 
