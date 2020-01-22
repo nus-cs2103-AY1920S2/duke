868c6cdc63a74e795b3ec6e9e67866bb9cc618bc
fatin99 <fatinnbs@gmail.com>
2020-01-22 16:19:35 +0800

    Level-1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,22 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.printf("Hello! I'm Duke\nWhat can I do for you?\n");
+
+        Scanner sc = new Scanner(System.in);
+        String command = sc.next();
+
+        while (!command.equals("bye")) {
+            if (command.equals("list")) {
+                System.out.printf("list\n");
+            } else if (command.equals("blah")) {
+                System.out.printf("blah\n");
+            }
+            command = sc.next();
+        }
+
+        System.out.printf("Bye. Hope to see you again soon!\n");
+        sc.close();
     }
 }
