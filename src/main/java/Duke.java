52f38b1771d13a75aaf9d54e56edb79d25b11505
IYVVVVV <1695275378@qq.com>
2020-01-23 00:16:22 +0800

    update Add and List function.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,14 +3,21 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        String[] events = new String[100];
+        int eventNumber = 0;
 
         printLogo();
         printGreet();
 
         String input = reader.nextLine();
 
-        while(!input.equals("bye")) {
-            echo(input);
+        while (!input.equals("bye")) {
+            if (input.equals("list")) {
+                list(events, eventNumber);
+            } else {
+                eventNumber++;
+                add(input, events, eventNumber);
+            }
             input = reader.nextLine();
         }
 
@@ -46,6 +53,21 @@ public class Duke {
     private static void exit() {
         printBreak();
         System.out.println("    Bye. Hope to see you again soon!");
+        printBreak();
+    }
+
+    private static void list(String[] events, int eventNumber) {
+        printBreak();
+        for (int i = 1; i <= eventNumber; i++) {
+            System.out.println("    " + i + ". " + events[eventNumber - 1]);
+        }
+        printBreak();
+    }
+
+    private static void add(String input, String[] events, int eventNumber) {
+        printBreak();
+        System.out.println("    added: " + input);
+        events[eventNumber - 1] = input;
         printBreak();
     }
 }
