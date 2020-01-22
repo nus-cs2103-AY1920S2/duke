559566f7e5680ceeba274e0c89f0c24ba625bcb8
KwanXinJie <xjkwan1998@gmail.com>
2020-01-23 00:39:40 +0800

    Revert "Lvl5-Test"
    This reverts commit 76ab8302a6c0eb172a64a6d4a9a0c9b86064fac7.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,80 +10,33 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("What can I do for you?");
-
         input();
         System.out.println("Bye. Hope to see you again soon!");
     }
 
     private static void input() {
         Scanner sc;
-        ArrayList<Task> arrList = new ArrayList<>();
-        String taskType, task, date, word, statement;
-        Task t;
-        String taskArray[] = new String[2];
+        String statement;
+        ArrayList<String> arrList = new ArrayList<>();
 
         sc = new Scanner(System.in);
         statement = sc.nextLine();
 
         while (!statement.equals("bye")) {
             if (statement.equals("list") && arrList.size() != 0) {
-                System.out.println("Here are the tasks in your list:");
                 for (int i=1; i <= arrList.size(); i++) {
-                    t = arrList.get(i-1);
-                    System.out.println(i + ". [" +t.getType()+ "][" +t.getStatusIcon()+ "]" +t.description);
+                    System.out.println(i +". " + arrList.get(i-1));
                 }
                 System.out.println();
             }
             else if (statement.equals("list")) {
-                DukeException error = new DukeException();
-                System.out.println(error.empty());
+                System.out.println("Empty");
                 System.out.println();
             }
             else {
-                taskArray = statement.split(" ");
-                if (taskArray.length ==1) {
-                    DukeException error = new DukeException();
-                    System.out.println(error.errorMsg(taskArray[0]));
-                    System.out.println("");
-                }
-                else {
-                    taskType = taskArray[0];
-                    if ((statement.split(" ")[0]).equals("done")) {
-                        int a = Integer.parseInt(statement.split(" ")[1]) - 1;
-                        if (a <= arrList.size() && a!=-1) {
-                            arrList.get(a).markAsDone();
-                            System.out.println("Nice! I've marked this task as done: ");
-                            System.out.println("  [" + arrList.get(a).getStatusIcon() + "] " + arrList.get(a).description);
-                            System.out.println();
-                        }
-                        else {
-                            DukeException error = new DukeException();
-                            System.out.println(error.outOfBound());
-                            System.out.println();
-                        }
-                    }
-                    else {
-                        statement = statement.substring(statement.indexOf(" "), statement.length());
-                        if (!taskType.equals("todo")) {
-                            task = statement.split("/")[0];
-                            date = statement.split("/")[1];
-                            word = date.substring(0, date.indexOf(" "));
-                            date = date.substring(date.indexOf(" ") + 1, date.length());
-                            if (task.equals("event")) {
-                                t = new Event(task, word, date);
-                            } else {
-                                t = new Deadline(task, word, date);
-                            }
-                        } else {
-                            t = new Todo(statement);
-                        }
-                        arrList.add(t);
-                        System.out.println("Got it. I've added this task:");
-                        System.out.println("  [" + t.getType() + "][" + t.getStatusIcon() + "]" + t.getTask());
-                        System.out.println("Now you have " + arrList.size() + " tasks in the list.");
-                        System.out.println();
-                    }
-                }
+                arrList.add(statement);
+                System.out.println("added: " + statement);
+                System.out.println();
             }
             statement = sc.nextLine();
         }
