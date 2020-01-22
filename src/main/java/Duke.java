7a4732f6db20f5c5d0ca01a39f9013613d8cc524
Wei Cheng <e0321478@u.nus.edu>
2020-01-22 16:50:49 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,20 +1,43 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Duke  {
-    public static void main(String[] args) {
-        Duke.greet();
-        Duke.chat();
+    private ArrayList<String> textStorage;
+
+
+    private Duke(ArrayList<String> textStorage){
+        this.textStorage = textStorage;
     }
-    public static void greet() {
+    public static void main(String[] args) {
+        Duke chatbot = new Duke(new ArrayList<String>());
+        chatbot.greet();
+        chatbot.chat();
+    }
+    public void greet() {
         System.out.println("Hello! I'm Duke\n" +
                 "     What can I do for you?");
     }
-    public static void chat() {
+    public void chat() {
         Scanner sc = new Scanner(System.in);
         String message = sc.nextLine();
         while(!message.equals("bye")) {
-            System.out.println(message);
+            if(message.equals("list")){
+                this.printText();
+            }
+            else {
+                System.out.println("added: " + message);
+                this.textStorage.add(message);
+            }
             message = sc.nextLine();
         }
-        System.out.println(message);
+        System.out.println("Bye. Hope to see you again soon!");
     }
+
+    public void printText() {
+        int counter = 1;
+        for(String text : this.textStorage){
+            System.out.println(counter + ". " + text);
+            counter++;
+    }
+    }
+
 }
