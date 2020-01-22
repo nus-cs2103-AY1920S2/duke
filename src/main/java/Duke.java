06bc2efe0eca39581667ede1dbab6984f9787104
Dylan Lim <60003775+limdylan@users.noreply.github.com>
2020-01-22 17:38:05 +0800

    Add echo commands and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,18 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("Hello from\n" + logo);
+        while (sc.hasNext()) {
+            String input = sc.next();
+            if (input.equals("bye")) {
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
+        System.out.println("Goodbye!");
+        sc.close();
     }
 }
