b282a3a88fd039f8d813e18c5d27a170c364ee19
CharmaineKoh <charmaine.koh@u.nus.edu>
2020-01-22 14:15:02 +0800

    Level-3: Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,9 +4,10 @@ public class Duke {
     /**
      * Add the ability to store whatever text entered by the user and display them
      * back to the user when requested.
+     * Add the ability to mark tasks as done.
      */
     public static void main(String[] args) {
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         int currentTask = 0;
         String horizontalLine = "____________________________________________________________";
         String logo = " ____        _        \n"
@@ -23,23 +24,35 @@ public class Duke {
 
         while (true) {
             Scanner sc = new Scanner(System.in);
-            String input = sc.nextLine();
+            String input = sc.next();
             if (input.equals("bye")) {
                 System.out.println("\t" + horizontalLine);
                 System.out.println("\tBye. Hope to see you again soon!");
                 System.out.println("\t" + horizontalLine);
                 break;
             } else if (input.equals("list")) {
+                System.out.println("\t" + horizontalLine);
+                System.out.println("\tHere are the tasks in your list:");
                 int taskNumber = 1;
                 while (list[taskNumber - 1] != null) {
-                    System.out.println(taskNumber + ". " + list[taskNumber - 1]);
+                    System.out.println("\t" + taskNumber + ". " + list[taskNumber - 1]);
                     taskNumber++;
                 }
+                System.out.println("\t" + horizontalLine);
+            } else if (input.equals("done")) {
+                int task = sc.nextInt();
+                Task doneTask = list[task - 1];
+                doneTask.setDone();
+                System.out.println("\t" + horizontalLine);
+                System.out.println("\tNice! I've marked this task as done: ");
+                System.out.println("\t" + list[task - 1]);
+                System.out.println("\t" + horizontalLine);
             } else {
                 System.out.println("\t" + horizontalLine);
                 System.out.println("\tadded: " + input);
                 System.out.println("\t" + horizontalLine);
-                list[currentTask] = input;
+                Task newTask = new Task(input);
+                list[currentTask] = newTask;
                 currentTask++;
             }
 
