3b7f3d3b43ac220091e719fe761495e123e7265e
Hoang Nguyen <tobeornottobeisgrammaticallyco@gmail.com>
2020-01-22 23:23:02 +0800

    Level-1 with committer name fixed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,24 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        ArrayList<String> memory = new ArrayList<>();
         System.out.println("Greeting, traveler. My name is Andrew. What can I do for you?");
         while (scan.hasNext()) {
             String input = scan.nextLine();
             if (input.equals("bye")) {
-                System.out.println("Farewell, my friend.");
+                System.out.println("I shall not trouble you anymore. Farewell, partner.");
                 break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < memory.size(); i++) {
+                    System.out.printf("%d - %s\n", i + 1, memory.get(i));
+                }
+            } else {
+                memory.add(input);
+                System.out.printf("Added: %s\n", input);
             }
-            System.out.println(input);
         }
     }
 }
