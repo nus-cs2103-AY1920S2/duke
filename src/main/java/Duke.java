520b4231ef6dbcb3af8f30b2cfb90de8c78fffd1
Wei Cheng <e0321478@u.nus.edu>
2020-01-22 16:14:52 +0800

    Level 1 : Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
-public class Duke {
+import java.util.Scanner;
+public class Duke  {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke.greet();
+        Duke.chat();
+    }
+    public static void greet() {
+        System.out.println("Hello! I'm Duke\n" +
+                "     What can I do for you?");
+    }
+    public static void chat() {
+        Scanner sc = new Scanner(System.in);
+        String message = sc.nextLine();
+        while(!message.equals("bye")) {
+            System.out.println(message);
+            message = sc.nextLine();
+        }
+        System.out.println(message);
     }
 }
