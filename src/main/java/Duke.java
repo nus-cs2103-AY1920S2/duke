2091ae6b84b4e01e969a5d440a39acd86abe3d00
Jarrod-Bob <e0309003@u.nus.edu>
2020-01-22 16:13:57 +0800

    Done Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,13 +8,37 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println("My name is Jarvis!\nHow may I provide my services on this fine day?");
+        String divider = "____________________________________________________________";
+        System.out.println("My name is Jarvis!\nHow may I provide my services on this fine day?\n" + divider);
 
+
+        String[] tasks = new String[100];
+        int i = 0;
         Scanner sc = new Scanner(System.in);
         String nextLine = sc.nextLine();
         while (!nextLine.equals("bye")) {
-            System.out.println(nextLine);
+
+            // to check if 'list' service is called
+            if (nextLine.equals("list")) {
+                int listStart = 1;
+                for (String task : tasks) {
+                    if (task == null) {
+                        System.out.println(divider);
+                        break;
+                    }
+                    System.out.println(listStart + ". " + task);
+                    listStart++;
+                }
+                nextLine = sc.nextLine();
+                break;
+            }
+
+            // normal addition of task
+            tasks[i] = nextLine;
+            System.out.println("added: " + nextLine);
+            System.out.println(divider);
             nextLine = sc.nextLine();
+            i++;
         }
         System.out.println("Hope my service has been of great help! See you again!");
     }
