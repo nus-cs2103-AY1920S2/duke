6b14b4995ac0d9e81132addb8c00985ac439aa68
alam8064 <59945273+alam8064@users.noreply.github.com>
2020-01-22 22:38:50 +0800

    Level-6

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,14 +10,13 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         Scanner sc = new Scanner(System.in);
-        Task[] data = new Task[100];
+        ArrayList<Task> data = new ArrayList<>();
         int slot = 0;
 
         while (sc.hasNext()) {
             String input = sc.nextLine();
             String[] inputArr = input.split("\\s+", 2);
             String action = inputArr[0];
-
             try {
                 if (input.equals("bye")) {
                     System.out.println("Bye. Hope to see you again soon!");
@@ -25,12 +24,18 @@ public class Duke {
                 } else if (input.equals("list")) {
                     System.out.println("Here are the tasks in your list:");
                     for (int i = 0; i < slot; i++) {
-                        System.out.println(i + 1 + ". " + data[i]);
+                        System.out.println(i + 1 + ". " + data.get(i));
                     }
                 } else if (action.equals("done")) {
                     int option = Integer.parseInt(inputArr[1]);
-                    data[option-1].setDone();
-                    System.out.println("Nice! I've marked this task as done:\n  " + data[option-1]);
+                    data.get(option - 1).setDone();
+                    System.out.println("Nice! I've marked this task as done:\n  " + data.get(option - 1));
+                } else if (action.equals("delete")) {
+                    int option = Integer.parseInt(inputArr[1]);
+                    System.out.println("Noted. I've removed this task: \n  " + data.get(option - 1));
+                    data.remove(option - 1);
+                    slot--;
+                    System.out.println("Now you have " + slot + " tasks in the list.");
                 } else {
                     switch (action) {
                         case "todo":
@@ -38,7 +43,7 @@ public class Duke {
                                 throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
                             } else {
                                 System.out.println("Got it. I've added this task: ");
-                                data[slot] = new Todo(inputArr[1]);
+                                data.add(new Todo(inputArr[1]));
                             }
                             break;
                         case "deadline":
@@ -50,7 +55,7 @@ public class Duke {
                                     throw new DukeException("☹ OOPS!!! You forgot to specify a date/time for the deadline.");
                                 } else {
                                     System.out.println("Got it. I've added this task: ");
-                                    data[slot] = new Deadline(dArr[0], dArr[1]);
+                                    data.add(new Deadline(dArr[0], dArr[1]));
                                 }
                             }
                             break;
@@ -63,14 +68,14 @@ public class Duke {
                                     throw new DukeException("☹ OOPS!!! You forgot to specify a date/time for the event.");
                                 } else {
                                     System.out.println("Got it. I've added this task: ");
-                                    data[slot] = new Event(eArr[0], eArr[1]);
+                                    data.add(new Event(eArr[0], eArr[1]));
                                 }
                             }
                             break;
                         default:
                             throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
                     }
-                    System.out.println("  " + data[slot]);
+                    System.out.println("  " + data.get(slot));
                     slot++;
                     System.out.println("Now you have " + slot + " tasks in the list.");
                 }
