1d2ae41dcd0c948de62a4729b5652d6cf6950192
Amoscheong97 <amoscheong9797@gmail.com>
2020-01-23 01:41:03 +0800

    Greet, Echo and Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,45 @@
+import java.util.Scanner;
 public class Duke {
+    public static void HorizontalLine() {
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void greeting(){
+        HorizontalLine();
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        HorizontalLine();
+    }
+
+    public static void exit(){
+        HorizontalLine();
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("");
+        HorizontalLine();
+    }
+
+    public static void echo(String line) {
+        HorizontalLine();
+        System.out.println(line);
+        HorizontalLine();
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        // Greet the user
+        greeting();
+
+        Scanner sc = new Scanner(System.in);
+
+        //Output the command the user types
+        // Exits when the user types 'bye'
+        String line = sc.nextLine();
+        while (!line.equals("bye")) {
+            // Output to the user
+            echo(line);
+            line = sc.nextLine();
+        }
+
+        // Say 'bye' to the user
+        exit();
     }
 }
