1a80f13abaf1d9833cf1009a07f1bbb276eaace0
Eugene Tan <eugenetanyc@gmail.com>
2020-01-23 00:17:08 +0800

    some minor spacing edits

--- src/main/java/AlreadyDoneException.java
+++ src/main/java/AlreadyDoneException.java
@@ -2,7 +2,7 @@ public class AlreadyDoneException extends DukeException {
 
     /**
      * Constructor for DeadlineException
-     * 
+     *
      * @param message any message to convey
      */
     public AlreadyDoneException(String message) {
