9b5216cbfba332d21910d23d7ed895ff5ac34ac1
duongphammmm <46336643+duongphammmm@users.noreply.github.com>
2020-01-24 00:07:41 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,23 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static Scanner sc = new Scanner(System.in);
     public static String machine = "Dude: ";
     public static String user = "dude: ";
+    public static ArrayList<String> list = new ArrayList<>();
 
     public static void main(String[] args) {
         greeting();
-        String command = sc.next();
+        String command = sc.nextLine();
         while (!command.equals("bye")) {
-            System.out.println(machine + command);
+            if (command.equals("list")) {
+                list();
+            } else {
+                addTask(command);
+            }
             System.out.print(user);
-            command = sc.next();
+            command = sc.nextLine();
         }
         System.out.println(machine + "Okay see ya dude!");
     }
@@ -22,5 +28,17 @@ public class Duke {
                 + "      What do you want dude?\n"
                 + user;
         System.out.print(welcome);
+    }
+
+    public static void list() {
+        System.out.println(machine + "Here's your list of tasks dude:");
+        for (int i = 1; i <= list.size(); i++) {
+            System.out.println("      " + i + ". " + list.get(i-1));
+        }
+    }
+
+    public static void addTask(String command) {
+        list.add(command);
+        System.out.println(machine + command + " is added");
     }
 }
