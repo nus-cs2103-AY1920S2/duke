1834769ed4d88a637f2a9b5a5fe48d46370ddd70
FyonnOh <>
2020-01-23 11:26:14 +0800

    Level 4: ToDos, Events, Deadlines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,31 +11,43 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("Konnichiwa! Watashi no namaeha Duke desu! Whatchu can i duwu for yuwu? (^.___.^)");
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> strArr = new ArrayList<>();
-        ArrayList<String> tickArr = new ArrayList<>();
+        ArrayList<Task> tasksArr = new ArrayList<>();
         while(sc.hasNext()) {
-            String input = sc.nextLine();
-            if (input.equals("bye")){
-                System.out.println("Sayonara~ （＾・ω・＾✿）");
-                break;
-            } else if (input.equals("list")) {
+            String command = sc.next();
+            if (command.equals("list")) {
                 System.out.println("Hewe awu de tasku in ywour listu! uwu \n");
-                for(int i=0; i< strArr.size(); i++) {
-                    String text = strArr.get(i);
-                    System.out.println(i + 1 + ". " + tickArr.get(i) +" "+ text);
+                for(int i=0; i<tasksArr.size(); i++) {
+                    System.out.println(i+1 + " "+ tasksArr.get(i));
                 }
+            } else if (command.equals("done")) {
+                int index = sc.nextInt()-1;
+                tasksArr.get(index).markAsDone();
+                System.out.println("Nicesu! watashi hasu marku the tasku as done desu ʕ ᵒ ᴥ ᵒʔ: \n");
+                System.out.println("        " + tasksArr.get(index) + "\n");
+            } else if (command.equals("todo")) {
+                ToDo t = new ToDo(sc.nextLine());
+                tasksArr.add(t);
+                System.out.println("Got it desu! uwu Watashi hasu added des tasku (´・(oo)・｀): \n");
+                System.out.println("        " + t + "\n");
+                System.out.println("nuw omae has " + tasksArr.size()+ " tasks in the list");
+            } else if (command.equals("deadline")) {
+                String str = sc.nextLine();
+                String[] splitStr = str.split("/by");
+                DeadLine t = new DeadLine(splitStr[0], splitStr[1]);
+                tasksArr.add(t);
+                System.out.println("Gotcha! Watashi hasu added des tasku [^._.^]ﾉ: \n");
+                System.out.println("        " + t + "\n");
+                System.out.println("nuw omae has " + tasksArr.size()+ " tasks in the list");
+            } else if (command.equals("event")) {
+                String str = sc.nextLine();
+                String[] splitStr = str.split("/at");
+                Event t = new Event(splitStr[0], splitStr[1]);
+                tasksArr.add(t);
+                System.out.println("Gotcha! Watashi hasu added des tasku (=＾● ⋏ ●＾=): \n");
+                System.out.println("        " + t + "\n");
+                System.out.println("nuw omae has " + tasksArr.size()+ " tasks in the list");
             } else {
-                String[] splited = input.split("\\s+");
-                if (splited[0].equals("done")) {
-                    System.out.println("Nicesu! watashi hasu marku the tasku as done desu! uwu" + "\n");
-                    int index = Integer.parseInt(splited[1]) -1;
-                    tickArr.set(index, "[✓] ");
-                    System.out.println(tickArr.get(index) + strArr.get(index));
-                } else {
-                    strArr.add(input);
-                    tickArr.add("[✗] ");
-                    System.out.println("(=´∇｀=) added:" + input +"!");
-                }
+                System.out.println("error!");
             }
         }
     }
