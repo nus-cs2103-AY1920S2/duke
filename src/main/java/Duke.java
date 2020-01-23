a1e6aacaedeead1da4b42bc86f42d62eb7ab147c
LiuChao93 <>
2020-01-23 18:38:07 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
-        String[] userInputList = new String[100];
+        Task[] userInputList = new Task[100];
         int count = 0;
         int count2 = 0;
         int count3 = 1;
@@ -18,23 +18,35 @@ public class Duke {
         while(!userInput.equals("bye")){
             if(userInput.equals("list")){
                 System.out.println("    ____________________________________________________________");
+                System.out.println("     Here are the tasks in your list:");
                 count2 = 0;
                 count3 = 1;
                 while(count2 < count){
-                    System.out.println("     " + count3 + ". " + userInputList[count2]);
+                    System.out.println("     " + count3 + ".[" + userInputList[count2].getStatusIcon() + "] " + userInputList[count2].getDescription());
                     count2++;
                     count3++;
-                    }
+                }
                 System.out.println("    ____________________________________________________________\n");
 
+            } else if(userInput.equals("done 2")) {
+                System.out.println("    ____________________________________________________________");
+                System.out.println("     Nice! I've marked this task as done: ");
+                count2 = 0;
+                while(count2 < count){
+                    if(userInputList[count2].getDone() == true) {
+                        System.out.println("       " + userInputList[count2].getStatusIcon() + " " + userInputList[count2].getDescription());
+                    }
+                    count2++;
+                }
+                System.out.println("    ____________________________________________________________");
             } else {
-                userInputList[count] = userInput;
+                Task t = new Task(userInput);
+                userInputList[count] = t;
                 count++;
                 System.out.println("    ____________________________________________________________");
                 System.out.println("     added: " + userInput);
                 System.out.println("    ____________________________________________________________\n");
             }
-
             userInput = myObj.nextLine();
         }
 
