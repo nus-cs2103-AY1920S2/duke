a99c4d8e92c8d8e88a50f20f3768a0c67503fb27
Ashleigh <ashleighaathar@gmail.com>
2020-01-23 15:35:07 +0800

    add duke greetings and echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "  ____\n" + " (.__.)\n" + "<|>\n" + " /\\" + "\n_  _";
+        Scanner sc = new Scanner(System.in);
+        System.out.println(logo);
+        System.out.println("Dude, what do you want? \nHurry, reply with 'dude, <command>'!");
+        String userInput = sc.nextLine();
+        while (!userInput.equals("dude, bye")) {
+            if (!userInput.split(" ")[0].equals("dude,")) {
+                System.out.println("dude, wrong command!");
+            }
+            else {
+                System.out.println(userInput);
+            }
+            userInput = sc.nextLine();
+        }
+        System.out.println("dude, stop wasting my time! shoo!");
+
     }
 }
+
