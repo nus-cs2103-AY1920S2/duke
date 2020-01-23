b3ab69df3b1f2e14bbbd11adc725b1fdf8fa0350
PangJiaDa <pangjiada52@gmail.com>
2020-01-23 21:03:03 +0800

    completed level 2, add & list functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,14 @@
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.ArrayList;
+
 
 public class Duke {
     private static void hLine() {
         System.out.println("____________________________________________________________");
     }
+
     private static void printWelcomeMessage() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -16,19 +20,40 @@ public class Duke {
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         hLine();
     }
+
     private static void printWithHLine(String message) {
         hLine();
         System.out.println(message);
         hLine();
     }
+
+    private static void printTasks(ArrayList<String> tasks) {
+        hLine();
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println((i + 1) + ". " + tasks.get(i));
+        }
+        hLine();
+    }
+
     public static void main(String[] args) {
         printWelcomeMessage();
+
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> tasks = new ArrayList<>();
+
         String input = scanner.nextLine();
         while (!input.equals("bye")) {
-            printWithHLine(input);
+            if (input.equals("list")) {
+                printTasks(tasks);
+            } else {
+                printWithHLine("added: " + input);
+                tasks.add(input);
+            }
+
             input = scanner.nextLine();
         }
+
         printWithHLine("Bye. Hope to see you again soon!");
     }
+
 }
