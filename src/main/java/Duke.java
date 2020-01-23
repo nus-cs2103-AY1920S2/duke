1f4b714aea8b1f267079ca751902307cb3796c68
junhaotan <>
2020-01-23 11:48:41 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,42 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
+
+    private Scanner sc;
+
+    public Duke() {
+        this.sc = new Scanner(System.in);
+    }
+
+    private void intro() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("What can I do for you?");
+    }
+
+    private void echo(String input) {
+        System.out.println("    " + input);
+        this.waitInput();
+    }
+
+    private void waitInput() {
+        String input = sc.nextLine();
+        switch (input) {
+            case "bye":
+                System.out.println("    Bye. Hope to see you again soon!");
+                break;
+            default:
+                echo(input);
+                break;
+        }
+    }
+
+    public void run() {
+        this.intro();
+        this.waitInput();
     }
 }
