fa423d39fb436e291ab899ecaf61ee42d0ace036
akhilvuputuri <akhil.vuputuri@temasek.nus.edu.sg>
2020-01-23 20:41:18 +0800

    Finish Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +8,31 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String greetingText = "    ____________________________________________________________\n" +
+                "     Hello! I'm Duke\n" +
+                "     What can I do for you?\n" +
+                "    ____________________________________________________________";
+        System.out.println(greetingText);
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                String byeText = "    ____________________________________________________________\n" +
+                        "     Bye. Hope to see you again soon!\n" +
+                        "    ____________________________________________________________";
+                System.out.println(byeText);
+                break;
+            } else {
+                System.out.println(wrapHorizontalLines(input));
+            }
+        }
+    }
+
+    public static String wrapHorizontalLines(String input) {
+        return "    ____________________________________________________________\n" +
+                "     " + input + "\n" +
+                "    ____________________________________________________________";
     }
 }
