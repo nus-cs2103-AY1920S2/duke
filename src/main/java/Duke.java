338e2f63e51a39f8f3aa67ac067cdb73ffc5cf00
buddhavineeth <buddhavineeth@gmail.com>
2020-01-23 23:56:24 +0800

    teach Duke to deal with errors such as incorrect user inputs

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Duke {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws DukeException {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Hello there! I am Duke, your personal assistant. May I know your name?");
         String name = scanner.nextLine();
@@ -21,48 +21,95 @@ public class Duke {
                     break;
 
                 case "todo":
-                    String taskName = command.split("todo ")[1];
-                    Todo newTodo = new Todo(taskName);
-                    System.out.println("\t" + "Awesome! I've added this todo:");
-                    System.out.println("\t\t" + newTodo);
-                    taskList.add(newTodo);
-                    System.out.println("\t" + "Now, you have " + taskList.size() + " tasks in the list.");
+                    try {
+                        String taskName = command.split("todo ")[1];
+                        if (taskName.equals("")) {
+                            throw new DukeException("Invalid Todo Format");
+                        }
+                        Todo newTodo = new Todo(taskName);
+                        System.out.println("\t" + "Awesome! I've added this todo:");
+                        System.out.println("\t\t" + newTodo);
+                        taskList.add(newTodo);
+                        System.out.println("\t" + "Now, you have " + taskList.size() + " tasks in the list.");
+                    }
+                    catch (DukeException e) {
+                        System.out.println("\t" + e);
+                        System.out.println("\t\t" + "Please do not leave todo description as empty.");
+                    }
+                    catch (Exception e) {
+                        System.out.println("\t\t" + "Please do not leave todo description as empty.");
+                    }
                     break;
 
                 case "deadline":
-                    String[] tokens = command.split("/by ");
-                    String dueDate = tokens[1];
-                    String deadlineName = tokens[0].split("deadline ")[1].trim();
-                    Deadline newDeadline = new Deadline(deadlineName, dueDate);
-                    System.out.println("\t" + "Awesome! I've added this deadline:");
-                    System.out.println("\t\t" + newDeadline);
-                    taskList.add(newDeadline);
-                    System.out.println("\t" + "Now, you have " + taskList.size() + " tasks in the list.");
+                    try {
+                        String[] tokens = command.split("/by ");
+                        String dueDate = tokens[1];
+                        String deadlineName = tokens[0].split("deadline ")[1].trim();
+                        if (dueDate.equals("") || deadlineName.equals("")) {
+                            throw new DukeException("Invalid Deadline Format");
+                        }
+                        Deadline newDeadline = new Deadline(deadlineName, dueDate);
+                        System.out.println("\t" + "Awesome! I've added this deadline:");
+                        System.out.println("\t\t" + newDeadline);
+                        taskList.add(newDeadline);
+                        System.out.println("\t" + "Now, you have " + taskList.size() + " tasks in the list.");
+                    }
+                    catch (DukeException e){
+                        System.out.println("\t" + e);
+                        System.out.println("\t\t" + "Please do not leave deadline description as empty.");
+                    }
+                    catch (Exception e) {
+                        System.out.println("\t\t" + "Please do not leave deadline description as empty.");
+                    }
                     break;
 
                 case "event":
-                    String[] words = command.split("/at ");
-                    String timePeriod = words[1];
-                    String eventName = words[0].split("event ")[1].trim();
-                    Event newEvent = new Event(eventName, timePeriod);
-                    System.out.println("\t" + "Awesome! I've added this event:");
-                    System.out.println("\t\t" + newEvent);
-                    taskList.add(newEvent);
-                    System.out.println("\t" + "Now, you have " + taskList.size() + " tasks in the list.");
+                    try {
+                        String[] words = command.split("/at ");
+                        String timePeriod = words[1];
+                        String eventName = words[0].split("event ")[1].trim();
+                        if (timePeriod.equals("") || eventName.equals("")) {
+                            throw new DukeException("Invalid Event Format");
+                        }
+                        Event newEvent = new Event(eventName, timePeriod);
+                        System.out.println("\t" + "Awesome! I've added this event:");
+                        System.out.println("\t\t" + newEvent);
+                        taskList.add(newEvent);
+                        System.out.println("\t" + "Now, you have " + taskList.size() + " tasks in the list.");
+                    }
+
+                    catch (DukeException e){
+                        System.out.println("\t" + e);
+                        System.out.println("\t\t" + "Please do not leave deadline description as empty.");
+                    }
+                    catch (Exception e) {
+                        System.out.println("\t\t" + "Please do not leave event description as empty.");
+                    }
                     break;
 
                 case "done":
-                    Task toComplete = taskList.get(Integer.parseInt(command.split(" ")[1]) - 1);
-                    toComplete.completeStatus();
-                    System.out.println("\t" + "Hooray! You've finally managed to finish this task:");
-                    System.out.println("\t\t" + toComplete);
+                    try {
+                        Task toComplete = taskList.get(Integer.parseInt(command.split(" ")[1]) - 1);
+                        toComplete.completeStatus();
+                        System.out.println("\t" + "Hooray! You've finally managed to finish this task:");
+                        System.out.println("\t\t" + toComplete);
+                    }
+                    catch (Exception e) {
+                        System.out.println(e);
+                    }
                     break;
 
                 case "delete":
-                    Task deleted = taskList.remove(Integer.parseInt(command.split(" ")[1]) - 1);
-                    System.out.println("\t" + "Got it! I've removed this task:");
-                    System.out.println("\t\t" + deleted);
-                    System.out.println("\t" + "Now, you have " + taskList.size() + " tasks in the list.");
+                    try {
+                        Task deleted = taskList.remove(Integer.parseInt(command.split(" ")[1]) - 1);
+                        System.out.println("\t" + "Got it! I've removed this task:");
+                        System.out.println("\t\t" + deleted);
+                        System.out.println("\t" + "Now, you have " + taskList.size() + " tasks in the list.");
+                    }
+                    catch (Exception e) {
+                        System.out.println(e);
+                    }
                     break;
 
                 case "bye":
@@ -70,7 +117,15 @@ public class Duke {
                     System.out.println("\t" + "Adios. It was my pleasure assisting you. Keep smiling " + name + ".");
                     break;
 
-                default: System.out.println("\t" + "Please format your input!");
+                default:
+                    try {
+                        throw new DukeException("Invalid Command Format");
+                    }
+                    catch (DukeException e) {
+                        System.out.println("\t" + e);
+                        System.out.println("\t\t" + "I know I am dumb. Please write your command in a way that" +
+                                " I can understand.");
+                    }
                     break;
             }
         }
