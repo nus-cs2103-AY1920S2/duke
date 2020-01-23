6f42980c1382d65ef514c648a16f03873df03df6
gb3h <gabriel.yfy@gmail.com>
2020-01-23 16:07:13 +0800

    Level-4

--- src/main/java/Akshay.java
+++ src/main/java/Akshay.java
@@ -17,7 +17,7 @@ public class Akshay {
         int count = 0;
         String input = sc.nextLine();
         while (!input.equals("bye")) {
-            String[] c = input.split(" ");
+            String[] c = input.split(" ", 2);
             switch (c[0]) {
                 case ("list"):
                     System.out.println(line);
@@ -33,10 +33,28 @@ public class Akshay {
                     curr.mark();
                     say("Marked as done:\n" + curr.toString());
                     break;
-                default:
-                    arr[count] = new Task(input);
+                case ("todo"):
+                    Task todo = new Todo(c[1]);
+                    arr[count] = todo;
                     count++;
-                    say("Added: " + input);
+                    say("Added: " + todo.toString());
+                    break;
+                case ("deadline"):
+                    String[] dl = c[1].split("/by",2);
+                    Task d = new Deadline(dl[0], dl[1]);
+                    arr[count] = d;
+                    count++;
+                    say("Added: " + d.toString());
+                    break;
+                case ("event"):
+                    String[] ev = c[1].split("/at",2);
+                    Task e = new Event(ev[0], ev[1]);
+                    arr[count] = e;
+                    count++;
+                    say("Added: " + e.toString());
+                    break;
+                default:
+                    say("Please enter correct input.");
             }
             input = sc.nextLine();
         }
