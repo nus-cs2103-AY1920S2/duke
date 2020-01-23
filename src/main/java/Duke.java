c2f9c873f6a1e0428e0ac2d6b430854d43aef6c1
agnesnatasya <e0313565@u.nus.edu>
2020-01-23 20:45:40 +0800

    Level-2, added list and add functionalities

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -15,10 +16,21 @@ public class Duke {
         System.out.println("\tWhat can I do for you?");
         System.out.println("\t" + line);
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> things = new ArrayList<>();
         String next = sc.nextLine();
         while (!next.equals("bye")) {
             System.out.println("\t" + line);
-            System.out.println("\t" + next);
+            if (next.equals("list")) {
+                int i = 1;
+                for (String s: things) {
+                    System.out.println("\t" + i + ". " + s);
+                    i++;
+
+                }
+            } else {
+                things.add(next);
+                System.out.println("\tadded: " + next);
+            }
             System.out.println("\t" + line);
             next = sc.nextLine();
         }
