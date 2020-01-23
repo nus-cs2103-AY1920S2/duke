4df81aea6f1bcc26c0f04199d985dbb343ad78ed
PangKimJin <pangkimjin@u.nus.edu>
2020-01-23 16:12:47 +0800

    implemented level 3.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -15,10 +15,28 @@ public class Duke {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         String[] arr = new String[100];
+        String[] doneArr = new String[100];
         int arrPointer = 1;
+        String doneLogo = "[✓]";
+        String notDoneLogo = "[✗]";
+        for (int i = 0; i < 100; i++) {
+            doneArr[i] = notDoneLogo;
+        }
         do {
             userInput = sc.nextLine();
-            if (!userInput.equals("list")) {
+            if (userInput.equals("list")) {                                                  //Lists out the tasks
+                System.out.println("Here are the tasks in your list: ");
+                for (int i = 1; i < arrPointer; i++) {
+                    System.out.println(i + "." + doneArr[i] + " " + arr[i]);
+                }
+            }
+            else if (userInput.split("\\s")[0].equals("done")) {
+                int doneTask = Integer.parseInt(userInput.split("\\s")[1]);
+                doneArr[doneTask] = doneLogo;
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(doneLogo + " " + arr[doneTask]);
+            }
+            else {
                 arr[arrPointer] = userInput;
                 arrPointer++;
                 if (!userInput.equals("bye")) {
@@ -26,11 +44,8 @@ public class Duke {
                 }
 
             }
-            else {
-                for (int i = 1; i < arrPointer; i++) {
-                    System.out.println(i + ". " + arr[i]);
-                }
-            }
+
+
         } while (!userInput.equals("bye"));
         System.out.println("Bye. Hope to see you again soon!");
     }
