e444e9c6277d961d678fd0dea77ac9c392a41e73
Sanchari Bandyopadhyay <sancharizxc@gmail.com>
2020-01-23 20:45:33 +0800

    Improvements to test case

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -3,7 +3,7 @@
 # create bin directory if it doesn't exist
 if [ ! -d "../bin" ]
 then
-    mkdir /Users/sanchari/Desktop/JAVA/duke/bin
+    mkdir /Users/sanchari/Desktop/JAVA/duke/text-ui-test/bin
 fi
 
 # delete output from previous run
@@ -13,7 +13,7 @@ then
 fi
 
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp /Users/sanchari/Desktop/JAVA/duke/bin -Xlint:none -d /Users/sanchari/Desktop/JAVA/duke/bin /Users/sanchari/Desktop/JAVA/duke/src/main/java/Duke.java
+if ! javac -cp /Users/sanchari/Desktop/JAVA/duke/text-ui-test/bin -Xlint:none -d /Users/sanchari/Desktop/JAVA/duke/text-ui-test/bin /Users/sanchari/Desktop/JAVA/duke/src/main/java/Duke.java
 
 then
     echo "********** BUILD FAILURE **********"
@@ -21,10 +21,10 @@ then
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath /Users/sanchari/Desktop/JAVA/duke/bin Duke < /Users/sanchari/Desktop/JAVA/duke/bin/input.txt > /Users/sanchari/Desktop/JAVA/duke/bin/ACTUAL.TXT
+java -classpath /Users/sanchari/Desktop/JAVA/duke/text-ui-test/bin Duke < /Users/sanchari/Desktop/JAVA/duke/text-ui-test/bin/input.txt > /Users/sanchari/Desktop/JAVA/duke/text-ui-test/sbin/ACTUAL.TXT
 
 # compare the output to the expected output
-diff /Users/sanchari/Desktop/JAVA/duke/bin/ACTUAL.TXT /Users/sanchari/Desktop/JAVA/duke/bin/EXPECTED.TXT
+diff /Users/sanchari/Desktop/JAVA/duke/text-ui-test/bin/ACTUAL.TXT /Users/sanchari/Desktop/JAVA/duke/text-ui-test/bin/EXPECTED.TXT
 if [ $? -eq 0 ]
 then
     echo "Test result: PASSED"
