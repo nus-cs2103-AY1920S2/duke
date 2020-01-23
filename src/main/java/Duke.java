5b67c3efeff0d3d2d7abc14ddec2173c9e21000f
Ashleigh <ashleighaathar@gmail.com>
2020-01-23 16:39:58 +0800

    add task class and ability to mark tasks as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,22 +2,28 @@ import java.util.Scanner;
 import java.util.ArrayList;
 public class Duke {
     public static void main(String[] args) {
-        ArrayList<String> userInputList = new ArrayList<>();
+        ArrayList<Task> userInputList = new ArrayList<>();
         String logo = "  ____\n" + " (.__.)\n" + "<|>\n" + " /\\" + "\n_  _";
         Scanner sc = new Scanner(System.in);
         System.out.println(logo);
-        System.out.println("☛ Dude, what do you want? \n☛ Give me a command!");
+        System.out.println("☛ dude, what do you want? \n☛ give me a command!");
         System.out.print("dude, ");
         String userInput = sc.nextLine();
         while (!userInput.equals("bye")) {
-            if (userInput.equals("list")) {
-                System.out.println("☛ You asked to:");
+            if (userInput.equals("list")) { // to list given tasks
+                System.out.println("☛ you asked to:");
                 for (int i = 0; i < userInputList.size(); i++) {
                     System.out.println("\t" + (i + 1) + ". " + userInputList.get(i));
                 }
             }
-            else {
-                userInputList.add(userInput);
+            else if (userInput.startsWith("done")) { // to mark tasks as done
+                System.out.println("☛ dude, you're fast!");
+                Integer taskNumber = Integer.parseInt(userInput.split(" ", 2)[1]) - 1;
+                userInputList.get(taskNumber).markAsDone();
+            }
+            else { // to add tasks to list
+                Task task = new Task(userInput);
+                userInputList.add(task);
                 System.out.println("☛ fine, I will: " + userInput);
             }
             System.out.print("dude, ");
---
+++ src/main/java/Task.java
@@ -0,0 +1,22 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "[✔]" : "[✘]"); //return tick or X symbols
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    public String toString() {
+        return getStatusIcon() + " " + this.description;
+    }
+
+}
