7f6585ba5a85a7652703229010f2ece9cf9aff3a
nhamhung <nhamhung.gttn@gmail.com>
2020-01-23 17:22:28 +0700

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
-
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -13,22 +12,39 @@ public class Duke {
         System.out.println("What can I do for you?");
 
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> items = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
 
         while (sc.hasNext()) {
-            String item = sc.nextLine();
-            if (item.equals("bye")) {
+            String t = sc.nextLine();
+            String input[] = t.split(" ");
+            if (input[0].equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 return;
-            }
-            if (item.equals("list")) {
-                for (int i = 1; i <= items.size(); i++) {
-                    System.out.println(i + ".  " + items.get(i - 1));
+            } else if (input[0].equals("list")) {
+                if (tasks.size() == 0) {
+                    System.out.println("There is no task in your list! Please continue...");
+                    continue;
+                }
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 1; i <= tasks.size(); i++) {
+                    System.out.println(i + ".  " + tasks.get(i - 1));
                 }
                 continue;
+            } else if (input[0].equals("done")) {
+                int idx = Integer.parseInt(input[1]) - 1;
+                if (tasks.size() <= idx || idx < 0) {
+                    System.out.println("Please key in the correct task index! This index does not match any task.");
+                    continue;
+                }
+                tasks.get(idx).finishTask();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(tasks.get(idx));
+            } else {
+                Task task = new Task(t);
+                System.out.println("   > added: " + t);
+                tasks.add(task);
+                continue;
             }
-            System.out.println("   > added: " + item);
-            items.add(item);
         }
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,22 @@
+public class Task {
+    private String task;
+    private boolean done;
+
+    public Task(String task) {
+        this.task = task;
+        this.done = false;
+    }
+
+    public void finishTask() {
+        this.done = true;
+    }
+
+    @Override
+    public String toString() {
+        if (done) {
+            return String.format("[✓] %s", this.task);
+        } else {
+            return String.format("[✗] %s", this.task);
+        }
+    }
+}
