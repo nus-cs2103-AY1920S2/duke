50703a5cf1cded15a5276854a620eab1c8b27e8a
LiuChao93 <>
2020-01-23 17:56:10 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
+        String[] userInputList = new String[100];
+        int count = 0;
+        int count2 = 0;
+        int count3 = 1;
 
         System.out.println("    ____________________________________________________________");
         System.out.println("     Hello! I'm Duke");
@@ -12,9 +16,25 @@ public class Duke {
         String userInput = myObj.nextLine();
 
         while(!userInput.equals("bye")){
-            System.out.println("    ____________________________________________________________");
-            System.out.println("     " +userInput);
-            System.out.println("    ____________________________________________________________\n");
+            if(userInput.equals("list")){
+                System.out.println("    ____________________________________________________________");
+                count2 = 0;
+                count3 = 1;
+                while(count2 < count){
+                    System.out.println("     " + count3 + ". " + userInputList[count2]);
+                    count2++;
+                    count3++;
+                    }
+                System.out.println("    ____________________________________________________________\n");
+
+            } else {
+                userInputList[count] = userInput;
+                count++;
+                System.out.println("    ____________________________________________________________");
+                System.out.println("     added: " + userInput);
+                System.out.println("    ____________________________________________________________\n");
+            }
+
             userInput = myObj.nextLine();
         }
 
