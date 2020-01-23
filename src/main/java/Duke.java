7addf1fdb1245600d266544a93af5f7d01a42e87
Firzan Armani <mrfirzanarmani@gmail.com>
2020-01-23 22:16:29 +0800

    Duke.java: Implement use of ToDos, Deadlines and Events as Tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,6 +4,7 @@ public class Duke {
     private static boolean dukeActive = true;
     private static final Scanner scanner = new Scanner(System.in);
     private static String userInput = "";
+    private static String userArgs = "";
     private static Task[] items = new Task[100];
     private static int itemsCounter = 0;
 
@@ -21,30 +22,51 @@ public class Duke {
         dukePrompt(new String[]{"Hey boss! Duke here, at your service.", "What do you need me to do?"});
         while (dukeActive) {
             userInput = readUserInput();
+            System.out.println();
 
             switch (userInput) {
             case "list":
-                dukePrompt("Here's what I've written down, boss.");
-                listItems();
+                dukePrompt(listItems());
                 break;
             case "bye":
                 dukeActive = false;
-                dukePrompt("Good bye boss! Call me if you need me. I'll be waiting!");
+                dukePrompt("Good bye, boss! Call me if you need me. I'll be waiting!");
                 break;
             default:
                 String[] splitString = userInput.split(" ", 2);
+                userInput = splitString[0];
+                userArgs = splitString[1];
 
-                switch (splitString[0]) {
+                switch (userInput) {
                 case "done":
                     int taskNo = Integer.parseInt(splitString[1]) - 1;
                     markAsDone(taskNo);
-                    dukePrompt(new String[]{"Nice! I've marked this task as done:",
+                    dukePrompt(new String[]{"Got it boss! Just to confirm, this is the one I marked as done",
                         items[taskNo].toString()});
                     break;
-                default:
-                    items[itemsCounter] = new Task(userInput);
-                    dukePrompt("Added: " + userInput);
+                case "deadline":
+                    items[itemsCounter] = new Deadline(userArgs);
                     itemsCounter++;
+                    dukePrompt(new String[]{"Oooh, this one's important eh, boss? I got it",
+                        items[(itemsCounter - 1)].toString(),
+                        "Now you have " + itemsCounter + " tasks in the list"});
+                    break;
+                case "todo":
+                    items[itemsCounter] = new ToDo(userArgs);
+                    itemsCounter++;
+                    dukePrompt(new String[]{"Got it, boss. I'll write this one down",
+                        items[(itemsCounter - 1)].toString(),
+                        "Now you have " + itemsCounter + " tasks in the list"});
+                    break;
+                case "event":
+                    items[itemsCounter] = new Event(userArgs);
+                    itemsCounter++;
+                    dukePrompt(new String[]{
+                        "A special event I see. Don't worry boss, I'll remind you",
+                        items[(itemsCounter - 1)].toString(),
+                        "Now you have " + itemsCounter + " tasks in the list"});
+                    break;
+                default:
                     break;
                 };
             }
@@ -66,14 +88,22 @@ public class Duke {
     }
 
     private static String readUserInput() {
-        System.out.printf("You:  ");
+        System.out.printf("\nYou:  ");
         return scanner.nextLine();
     }
 
-    private static void listItems() {
-        for (int i = 0; i < itemsCounter; i++) {
-            System.out.println("      " + (i + 1) + ". " + items[i]);
+    private static String[] listItems() {
+        String[] temp;
+        if (itemsCounter == 0) {
+            temp = new String[]{"Boss, my notepad is empty. You sure you told me anything?"};
+        } else {
+            temp = new String[itemsCounter + 1];
+            temp[0] = "Here's what I've written down, boss.";
+            for (int i = 0; i < itemsCounter; i++) {
+                temp[i + 1] = (i + 1) + ". " + items[i];
+            }
         }
+        return temp;
     }
 
     private static void markAsDone(int taskNo) {
