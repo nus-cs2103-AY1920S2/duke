39665580f4ca17d2e19bdb85971dfaaea6ca00ce
buddhavineeth <buddhavineeth@gmail.com>
2020-01-23 21:49:36 +0800

    add list to store user inputs

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -10,16 +11,24 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("May I know your name?");
-        String name = scanner.next();
+
+        String name = scanner.nextLine();
+        ArrayList<String> storedInfo = new ArrayList<>();
 
         while (true) {
             System.out.println("How may I help you " + name + "?");
-            String command = scanner.next();
-            if (command.toLowerCase().equals("bye")) {
+            String command = scanner.nextLine();
+            if (command.toLowerCase().equals("list")) {
+                for (int currNum = 1; currNum <= storedInfo.size(); currNum++) {
+                    System.out.println(currNum + ". " + storedInfo.get(currNum - 1));
+                }
+            }
+            else if (command.toLowerCase().equals("bye")) {
                 System.out.println("Adios amigo. It was my pleasure assisting you. Keep smiling " + name + ".");
                 break;
             } else {
-                System.out.println(command);
+                storedInfo.add(command);
+                System.out.println("added: " + command);
             }
         }
     }
