d17bc6b4ee737c2a1d68682b5dcbc2a733ebdf6a
don-tay <dontay0209@gmail.com>
2020-01-23 11:15:00 +0800

    Add echo input command and exit command

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,12 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        String in = sc.next();
+        while (!in.equals("bye")){
+            System.out.println(in);
+            in = sc.next();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
