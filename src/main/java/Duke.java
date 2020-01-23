46056f664ceaf31c05d5e3b67e8722bed67fb630
PangJiaDa <pangjiada52@gmail.com>
2020-01-23 20:50:01 +0800

    completed level 1, greet, echo and exit functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,34 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
+    private static void hLine() {
+        System.out.println("____________________________________________________________");
+    }
+    private static void printWelcomeMessage() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("testing");
+        hLine();
+        System.out.println(logo);
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        hLine();
+    }
+    private static void printWithHLine(String message) {
+        hLine();
+        System.out.println(message);
+        hLine();
+    }
+    public static void main(String[] args) {
+        printWelcomeMessage();
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        while (!input.equals("bye")) {
+            printWithHLine(input);
+            input = scanner.nextLine();
+        }
+        printWithHLine("Bye. Hope to see you again soon!");
     }
 }
