8518165d1031567cb80d4b80adfb901ac07b4c62
yuetong <leeyuetong@gmail.com>
2020-01-23 19:55:58 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,19 +2,27 @@ import java.util.*;
 
 public class Duke {
     public static void main(String[] args) {
+        ArrayList<String> lst = new ArrayList<String>();
         Scanner sc = new Scanner(System.in);
         System.out.println("Hey! I'm Duke!");
         System.out.println("What can I help you with?");
         boolean done = false;
         while (done != true) {
             String output = sc.nextLine();
-            if (!output.equals("bye")) {
-                System.out.println(output + "!");
-            } else {
+            if (output.equals("bye")) {
+                System.out.println("Bye! See you around:)");
                 done = true;
+            } else if (output.equals("list")) {
+                int counter = 1;
+                for (int i = 0; i < lst.size(); i++) {
+                    System.out.println(counter +". " + lst.get(i));
+                    counter++;
+                }
+            } else {
+                System.out.println("added: " + output);
+                lst.add(output);
             }
         }
-        System.out.println("Bye!! Hope to see you again soon!");
         sc.close();
     }
 }
