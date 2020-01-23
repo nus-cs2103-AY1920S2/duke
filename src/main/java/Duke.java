e9e7cfa1d849d15ed0893b86957f0465e3a052fd
garysyndromes <garylimky@hotmail.com>
2020-01-23 11:07:36 +0800

    added support for tracking three types of task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,7 +16,6 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         String exitCommand = "bye";
         ArrayList<Task> listOfText = new ArrayList<Task>();
-        int counter = 1;
 
         while (sc.hasNext()) {
             String input = sc.nextLine();
@@ -25,11 +24,17 @@ public class Duke {
             }
 
             if (input.toLowerCase().equals("list")) {
-                for (int i = 0; i < listOfText.size(); i++) {
-                    System.out.println(listOfText.get(i));
+                int counter = 1;
+                if (listOfText.size() == 0) {
+                    System.out.println("To Do List is empty! Congratulations!");
                 }
-                continue;
+                for (int i = 0; i < listOfText.size(); i++) {
+                    System.out.println(counter + ". " + listOfText.get(i));
+                    counter++;
+                }
+
             }
+
             String[] splitStr = input.split("\\s+");
             if (splitStr[0].toLowerCase().equals("done")) {
                 int index = Integer.parseInt(splitStr[1]) - 1;
@@ -37,14 +42,65 @@ public class Duke {
                 currentTask.markAsDone();
                 System.out.println("Nice! I've marked this task as done and dusted:");
                 System.out.println(currentTask);
-                continue;
 
             }
-            System.out.println("added: " + input);
-            Task t = new Task(counter + ". " + input);
-            listOfText.add(t);
-            counter++;
 
+            if (splitStr[0].toLowerCase().equals("deadline")) {
+                String date = "";
+                String deadline = "";
+                for (int i = 1; i < splitStr.length; i++) {
+                    if ((splitStr[i].equals("/by"))) {
+                        date = splitStr[i + 1];
+                        break;
+                    } else {
+                        deadline += splitStr[i] + " ";
+                    }
+                }
+                deadline = deadline.substring(0, deadline.length() - 1);
+                Deadline d = new Deadline(deadline, date);
+                listOfText.add(d);
+                System.out.println("Got you covered! Added this task to the list: ");
+                System.out.println(d);
+                System.out.println("Now you have " + listOfText.size() + " tasks in the list.");
+
+            }
+
+            if (splitStr[0].toLowerCase().equals("event")) {
+                //handle date
+                String at = "";
+                String event = "";
+                for (int i = 1; i < splitStr.length; i++) {
+                    if ((splitStr[i].equals("/at"))) {
+                        for (int j = i + 1; j < splitStr.length; j++) {
+                            at += splitStr[j] + " ";
+                        }
+                        break;
+                    } else {
+                        event += splitStr[i] + " ";
+                    }
+                }
+                event = event.substring(0, event.length() - 1);
+                at = at.substring(0,at.length() - 1);
+
+                Event e = new Event(event, at);
+                listOfText.add(e);
+                System.out.println("Got you covered! Added this task to the list: ");
+                System.out.println(e);
+                System.out.println("Now you have " + listOfText.size() + " tasks in the list.");
+            }
+
+            if (splitStr[0].toLowerCase().equals("todo")) {
+                String todo = "";
+                for (int i = 1; i < splitStr.length; i ++) {
+                    todo += splitStr[i] + " ";
+                }
+                todo.substring(0,todo.length() - 1);
+                Todo t = new Todo(todo);
+                listOfText.add(t);
+                System.out.println("Got you covered! Added this task to the list: ");
+                System.out.println(t);
+                System.out.println("Now you have " + listOfText.size() + " tasks in the list.");
+            }
 
         }
 
