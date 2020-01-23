109fbd71ee97da099d570d4270c492c4be275f77
Li Zi Ying <ziying338@gmail.com>
2020-01-23 13:36:04 +0800

    Completed Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner scanner = new Scanner(System.in);
+        String line = "\n_____________________ \n";
+
+        System.out.println(line + "Hello! I'm Duke \nWhat can I do for you?" + line);
+
+        while (scanner.hasNext()) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(line + "Bye. Hope to see you again soon!" + line);
+                break;
+            } else {
+                System.out.println(line + " > " + input + line);
+            }
+        }
+        scanner.close();
+        // String logo = " ____        _        \n"
+        //         + "|  _ \\ _   _| | _____ \n"
+        //         + "| | | | | | | |/ / _ \\\n"
+        //         + "| |_| | |_| |   <  __/\n"
+        //         + "|____/ \\__,_|_|\\_\\___|\n";
+        // System.out.println("Hello from\n" + logo);
     }
 }
