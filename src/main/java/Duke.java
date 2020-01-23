173dc442bfebf45a7b7a40ba7c4310decd411175
seanlowcy77 <seanlowcy77@Seans-MacBook-Pro-4.local>
2020-01-23 18:01:01 +0800

    Add Error handling for incorrect inputs

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,6 +11,7 @@ public class Duke {
         }
     }
 
+
     public void run() {
 
         System.out.println("Hello ! I'm Ashley Bot\nOi What u want\n");
@@ -33,62 +34,153 @@ public class Duke {
             } else if (str.equals("list")) {
                 printList();
 
-            } else if (str.contains("done ")) {
+            } else if (str.contains("done")) {
+                done(str);
 
-                str = str.substring(5);
-                str = str.trim();
-                int i = Integer.parseInt(str) - 1;
+            } else if (str.contains("todo")) {
+                todo(str);
 
-                if (!list[i].isDone) {
-                    list[i].isDone = true;
-                    System.out.println("Nice! You have done this:\n" + list[i].toString());
-                } else {
-                    System.out.println("You have already done\n" + list[i].toString() + "\nNo need to do it again!");
-                }
-
-            } else if (str.contains("todo ")) {
-
-                str = str.substring(5);
-                str = str.trim();
-                Todo todo = new Todo(str);
-                list[latest_index++] = todo;
-
-                System.out.println("Got it. I have added this task:\n" + todo.toString() +
-                        "\nNow you have a total of " + latest_index + " Tasks in your list");
-
-            } else if (str.contains("deadline ")) {
-
-                str = str.substring(9);
-                str = str.trim();
-                String[] strings = str.split("/by");
-
-                Deadline deadline = new Deadline(strings[0],strings[1]);
-                list[latest_index++] = deadline;
-
-                System.out.println("Got it. I have added this task:\n" + deadline.toString() +
-                        "\nNow you have a total of " + latest_index + " Tasks in your list");
+            } else if (str.contains("deadline")) {
+                deadline(str);
 
             } else if (str.contains("event ")) {
-
-                str = str.substring(6);
-                str = str.trim();
-                String[] strings = str.split("/at");
-
-                Event event = new Event(strings[0],strings[1]);
-                list[latest_index++] = event;
-
-                System.out.println("Got it. I have added this task:\n" + event.toString() +
-                        "\nNow you have a total of " + latest_index + " Tasks in your list");
-
-
+                event(str);
 
             } else {
-                Task task = new Task(str);
-                list[latest_index++] = task;
-                System.out.println("General task added: " + str);
+                System.out.println("OOPs Idk what that means :/\n Try 'todo', 'event' or 'deadline' commands instead!");
 
             }
             System.out.println("--------------------------------------------------------------\n\n");
         }
+    }
+
+
+    public void todo(String string) {
+
+        int repeat = checkRepeats(string,"todo");
+
+        if (repeat > 1) {
+            System.out.println("Please use 'todo' only once!");
+            return;
+        }
+
+        string = string.replace("todo", "");
+        string = string.trim();
+
+
+        if (checkEmpty(string)) {
+            return;
+        }
+        Todo todo = new Todo(string);
+        list[latest_index++] = todo;
+
+        System.out.println("Got it. I have added this task:\n" + todo.toString() +
+                "\nNow you have a total of " + latest_index + " Tasks in your list");
+    }
+
+
+    public void done(String string) {
+        string = string.replace("done","");
+        string = string.trim();
+        try {
+            int i = Integer.parseInt(string) - 1;
+
+            if (!list[i].isDone) {
+                list[i].isDone = true;
+                System.out.println("Nice! You have done this:\n" + list[i].toString());
+            } else {
+                System.out.println("You have already done\n" + list[i].toString() + "\nNo need to do it again!");
+            }
+        } catch (NullPointerException e) {
+            System.out.println("There is no such number on your List! :/");
+
+        } catch (NumberFormatException e) {
+            System.out.println("Please input a number instead!");
+        }
+    }
+
+
+    public void deadline(String string) {
+
+        int repeat = checkRepeats(string,"/by");
+
+        if (repeat > 1) {
+            System.out.println("Please use '/by' only once!");
+            return;
+        }
+
+        string = string.replace("deadline","");
+        string = string.trim();
+
+        String[] strings = string.split("/by");
+
+        try {
+            if (checkEmpty(strings[0])) {
+                return;
+            }
+
+            Deadline deadline = new Deadline(strings[0], strings[1]);
+            list[latest_index++] = deadline;
+
+            System.out.println("Got it. I have added this task:\n" + deadline.toString() +
+                    "\nNow you have a total of " + latest_index + " Tasks in your list");
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Please give me an approximate timing using '/by'!");
+        }
+    }
+
+
+    public void event(String string) {
+
+        int repeat = checkRepeats(string,"/at");
+
+        if (repeat > 1) {
+            System.out.println("Please use '/at' only once!");
+            return;
+        }
+
+        string = string.replace("event","");
+        string = string.trim();
+
+        String[] strings = string.split("/at");
+
+        try {
+            if (checkEmpty(strings[0])) {
+                return;
+            }
+
+            Event event = new Event(strings[0],strings[1]);
+            list[latest_index++] = event;
+
+            System.out.println("Got it. I have added this task:\n" + event.toString() +
+                    "\nNow you have a total of " + latest_index + " Tasks in your list");
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Please give me an approximate timing using '/at'!");
+
+        }
+    }
+
+    public int checkRepeats(String string, String repeat) {
+
+        int count = 0;
+        String[] strings = string.split(" ");
+        for (String s : strings) {
+            if (s.equals(repeat)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+    public boolean checkEmpty(String cmd) {
+
+        if (cmd.isEmpty()) {
+            System.out.println("Please do not give an empty command :(");
+            return true;
+        }
+        return false;
     }
 }
