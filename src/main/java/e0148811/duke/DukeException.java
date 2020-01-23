b258b340a16ff93aa21ad074aa8190875fc2df74
Liao Meng E0148811 <lyhlm5@hotmail.com>
2020-01-23 23:55:36 +0800

    Refine exception message

--- src/main/java/e0148811/duke/DukeException.java
+++ src/main/java/e0148811/duke/DukeException.java
@@ -2,6 +2,6 @@ package e0148811.duke;
 
 public class DukeException extends Exception {
     public DukeException(String message) {
-        super(message);
+        super("ERROR. Please refer to the following guide and try again:\n" + message);
     }
 }
