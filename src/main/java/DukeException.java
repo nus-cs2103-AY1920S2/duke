3b04c1b05c2a214b7db4de06b03bd063300f078c
TheFirstCouchPotato97 <39809238+TheFirstCouchPotato97@users.noreply.github.com>
2020-01-23 22:18:43 +0800

    Add custom exception type
    This program's exceptions will be subclasses of DukeException.

---
+++ src/main/java/DukeException.java
@@ -0,0 +1,10 @@
+public class DukeException extends Exception {
+    /**
+     * Constructs a new custom exception.
+     *
+     * @param message the message of the exception.
+     */
+    public DukeException(String message) {
+        super(message);
+    }
+}
