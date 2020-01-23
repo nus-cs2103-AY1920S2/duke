3d7ce273b0df72e28575055cec991ef511167d68
buddhavineeth <buddhavineeth@gmail.com>
2020-01-23 22:22:42 +0800

    add task class and ability to mark as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,23 +11,27 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("May I know your name?");
-
         String name = scanner.nextLine();
-        ArrayList<String> storedInfo = new ArrayList<>();
+        ArrayList<Task> taskList = new ArrayList<>();
 
         while (true) {
             System.out.println("How may I help you " + name + "?");
             String command = scanner.nextLine();
             if (command.toLowerCase().equals("list")) {
-                for (int currNum = 1; currNum <= storedInfo.size(); currNum++) {
-                    System.out.println(currNum + ". " + storedInfo.get(currNum - 1));
+                for (int currNum = 1; currNum <= taskList.size(); currNum++) {
+                    System.out.println(currNum + ". " + taskList.get(currNum - 1));
                 }
             }
             else if (command.toLowerCase().equals("bye")) {
                 System.out.println("Adios amigo. It was my pleasure assisting you. Keep smiling " + name + ".");
                 break;
+            } else if (command.toLowerCase().split(" ")[0].equals("done")){
+                Task toComplete = taskList.get(Integer.parseInt(command.split(" ")[1]) - 1);
+                toComplete.completeStatus();
+                System.out.println("Hooray! You've finally managed to finish this task:");
+                System.out.println(toComplete);
             } else {
-                storedInfo.add(command);
+                taskList.add(new Task(command));
                 System.out.println("added: " + command);
             }
         }
---
+++ src/main/java/Task.java
@@ -0,0 +1,23 @@
+public class Task {
+    protected String identifier;
+    protected boolean isCompleted;
+
+    public Task(String identifier) {
+        this.identifier = identifier;
+        this.isCompleted = false;
+    }
+
+    public String getStatusIcon() {
+        return (isCompleted ? "\u2713" : "\u2717");
+    }
+
+    public boolean completeStatus() {
+        this.isCompleted = true;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + this.identifier;
+    }
+}
