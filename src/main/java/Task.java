5f5500e7f1541047044139fb8c3279d9a285c6fa
Firzan Armani <mrfirzanarmani@gmail.com>
2020-01-23 21:18:50 +0800

    Create Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,34 @@
+public class Task {
+    private String taskName;
+    private boolean taskDone;
+
+    public Task(String taskName) {
+        this.taskName = taskName;
+        this.taskDone = false;
+    }
+
+    public String getTaskName() {
+        return this.taskName;
+    }
+
+    public boolean getTaskDone() {
+        return this.taskDone;
+    }
+
+    public void setTaskDone() {
+        this.taskDone = true;
+    }
+
+    /**
+     * Return a string of the details of the current Task object.
+     * Contains the task done status and the task name.
+     *
+     * @return A String representation of the task details.
+     */
+    public String toString() {
+        return "[" + (getTaskDone()
+                ? Character.toString(0x2713)
+                : Character.toString(0x2717))
+            + "] " + getTaskName();
+    }
+}
