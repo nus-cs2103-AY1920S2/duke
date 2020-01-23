d70718c4fc2de262a0097577e04f50364d141b95
AdarshChugani <adarsh.chugani@gmail.com>
2020-01-23 19:17:15 +0800

    Level-2: Add,List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,9 @@ public class Duke {
         System.out.println("Hello, I'm Duke");
         System.out.println("What can I do for you?\n");
         Scanner sc = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int x = 0;
+        int y = 0;
         while(true) {
             String in = sc.nextLine();
             if (in.equals("bye")) {
@@ -12,8 +15,16 @@ public class Duke {
                 break;
             }
 
+            else if(in.equals("list")) {
+                for(y = 0; y < x; y++) {
+                    System.out.printf("%d. %s\n", y+1, tasks[y]);
+                }
+            }
+
             else {
-                System.out.println(in);
+                tasks[x] = in;
+                System.out.printf("added: %s\n", tasks[x]);
+                x++;
             }
         }
 
