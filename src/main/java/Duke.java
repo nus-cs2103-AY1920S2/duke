e5e6d4be6be12e6461a03dfb3f8b799e17b79537
ncslzh <ncslzh@gmail.com>
2020-01-23 18:06:08 +0800

    added ability to Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -15,14 +15,21 @@ public class Duke {
 
         Scanner sc = new Scanner(System.in);
         String input;
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         int numTasks = 0;
 
         while (!(input = sc.nextLine()).equals("bye")) {
             if (input.equals("list")) {
                 printList(list, numTasks);
+            } else if (input.substring(0, 4).equals("done")) {
+                // use substring to separate "done" and task number
+                int taskNum = Integer.parseInt(input.substring(5));
+                list[taskNum - 1].markAsDone();
+                System.out.println("I've finally done this task:");
+                System.out.println(list[taskNum - 1].toString());
             } else {
-                list[numTasks] = input;
+                Task newTask = new Task(input);
+                list[numTasks] = newTask;
                 numTasks++;
                 System.out.println("added: " + input + "\n");
             }
@@ -30,7 +37,7 @@ public class Duke {
         System.out.println("See you soon!");
     }
 
-    public static void printList(String[] list, int numTasks) {
+    public static void printList(Task[] list, int numTasks) {
         for (int i = 1; i <= numTasks; i++) {
             System.out.printf("%d. %s\n", i, list[i - 1]);
         }
