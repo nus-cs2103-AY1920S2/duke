8d91c34c4845c80822529283109fc081f340b22f
JingYing <e0323887@u.nus.edu>
2020-01-23 16:56:18 +0800

    Released Level-3

--- src/main/java/Lister.java
+++ src/main/java/Lister.java
@@ -1,25 +1,32 @@
 import java.util.ArrayList;
-import java.util.Iterator;
 
 public class Lister {
-    private ArrayList<String> store;
+    private ArrayList<Task> store;
 
     public Lister() {
         store = new ArrayList<>();
     }
 
     public void record(String command) {
+        int x = command.indexOf(' ');
         if (command.equals("list")) {
-            Iterator<String> iter = store.iterator();
-            int counter = 1;
-            while (iter.hasNext()) {
-                System.out.println(counter + ". " + iter.next());
-                counter++;
+            System.out.println("Here are the tasks in your list: ");
+            for (int i=0; i<store.size(); i++) {
+                System.out.println((i+1) + "." + returnTask(store.get(i)));
             }
+        } else if (command.substring(0,x).equals("done")) {
+            int y = Integer.valueOf(command.substring(x+1)) - 1;
+            store.get(y).markAsDone();
+            System.out.println("Nice! I've marked this task as done:");
+            System.out.println(returnTask(store.get(y)));
         } else {
-            store.add(command);
+            store.add(new Task(command));
             System.out.println("added: " + command);
         }
+    }
+
+    public String returnTask(Task task) {
+        return ("[" + task.getStatusIcon() + "] " + task.getDescription());
     }
 
 }
