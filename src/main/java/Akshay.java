64217d14305e70be54e8ff30ce21ab30d98a8d3f
gb3h <gabriel.yfy@gmail.com>
2020-01-23 15:38:09 +0800

    Level-3

--- src/main/java/Akshay.java
+++ src/main/java/Akshay.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class Akshay {
     private static String line = "~~~~~~~~~~~~~~~~~~~~~~~~";
+
     private static void say(String s) {
         System.out.println(line);
         System.out.println(s);
@@ -10,29 +11,33 @@ public class Akshay {
     }
 
     public static void main(String[] args) {
-
         say("Hello I am [AKSHAY]!\nHow may I help you?");
-
-
-        String[] arr = new String[100];
+        Task[] arr = new Task[100];
         Scanner sc =  new Scanner(System.in);
         String input = "";
         int count = 0;
         while (!input.equals("bye")) {
             input = sc.nextLine();
-            switch (input) {
+            String[] c = input.split(" ");
+            switch (c[0]) {
                 case ("list"):
                     System.out.println(line);
+                    System.out.println("Here are the items in your list:");
                     for (var i = 0; i < count; i++) {
-                        System.out.println(i + 1 + ": " + arr[i]);
+                        Task t = arr[i];
+                        System.out.println(i + 1 + ": " + t.toString());
                     }
                     System.out.println(line);
                     break;
+                case ("done"):
+                    Task curr = arr[Integer.parseInt(c[1]) - 1];
+                    curr.mark();
+                    say("Marked as done:\n" + curr.toString());
+                    break;
                 default:
-                    arr[count] = input;
+                    arr[count] = new Task(input);
                     count++;
                     say("Added: " + input);
-
             }
         }
         say("Bye! Hope to see you again!");
