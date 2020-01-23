3e227f8d3c17e4b0ebecea2b477e9f8e9cd9076c
yuetong <leeyuetong@gmail.com>
2020-01-23 20:16:55 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Duke {
     public static void main(String[] args) {
-        ArrayList<String> lst = new ArrayList<String>();
+        ArrayList<Task> lst = new ArrayList<Task>();
         Scanner sc = new Scanner(System.in);
         System.out.println("Hey! I'm Duke!");
         System.out.println("What can I help you with?");
@@ -15,14 +15,20 @@ public class Duke {
             } else if (output.equals("list")) {
                 int counter = 1;
                 for (int i = 0; i < lst.size(); i++) {
-                    System.out.println(counter +". " + lst.get(i));
+                    System.out.println(counter +".[" + lst.get(i).getStatusIcon() + "] " +lst.get(i).getD() );
                     counter++;
                 }
+            } else if (output.split(" ")[0].equals("done")) {
+                int space = Integer.parseInt(output.split(" ")[1]) -1;
+                lst.get(space).doAct();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("[" + lst.get(space).getStatusIcon() + "] " + lst.get(space).getD());
             } else {
                 System.out.println("added: " + output);
-                lst.add(output);
+                lst.add(new Task(output));
             }
         }
         sc.close();
     }
 }
+
