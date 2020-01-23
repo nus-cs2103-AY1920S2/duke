aa408b280b8f4e3e629866d27eb7592c86daf6b8
JingYing <e0323887@u.nus.edu>
2020-01-23 13:36:30 +0800

    Added Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,26 @@
+import java.util.Scanner;
+
 public class Duke {
 
-    public static void main(String[] args) {
+    public void echo() {
+        Scanner sc = new Scanner(System.in);
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello I am\n" + logo + "\nWhat can I do for you?");
+
+        while (sc.hasNext()) {
+            String command = sc.next();
+            if(command.equals("bye")) {
+                System.out.println("Bye! Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(command);
+            }
+        }
     }
+
 }
