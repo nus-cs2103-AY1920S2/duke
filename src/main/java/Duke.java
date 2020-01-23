6722a176bee640c3c373e5576fe85ac3e45d46bb
ncslzh <ncslzh@gmail.com>
2020-01-23 17:15:55 +0800

    added Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        runDuke();
+    }
+
+    public static void greeting() {
+        System.out.println("Hello! I'm Duchess");
+        System.out.println("What can I do for you?");
+    }
+
+    public static void runDuke() {
+        greeting();
+
+        Scanner sc = new Scanner(System.in);
+        String input;
+        while (!(input = sc.nextLine()).equals("bye")) {
+            System.out.println(input + "\n");
+        }
+        System.out.println("See you soon!");
     }
 }
