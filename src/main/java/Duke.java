46b691d8701371cfe81c22eb6fd7b7cbd509be2e
Ashleigh <ashleighaathar@gmail.com>
2020-01-23 16:11:10 +0800

    add ability to store input and display on request remove need to add 'dude' to each command

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,21 +1,29 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Duke {
     public static void main(String[] args) {
+        ArrayList<String> userInputList = new ArrayList<>();
         String logo = "  ____\n" + " (.__.)\n" + "<|>\n" + " /\\" + "\n_  _";
         Scanner sc = new Scanner(System.in);
         System.out.println(logo);
-        System.out.println("Dude, what do you want? \nHurry, reply with 'dude, <command>'!");
+        System.out.println("☛ Dude, what do you want? \n☛ Give me a command!");
+        System.out.print("dude, ");
         String userInput = sc.nextLine();
-        while (!userInput.equals("dude, bye")) {
-            if (!userInput.split(" ")[0].equals("dude,")) {
-                System.out.println("dude, wrong command!");
+        while (!userInput.equals("bye")) {
+            if (userInput.equals("list")) {
+                System.out.println("You asked to:");
+                for (String item : userInputList) {
+                    System.out.println("☛ " + item);
+                }
             }
             else {
-                System.out.println(userInput);
+                userInputList.add(userInput);
+                System.out.println("☛ fine, I will: " + userInput);
             }
+            System.out.print("dude, ");
             userInput = sc.nextLine();
         }
-        System.out.println("dude, stop wasting my time! shoo!");
+        System.out.println("☛ dude, stop wasting my time! shoo!");
 
     }
 }
