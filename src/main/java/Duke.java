35ead9362919ffa6723af75f19c0258d1e8a4614
duongphammmm <46336643+duongphammmm@users.noreply.github.com>
2020-01-24 00:13:37 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,19 +5,22 @@ public class Duke {
     public static Scanner sc = new Scanner(System.in);
     public static String machine = "Dude: ";
     public static String user = "dude: ";
-    public static ArrayList<String> list = new ArrayList<>();
+    public static ArrayList<Task> list = new ArrayList<>();
 
     public static void main(String[] args) {
         greeting();
-        String command = sc.nextLine();
+        String command = sc.next();
         while (!command.equals("bye")) {
             if (command.equals("list")) {
                 list();
+            } else if (command.equals("done")) {
+                markAsDone();
             } else {
+                command += sc.nextLine();
                 addTask(command);
             }
             System.out.print(user);
-            command = sc.nextLine();
+            command = sc.next();
         }
         System.out.println(machine + "Okay see ya dude!");
     }
@@ -38,7 +41,16 @@ public class Duke {
     }
 
     public static void addTask(String command) {
-        list.add(command);
-        System.out.println(machine + command + " is added");
+        Task task = new Task(command);
+        list.add(task);
+        System.out.println(machine + task + " is added");
     }
+    public static void markAsDone() {
+        int i = sc.nextInt();
+        Task task = list.get(i-1);
+        task.markAsDone();
+        System.out.println(machine + "Alright dude this task is marked as done:");
+        System.out.println("      " + i + ". " + task);
+    }
+
 }
