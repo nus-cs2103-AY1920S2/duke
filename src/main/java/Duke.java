e699920c90ed210d9585ce5c3334ab0bdb8cde16
Mikhail Faiz <mikhailfaiz@hotmail.com>
2020-01-23 18:22:55 +0800

    levelone

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.util.*;
+import java.util.logging.Level;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +9,8 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        Levelone lvl = new Levelone();
+        System.out.println(lvl.reply(sc.nextLine()));
     }
 }
--- src/main/java/Levelone.java
+++ src/main/java/Levelone.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class Levelone {
+
+    public Levelone(){}
+
+    public String reply(String s) {
+        String out = null;
+        if (s.equals("list")) {
+            out = "list";
+        } else if (s.equals("bye")) {
+            out = "Bye. Hope to see you again soon!";
+        } else {
+            out = s;
+        }
+        return out;
+    }
+
+
 }
