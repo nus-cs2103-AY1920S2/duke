71aba86e5a3d317c0728684788d1ea629c65eab4
duongphammmm <46336643+duongphammmm@users.noreply.github.com>
2020-01-24 00:01:02 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static Scanner sc = new Scanner(System.in);
+    public static String machine = "Dude: ";
+    public static String user = "dude: ";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greeting();
+        String command = sc.next();
+        while (!command.equals("bye")) {
+            System.out.println(machine + command);
+            System.out.print(user);
+            command = sc.next();
+        }
+        System.out.println(machine + "Okay see ya dude!");
+    }
+
+    public static void greeting() {
+        String welcome = "\n"
+                + machine + "Hi dude! I'm your Dude\n"
+                + "      What do you want dude?\n"
+                + user;
+        System.out.print(welcome);
     }
 }
