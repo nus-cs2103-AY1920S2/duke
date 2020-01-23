689973a7fab59a1acede5fda462b818b2a27a064
HemanshuGandhi <hemanshu.gandhi@gmail.com>
2020-01-23 23:13:02 +0800

    init skeletal version of Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,32 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(logo);
+
+        System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
+
+        Duke.runDuke();
+    }
+
+    // Echoes commands input by the user
+    // Terminates when user gives exit signal
+    private static void runDuke() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String nextInput = scanner.nextLine();
+            // Validation check
+            if (nextInput.equals("bye")) {
+                Duke.stopDuke();
+                break;
+            }
+            // Echo user command
+            System.out.println("    " + nextInput);
+        }
+    }
+
+    // Print a closing message before stopping Duke
+    private static void stopDuke() {
+        String closingMessage = "Bye. Hope to see you again soon!";
+        System.out.println("    " + closingMessage);
     }
 }
