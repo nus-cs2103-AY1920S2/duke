94e2c64d7daa501554867bc901c3a382ad7a86bb
grrrrnt <grantlee97@gmail.com>
2020-01-23 18:53:59 +0800

    Complete level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Duke {
-    static List<String> todo;
+    static List<Task> todo;
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -25,6 +25,9 @@ public class Duke {
             } else if (input.equals("bye")) {
                 exit();
                 break;
+            } else if (input.split(" ")[0].equals("done")) {
+                int taskNum = Integer.parseInt(input.split(" ")[1]);
+                done(taskNum);
             } else {
                 add(input);
             }
@@ -32,16 +35,23 @@ public class Duke {
     }
 
     public static void add(String s) {
-        todo.add(s);
+        todo.add(new Task(s));
         System.out.println("added: " + s);
     }
 
     public static void showList() {
+        System.out.println("Here are the tasks in your list:");
         int count = 1;
-        for (String task : todo) {
-            System.out.println(count + ". " + task);
+        for (Task t : todo) {
+            System.out.println(count + ".[" + t.getStatusIcon() + "] " + t);
             count++;
         }
+    }
+
+    public static void done(int i) {
+        todo.get(i-1).markAsDone();
+        System.out.println("Nice! I've marked this task as done: \n" +
+                "  [" + todo.get(i-1).getStatusIcon() + "] " + todo.get(i-1));
     }
 
     public static void exit() {
