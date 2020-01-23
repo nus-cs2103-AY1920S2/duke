227649b59f162b50f348e82f0cf6566f1c6a2ccc
gb3h <gabriel.yfy@gmail.com>
2020-01-23 15:14:22 +0800

    Level-2

--- src/main/java/Akshay.java
+++ src/main/java/Akshay.java
@@ -1,20 +1,40 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Akshay {
-    public static void main(String[] args) {
-        String line = "~~~~~~~~~~~~~~~~~~~~~~~~";
+    private static String line = "~~~~~~~~~~~~~~~~~~~~~~~~";
+    private static void say(String s) {
         System.out.println(line);
-        System.out.println("Hello from\n[A K S H A Y]\nHow may I help you?");
+        System.out.println(s);
         System.out.println(line);
+    }
 
+    public static void main(String[] args) {
+
+        say("Hello I am [AKSHAY]!\nHow may I help you?");
+
+
+        String[] arr = new String[100];
         Scanner sc =  new Scanner(System.in);
         String input = "";
+        int count = 0;
         while (!input.equals("bye")) {
             input = sc.nextLine();
-            System.out.println(line);
-            System.out.println(input);
-            System.out.println(line);
+            switch (input) {
+                case ("list"):
+                    System.out.println(line);
+                    for (var i = 0; i < count; i++) {
+                        System.out.println(i + 1 + ": " + arr[i]);
+                    }
+                    System.out.println(line);
+                    break;
+                default:
+                    arr[count] = input;
+                    count++;
+                    say("Added: " + input);
+
+            }
         }
-        System.out.println("Bye! Hope to see you again!");
+        say("Bye! Hope to see you again!");
     }
 }
