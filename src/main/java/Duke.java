081fadb47e34497ae63917df3846cf98ee4cd669
sheexiong <chen97xiong@yahoo.com>
2020-01-23 22:33:08 +0800

    Update for Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,26 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        String separator = "---------------------------------------------------------";
         System.out.println("Hello from\n" + logo);
+        System.out.println("What can I do for you? :)\n");
+        
+        Scanner scanner = new Scanner(System.in);
+
+        String input = "";
+        input = scanner.nextLine();
+
+        while (!input.equals("bye")) {
+            System.out.println(separator);
+            System.out.println(input);
+            System.out.println(separator);
+            input = scanner.nextLine();
+        }
+
+        // exit the app
+        System.out.println(separator);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(separator);
+
     }
 }
