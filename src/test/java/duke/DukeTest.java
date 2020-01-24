fb71dd3f87380cba45c0d271a7c1937ffacb05fe
Branson <branson.ng@u.nus.edu>
2020-01-24 22:27:34 +0800

    attempt at testing

---
+++ src/test/java/duke/DukeTest.java
@@ -0,0 +1,37 @@
+package duke;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DukeTest {
+  private final ByteArrayInputStream in = new ByteArrayInputStream(("Hello").getBytes());
+  private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+  private final PrintStream outContent = new PrintStream(out);
+
+  @BeforeEach
+  public void setUpStreams() {
+    System.setOut(outContent);
+  }
+
+  @Test
+  public void testBot() {
+    // ByteArrayInputStream in = new ByteArrayInputStream(
+    // ("Hello" + System.lineSeparator() + "2" + System.lineSeparator() + "uwu" +
+    // System.lineSeparator() + "bye")
+    // .getBytes());
+    Duke bot = new Duke();
+    bot.start();
+    System.setIn(in);
+    // assertEquals("Hello\n2\nuwu\nBye. Hope to see you again soon!\n",
+    // outContent.toString());
+    assertEquals("Hello\n", outContent.toString());
+
+  }
+
+}
