15ad533950796a636407e364d4137e591fe5b918
jiachen <jiachen247@gmail.com>
2020-01-24 18:15:42 +0800

    Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,31 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
+
+abstract class DukeException extends Exception {
+    public DukeException(String errMsg) {
+        super(errMsg);
+    }
+
+    @Override
+    public String toString(){
+        return "☹ OOPS!!! " + this.getMessage();
+    }
+}
+
+class InvalidDukeCommandException extends DukeException {
+    public InvalidDukeCommandException() {
+        super("I'm sorry, but I don't know what that means :-(");
+    }
+}
+
+class InvalidDukeFormatException extends DukeException {
+
+    public InvalidDukeFormatException(String errMsg) {
+        super(errMsg);
+    }
+}
+
 
 abstract class Task {
     protected String description;
@@ -20,14 +46,17 @@ abstract class Task {
 
     @Override
     public String toString(){
-        return "[" + this.getStatusIcon() + "] " + this.description;
+        return "[" + this.getStatusIcon() + "]" + this.description;
     }
 }
 
 class TodoTask extends Task {
 
-    public TodoTask(String description) {
+    public TodoTask(String description) throws InvalidDukeFormatException {
         super(description);
+        if(description.equals("")){
+            throw new InvalidDukeFormatException("The description of a todo cannot be empty.");
+        }
     }
 
     @Override
@@ -40,9 +69,13 @@ class DeadlineTask extends Task {
 
     private String by;
 
-    public DeadlineTask(String description, String deadline) {
+    public DeadlineTask(String description, String deadline) throws InvalidDukeFormatException {
         super(description);
         this.by = deadline;
+
+        if (deadline.isEmpty()) {
+            throw new InvalidDukeFormatException("Missing /by clause or missing by when!");
+        }
     }
 
     @Override
@@ -55,9 +88,13 @@ class EventTask extends Task {
 
     private String at;
 
-    public EventTask(String description, String at) {
+    public EventTask(String description, String at) throws InvalidDukeFormatException {
         super(description);
         this.at = at;
+
+        if (at.isEmpty()) {
+            throw new InvalidDukeFormatException("Missing /at clause or missing at when!");
+        }
     }
 
     @Override
@@ -67,6 +104,29 @@ class EventTask extends Task {
 }
 
 public class Duke {
+
+    static final String ExitCommand = "bye";
+    static final String ListCommand = "list";
+    static final String DoneCommand = "done";
+    static final String TodoCommand = "todo";
+    static final String DeadlineCommand = "deadline";
+    static final String EventCommand = "event";
+
+    static final String[] validCommands = {
+            ExitCommand,
+            ListCommand,
+            DoneCommand,
+            TodoCommand,
+            DeadlineCommand,
+            EventCommand
+    };
+
+    public static void verifyDukeCommand(String command) throws InvalidDukeCommandException {
+        if (!Arrays.asList(validCommands).contains(command)) {
+            throw new InvalidDukeCommandException();
+        }
+    }
+
     public static void addTaskReport(Task task, int numOfTasks){
         System.out.println("\t Got it. I've added this task: \n" +
                 "\t\t"+ task +"\n" +
@@ -76,13 +136,6 @@ public class Duke {
     public static void main(String[] args) {
 
         ArrayList<Task> tasks = new ArrayList<>();
-
-        final String ExitCommand = "bye";
-        final String ListCommand = "list";
-        final String DoneCommand = "done";
-        final String TodoCommand = "todo";
-        final String DeadlineCommand = "deadline";
-        final String EventCommand = "event";
 
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -97,19 +150,27 @@ public class Duke {
 
 
         Scanner scanner = new Scanner(System.in);
-        Task task;
         String desc = "";
         String timestamp = "";
-        String[] taskInfo;
 
         main:
         while (scanner.hasNextLine()) {
             System.out.println("\t____________________________________________________________");
             String line = scanner.nextLine().trim();
-            String[] seperatedLine = line.split(" ", 2);
-            String command = seperatedLine[0];
-            String parameters = seperatedLine.length > 1 ? seperatedLine[1] : "";
+            String[] separateLine = line.split(" ", 2);
+            String command = separateLine[0];
+            String parameters = separateLine.length > 1 ? separateLine[1] : "";
 
+            try {
+                verifyDukeCommand(command);
+            } catch (InvalidDukeCommandException invalidDukeCommandException) {
+                System.out.println("\t " + invalidDukeCommandException);
+                System.out.println("\t____________________________________________________________");
+                continue;
+            }
+
+            Task task;
+            String[] taskInfo;
             switch (command) {
                 case ExitCommand:
                     System.out.println("\t Bye. Hope to see you again soon!");
@@ -121,15 +182,36 @@ public class Duke {
                     }
                     break;
                 case DoneCommand:
-                    int taskId = Integer.parseInt(line.split(" ")[1]);
-                    task = tasks.get(taskId  - 1);
+                    try {
+                        String[] splited = line.split(" ");
+                        if(splited.length < 2){
+                            throw new InvalidDukeFormatException("The index of a done cannot be empty.");
+                        }
+                        int taskId = Integer.parseInt(splited[1]);
+                        if (taskId <= 0 || taskId > tasks.size()){
+                            throw new InvalidDukeFormatException("Invalid task index provided!");
+                        }
+
+                        task = tasks.get(taskId  - 1);
+                    } catch (InvalidDukeFormatException | ArrayIndexOutOfBoundsException e) {
+                        System.out.println("\t " + e);
+                        System.out.println("\t____________________________________________________________");
+                        continue;
+                    }
+
                     task.markAsDone();
                     System.out.println("\t Nice! I've marked this task as done: ");
                     System.out.println("\t\t" + task);
                     break;
 
                 case TodoCommand:
-                    task = new TodoTask(parameters);
+                    try {
+                        task = new TodoTask(parameters);
+                    } catch (InvalidDukeFormatException e) {
+                        System.out.println("\t " + e);
+                        System.out.println("\t____________________________________________________________");
+                        continue;
+                    }
                     tasks.add(task);
                     addTaskReport(task, tasks.size());
                     break;
@@ -143,7 +225,13 @@ public class Duke {
                     if (taskInfo.length > 1 ){
                         timestamp = taskInfo[1].trim();
                     }
-                    task = new DeadlineTask(desc, timestamp);
+                    try {
+                        task = new DeadlineTask(desc, timestamp);
+                    } catch (InvalidDukeFormatException e) {
+                        System.out.println("\t " + e);
+                        System.out.println("\t____________________________________________________________");
+                        continue;
+                    }
                     tasks.add(task);
                     addTaskReport(task, tasks.size());
                     break;
@@ -157,13 +245,16 @@ public class Duke {
                     if (taskInfo.length > 1 ){
                         timestamp = taskInfo[1].trim();
                     }
-                    task = new EventTask(desc, timestamp);
+                    try {
+                        task = new EventTask(desc, timestamp);
+                    } catch (InvalidDukeFormatException e) {
+                        System.out.println("\t " + e);
+                        System.out.println("\t____________________________________________________________");
+                        continue;
+                    }
                     tasks.add(task);
                     addTaskReport(task, tasks.size());
                     break;
-
-                default:
-                    System.out.println("\t Unknown command! Please try again!");
             }
 
             System.out.println("\t____________________________________________________________");
