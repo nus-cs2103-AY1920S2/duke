9df156341398db11232421736a2233fa0b2f77b7
Sanchari Bandyopadhyay <sancharizxc@gmail.com>
2020-01-24 15:42:18 +0800

    Completed A-UiTesting

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -12,7 +12,7 @@ then
     rm ACTUAL.TXT
 fi
 
-# compile the code into the bin folder, terminates if error occurred
+# compile the code into the bin folder, terminates if error occurred-
 if ! javac -cp ../src -Xlint:none -d ../bin ../src/main/java/*.java
 then
     echo "********** BUILD FAILURE **********"
