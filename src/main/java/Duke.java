d983452cfa7bb15533dbd4e4b635fe7cd99c4120
teriaiw <50551345+teriaiw@users.noreply.github.com>
2020-01-24 16:52:35 +0800

    add Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "     ____        _        \n"
+                + "     |  _ \\ _   _| | _____ \n"
+                + "     | | | | | | | |/ / _ \\\n"
+                + "     | |_| | |_| |   <  __/\n"
+                + "     |____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("    -----------------------------------\n"
+                + "      Hello! I'm\n" + logo);
+        System.out.println("      What can I do for you?\n"
+                + "    -----------------------------------");
+
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String input = sc.nextLine();
+            if (!input.equals("bye")) {
+                System.out.println("    -----------------------------------\n"
+                        + "      " + input +"\n"
+                        + "    -----------------------------------");
+            } else {
+                System.out.println("    -----------------------------------\n"
+                        + "      Bye. Hope to see you again soon!\n"
+                        + "    -----------------------------------");
+                System.exit(0);
+            }
+        }
     }
 }
