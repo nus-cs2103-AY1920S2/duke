254e72b1f52eb40416030b078dd89154ac7746aa
Zhang Yuanyu <e0322803@u.nus.edu>
2020-01-24 12:57:59 +0800

    Level 7 Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,122 +1,182 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Duke {
+    private static void loadFileContents(String filePath , ArrayList<Task> tasks) throws FileNotFoundException {
+        File f = new File(filePath); // create a File for the given file path
+        Scanner s = new Scanner(f); // create a Scanner using the File as the source
+        while (s.hasNext()) {
+            String nextLine = s.nextLine();
+            String[] taskString = nextLine.split("/");
+            String taskTitle = taskString[0];
+            switch (taskTitle){
+                case "T":
+                    Task todoTask = new Todo(taskString[2]);
+                    if(taskString[1].equals("1")){
+                        todoTask.markDone();
+                    }
+                    tasks.add(todoTask);
+                    break;
+
+                case "D":
+                    Task deadlineTask = new Deadline(taskString[2], taskString[3]);
+                    if(taskString[1].equals("1")){
+                        deadlineTask.markDone();
+                    }
+                    tasks.add(deadlineTask);
+                    break;
+
+                case "E":
+                    Task eventTask = new Event(taskString[2], taskString[3]);
+                    if(taskString[1].equals("1")){
+                        eventTask.markDone();
+                    }
+                    tasks.add(eventTask);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+
+    private static void appendToFile(String filePath, String textToAppend) throws IOException {  //append to file
+        FileWriter fw = new FileWriter(filePath, true); // create a FileWriter in append mode
+        fw.write(textToAppend);
+        fw.close();
+    }
+
+    private static void writeToFile(String filePath, String textToAdd) throws IOException { //overwrite file
+        FileWriter fw = new FileWriter(filePath);
+        fw.write(textToAdd);
+        fw.close();
+    }
+
+    public static ArrayList<Task> tasks = new ArrayList<>();
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        /*String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);*/
         String line = "    ____________________________________________________________" + "\n";
         String fiveSpaces = "      ";
         System.out.println(line + fiveSpaces + "Hello! I'm Duke\n" + fiveSpaces + "Whatcha wanna do?\n" + line);
-        ArrayList<Task> tasks = new ArrayList<>();
-        boolean continueDoLoop = true;
-        do {
-            try {
-                while (scanner.hasNextLine()) {
-                    Scanner commandScanner = new Scanner(scanner.nextLine());
-                    String word = commandScanner.next();
-                    if (word.equals("bye")) {
-                        System.out.println(line + fiveSpaces + "See ya later alligator!\n" + line);
-                        continueDoLoop = false;
-                        break;
-                    } else {
-                        String taskName = "";
-                        switch (word) {
-                            case "list":
-                                System.out.print(line);
-                                System.out.println("     Here are the tasks in your list:");
-                                /*for (Task task : tasks) {
-                                    System.out.println("      " + Integer.toString(task.thisTaskNum) + "." + task);
-                                }*/
-                                for(int i = 0; i < tasks.size(); i++){
-                                    System.out.println("      " + Integer.toString(i + 1) + "." + tasks.get(i));
-                                }
-                                System.out.println(line);
-                                break;
+        String filePath = "/Users/freddy/Desktop/duke/src/main/java/tasks.txt";
+        try{
+            loadFileContents(filePath, tasks);
+        }
+        catch (FileNotFoundException ex){
+            System.out.println("File not found");
+        }
 
-                            case "done":
-                                int doneTaskNum = Integer.parseInt(commandScanner.next()) - 1;
-                                tasks.get(doneTaskNum).isDone = true;
-                                System.out.print(line);
-                                System.out.println("     Nice! I've marked this task as done:");
-                                System.out.println("       " + tasks.get(doneTaskNum).toString());
-                                System.out.println(line);
-                                break;
+        while (scanner.hasNextLine()) {
+            try{
+                Scanner commandScanner = new Scanner(scanner.nextLine());
+                String word = commandScanner.next();
+                if (word.equals("bye")) {
+                    System.out.println(line + fiveSpaces + "See ya later alligator!\n" + line);
+                    break;
+                } else {
+                    String taskName = "";
+                    switch (word) {
+                        case "list":
+                            System.out.print(line);
+                            System.out.println("     Here are the tasks in your list:");
+                            for (int i = 0; i < tasks.size(); i++) {
+                                System.out.println("      " + Integer.toString(i + 1) + "." + tasks.get(i));
+                            }
+                            System.out.println(line);
+                            break;
 
-                            case "delete":
-                                int deleteTaskNum = Integer.parseInt(commandScanner.next()) - 1;
-                                System.out.print(line);
-                                System.out.println("     Noted. I've removed this task:");
-                                System.out.println("       " + tasks.get(deleteTaskNum).toString());
-                                tasks.remove(deleteTaskNum);
-                                System.out.println("     Now you have " + Integer.toString(tasks.size()) + " tasks in the list.");
-                                System.out.println(line);
-                                break;
+                        case "done":
+                            int doneTaskNum = Integer.parseInt(commandScanner.next()) - 1;
+                            tasks.get(doneTaskNum).isDone = true;
+                            System.out.print(line);
+                            System.out.println("     Nice! I've marked this task as done:");
+                            System.out.println("       " + tasks.get(doneTaskNum).toString());
+                            System.out.println(line);
+                            writeToFile(filePath, Task.toStringDukeTasks(tasks));
+                            break;
 
-                            case "todo":
-                                if (!commandScanner.hasNext()) {
-                                    throw new DukeException(word);
-                                } else {
-                                    while (commandScanner.hasNext()) {
-                                        taskName += commandScanner.next() + " ";
-                                    }
-                                    taskName = taskName.substring(0, taskName.length() - 1);
-                                    Task todoTask = new Todo(taskName);
-                                    tasks.add(todoTask);
-                                    System.out.print(line);
-                                    System.out.println("     Got it. I've added this task:");
-                                    System.out.println("       " + todoTask.toString());
-                                    System.out.println("     Now you have " + Integer.toString(tasks.size()) + " tasks in the list.");
-                                    System.out.println(line);
-                                    break;
-                                }
+                        case "delete":
+                            int deleteTaskNum = Integer.parseInt(commandScanner.next()) - 1;
+                            System.out.print(line);
+                            System.out.println("     Noted. I've removed this task:");
+                            System.out.println("       " + tasks.get(deleteTaskNum).toString());
+                            tasks.remove(deleteTaskNum);
+                            System.out.println("     Now you have " + Integer.toString(tasks.size()) + " tasks in the list.");
+                            System.out.println(line);
+                            writeToFile(filePath, Task.toStringDukeTasks(tasks));
+                            break;
 
-                            case "deadline":
+                        case "todo":
+                            if (!commandScanner.hasNext()) {
+                                throw new DukeException(word);
+                            } else {
                                 while (commandScanner.hasNext()) {
                                     taskName += commandScanner.next() + " ";
                                 }
                                 taskName = taskName.substring(0, taskName.length() - 1);
-                                String deadlineTask[] = taskName.split("/by ");
-                                Task newDeadLineTask = new Deadline(deadlineTask[0], deadlineTask[1]);
-                                tasks.add(newDeadLineTask);
+                                Task todoTask = new Todo(taskName);
+                                tasks.add(todoTask);
                                 System.out.print(line);
                                 System.out.println("     Got it. I've added this task:");
-                                System.out.println("       " + newDeadLineTask.toString());
+                                System.out.println("       " + todoTask.toString());
                                 System.out.println("     Now you have " + Integer.toString(tasks.size()) + " tasks in the list.");
                                 System.out.println(line);
+                                appendToFile(filePath, todoTask.toStringTaskstxt());
                                 break;
+                            }
 
-                            case "event":
-                                while (commandScanner.hasNext()) {
-                                    taskName += commandScanner.next() + " ";
-                                }
-                                taskName = taskName.substring(0, taskName.length() - 1);
-                                String eventTask[] = taskName.split("/at ");
-                                Task newEventTask = new Event(eventTask[0], eventTask[1]);
-                                tasks.add(newEventTask);
-                                System.out.print(line);
-                                System.out.println("     Got it. I've added this task:");
-                                System.out.println("       " + newEventTask.toString());
-                                System.out.println("     Now you have " + Integer.toString(tasks.size()) + " tasks in the list.");
-                                System.out.println(line);
-                                break;
+                        case "deadline":
+                            while (commandScanner.hasNext()) {
+                                taskName += commandScanner.next() + " ";
+                            }
+                            taskName = taskName.substring(0, taskName.length() - 1);
+                            String deadlineTask[] = taskName.split("/by ");
+                            Task newDeadLineTask = new Deadline(deadlineTask[0], deadlineTask[1]);
+                            tasks.add(newDeadLineTask);
+                            System.out.print(line);
+                            System.out.println("     Got it. I've added this task:");
+                            System.out.println("       " + newDeadLineTask.toString());
+                            System.out.println("     Now you have " + Integer.toString(tasks.size()) + " tasks in the list.");
+                            System.out.println(line);
+                            appendToFile(filePath, newDeadLineTask.toStringTaskstxt());
+                            break;
 
-                            default:
-                                throw new DukeException("");
-                        }
+                        case "event":
+                            while (commandScanner.hasNext()) {
+                                taskName += commandScanner.next() + " ";
+                            }
+                            taskName = taskName.substring(0, taskName.length() - 1);
+                            String eventTask[] = taskName.split("/at ");
+                            Task newEventTask = new Event(eventTask[0], eventTask[1]);
+                            tasks.add(newEventTask);
+                            System.out.print(line);
+                            System.out.println("     Got it. I've added this task:");
+                            System.out.println("       " + newEventTask.toString());
+                            System.out.println("     Now you have " + Integer.toString(tasks.size()) + " tasks in the list.");
+                            System.out.println(line);
+                            appendToFile(filePath, newEventTask.toStringTaskstxt());
+                            break;
+
+                        default:
+                            throw new DukeException("");
+
                     }
                 }
-            } catch (DukeException ex) {
+            }
+            catch (DukeException ex) {
                 System.out.print(line);
                 System.out.println(ex);
                 System.out.println(line);
             }
-        } while(continueDoLoop);
+            catch (IOException io) {
+                System.out.println("file not found");
+            }
+        }
     }
 }
 
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,26 +1,51 @@
+import java.util.ArrayList;
+
 public class Task {
         protected String description;
         protected boolean isDone;
-        /*protected static int taskNum = 0;
-        protected int thisTaskNum = 0;*/
 
         public Task(String description) {
             this.description = description;
             this.isDone = false;
-            /*taskNum++;
-            thisTaskNum = taskNum;*/
         }
 
         public String getStatusIcon() {
             return (isDone ? "[Done] " : "[Not Done] "); //return tick or X symbols
         }
 
+        public String getStatusIconInBin(){
+            return (isDone ? "1" : "0"); //1 means done, 0 means not done
+        }
+
         public void markDone(){
             this.isDone = true;
         }
 
-        @Override
-        public String toString(){
-            return getStatusIcon() + description;
+    public static String toStringDukeTasks(ArrayList<Task> tasks) {
+        String taskString = "";
+        for (Task task : tasks) {
+            if (task instanceof Todo) {
+                taskString += "T/" + task.getStatusIconInBin() + "/";
+                taskString += task.description + "\n";
+            }
+            else if (task instanceof Deadline) {
+                taskString += "D/" + task.getStatusIconInBin() + "/";
+                taskString += task.description + "/" + ((Deadline) task).day + "\n";
+            }
+            else {               //instance of event
+                taskString += "E/" + task.getStatusIconInBin() + "/";
+                taskString += task.description + "/" + ((Event) task).dayAndDuration + "\n";
+            }
         }
+        return taskString;
+    }
+
+    public String toStringTaskstxt(){
+            return "";
+    }
+
+    @Override
+    public String toString() {
+        return getStatusIcon() + description;
+    }
 }
