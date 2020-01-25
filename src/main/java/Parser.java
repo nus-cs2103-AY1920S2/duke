f63d06216e943df037ebc2e8a0e47eefaa0f0db7
chifa3 <foochifa@gmail.com>
2020-01-25 10:21:21 +0800

    modify dateParser to throw BadDateException

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -7,14 +7,12 @@ public class Parser {
     public static DateTimeFormatter DATE_FORMATTER =
             DateTimeFormatter.ofPattern("dd MMM uuuu");
 
-    public static LocalDate dateParser(String str) {
+    public static LocalDate dateParser(String str) throws BadDateException {
         try {
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MM uu");
             return LocalDate.parse(str, formatter);
         } catch (DateTimeParseException e) {
-            System.out.println("Sorry I don't recognise this date format!\n" +
-                    "Please make sure the format is: dd mm yy");
-            return null;
+            throw new BadDateException("Bad date format");
         }
     }
 }
