e53c03f2fa40f93b08c36a5d92e30a694d1cd573
JJiahui <jiangjiahui2048@gmail.com>
2020-01-25 20:26:06 +0800

    Level-6

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,15 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+
+
+    public static class DukeException extends Exception {
+        public DukeException(String msg){
+            super(msg);
+        }
+    }
+
+
     private ArrayList<Task> tasks = new ArrayList<>();
     private final String LINE = "\t__________________________________________________________";
     private void response(String... strs){
@@ -10,9 +19,6 @@ public class Duke {
             System.out.println("\t" + s);
         }
         System.out.println(LINE);
-    }
-    private void error(String s){
-        response("☹ OOPS!!! " + s);
     }
     public static void main(String[] args) {
         new Duke().start();
@@ -31,82 +37,91 @@ public class Duke {
             input = sc.nextLine().trim();
             inputSc = new Scanner(input);
             command = inputSc.next();
-            switch(command){
-                case "bye":
-                    response("Goodbye! Hope to hear from you soon :)");
-                    break mainLoop;
-                case "list":
-                    listTasks();
-                    break;
-                case "done":
-                    if (tasks.size() == 0){
-                        error("There are no tasks in the list :(");
+            try {
+                switch (command) {
+                    case "bye":
+                        response("Goodbye! Hope to hear from you soon :)");
+                        break mainLoop;
+                    case "list":
+                        listTasks();
                         break;
-                    }
-                    if (!inputSc.hasNextInt()){
-                        error("The index of the task is missing :/");
+                    case "done":
+                        if (tasks.size() == 0) {
+                            throw new DukeException("There are no tasks in the list :(");
+                        }
+                        if (!inputSc.hasNextInt()) {
+                            throw new DukeException("The index of the task is missing :/");
+                        }
+                        int index = inputSc.nextInt() - 1;
+                        if (index < 0 || index >= tasks.size()) {
+                            throw new DukeException("Please enter a valid index, from 1 to " + tasks.size());
+                        }
+                        Task t = tasks.get(index);
+                        t.markAsDone();
+                        response("Awesome! I've marked this task as done:", t.toString());
                         break;
-                    }
-                    int index = inputSc.nextInt() - 1;
-                    if (index < 0 || index >= tasks.size()){
-                        error("Please enter a valid index, from 1 to " + tasks.size());
+                    case "delete":
+                        if (tasks.size() == 0) {
+                            throw new DukeException("There are no tasks in the list :(");
+                        }
+                        if (!inputSc.hasNextInt()) {
+                            throw new DukeException("The index of the task is missing :/");
+                        }
+                        int i = inputSc.nextInt() - 1;
+                        if (i < 0 || i >= tasks.size()) {
+                            throw new DukeException("Please enter a valid index, from 1 to " + tasks.size());
+                        }
+                        Task task = tasks.get(i);
+                        tasks.remove(i);
+                        response("Noted. I've removed this task: ", task.toString());
                         break;
-                    }
-                    Task t = tasks.get(index);
-                    t.markAsDone();
-                    response("Awesome! I've marked this task as done:", t.toString());
-                    break;
-                case "todo":
-                    name = input.substring(command.length()).trim();
-                    if (name.length() == 0){
-                        error("The description of a todo cannot be empty.");
+                    case "todo":
+                        name = input.substring(command.length()).trim();
+                        if (name.length() == 0) {
+                            throw new DukeException("The description of a todo cannot be empty.");
+                        }
+                        Todo todo = new Todo(name);
+                        addTask(todo);
                         break;
-                    }
-                    Todo todo = new Todo(name);
-                    addTask(todo);
-                    break;
-                case "deadline":
-                    rest = input.substring(command.length()).trim();
-                    int byIndex = rest.indexOf("/by");
-                    if (byIndex == -1){
-                        error("Please include the deadline datetime after the \"/by\" keyword");
+                    case "deadline":
+                        rest = input.substring(command.length()).trim();
+                        int byIndex = rest.indexOf("/by");
+                        if (byIndex == -1) {
+                            throw new DukeException("Please include the deadline datetime after the \"/by\" keyword");
+                        }
+                        name = rest.substring(0, byIndex).trim();
+                        if (name.length() == 0) {
+                            throw new DukeException("The description of a deadline cannot be empty.");
+                        }
+                        datetime = rest.substring(byIndex + 3).trim();
+                        if (datetime.length() == 0) {
+                            throw new DukeException("The deadline datetime cannot be empty.");
+                        }
+                        Deadline dl = new Deadline(name, datetime);
+                        addTask(dl);
                         break;
-                    }
-                    name = rest.substring(0, byIndex).trim();
-                    if (name.length() == 0){
-                        error("The description of a deadline cannot be empty.");
+                    case "event":
+                        rest = input.substring(command.length()).trim();
+                        int atIndex = rest.indexOf("/at");
+                        if (atIndex == -1) {
+                            throw new DukeException("Please include the event datetime after the \"/at\" keyword");
+                        }
+                        name = rest.substring(0, atIndex).trim();
+                        if (name.length() == 0) {
+                            throw new DukeException("The description of a event cannot be empty.");
+                        }
+                        datetime = rest.substring(atIndex + 3).trim();
+                        if (datetime.length() == 0) {
+                            throw new DukeException("The event datetime cannot be empty.");
+                        }
+                        Event e = new Event(name, datetime);
+                        addTask(e);
                         break;
-                    }
-                    datetime = rest.substring(byIndex + 3).trim();
-                    if (datetime.length() == 0){
-                        error("The deadline datetime cannot be empty.");
-                        break;
-                    }
-                    Deadline dl = new Deadline(name, datetime);
-                    addTask(dl);
-                    break;
-                case "event":
-                    rest = input.substring(command.length()).trim();
-                    int atIndex = rest.indexOf("/at");
-                    if (atIndex == -1){
-                        error("Please include the event datetime after the \"/at\" keyword");
-                        break;
-                    }
-                    name = rest.substring(0, atIndex).trim();
-                    if (name.length() == 0){
-                        error("The description of a event cannot be empty.");
-                        break;
-                    }
-                    datetime = rest.substring(atIndex + 3).trim();
-                    if (datetime.length() == 0){
-                        error("The event datetime cannot be empty.");
-                        break;
-                    }
-                    Event e = new Event(name, datetime);
-                    addTask(e);
-                    break;
-                default:
-                    error("I'm sorry, but I don't know what that means :-(");
+                    default:
+                        throw new DukeException("I'm sorry, but I don't know what that means :-(");
+                }
+            } catch (DukeException e){
+                response("☹ OOPS!!! " + e.getMessage());
             }
         }
     }
