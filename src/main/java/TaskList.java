62161e8a660747bde021c7fe663a120574e78e9b
chifa3 <foochifa@gmail.com>
2020-01-25 13:30:08 +0800

    implement new getters in Tasklist

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,9 +1,39 @@
 import java.util.ArrayList;
 
 public class TaskList {
-    ArrayList<Task> tasks;
+    private ArrayList<Task> tasks;
+
+    public int getSize() {
+        return tasks.size();
+    }
+
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+
+    public Task getTask(int idx) {
+        return tasks.get(idx);
+    }
+
+    public void removeTask(int idx) {
+        tasks.remove(idx);
+    }
+
+    public ArrayList<Task> getList() {
+        return tasks;
+    }
+
+    public void printTask() {
+        for (Task task: tasks) {
+            System.out.println(task.getId() + "." + task);
+        }
+    }
 
     public TaskList(ArrayList<Task> tasks) {
         this.tasks = tasks;
+    }
+
+    public TaskList() {
+        tasks = new ArrayList<>();
     }
 }
