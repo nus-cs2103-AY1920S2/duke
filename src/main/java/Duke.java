985c958f19e7133abcae9c3a25b65c98eb7099d2
JJiahui <jiangjiahui2048@gmail.com>
2020-01-25 17:17:07 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,31 +1,47 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    private static final String LINE = "\t__________________________________________________________";
-    private static void response(String s){
+    private ArrayList<Task> tasks = new ArrayList<>();
+    private final String LINE = "\t__________________________________________________________";
+    private void response(String s){
         System.out.println(LINE);
         System.out.println("\t" + s);
         System.out.println(LINE);
     }
     public static void main(String[] args) {
+        new Duke().start();
+    }
+    private void start(){
         String logo = " ____        _        \n"
-                    + "|  _ \\ _   _| | _____ \n"
-                    + "| | | | | | | |/ / _ \\\n"
-                    + "| |_| | |_| |   <  __/\n"
-                    + "|____/ \\__,_|_|\\_\\___|\n";
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         Scanner sc = new Scanner(System.in);
         String input;
-        while (true){
+        mainLoop: while (true){
             input = sc.nextLine().trim();
-            if (input.equals("bye")){
-                response("Goodbye! Hope to hear from you soon :)");
-                break;
-            } else {
-                response(input);
+            switch(input){
+                case "bye":
+                    response("Goodbye! Hope to hear from you soon :)");
+                    break mainLoop;
+                case "list":
+                    listTasks();
+                    break;
+                default:
+                    tasks.add(new Task(input));
+                    response("added: " + input);
             }
         }
+    }
 
-
+    private void listTasks() {
+        System.out.println(LINE);
+        for (int i=0; i<tasks.size(); i++){
+            System.out.println("\t" + (i+1) + ": " + tasks.get(i));
+        }
+        System.out.println(LINE);
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,10 @@
+public class Task {
+    String name;
+    public Task(String name){
+        this.name = name;
+    }
+    @Override
+    public String toString(){
+        return name;
+    }
+}
