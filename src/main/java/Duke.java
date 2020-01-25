f093ab6962393edefc3f0318a5b74e3105a6879e
sigmund-c <sigmund@u.nus.edu>
2020-01-25 12:03:32 +0800

    add level.1 Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,55 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+
+        String charmander = "              _.--\"\"`-..\n" +
+                "            ,'          `.\n" +
+                "          ,'          __  `.\n" +
+                "         /|          \" __   \\\n" +
+                "        , |           / |.   .\n" +
+                "        |,'          !_.'|   |\n" +
+                "      ,'             '   |   |\n" +
+                "     /              |`--'|   |\n" +
+                "    |                `---'   |\n" +
+                "     .   ,                   |                       ,\".\n" +
+                "      ._     '           _'  |                    , ' \\ `\n" +
+                "  `.. `.`-...___,...---\"\"    |       __,.        ,`\"   L,|\n" +
+                "  |, `- .`._        _,-,.'   .  __.-'-. /        .   ,    \\\n" +
+                "-:..     `. `-..--_.,.<       `\"      / `.        `-/ |   .\n" +
+                "  `,         \"\"\"\"'     `.              ,'         |   |  ',,\n" +
+                "    `.      '            '            /          '    |'. |/\n" +
+                "      `.   |              \\       _,-'           |       ''\n" +
+                "        `._'               \\   '\"\\                .      |\n" +
+                "           |                '     \\                `._  ,'\n" +
+                "           |                 '     \\                 .'|\n" +
+                "           |                 .      \\                | |\n" +
+                "           |                 |       L              ,' |\n" +
+                "           `                 |       |             /   '\n" +
+                "            \\                |       |           ,'   /\n" +
+                "          ,' \\               |  _.._ ,-..___,..-'    ,'\n" +
+                "         /     .             .      `!             ,j'\n" +
+                "        /       `.          /        .           .'/\n" +
+                "       .          `.       /         |        _.'.'\n" +
+                "        `.          7`'---'          |------\"'_.'\n" +
+                "       _,.`,_     _'                ,''-----\"'\n" +
+                "   _,-_    '       `.     .'      ,\\\n" +
+                "   -\" /`.         _,'     | _  _  _.|\n" +
+                "    \"\"--'---\"\"\"\"\"'        `' '! |! /\n" +
+                "                            `\" \" -'";
+
+        System.out.println(charmander);
+        System.out.println("Char! Charmander!");
+
+        String command = sc.nextLine();
+
+        while (!command.equals("bye")){
+            System.out.println(command);
+
+            command = sc.nextLine();
+        }
+
+        System.out.println("Charmander! Char char!");
     }
 }
