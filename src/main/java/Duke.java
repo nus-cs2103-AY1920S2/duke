7a8e1b5733ebdc86f6940f6de66075710212b221
ShengXue97 <simshengxue1997@gmail.com>
2020-01-26 03:51:29 +0800

    Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,32 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static String indent = "    ";
+    private static String line = "    ____________________________________________________________";
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        String logo = indent + " ____        _        \n"
+                + indent + "|  _ \\ _   _| | _____ \n"
+                + indent + "| | | | | | | |/ / _ \\\n"
+                + indent + "| |_| | |_| |   <  __/\n"
+                + indent + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println(line + "\n" + indent + "Hello! I'm Snow\n" + indent + "What can I do for you?\n" + logo + line + "\n");
+
+        String input = "";
+        while (true) {
+            input = sc.nextLine();
+            if (input.equals("bye")){
+                System.out.println(line);
+                System.out.println(indent + "Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            } else {
+                System.out.println(line);
+                System.out.println(indent + input);
+                System.out.println(line);
+            }
+        }
+
+
     }
 }
