16365e0a5af5b36637d236c4ceb01d6bc38533a0
Ray-97 <rc97@u.nus.edu>
2020-01-25 18:30:30 +0800

    trying level 8

---
+++ src/main/java/DateTimeParser.java
@@ -0,0 +1,9 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateTimeParser {
+
+    private DateTimeFormatter defaultFormatter = DateTimeFormatter.ofPattern("yyyy MM dd");
+
+
+}
--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,20 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
     protected String by;
+    protected LocalDateTime dateTime;
+    private DateTimeFormatter defaultFormatter = DateTimeFormatter.ofPattern("yyyy MM dd");
 
     public Deadline(String description, String by) {
         super(description);
         this.by = by;
+        this.dateTime = LocalDateTime.parse(by, defaultFormatter);;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + dateTime.toString() + ")";
     }
 }
