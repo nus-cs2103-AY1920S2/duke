5a9ac5f32e7a3d7d0abb7d372d95c513dfa9b1b8
JeremyLoh <jeremyloh03@gmail.com>
2020-01-25 23:36:32 +0800

    Deadline: Changed due date type from String to LocalDate Event: Changed due date type from String to LocalDate

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,23 +1,27 @@
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 /**
  * Represent tasks that need to be done before a specific date/time
  * e.g., submit report by 11/10/2019 5pm
  */
 public class Deadline implements Task {
     protected String description;
-    protected String deadline;
+    protected LocalDate deadline;
     protected boolean isDone;
 
     public Deadline(String description, String deadline) {
         this(description, deadline, false);
     }
 
-    public Deadline(String description, String deadline, boolean isDone) {
+    public Deadline(String description, String deadline, boolean isDone) throws DateTimeException {
         this.description = description;
-        this.deadline = deadline;
+        this.deadline = LocalDate.parse(deadline);
         this.isDone = isDone;
     }
 
-    public String getDeadline() {
+    public LocalDate getDeadline() {
         return deadline;
     }
 
@@ -57,8 +61,11 @@ public class Deadline implements Task {
 
     @Override
     public String toString() {
-        // e.g. format: [D][✗] return book (by: June 6th)
+        // Deadline in yyyy-mm-dd format (e.g. 2020-10-15)
+        // Output in MMM d yyyy e.g. (Oct 15 2020)
+        // e.g. format: [D][✗] return book (by: Oct 15 2020)
         return String.format("[D][%s] %s (by: %s)", getStatusIcon(),
-                description, deadline);
+                description,
+                deadline.format(DateTimeFormatter.ofPattern("MMM d yyyy")));
     }
 }
