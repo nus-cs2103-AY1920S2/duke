30558121481d20dee4d79ef92301dd631b489cab
Kangwkk <846199604@qq.com>
2020-01-26 04:45:17 +0800

    Some trivial updates

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,6 +10,68 @@ public class Duke {
         System.out.println(horizontalLine + s + "\n" + horizontalLine);
     }
 
+    //get a list which listing all the tasks recorded
+    public static void gettingList(int index, Task[] list) {
+        StringBuilder li = new StringBuilder("    \uD83D\uDCDC Here are the tasks in your list:\n");
+        for (int i = 0; i < index; i++) {
+            li.append("         ").append(i + 1).append(": ").append(list[i]).append("\n");
+        }
+        typeSetting(li.toString());
+    }
+
+    //The specific way to split string (exclusive for deadline and event")
+    public static String[] stringSplitting(StringTokenizer st) {
+        String temp = st.nextToken("/").substring(1);
+        String description = temp.substring(0, temp.length() - 1);
+        String byOrAt = st.nextToken("/").substring(3);
+        return new String[]{description, byOrAt};
+    }
+
+    //to process different requests which is decided by the first token of the message user entered
+    public static Task[] processRequest(String str, Task[] list, int index) {
+        StringTokenizer st = new StringTokenizer(str);
+        String first = st.nextToken(" ");
+        switch (first) {
+            //decide which action to be done by the first token
+            case "done":
+                int num = Integer.parseInt(str.substring(5));
+                list[num - 1].markAsDone();
+                typeSetting("    \uD83D\uDC4D Nice! I've marked this task as done: " + num
+                        + "\n" + "      " + list[num - 1]);
+                break;
+
+            case "todo":
+                Todo td = new Todo(st.nextToken("").substring(1));
+                list[index] = td;
+                index++;
+                typeSetting("    Got it. I've added this task: \n      " +
+                        td + "\n" +
+                        "    Now you have " + index + " tasks in the list.");
+                break;
+
+            case "deadline":
+                String[] strings = stringSplitting(st);
+                Deadline ddl = new Deadline(strings[0], strings[1]);
+                list[index] = ddl;
+                index++;
+                typeSetting("    Got it. I've added this task: \n      " +
+                        ddl + "\n" +
+                        "    Now you have " + index + " tasks in the list.");
+                break;
+
+            case "event":
+                String[] strings2 = stringSplitting(st);
+                Event ev = new Event(strings2[0], strings2[1]);
+                list[index] = ev;
+                index++;
+                typeSetting("    Got it. I've added this task: \n      " +
+                        ev + "\n" +
+                        "    Now you have " + index + " tasks in the list.");
+                break;
+        }
+        return list;
+    }
+
     public static void main(String[] args) {
         //setting up
         Scanner sc = new Scanner(System.in);
@@ -22,74 +84,35 @@ public class Duke {
 
         //talking to Bob
         String str = sc.nextLine();
-        while (!exiting) {//check if the user want to exit
+        while (!exiting) {
+            //check if the user want to exit
             while (!str.equals("bye")) {
                 if (str.equals("list")) {
                     //print out the whole list
-                    StringBuilder li = new StringBuilder("    \uD83D\uDCDC Here are the tasks in your list:\n");
-                    for (int i = 0; i < index; i++) {
-                        li.append("         ").append(i + 1).append(": ").append(list[i]).append("\n");
-                    }
-                    typeSetting(li.toString());
+                    gettingList(index, list);
                 } else {
-                    StringTokenizer st = new StringTokenizer(str);
-                    String first = st.nextToken(" ");
-                    switch (first) {
-                        //decide which action to be done by the first token
-                        case "done":
-                            int num = Integer.parseInt(str.substring(5));
-                            list[num - 1].markAsDone();
-                            typeSetting("    \uD83D\uDC4D Nice! I've marked this task as done: " + num
-                                    + "\n" + "      " + list[num - 1]);
+                    list = processRequest(str, list, index);
+                    index = 0;
+                    for (Task t: list) {
+                        if (t == null)
                             break;
-
-                        case "todo":
-                            Todo td = new Todo(st.nextToken("").substring(1));
-                            list[index] = td;
+                        else
                             index++;
-                            typeSetting("    Got it. I've added this task: \n      " +
-                                        td + "\n" +
-                                        "    Now you have " + index + " tasks in the list.");
-                            break;
-
-                        case "deadline":
-                            String temp = st.nextToken("/").substring(1);
-                            String des1 = temp.substring(0, temp.length() - 1);
-                            String by = st.nextToken("/").substring(3);
-                            Deadline ddl = new Deadline(des1, by);
-                            list[index] = ddl;
-                            index++;
-                            typeSetting("    Got it. I've added this task: \n      " +
-                                    ddl + "\n" +
-                                    "    Now you have " + index + " tasks in the list.");
-                            break;
-
-                        case "event":
-                            String temp2 = st.nextToken("/").substring(1);
-                            String des2 = temp2.substring(0, temp2.length() - 1);
-                            String at = st.nextToken("/").substring(3);
-                            Event ev = new Event(des2, at);
-                            list[index] = ev;
-                            index++;
-                            typeSetting("    Got it. I've added this task: \n      " +
-                                    ev + "\n" +
-                                    "    Now you have " + index + " tasks in the list.");
-                            break;
                     }
                 }
                 str = sc.nextLine();
             }
 
             //exit confirmation
-            typeSetting("    Are you sure you want to leave me alone?\uD83E\uDD7A (y/n)\n");
+            typeSetting("    Are you sure you want to leave me alone? \uD83E\uDD7A (y/n)\n");
 
             if (sc.nextLine().equals("y")) {
                 //confirm to leave and leaving message
                 exiting = true;
-                typeSetting("    Bye. Hope to see you again soon!\uD83D\uDE1E\n");
+                typeSetting("    Bye. Hope to see you again soon! \uD83D\uDE1E\n");
             } else {
                 //not leaving and continue to interact with Bob
-                typeSetting("    I know you are the best!\uD83D\uDE06\n    Then, what's next?\n");
+                typeSetting("    I know you are the best! \uD83D\uDE06\n    Then, what's next?\n");
                 str = sc.nextLine();
             }
         }
