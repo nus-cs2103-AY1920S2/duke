f812daa559aefd7a8a482d1c835702940d89e67a
ShengXue97 <simshengxue1997@gmail.com>
2020-01-26 05:52:12 +0800

    Level 5. Handle Errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -32,40 +32,112 @@ public class Duke {
                 }
                 System.out.println(line);
             } else if (input.split(" ")[0].equals("done")){
-                int num = Integer.parseInt(input.split(" ")[1]);
-                toDo.get(num - 1).markAsDone();
-                System.out.println(line);
-                System.out.println(indent + "Nice! I've marked this task as done: ");
-                System.out.println(indent + indent + toDo.get(num - 1));
-                System.out.println(line);
+                if (input.split(" ").length == 1){
+                    System.out.println(line);
+                    System.out.println(indent + "☹ OOPS!!! The number of a done command cannot be empty.");
+                    System.out.println(line);
+                } else {
+                    try{
+                        int num = Integer.parseInt(input.split(" ")[1]);
+                        if (toDo.size() == 0){
+                            System.out.println(line);
+                            System.out.println(indent + "☹ OOPS!!! You have no tasks currently.");
+                            System.out.println(line);
+                        }  else if (num  == 0){
+                            System.out.println(line);
+                            System.out.println(indent + "☹ OOPS!!! Task number cannot be 0");
+                            System.out.println(line);
+                        } else if (num > toDo.size()){
+                            System.out.println(line);
+                            System.out.println(indent + "☹ OOPS!!! The number of a done command cannot be greater than the number of tasks " +
+                                    "you have.");
+                            System.out.println(line);
+                        } else {
+                            toDo.get(num - 1).markAsDone();
+                            System.out.println(line);
+                            System.out.println(indent + "Nice! I've marked this task as done: ");
+                            System.out.println(indent + indent + toDo.get(num - 1));
+                            System.out.println(line);
+                        }
+                    }catch (NumberFormatException ex) {
+                        //handle exception here
+                        System.out.println(line);
+                        System.out.println(indent + "☹ OOPS!!! Enter a valid number after done command");
+                        System.out.println(line);
+                    }
+                }
             } else if (input.split(" ")[0].equals("todo")){
-                String description = input.replaceFirst("todo ", "");
-                Task newTask = new ToDos(description);
-                toDo.add(newTask);
-                System.out.println(line);
-                System.out.println(indent + "Got it. I've added this task:");
-                System.out.println(indent + indent + newTask);
-                System.out.println(indent + "Now you have " + String.valueOf(toDo.size()) + " tasks in the list.");
-                System.out.println(line);
+                if (input.split(" ").length == 1){
+                    System.out.println(line);
+                    System.out.println(indent + "☹ OOPS!!! The description of a todo cannot be empty.");
+                    System.out.println(line);
+                } else {
+                    String description = input.replaceFirst("todo ", "");
+                    Task newTask = new ToDos(description);
+                    toDo.add(newTask);
+                    System.out.println(line);
+                    System.out.println(indent + "Got it. I've added this task:");
+                    System.out.println(indent + indent + newTask);
+                    System.out.println(indent + "Now you have " + String.valueOf(toDo.size()) + " tasks in the list.");
+                    System.out.println(line);
+                }
+
             } else if (input.split(" ")[0].equals("deadline")){
-                String description = input.replaceFirst("deadline ", "").split("/")[0].trim();
-                String datetime = input.replaceFirst("deadline ", "").split("/")[1].replaceFirst("by ", "");
-                Task newTask = new Deadlines(description, datetime);
-                toDo.add(newTask);
-                System.out.println(line);
-                System.out.println(indent + "Got it. I've added this task:");
-                System.out.println(indent + indent + newTask);
-                System.out.println(indent + "Now you have " + String.valueOf(toDo.size()) + " tasks in the list.");
-                System.out.println(line);
+                if (input.split(" ").length == 1){
+                    System.out.println(line);
+                    System.out.println(indent + "☹ OOPS!!! The description of a deadline cannot be empty.");
+                    System.out.println(line);
+                } else {
+                    String description = input.replaceFirst("deadline ", "").split("/")[0].trim();
+                    if (!input.contains("/by ")) {
+                        System.out.println(line);
+                        System.out.println(indent + "☹ OOPS!!! The /by of a deadline cannot be empty.");
+                        System.out.println(line);
+                    } else if (input.split(" ").length < 4) {
+                        System.out.println(line);
+                        System.out.println(indent + "☹ OOPS!!! The task or /by of a deadline cannot be empty.");
+                        System.out.println(line);
+                    } else {
+                        String datetime = input.replaceFirst("deadline ", "").split("/")[1].replaceFirst("by ", "");
+                        Task newTask = new Deadlines(description, datetime);
+                        toDo.add(newTask);
+                        System.out.println(line);
+                        System.out.println(indent + "Got it. I've added this task:");
+                        System.out.println(indent + indent + newTask);
+                        System.out.println(indent + "Now you have " + String.valueOf(toDo.size()) + " tasks in the list.");
+                        System.out.println(line);
+                    }
+                }
             } else if (input.split(" ")[0].equals("event")){
-                String description = input.replaceFirst("event ", "").split("/")[0].trim();
-                String datetime = input.replaceFirst("event ", "").split("/")[1].replaceFirst("at ", "");
-                Task newTask = new Events(description, datetime);
-                toDo.add(newTask);
+                if (input.split(" ").length == 1){
+                    System.out.println(line);
+                    System.out.println(indent + "☹ OOPS!!! The description of an event cannot be empty.");
+                    System.out.println(line);
+                } else {
+                    if (!input.contains("/at ")) {
+                        System.out.println(line);
+                        System.out.println(indent + "☹ OOPS!!! The /at of an event cannot be empty.");
+                        System.out.println(line);
+                    } else if (input.split(" ").length < 4) {
+                        System.out.println(line);
+                        System.out.println(indent + "☹ OOPS!!! The task or /at of an event cannot be empty.");
+                        System.out.println(line);
+                    } else {
+                        String description = input.replaceFirst("event ", "").split("/")[0].trim();
+                        String datetime = input.replaceFirst("event ", "").split("/")[1].replaceFirst("at ", "");
+                        Task newTask = new Events(description, datetime);
+                        toDo.add(newTask);
+                        System.out.println(line);
+                        System.out.println(indent + "Got it. I've added this task:");
+                        System.out.println(indent + indent + newTask);
+                        System.out.println(indent + "Now you have " + String.valueOf(toDo.size()) + " tasks in the list.");
+                        System.out.println(line);
+                    }
+                }
+
+            } else {
                 System.out.println(line);
-                System.out.println(indent + "Got it. I've added this task:");
-                System.out.println(indent + indent + newTask);
-                System.out.println(indent + "Now you have " + String.valueOf(toDo.size()) + " tasks in the list.");
+                System.out.println(indent + "☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
                 System.out.println(line);
             }
         }
