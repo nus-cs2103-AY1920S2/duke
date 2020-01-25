a9aa715a27f49d2d937d77e0c54abce453572628
ShengXue97 <simshengxue1997@gmail.com>
2020-01-26 04:18:47 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,7 +7,7 @@ public class Duke {
     private static String line = "    ____________________________________________________________";
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        List<String> toDo = new ArrayList<String>();
+        List<Task> toDo = new ArrayList<Task>();
 
         String logo = indent + " ____        _        \n"
                 + indent + "|  _ \\ _   _| | _____ \n"
@@ -26,12 +26,21 @@ public class Duke {
                 break;
             } else if (input.equals("list")){
                 System.out.println(line);
+                System.out.println(indent + "Here are the tasks in your list:");
                 for (int i = 0; i < toDo.size(); i++){
-                    System.out.println(indent + String.valueOf(i + 1) + ". " + toDo.get(i));
+                    System.out.println(indent + String.valueOf(i + 1) + "." + toDo.get(i));
                 }
                 System.out.println(line);
+            } else if (input.split(" ")[0].equals("done")){
+                int num = Integer.parseInt(input.split(" ")[1]);
+                toDo.get(num - 1).markAsDone();
+                System.out.println(line);
+                System.out.println(indent + "Nice! I've marked this task as done: ");
+                System.out.println(indent + indent + toDo.get(num - 1));
+                System.out.println(line);
             } else {
-                toDo.add(input);
+                Task newTask = new Task(input);
+                toDo.add(newTask);
                 System.out.println(line);
                 System.out.println(indent + "added: " + input);
                 System.out.println(line);
