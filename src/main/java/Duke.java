bca913bba65fde1aca15b19bba611ccc1b4866d9
Kangwkk <846199604@qq.com>
2020-01-25 21:38:54 +0800

    the first increment.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello, I'm Bob.\nWhat can I do for you? \uD83D\uDE03");
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        boolean exiting = false;
+        while (exiting != true) {
+            while (!str.equals("bye")) {
+                System.out.println(str);
+                str = sc.next();
+            }
+            System.out.println("Are you sure you want to leave me alone?\uD83E\uDD7A (y/n)");
+            if (sc.next().equals("y")) {
+                exiting = true;
+            } else {
+                System.out.println("I know you are the best!\uD83D\uDE06");
+                str = sc.next();
+            }
+        }
+        System.out.println("Bye. Hope to see you again soon!\uD83D\uDE1E");
     }
 }
--- src/test/java/DukeTest.java
+++ src/test/java/DukeTest.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class DukeTest {
     public static void main(String[] args) {
-        System.out.println("Hello from kwk");
+        System.out.println("Hello, I'm Duke.\nWhat can i do for you?");
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        while (!str.equals("bye")) {
+            System.out.println(str);
+            str = sc.next();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
