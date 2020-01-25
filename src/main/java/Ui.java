2fe31eff81e4e2cbd331afde7925244a5b4b7cbf
chifa3 <foochifa@gmail.com>
2020-01-25 13:28:37 +0800

    implement new UI commands

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,9 +1,31 @@
 public class Ui {
-    private static String LOGO = " ____        _        \n"
-            + "|  _ \\ _   _| | _____ \n"
-            + "| | | | | | | |/ / _ \\\n"
-            + "| |_| | |_| |   <  __/\n"
-            + "|____/ \\__,_|_|\\_\\___|\n";
-    private static String LINEBREAK = "_____________________________" +
-            "_______________________________";
+    private String logo;
+    private String linebreak;
+
+    public void introduction() {
+        System.out.println(linebreak);
+        System.out.println("Hello I am \n" + logo
+                + "\nWhat can I do for you?");
+        System.out.println(linebreak);
+    }
+
+    public void exit() {
+        System.out.println(linebreak);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(linebreak);
+    }
+
+    public void lineBreak() {
+        System.out.println(linebreak);
+    }
+
+    public Ui() {
+        logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        linebreak = "_____________________________" +
+                "_______________________________";
+    }
 }
