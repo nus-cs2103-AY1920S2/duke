08744cdb4764450203f6092dddcc9d6dd4ab2a59
Nathanael Seen <e0345677@u.nus.edu>
2020-01-26 20:01:02 +0800

    Duke.java: echo for commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Duke {
     private static final String LF = "\n";
     private static final String WELCOME_MSG = "Hello! I'm Duke" + LF + "What can I do for you?" + LF;
 
     public static void main(String[] args) {
         System.out.println(WELCOME_MSG);
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            try {
+                String line = br.readLine();
+
+                System.out.println(line + LF);
+            } catch (IOException e) {
+
+            }
+        }
     }
 }
