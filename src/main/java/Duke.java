33761b7f7f0539b05f1a6799f9707cec1be2d8b0
gruntultra <hakwee123@hotmail.com>
2020-01-26 08:39:57 +0800

    Added in Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+
+        while(true) {
+            String readtext = scanner.nextLine();
+            if(readtext.equals("bye")){
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(readtext);
+            }
+        }
+
     }
 }
