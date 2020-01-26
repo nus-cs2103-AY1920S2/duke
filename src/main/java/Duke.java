0e75d5514e666ed282f8a26c7cf871be5112ba67
Nathanael Seen <e0345677@u.nus.edu>
2020-01-26 19:48:52 +0800

    Duke.java: change welcome message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,8 @@
 public class Duke {
+    private static final String LF = "\n";
+    private static final String WELCOME_MSG = "Hello! I'm Duke" + LF + "What can I do for you?" + LF;
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(WELCOME_MSG);
     }
 }
