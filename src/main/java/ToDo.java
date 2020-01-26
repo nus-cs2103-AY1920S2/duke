d9d44da0ec34df486607f77654d10f3623b5e014
Benjamin Lau <capeguy@capeguy.net>
2020-01-26 21:16:04 +0800

    A-TextUiTesting

---
+++ src/main/java/ToDo.java
@@ -0,0 +1,14 @@
+public class ToDo extends Task {
+
+    protected String at;
+
+    public ToDo(String description, String at) {
+        super(description);
+        this.at = at;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (at: " + at + ")";
+    }
+}
