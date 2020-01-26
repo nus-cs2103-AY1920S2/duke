5d3952a13825ee2d47964aeb683a58e9a46506ea
LE TRUNG HIEU <lth08091998@gmail.com>
2020-01-26 14:01:03 +0800

    Simple bot echoing command

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,29 @@
+import java.util.Scanner;
+import java.io.IOException;
+
+/**
+ * A chatbot interface.
+ */
 public class Duke {
+    static String terminateCommand = "bye";
+
+    /**
+     * Long-polling for user commands.
+     */
+    private static void inputProcessor() {
+        Scanner scanner = new Scanner(System.in);
+        String userCommand = scanner.nextLine();
+
+        while (!userCommand.equals(terminateCommand)) {
+            System.out.println(userCommand);
+            userCommand = scanner.nextLine();
+        }
+    }
+
+    /**
+     * Main entry to the bot program.
+     * @param args external parameters.
+     */
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +31,6 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        inputProcessor();
     }
 }
