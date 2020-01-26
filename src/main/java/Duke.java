4c06c90df694ee2898c2dbea9b0df0448c45dbab
Sarah Chua <chuaxinhuisarah97@gmail.com>
2020-01-26 22:28:43 +0800

    Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,10 +2,12 @@ import java.util.Scanner;
 
 public class Duke {
 
+    public static int counter = 0; // this has to be a global variable
+
     public static void main(String[] args) {
 
         Task[] tasks = new Task[100]; // declare array for dealing with tasks
-        int counter = 0;
+        //int counter = 0;
 
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
@@ -24,45 +26,87 @@ public class Duke {
             else {
                 String[] time_split = user_input.split("/"); // the 2nd half contains the deadline
                 if (splitstr[0].toLowerCase().equals("todo")) {
-                    String task_description = user_input.replace(splitstr[0], "");
-
-                    tasks[counter] = new Todo(task_description, "[T]"); // no deadline
-                    Task current_task = tasks[counter]; // we just use this for reference
-                    counter++;
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println(current_task.getTaskType() + current_task.getCompletionStatus()
-                            + current_task.getDescription());
+                    try {
+                        addTodo(tasks, user_input, time_split, splitstr);
+                    }
+                    catch (DukeException exception) {
+                        System.out.println(exception);
+                    }
                 }
                 else if (splitstr[0].toLowerCase().equals("deadline")) {
-                    String task_description = time_split[0].replace(splitstr[0], "");
-                    String deadline = time_split[1].replace("by ","");
-
-                    tasks[counter] = new Deadline(task_description, "[D]", deadline);
-                    Task current_task = tasks[counter];
-                    counter++;
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println(current_task.getTaskType() + current_task.getCompletionStatus()
-                            + current_task.getDescription() + " (by: " + deadline + ")");
-                    //print something else here
+                    try {
+                        addDeadline(tasks, user_input, time_split, splitstr);
+                    }
+                    catch (DukeException exception) {
+                        System.out.println(exception);
+                    }
+                }
+                else if (splitstr[0].toLowerCase().equals("event")) {
+                    try {
+                        addEvent(tasks, user_input, time_split, splitstr);
+                    }
+                    catch (DukeException exception) {
+                        System.out.println(exception);
+                    }
                 }
                 else {
-                    String task_description = time_split[0].replace(splitstr[0], "");
-                    String deadline = time_split[1].replace("at ","");
-
-                    tasks[counter] = new Event(task_description, "[E]", deadline);
-                    Task current_task = tasks[counter];
-                    counter++;
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println(current_task.getTaskType() + current_task.getCompletionStatus()
-                            + current_task.getDescription() + " (at: " + deadline + ")");
-                    // print something else here
+                    System.out.println("\u2639 OOPS!!! I'm sorry, but I don't know what that means :-(");
                 }
-                System.out.println("Now you have " + Integer.toString(counter) + " tasks in the list.");
+                //System.out.println("Now you have " + Integer.toString(counter) + " tasks in the list.");
             }
             user_input = sc.nextLine();
         }
 
         System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public static void addTodo(Task[] tasks, String user_input, String[] time_split, String[] splitstr) throws DukeException {
+        if (splitstr.length == 1) {
+            throw new DukeException("\u2639 OOPS!!! The description of a todo cannot be empty.");
+        }
+        String task_description = user_input.replace(splitstr[0], "");
+
+        tasks[counter] = new Todo(task_description, "[T]"); // no deadline
+        Task current_task = tasks[counter]; // we just use this for reference
+        counter++;
+        System.out.println("Got it. I've added this task:");
+        System.out.println(current_task.getTaskType() + current_task.getCompletionStatus()
+                + current_task.getDescription());
+        System.out.println("Now you have " + Integer.toString(counter) + " tasks in the list.");
+    }
+
+    public static void addDeadline(Task[] tasks, String user_input, String[] time_split, String[] splitstr) throws DukeException {
+        if (splitstr.length == 1) {
+            throw new DukeException("\u2639 OOPS!!! The description of a deadline cannot be empty.");
+        }
+        String task_description = time_split[0].replace(splitstr[0], "");
+        String deadline = time_split[1].replace("by ","");
+
+        tasks[counter] = new Deadline(task_description, "[D]", deadline);
+        Task current_task = tasks[counter];
+        counter++;
+        System.out.println("Got it. I've added this task:");
+        System.out.println(current_task.getTaskType() + current_task.getCompletionStatus()
+                + current_task.getDescription() + " (by: " + deadline + ")");
+        //print something else here
+        System.out.println("Now you have " + Integer.toString(counter) + " tasks in the list.");
+    }
+
+    public static void addEvent(Task[] tasks, String user_input, String[] time_split, String[] splitstr) throws DukeException {
+        if (splitstr.length == 1) {
+            throw new DukeException("\u2639 OOPS!!! The description of an event cannot be empty.");
+        }
+        String task_description = time_split[0].replace(splitstr[0], "");
+        String deadline = time_split[1].replace("at ","");
+
+        tasks[counter] = new Event(task_description, "[E]", deadline);
+        Task current_task = tasks[counter];
+        counter++;
+        System.out.println("Got it. I've added this task:");
+        System.out.println(current_task.getTaskType() + current_task.getCompletionStatus()
+                + current_task.getDescription() + " (at: " + deadline + ")");
+        // print something else here
+        System.out.println("Now you have " + Integer.toString(counter) + " tasks in the list.");
     }
 
     public static void list(Task[] tasks, int counter) {
