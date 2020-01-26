e8d84071e1db4bb4f4a213e6efbd3d51310f3c34
bscrow <boshen@live.com>
2020-01-26 13:32:19 +0800

    Post Level-3: Updated program to comply to coding style requirements

--- src/main/java/Cathulhu.java
+++ src/main/java/Cathulhu.java
@@ -7,29 +7,11 @@ public class Cathulhu {
         Scanner sc = new Scanner(System.in);
         String cmd = "";
         ArrayList<Task> tasks = new ArrayList<>();
-        String catLeaves = "......................................................................\n"
-        +".......................................Iä! Iä! .......................\n"
-        +"...........................................Cthul......................\n"
-        +"............................................hu fh.....................\n"
-        +".........','...............................tagn!......................\n"
-        +"........'Ph'nglui mglw'nfah Cthulhu R'lyeh wgah.......................\n"
-        +".......'nagl fhatgn! Iä! Iä! Cthulhu fhtagn! P........................\n"
-        +".......h'nglui mglw'nfah Cthulhu R'lyeh wgah'n........................\n"
-        +"........agl fhtagn! Iä! Iä! Cthulhu fhtagn! P.........................\n"
-        +"...............h'nglui mglw'nfah Cthulhu R'ly;........................\n"
-        +"................yeh wgah'nagl fhtagn! Iä! Iä!,........................\n"
-        +"...............Cthul...hufht.....agn!Ph'nglui ........................\n"
-        +"..............mglw.....'nfah.....Cthu.....lhu.........................\n"
-        +".............R'ly........ehw....gah'.......nag........................\n"
-        +".............lf...........ht....ag..........n!........................\n"
-        +"......................................................................\n";
 
         while (true) {
             System.out.println("\t-------------C-A-T-H-U-L-H-U-------------");
             cmd = sc.nextLine().strip();
             if ( cmd.equalsIgnoreCase("bye") ) {
-                System.out.println("\tBe gone then, mortal.\n");
-                System.out.println(catLeaves);
                 break;
             } else if ( cmd.equalsIgnoreCase("list") ) {
                 for (int i = 1; i <= tasks.size(); i++) {
@@ -39,10 +21,10 @@ public class Cathulhu {
                 int taskIndex = Integer.parseInt(cmd.split("[ ]+")[1])-1;
                 if (tasks.size() > taskIndex) {
                     tasks.get(taskIndex).markAsDone();
-                    System.out.println("Mortal, thou have completed this task:");
-                    System.out.println(tasks.get(taskIndex));
+                    System.out.println("\tMortal, thou have completed this task:");
+                    System.out.println("\t  " + tasks.get(taskIndex));
                 } else {
-                    System.out.println("Mortal, thou have no such task! ");
+                    System.out.println("Mortal, thou have no such task!");
                 }
             } else {
                 tasks.add(new Task(cmd));
@@ -53,22 +35,57 @@ public class Cathulhu {
         sc.close();
     }
 
-    public static void main(String[] args) {
+
+    /**
+     * Shows the welcome message for this program
+     */
+    public void showWelcome() {
         String logo = "         _..._                                                                          \n"
-        +"      .-'_..._''.                                         .---.                         \n"
-        +"    .' .'      '.\\                    .                   |   |   .                     \n"
-        +"   / .'                             .'|                   |   | .'|                     \n"
-        +"  . '                           .| <  |                   |   |<  |                     \n"
-        +"  | |                 __      .' |_ | |                   |   | | |                     \n"
-        +"  | |              .:--.'.  .'     || | .'''-.     _    _ |   | | | .'''-.     _    _   \n"
-        +"  . '             / |   \\ |'--.  .-'| |/.'''. \\   | '  / ||   | | |/.'''. \\   | '  / |  \n"
-        +"   \\ '.          .`\" __ | |   |  |  |  /    | |  .' | .' ||   | |  /    | |  .' | .' |  \n"
-        +"    '. `._____.-'/ .'.''| |   |  |  | |     | |  /  | /  ||   | | |     | |  /  | /  |  \n"
-        +"      `-.______ / / /   | |_  |  '.'| |     | | |   `'.  |'---' | |     | | |   `'.  |  \n"
-        +"               `  \\ \\._,\\ '/  |   / | '.    | '.'   .'|  '/     | '.    | '.'   .'|  '/ \n"
-        +"                  `--'  `\"   `'-'  '---'   '---'`-'  `--'      '---'   '---'`-'  `--'  \n";
+                +"      .-'_..._''.                                         .---.                         \n"
+                +"    .' .'      '.\\                    .                   |   |   .                     \n"
+                +"   / .'                             .'|                   |   | .'|                     \n"
+                +"  . '                           .| <  |                   |   |<  |                     \n"
+                +"  | |                 __      .' |_ | |                   |   | | |                     \n"
+                +"  | |              .:--.'.  .'     || | .'''-.     _    _ |   | | | .'''-.     _    _   \n"
+                +"  . '             / |   \\ |'--.  .-'| |/.'''. \\   | '  / ||   | | |/.'''. \\   | '  / |  \n"
+                +"   \\ '.          .`\" __ | |   |  |  |  /    | |  .' | .' ||   | |  /    | |  .' | .' |  \n"
+                +"    '. `._____.-'/ .'.''| |   |  |  | |     | |  /  | /  ||   | | |     | |  /  | /  |  \n"
+                +"      `-.______ / / /   | |_  |  '.'| |     | | |   `'.  |'---' | |     | | |   `'.  |  \n"
+                +"               `  \\ \\._,\\ '/  |   / | '.    | '.'   .'|  '/     | '.    | '.'   .'|  '/ \n"
+                +"                  `--'  `\"   `'-'  '---'   '---'`-'  `--'      '---'   '---'`-'  `--'  \n";
         System.out.println(logo + "\n\n\tIt's you again, mortal. \n\tWhy have thou summoned meow this time?");
+    }
+
+
+    /**
+     * Shows the goodbye message for this program
+     */
+    public void showGoodbye() {
+        String catLeaves = "......................................................................\n"
+                +".......................................Iä! Iä! .......................\n"
+                +"...........................................Cthul......................\n"
+                +"............................................hu fh.....................\n"
+                +".........','...............................tagn!......................\n"
+                +"........'Ph'nglui mglw'nfah Cthulhu R'lyeh wgah.......................\n"
+                +".......'nagl fhatgn! Iä! Iä! Cthulhu fhtagn! P........................\n"
+                +".......h'nglui mglw'nfah Cthulhu R'lyeh wgah'n........................\n"
+                +"........agl fhtagn! Iä! Iä! Cthulhu fhtagn! P.........................\n"
+                +"...............h'nglui mglw'nfah Cthulhu R'ly;........................\n"
+                +"................yeh wgah'nagl fhtagn! Iä! Iä!,........................\n"
+                +"...............Cthul...hufht.....agn!Ph'nglui ........................\n"
+                +"..............mglw.....'nfah.....Cthu.....lhu.........................\n"
+                +".............R'ly........ehw....gah'.......nag........................\n"
+                +".............lf...........ht....ag..........n!........................\n"
+                +"......................................................................\n";
+        System.out.println("\tBe gone then, mortal.\n");
+        System.out.println(catLeaves);
+    }
+
+
+    public static void main(String[] args) {
         Cathulhu newCathulhu = new Cathulhu();
+        newCathulhu.showWelcome();
         newCathulhu.interact();
+        newCathulhu.showGoodbye();
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,21 +2,35 @@ public class Task {
     protected String description;
     protected boolean isDone;
 
+    /**
+     * Constructor for the Task class
+     * @param description String containing the description of the task
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /**
+     * Getter for the status icons, depending on whether the task has been completed:
+     * "Y" is returned if the task is completed
+     * "N" is returned if the task is incomplete
+     * @return String for the status icons
+     */
     public String getStatusIcon() {
-        //return (isDone ? "T" : "X"); // as ticks and X don't parse well
-        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+        return (isDone ? "Y" : "N"); // as ticks and X don't parse well on powershell
+        //return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
     }
 
+    /**
+     * Sets the status of this task to done (Default: not done)
+     */
     public void markAsDone() {
         this.isDone = true;
     }
 
+    @Override
     public String toString() {
-        return this.getStatusIcon() + " " + this.description;
+        return "[" + this.getStatusIcon() + "] " + this.description;
     }
 }
