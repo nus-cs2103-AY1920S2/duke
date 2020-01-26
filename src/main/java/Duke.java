bdb8df9271c599ac73462635ff9890801e8894d9
Danish Lukawski <5350946+DANamic@users.noreply.github.com>
2020-01-26 20:32:44 +0800

    Level 5. Handle Errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -18,37 +18,56 @@ public class Duke {
             String input = sc.nextLine();
             String[] inputBreakdown = input.split(" ", 2);
 
-            switch (inputBreakdown[0]) {
+            switch (inputBreakdown[0].toLowerCase()) {
                 case "bye":
                     print("Bye. Hope to see you again soon!");
                     dukeRunning = false;
                     break;
 
                 case "deadline":
-                    String[] byDeadline = inputBreakdown[1].split(" /by ");
-                    taskList.add(new Deadline(false, taskNo++, byDeadline[0], byDeadline[1]));
-                    print("Got it. I've added this task:\n\t[D][✗] "
-                            + byDeadline[0] + " (by: " + byDeadline[1] + ")" +
-                            "\nNow you have " + taskList.size() + " task(s) in the list.");
+                    try {
+                        String[] byDeadline = inputBreakdown[1].split(" /by ");
+
+                        try {
+                            taskList.add(new Deadline(false, taskNo++, byDeadline[0], byDeadline[1]));
+                            print("Got it. I've added this task:\n\t[D][✗] "
+                                    + byDeadline[0] + " (by: " + byDeadline[1] + ")" +
+                                    "\nNow you have " + taskList.size() + " task(s) in the list.");
+                        } catch (ArrayIndexOutOfBoundsException e) {
+                            taskNo--;
+                            print(new MissingByDeadlineException().toString());
+                        }
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        print(new MissingDetailsException().toString());
+                    }
                     break;
 
                 case "done":
-                    if (inputBreakdown.length <= 1) {
-                        print("Invalid task number!");
-                    } else {
+                    try {
                         int doneTaskNo = Integer.parseInt(inputBreakdown[1]) - 1;
                         taskList.get(doneTaskNo).taskCompleted = true;
                         print("Nice! I've marked this task as done:\n\t[✓] " + taskList.get(doneTaskNo).taskName);
+                    } catch (IndexOutOfBoundsException | NumberFormatException e) {
+                        print(new UnknownTaskException().toString());
                     }
-
                     break;
 
                 case "event":
-                    String[] atEvent = inputBreakdown[1].split(" /at ");
-                    taskList.add(new Event(false, taskNo++, atEvent[0], atEvent[1]));
-                    print("Got it. I've added this task:\n\t[E][✗] "
-                            + atEvent[0] + " (by: " + atEvent[1] + ")" +
-                            "\nNow you have " + taskList.size() + " task(s) in the list.");
+                    try {
+                        String[] atEvent = inputBreakdown[1].split(" /at ");
+
+                        try {
+                            taskList.add(new Event(false, taskNo++, atEvent[0], atEvent[1]));
+                            print("Got it. I've added this task:\n\t[E][✗] "
+                                    + atEvent[0] + " (by: " + atEvent[1] + ")" +
+                                    "\nNow you have " + taskList.size() + " task(s) in the list.");
+                        } catch (ArrayIndexOutOfBoundsException e) {
+                            taskNo--;
+                            print(new MissingAtEventException().toString());
+                        }
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        print(new MissingDetailsException().toString());
+                    }
                     break;
 
                 case "list":
@@ -56,15 +75,18 @@ public class Duke {
                     break;
 
                 case "todo":
-                    taskList.add(new Todo(false, taskNo++, inputBreakdown[1]));
-                    print("Got it. I've added this task:\n\t[T][✗] "
-                            + inputBreakdown[1] + "\nNow you have " + taskList.size() + " task(s) in the list.");
+                    try {
+                        taskList.add(new Todo(false, taskNo++, inputBreakdown[1]));
+                        print("Got it. I've added this task:\n\t[T][✗] "
+                                + inputBreakdown[1] + "\nNow you have " + taskList.size() + " task(s) in the list.");
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        taskNo--;
+                        print(new MissingDetailsException().toString());
+                    }
                     break;
 
                 default:
-                    //taskList.add(new Task(false, taskNo++, input));
-                    print("Please try: bye, deadline, done, event, list, todo.");
-                    break;
+                    print(new UnknownCommandException().toString());
             }
         }
     }
@@ -85,7 +107,7 @@ public class Duke {
         } else {
             System.out.println("Here are the task(s) in your list:");
 
-            for (Task task :tasksList) {
+            for (Task task : tasksList) {
                 System.out.println(task);
             }
         }
@@ -106,7 +128,7 @@ public class Duke {
 
         @Override
         public String toString() {
-            if(taskCompleted) {
+            if (taskCompleted) {
                 return taskNo + ".[✓] " + taskName;
             } else {
                 return taskNo + ".[✗] " + taskName;
@@ -124,7 +146,7 @@ public class Duke {
 
         @Override
         public String toString() {
-            if(taskCompleted) {
+            if (taskCompleted) {
                 return taskNo + ".[D][✓] " + taskName + " " + byDeadline;
             } else {
                 return taskNo + ".[D][✗] " + taskName + " " + byDeadline;
@@ -142,7 +164,7 @@ public class Duke {
 
         @Override
         public String toString() {
-            if(taskCompleted) {
+            if (taskCompleted) {
                 return taskNo + ".[E][✓] " + taskName + " " + atEvent;
             } else {
                 return taskNo + ".[E][✗] " + taskName + " " + atEvent;
@@ -157,11 +179,57 @@ public class Duke {
 
         @Override
         public String toString() {
-            if(taskCompleted) {
+            if (taskCompleted) {
                 return taskNo + ".[T][✓] " + taskName;
             } else {
                 return taskNo + ".[T][✗] " + taskName;
             }
+        }
+    }
+
+    static abstract class DukeException extends Exception {
+
+        @Override
+        public abstract String toString();
+    }
+
+    static class MissingAtEventException extends DukeException {
+
+        @Override
+        public String toString() {
+            return "☹ OOPS!!! Remember to use \"/at\" for Events.";
+        }
+    }
+
+    static class MissingByDeadlineException extends DukeException {
+
+        @Override
+        public String toString() {
+            return "☹ OOPS!!! Remember to use \"/by\" for Deadlines.";
+        }
+    }
+
+    static class MissingDetailsException extends DukeException {
+
+        @Override
+        public String toString() {
+            return "☹ OOPS!!! The description of a todo cannot be empty.";
+        }
+    }
+
+    static class UnknownCommandException extends DukeException {
+
+        @Override
+        public String toString() {
+            return "☹ OOPS!!! I'm sorry, but I don't know what that means :-(";
+        }
+    }
+
+    static class UnknownTaskException extends DukeException {
+
+        @Override
+        public String toString() {
+            return "☹ OOPS!!! I'm sorry, the Task Number entered does not exist.";
         }
     }
 }
