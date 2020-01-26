76bc9a4e3147b070730df5ea8c8c685f656a5f7d
gruntultra <hakwee123@hotmail.com>
2020-01-26 22:32:00 +0800

    A-TextUITesting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,12 +4,12 @@ import java.util.ArrayList;
 public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String logo = " ____        _        \n"
+        /*String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from\n" + logo);*/
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         ArrayList<Task> todo = new ArrayList<>();
         while(true) {
