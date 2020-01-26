9488d297e0440e1bc3b971d9559b43198ddd93fc
Exeexe93 <e0318484@u.nus.edu>
2020-01-26 19:44:48 +0800

    Save any editing to the hard disk

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,17 +1,19 @@
 package duke;
 
+import duke.filemanager.FileManager;
 import duke.task.Task;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
-    static String indentation = "       ";
-    static int numOfIndentation = 7;
+    static int numOfIndentation = 4;
     static Database database;
     static ReplyFormat message;
     static String userInput;
     static Scanner user;
     static boolean dialogContinue;
+    static FileManager fileManager;
 
     /**
      * Main program starts here.
@@ -25,6 +27,7 @@ public class Duke {
             System.out.println(userInput);
             processUserInput(userInput);
         }
+        closeScanners();
     }
 
     /**
@@ -32,11 +35,24 @@ public class Duke {
      */
     public static void initialise() {
         message = new ReplyFormat();
-        message.setIndentationInFront(4);
+        message.setIndentationInFront(numOfIndentation);
         database = new Database();
         user = new Scanner(System.in);
         userInput = "";
         dialogContinue = true;
+        try {
+            fileManager = new FileManager();
+            fileManager.getTaskListing(database);
+        } catch(DukeException e) {
+            reply(e.getMessage());
+        }
+    }
+
+    /**
+     * Close all scanners if available
+     */
+    public static void closeScanners() {
+        user.close();
     }
 
     /**
@@ -60,6 +76,7 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        message.clearMessage();
         message.addSentence("Hello! I'm ", 1);
         message.addParagraph(logo, 15);
         message.addEmptyLine();
@@ -79,19 +96,24 @@ public class Duke {
                 } else if (userInput.startsWith("delete")) {
                     try {
                         int num = Integer.parseInt(userInput.substring(7).strip());
-                        replyDelete(num);
+                        Task task = database.deleteTask(num);
+                        fileManager.saveTasks(database);
+                        replyDelete(task);
                     } catch(NumberFormatException e) {
                         throw new DukeException("Please input in this format: delete [number]");
                     }
                 }else if (userInput.startsWith("done")) {
                     try{
                         int num = Integer.parseInt(userInput.substring(5).strip());
+                        database.markDone(num);
+                        fileManager.saveTasks(database);
                         replyDone(num);
                     } catch(NumberFormatException e) {
                         throw new DukeException("Please input in this format: done [number]");
                     }
                 } else {
                     database.addData(userInput);
+                    fileManager.saveTasks(database);
                     replyAdded();
                 }
             } else {
@@ -117,51 +139,52 @@ public class Duke {
      * Reply all the data in the database in list format
      */
     public static void replyListing() {
+        List<Task> tasks = database.getListing();
         message.clearMessage();
-        message.addSentence("Here are the tasks in your list:", 1);
-        message.addList(database.getListing());
-        System.out.print(message.replyMessage());
-    }
-
-    /**
-     * Reply the user that respective duke.task has been added to the list
-     */
-    public static void replyAdded() {
-        int AmtOfTask = database.getAmountOfTask();
-        message.clearMessage();
-        message.addSentence("Got it. I've added this duke.task:", 1);
-        message.addSentence(database.getTask(AmtOfTask).toString(), 3);
-        if (AmtOfTask > 1) {
-            message.addSentence("Now you have "+ AmtOfTask +" tasks in the list.", 1);
+        if (tasks.isEmpty()) {
+            message.addSentence("Horray! You do not have any task now!", 1);
         } else {
-            message.addSentence("Now you have 1 duke.task in the list.", 1);
+            message.addSentence("Here are the tasks in your list:", 1);
+            message.addList(tasks);
         }
         System.out.print(message.replyMessage());
     }
 
     /**
-     * Reply the user that the duke.task has marked done
-     * @param num index where the duke.task located
-     * @throws DukeException when no duke.task found in that index
+     * Reply the user that respective task has been added to the list
      */
-    public static void replyDone(int num) throws DukeException{
-        database.markDone(num);
+    public static void replyAdded() {
+        int AmtOfTask = database.getAmountOfTask();
+        message.clearMessage();
+        message.addSentence("Got it. I've added this task:", 1);
+        message.addSentence(database.getTask(AmtOfTask).toString(), 3);
+        if (AmtOfTask > 1) {
+            message.addSentence("Now you have "+ AmtOfTask +" tasks in the list.", 1);
+        } else {
+            message.addSentence("Now you have 1 task in the list.", 1);
+        }
+        System.out.print(message.replyMessage());
+    }
+
+    /**
+     * Reply the user that the task has marked done
+     * @param num index where the task located
+     */
+    public static void replyDone(int num) {
         Task task = database.getTask(num);
         message.clearMessage();
-        message.addSentence("Nice! I've marked this duke.task as done:", 1);
+        message.addSentence("Nice! I've marked this task as done:", 1);
         message.addSentence("  " + task.toString(), 3);
         System.out.print(message.replyMessage());
     }
 
     /**
-     * Reply the user that the duke.task has deleted
-     * @param num index where the duke.task located
-     * @throws DukeException when no duke.task found in that index
+     * Reply the user that the task has deleted
+     * @param task task that being deleted
      */
-    public static void replyDelete(int num) throws DukeException{
-        Task task = database.deleteTask(num);
+    public static void replyDelete(Task task) {
         message.clearMessage();
-        message.addSentence("Noted. I've removed this duke.task:", 1);
+        message.addSentence("Noted. I've removed this task:", 1);
         message.addSentence("  " + task.toString(), 3);
         System.out.print(message.replyMessage());
     }
