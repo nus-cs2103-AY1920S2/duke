ae032946dc7284ff16855854196af4eb7ef43ef6
bscrow <boshen@live.com>
2020-01-26 17:29:48 +0800

    Completed Level-5 and A-Exceptions

--- src/main/java/Cathulhu.java
+++ src/main/java/Cathulhu.java
@@ -7,68 +7,98 @@ public class Cathulhu {
 
         Scanner sc = new Scanner(System.in);
         ArrayList<Task> tasks = new ArrayList<>();
+        boolean byebye = false;
 
-        while (true) {
+        while (!byebye) {
 
-            String[] cmdString  = sc.nextLine()
+            String[] cmdString = sc.nextLine()
                     .strip()
                     .split(" ", 2);
 
-            if ( cmdString[0].equalsIgnoreCase("bye") ) { //bye
-                break;
+            System.out.println("\t-------------C-A-T-H-U-L-H-U-------------");
 
-            } else if ( cmdString[0].equalsIgnoreCase("list") ) { //list
-                System.out.println("\t-------------C-A-T-H-U-L-H-U-------------");
-                if (tasks.size()==0) {
-                    System.out.println("\tYou are free now, mortal.");
-                } else {
-                    System.out.println("\tHere are your tasks, mortal:\n");
-                    for (int i = 1; i <= tasks.size(); i++) {
-                        System.out.println("\t" + i + ". " + tasks.get(i - 1));
-                    }
-                }
-
-            } else if (cmdString[0].equalsIgnoreCase("done")) { //done
-                System.out.println("\t-------------C-A-T-H-U-L-H-U-------------");
-                int taskIndex = Integer.parseInt(cmdString[1])-1;
-                if (tasks.size() > taskIndex) {
-                    tasks.get(taskIndex).markAsDone();
-                    System.out.println("\tMortal, thou have completed this task:");
-                    System.out.println("\t  " + tasks.get(taskIndex));
-                } else {
-                    System.out.println("Mortal, thou have no such task!");
-                }
-
-            } else if (cmdString[0].equalsIgnoreCase("todo")){ //todo
-                System.out.println("\t-------------C-A-T-H-U-L-H-U-------------");
-                tasks.add(new ToDo(cmdString[1]));
-                System.out.printf("\tTask added: \n\t  %s\n\tYou have %d tasks now, mortal\n",
-                        tasks.get(tasks.size()-1), tasks.size());
-
-            } else if (cmdString[0].equalsIgnoreCase("deadline")){ //deadline
-                System.out.println("\t-------------C-A-T-H-U-L-H-U-------------");
-                String[] byString = cmdString[1].split("/by ", 2);
-                tasks.add(new Deadline(byString[0], byString[1]));
-                System.out.printf("\tTask added: \n\t  %s\n\tYou have %d tasks now, mortal\n",
-                        tasks.get(tasks.size()-1), tasks.size());
-
-            } else if (cmdString[0].equalsIgnoreCase("event")){ //event
-                System.out.println("\t-------------C-A-T-H-U-L-H-U-------------");
-                String[] atString = cmdString[1].split("/at ", 2);
-                tasks.add(new Event(atString[0], atString[1]));
-                System.out.printf("\tTask added: \n\t  %s\n\tYou have %d tasks now, mortal\n",
-                        tasks.get(tasks.size()-1), tasks.size());
-
-            } else {
-                System.out.println("\t-------------C-A-T-H-U-L-H-U-------------");
-                System.out.println("\tMortal, that's an invalid Task!");
+            try {
+                byebye = parse(tasks, cmdString);
+            } catch (CathulhuException e) {
+                System.out.println(e.getMessage());
             }
 
             System.out.println("\t----------------M-E-O-W-S----------------\n\n");
         }
+
         sc.close();
     }
 
+    /**
+     * Parses the command user enters
+     * @param tasks ArrayList<Task> that stores all the current tasks
+     * @param cmdString String[] containing the user command, split by the first whitespace character
+     * @return boolean. true indicates that the user has terminated the program (by typing "bye")
+     * @throws CathulhuException for invalid user commands
+     */
+    private boolean parse(ArrayList<Task> tasks, String[] cmdString) throws CathulhuException {
+        if ( cmdString[0].equalsIgnoreCase("bye") ) { //bye
+            return true;
+
+        } else if ( cmdString[0].equalsIgnoreCase("list") ) { //list
+
+            if (tasks.size()==0) {
+                System.out.println("\tYou are free now, mortal.");
+            } else {
+                System.out.println("\tHere are your tasks, mortal:\n");
+                for (int i = 1; i <= tasks.size(); i++) {
+                    System.out.println("\t" + i + ". " + tasks.get(i - 1));
+                }
+            }
+
+        } else if (cmdString[0].equalsIgnoreCase("done")) { //done
+            int taskIndex = Integer.parseInt(cmdString[1])-1;
+            if (tasks.size() <= taskIndex) {
+                throw new CathulhuException("Mortal, thou have no such task!");
+            }
+            tasks.get(taskIndex).markAsDone();
+            System.out.println("\tMortal, thou have completed this task:");
+            System.out.println("\t  " + tasks.get(taskIndex));
+
+        } else if (cmdString[0].equalsIgnoreCase("todo")){ //todo
+            if (cmdString.length==1) {
+                throw new CathulhuException("A \"todo\" task must have a description, mortal!");
+            }
+            tasks.add(new ToDo(cmdString[1]));
+            System.out.printf("\tTask added: \n\t  %s\n\tYou have %d tasks now, mortal\n",
+                    tasks.get(tasks.size()-1), tasks.size());
+
+        } else if (cmdString[0].equalsIgnoreCase("deadline")){ //deadline
+            if (cmdString.length==1) {
+                throw new CathulhuException("A \"deadline\" task must have a description and a deadline, mortal!");
+            }
+            String[] byString = cmdString[1].split("/by ", 2);
+            if (byString.length==1) {
+                throw new CathulhuException("A \"deadline\" task must have a deadline, mortal!");
+            }
+            tasks.add(new Deadline(byString[0], byString[1]));
+            System.out.printf("\tTask added: \n\t  %s\n\tYou have %d tasks now, mortal\n",
+                    tasks.get(tasks.size()-1), tasks.size());
+
+        } else if (cmdString[0].equalsIgnoreCase("event")){ //event
+            if (cmdString.length==1) {
+                throw new CathulhuException("An \"event\" task must have a description and a time, mortal!");
+            }
+            String[] atString = cmdString[1].split("/at ", 2);
+            if (atString.length==1) {
+                throw new CathulhuException("An \"event\" task must have a time, mortal!");
+            }
+            tasks.add(new Event(atString[0], atString[1]));
+            System.out.printf("\tTask added: \n\t  %s\n\tYou have %d tasks now, mortal\n",
+                    tasks.get(tasks.size()-1), tasks.size());
+
+        } else {
+            throw new CathulhuException("\tMortal, that's an invalid Task!");
+
+        }
+
+        return false;
+    }
 
     /**
      * Shows the welcome message for this program
---
+++ src/main/java/CathulhuException.java
@@ -0,0 +1,10 @@
+public class CathulhuException extends Exception{
+
+    /**
+     * Exceptions thrown for invalid Cathulhu inputs
+     * @param message String message to be displayed
+     */
+    public CathulhuException(String message) {
+        super(message);
+    }
+}
