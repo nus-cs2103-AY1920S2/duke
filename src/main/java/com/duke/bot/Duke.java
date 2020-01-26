a79dfeb703648f887d2e482bb276ba2761ef918b
Kevin Putera <kevintiosanna@gmail.com>
2020-01-26 15:53:31 +0700

    Duke level-2

--- src/main/java/com/duke/bot/Duke.java
+++ src/main/java/com/duke/bot/Duke.java
@@ -1,23 +1,35 @@
 package com.duke.bot;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Duke {
     private final Scanner scanner = new Scanner(System.in);
+    private List<String> items = new ArrayList<>();
 
     public Duke() {
         print(List.of("Hello! I'm Duke", "What can I do for you?"));
     }
 
     public void start() {
-        String command = scanner.nextLine();
-        while (!command.equalsIgnoreCase("bye")) {
-            print(List.of(command));
-            command = scanner.nextLine();
+        String input = scanner.nextLine();
+        while (!input.equalsIgnoreCase("bye")) {
+            if (input.equalsIgnoreCase("list")) {
+                print(IntStream.range(0, items.size())
+                        .boxed()
+                        .map(index -> String.format("%d. %s", index + 1, items.get(index)))
+                        .collect(Collectors.toUnmodifiableList()));
+            } else {
+                items.add(input);
+                print(List.of("added: " + input));
+            }
+            input = scanner.nextLine();
         }
 
-        if (command.equalsIgnoreCase("bye")) {
+        if (input.equalsIgnoreCase("bye")) {
             print(List.of("Bye. Hope to see you again soon!"));
         }
     }
