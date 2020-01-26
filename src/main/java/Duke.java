c63f269afaebbf2abee19de0bb4ef99b841f6b48
Raivat Shah <hiraivat@gmail.com>
2020-01-26 15:12:37 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,19 +9,26 @@ public class Duke {
         System.out.println("What can I do for you?");
         //initiate objects
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Task> list = new ArrayList<>();
         //read user input
         while(true) {
             String input = sc.nextLine();
+            String firstWord = input.split(" ")[0];
             if (input.equals("bye")){
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             } else if (input.equals("list")){
+                System.out.println("Here are the tasks in your list:");
                 for(int i = 0; i < list.size(); i++) {
                     System.out.println(String.format("%d. %s", i+1, list.get(i)));
                 }
+            } else if (firstWord.equals("done")){
+                int num = Integer.parseInt(input.split(" ")[1]);
+                System.out.println("Nice! I've marked this task as done: ");
+                list.get(num-1).markAsDone();
+                System.out.println(list.get(num-1));
             } else {
-                list.add(input);
+                list.add(new Task(input));
                 System.out.println("added: " + input);
             }
         }
---
+++ src/main/java/Task.java
@@ -0,0 +1,36 @@
+/**
+ * Represents a Task to be managed/stored by the command line application Duke
+ */
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    /**
+     * Creates a new Task with the given description and sets isDone to false by default
+     * @param description
+     */
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    /**
+     * Gets the status icon of this Task based on whether it is done or not
+     * @return a String containing the status icon (tick-mark or cross)
+     */
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+
+    /**
+     * Marks this task as done by setting the isDone property to true
+     */
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s", this.getStatusIcon(), this.description);
+    }
+}
