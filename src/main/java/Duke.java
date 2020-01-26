7f04eb9e7d9743791ec08c7751d506f9bd8c9f79
fatin99 <fatinnbs@gmail.com>
2020-01-26 22:21:16 +0800

    specified imports

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,9 @@
-import java.util.*;
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Duke {
     public static ArrayList<Task> newList;
