3a88c0da978959bedf9c4e38699afcf9a1663f0d
Danish Lukawski <5350946+DANamic@users.noreply.github.com>
2020-01-26 18:51:30 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,26 +5,46 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         //Duke Setup
-        List<String> todoList = new ArrayList<>();
+        boolean dukeRunning = true;
+        int taskNo = 0;
+        List<Task> taskList = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
 
         //Welcome Text
         print("Hello! I'm Duke\nWhat can I do for you?");
 
         //Main Program Loop, possible change to Switch Case later on
-        while (true) {
+        while (dukeRunning) {
             String input = sc.nextLine();
+            String[] inputBreakdown = input.split(" ", 2);
 
-            if (input.equals("bye")) {
-                print("Bye. Hope to see you again soon!");
-                break;
-            } else {
-                if (input.equals("list")) {
-                    showList(todoList);
-                } else {
-                    todoList.add(input);
+            switch (inputBreakdown[0]) {
+                case "bye":
+                    print("Bye. Hope to see you again soon!");
+                    dukeRunning = false;
+                    break;
+
+                case "done":
+                    if (inputBreakdown.length <= 1) {
+                        print("Invalid task number!");
+                    } else {
+                        int doneTaskNo = Integer.parseInt(inputBreakdown[1]) - 1;
+                        Task doneTask = taskList.get(doneTaskNo);
+
+                        taskList.set(doneTaskNo, new Task(true, doneTaskNo, doneTask.taskName));
+                        print("Nice! I've marked this task as done:\n[✓] " + doneTask.taskName);
+                    }
+
+                    break;
+
+                case "list":
+                    showList(taskList);
+                    break;
+
+                default:
+                    taskList.add(new Task(false, taskNo++, input));
                     print("added: " + input);
-                }
+                    break;
             }
         }
     }
@@ -37,13 +57,40 @@ public class Duke {
     }
 
     //Custom showList Method to print the list with the horizontal borders + running index
-    static void showList(List<String> todoList) {
+    static void showList(List<Task> tasksList) {
         System.out.println("____________________________________________________________");
 
-        for(int i = 0; i < todoList.size(); i++) {
-            System.out.println(i+1 + ". " + todoList.get(i));
+        if (tasksList.size() == 0) {
+            System.out.println("List is empty.");
+        } else {
+            System.out.println("Here are the tasks in your list:");
+
+            for (Task task :tasksList) {
+                System.out.println(task);
+            }
         }
 
         System.out.println("____________________________________________________________\n");
+    }
+
+    private static class Task {
+        boolean taskCompleted;
+        int taskNo;
+        String taskName;
+
+        Task(boolean taskCompleted, int taskNo, String taskName) {
+            this.taskCompleted = taskCompleted;
+            this.taskNo = taskNo + 1;
+            this.taskName = taskName;
+        }
+
+        @Override
+        public String toString() {
+            if(taskCompleted) {
+                return taskNo + ".[✓] " + taskName;
+            } else {
+                return taskNo + ".[✗] " + taskName;
+            }
+        }
     }
 }
