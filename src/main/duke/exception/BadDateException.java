ba55424422d36ddfec15f82048df656255fe31f9
chifa3 <foochifa@gmail.com>
2020-01-26 22:41:00 +0800

    organised files and folders

--- src/main/duke/exception/BadDateException.java
+++ src/main/duke/exception/BadDateException.java
@@ -1,3 +1,5 @@
+package main.exception;
+
 public class BadDateException extends DukeException {
     public BadDateException(String errorMsg) {
         super(errorMsg);
--- src/main/duke/exception/BadDescriptionException.java
+++ src/main/duke/exception/BadDescriptionException.java
@@ -1,3 +1,5 @@
+package main.exception;
+
 public class BadDescriptionException extends DukeException {
     public BadDescriptionException (String errorMsg) {
         super(errorMsg);
