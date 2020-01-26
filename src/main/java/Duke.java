5b645c90c6a21b7828eb820707a74d798f9c455f
Danish Lukawski <5350946+DANamic@users.noreply.github.com>
2020-01-26 19:32:28 +0800

    Level 4. ToDos, Events, Deadlines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,7 +13,7 @@ public class Duke {
         //Welcome Text
         print("Hello! I'm Duke\nWhat can I do for you?");
 
-        //Main Program Loop, possible change to Switch Case later on
+        //Main Program now in Switch, might need to turn cases into separate methods soon
         while (dukeRunning) {
             String input = sc.nextLine();
             String[] inputBreakdown = input.split(" ", 2);
@@ -24,26 +24,46 @@ public class Duke {
                     dukeRunning = false;
                     break;
 
+                case "deadline":
+                    String[] byDeadline = inputBreakdown[1].split(" /by ");
+                    taskList.add(new Deadline(false, taskNo++, byDeadline[0], byDeadline[1]));
+                    print("Got it. I've added this task:\n\t[D][✗] "
+                            + byDeadline[0] + " (by: " + byDeadline[1] + ")" +
+                            "\nNow you have " + taskList.size() + " task(s) in the list.");
+                    break;
+
                 case "done":
                     if (inputBreakdown.length <= 1) {
                         print("Invalid task number!");
                     } else {
                         int doneTaskNo = Integer.parseInt(inputBreakdown[1]) - 1;
-                        Task doneTask = taskList.get(doneTaskNo);
-
-                        taskList.set(doneTaskNo, new Task(true, doneTaskNo, doneTask.taskName));
-                        print("Nice! I've marked this task as done:\n[✓] " + doneTask.taskName);
+                        taskList.get(doneTaskNo).taskCompleted = true;
+                        print("Nice! I've marked this task as done:\n\t[✓] " + taskList.get(doneTaskNo).taskName);
                     }
 
+                    break;
+
+                case "event":
+                    String[] atEvent = inputBreakdown[1].split(" /at ");
+                    taskList.add(new Event(false, taskNo++, atEvent[0], atEvent[1]));
+                    print("Got it. I've added this task:\n\t[E][✗] "
+                            + atEvent[0] + " (by: " + atEvent[1] + ")" +
+                            "\nNow you have " + taskList.size() + " task(s) in the list.");
                     break;
 
                 case "list":
                     showList(taskList);
                     break;
 
+                case "todo":
+                    taskList.add(new Todo(false, taskNo++, inputBreakdown[1]));
+                    print("Got it. I've added this task:\n\t[T][✗] "
+                            + inputBreakdown[1] + "\nNow you have " + taskList.size() + " task(s) in the list.");
+                    break;
+
                 default:
-                    taskList.add(new Task(false, taskNo++, input));
-                    print("added: " + input);
+                    //taskList.add(new Task(false, taskNo++, input));
+                    print("Please try: bye, deadline, done, event, list, todo.");
                     break;
             }
         }
@@ -63,7 +83,7 @@ public class Duke {
         if (tasksList.size() == 0) {
             System.out.println("List is empty.");
         } else {
-            System.out.println("Here are the tasks in your list:");
+            System.out.println("Here are the task(s) in your list:");
 
             for (Task task :tasksList) {
                 System.out.println(task);
@@ -73,7 +93,7 @@ public class Duke {
         System.out.println("____________________________________________________________\n");
     }
 
-    private static class Task {
+    static class Task {
         boolean taskCompleted;
         int taskNo;
         String taskName;
@@ -90,6 +110,57 @@ public class Duke {
                 return taskNo + ".[✓] " + taskName;
             } else {
                 return taskNo + ".[✗] " + taskName;
+            }
+        }
+    }
+
+    static class Deadline extends Task {
+        String byDeadline;
+
+        Deadline(boolean taskCompleted, int taskNo, String taskName, String byDeadline) {
+            super(taskCompleted, taskNo, taskName);
+            this.byDeadline = "(by: " + byDeadline + ")";
+        }
+
+        @Override
+        public String toString() {
+            if(taskCompleted) {
+                return taskNo + ".[D][✓] " + taskName + " " + byDeadline;
+            } else {
+                return taskNo + ".[D][✗] " + taskName + " " + byDeadline;
+            }
+        }
+    }
+
+    static class Event extends Task {
+        String atEvent;
+
+        Event(boolean taskCompleted, int taskNo, String taskName, String atEvent) {
+            super(taskCompleted, taskNo, taskName);
+            this.atEvent = "(at: " + atEvent + ")";
+        }
+
+        @Override
+        public String toString() {
+            if(taskCompleted) {
+                return taskNo + ".[E][✓] " + taskName + " " + atEvent;
+            } else {
+                return taskNo + ".[E][✗] " + taskName + " " + atEvent;
+            }
+        }
+    }
+
+    static class Todo extends Task {
+        Todo(boolean taskCompleted, int taskNo, String taskName) {
+            super(taskCompleted, taskNo, taskName);
+        }
+
+        @Override
+        public String toString() {
+            if(taskCompleted) {
+                return taskNo + ".[T][✓] " + taskName;
+            } else {
+                return taskNo + ".[T][✗] " + taskName;
             }
         }
     }
