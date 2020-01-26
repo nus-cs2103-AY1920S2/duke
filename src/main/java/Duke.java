46fd3ff2e64f646262d172da598951f5ae099481
Nathanael Seen <e0345677@u.nus.edu>
2020-01-26 20:24:41 +0800

    Add support for adding and listing tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,27 +1,45 @@
-import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Duke {
     private static final String LF = "\n";
     private static final String WELCOME_MSG = "Hello! I'm Duke" + LF + "What can I do for you?" + LF;
     private static final String BYE_MSG = "Bye, hope to see you again soon!" + LF;
     private static final String BYE_CMD = "bye";
+    private static final String LIST_CMD = "list";
 
     public static void main(String[] args) {
         System.out.println(WELCOME_MSG);
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        List<String> tasks = new ArrayList<>();
+
+        mainLoop:
         while (true) {
             try {
-                String line = br.readLine();
+                String cmd = br.readLine();
 
-                if (line.equals(BYE_CMD)) {
-                    System.out.println(BYE_MSG);
-                    break;
-                } else {
-                    System.out.println(line + LF);
+                switch (cmd) {
+                    case LIST_CMD:
+                        System.out.println("Here are your tasks:");
+
+                        for (String task : tasks) {
+                            System.out.println(task);
+                        }
+
+                        System.out.println();
+                        break;
+                    case BYE_CMD:
+                        System.out.println(BYE_MSG);
+                        break mainLoop;
+                    default:
+                        tasks.add(cmd);
+                        System.out.println("added: " + cmd + LF);
+                        break;
                 }
             } catch (IOException e) {
                 System.out.println("Sorry, an error has occurred:");
