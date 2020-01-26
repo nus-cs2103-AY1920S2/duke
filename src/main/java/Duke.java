a3fdb7565155e355fc9feba2ac2fe0c6f2963ed2
Samuel Van Allen <samuel.huanghaoyi@gmail.com>
2020-01-26 15:41:14 +0800

    Implemented greet, echo, and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,51 @@
+import javafx.application.Application;
+
+import java.util.Scanner;
+
 public class Duke {
+
+    protected static Scanner scanner;
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printWelcomeMessage();
+
+        scanner = new Scanner(System.in);
+
+        while (true)  {
+            processInput(scanner);
+        }
+    }
+
+    public static void printWelcomeMessage() {
+        ProgramOutputHandler.printLineSeperator();
+        ProgramOutputHandler.printWithIndentation("Hello! I'm Duck");
+        ProgramOutputHandler.printWithIndentation("What can I do for you?");
+        ProgramOutputHandler.printLineSeperator();
+        ProgramOutputHandler.nextLine();
+    }
+
+    public static void printGoodbyeMessage() {
+        ProgramOutputHandler.printLineSeperator();
+        ProgramOutputHandler.printWithIndentation("Bye. Hope to see you again soon!");
+        ProgramOutputHandler.printLineSeperator();
+        ProgramOutputHandler.nextLine();
+    }
+
+    public static void processInput(Scanner sc) {
+        String input = sc.nextLine();
+        if (input.equals("bye")) {
+            printGoodbyeMessage();
+            System.exit(0);
+            return;
+        }
+
+        echoInput(input);
+    }
+
+    public static void echoInput(String input) {
+        ProgramOutputHandler.printLineSeperator();
+        ProgramOutputHandler.printWithIndentation(input);
+        ProgramOutputHandler.printLineSeperator();
+        ProgramOutputHandler.nextLine();
     }
 }
--- src/main/java/ProgramOutputHandler.java
+++ src/main/java/ProgramOutputHandler.java
@@ -2,13 +2,15 @@ public class ProgramOutputHandler {
     protected static final String OUTPUT_HORIZONTAL_LINE = "____________________________________________________________";
     protected static final String OUTPUT_PREFIX_TAB = "    ";
 
-    public static void PrintLineSeperator()
-    {
-        PrintWithIndentation(OUTPUT_HORIZONTAL_LINE);
+    public static void printLineSeperator() {
+        printWithIndentation(OUTPUT_HORIZONTAL_LINE);
     }
 
-    public static void PrintWithIndentation(String text)
-    {
+    public static void printWithIndentation(String text) {
         System.out.println(OUTPUT_PREFIX_TAB + text);
+    }
+
+    public static void nextLine() {
+        System.out.print("\n");
     }
 }
