8e6c97a66bbba1cc8e586c05f5d43abb0b397c75
chifa3 <foochifa@gmail.com>
2020-01-26 23:51:04 +0800

    implement handling of BadDescriptionException for missing '/' in event and deadline cases

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -33,10 +33,14 @@ public class Duke {
             case DONE:
                 try {
                     String strDone = sc.nextLine();
-                    if (strDone.isEmpty()) throw new DukeDescriptionException("Empty Description");
+                    if (strDone.isEmpty()) {
+                        throw new DukeDescriptionException("Empty Description");
+                    }
                     int done = Integer.parseInt(strDone.substring(1)); //there must be a space between command and input
-                    if (done <= 0 || done > tasklist.getSize())
-                        throw new BadDescriptionException("" + done);
+                    if (done <= 0 || done > tasklist.getSize()) {
+                        throw new BadDescriptionException("Description for done cannot be "
+                                + done);
+                    }
                     tasklist.getTask(done - 1).setDone(true);
                     System.out.println("Nice! I've marked this task as done: \n" +
                             "  " + tasklist.getTask(done - 1));
@@ -47,7 +51,9 @@ public class Duke {
                 break;
             case TODO:
                 String todo = sc.nextLine();
-                if (todo.isEmpty()) throw new DukeDescriptionException("Empty Description");
+                if (todo.isEmpty()) {
+                    throw new DukeDescriptionException("Empty Description");
+                }
                 Task taskToDo = new Todo(todo.substring(1));
                 tasklist.addTask(taskToDo);
                 System.out.println("I've added this task: \n" +
@@ -57,8 +63,13 @@ public class Duke {
                 break;
             case EVENT:
                 String event = sc.nextLine();
-                if (event.isEmpty()) throw new DukeDescriptionException("Empty Description");
+                if (event.isEmpty()) {
+                    throw new DukeDescriptionException("Empty Description");
+                }
                 int eventDate = event.indexOf("/");
+                if (eventDate == -1) {
+                    throw new BadDescriptionException("Missing '/' in Description");
+                }
                 Task taskEvent = new Event(event.substring(1, eventDate),
                         event.substring(eventDate + 4));
                 tasklist.addTask(taskEvent);
@@ -72,6 +83,9 @@ public class Duke {
                 String deadline = sc.nextLine();
                 if (deadline.isEmpty()) throw new DukeDescriptionException("Empty Description");
                 int dLineDate = deadline.indexOf("/");
+                if (dLineDate == -1) {
+                    throw new BadDescriptionException("Missing '/' in Description");
+                }
                 Task taskDLine = new Deadline(deadline.substring(1, dLineDate),
                         deadline.substring(dLineDate + 4));
                 tasklist.addTask(taskDLine);
@@ -84,10 +98,13 @@ public class Duke {
             case DELETE:
                 try {
                     String strDelete = sc.nextLine();
-                    if (strDelete.isEmpty()) throw new DukeDescriptionException("Empty Description");
+                    if (strDelete.isEmpty()) {
+                        throw new DukeDescriptionException("Empty Description");
+                    }
                     int delete = Integer.parseInt(strDelete.substring(1)); //there must be a space between command and input
                     if (delete <= 0 || delete > tasklist.getSize())
-                        throw new BadDescriptionException("" + delete);
+                        throw new BadDescriptionException("Description for delete cannot be "
+                                + delete);
                     Task deletedTask = tasklist.getTask(delete - 1);
                     tasklist.removeTask(delete - 1);
                     calender.removeTask(deletedTask);
@@ -128,8 +145,7 @@ public class Duke {
                 } catch (DukeDescriptionException e) {
                     System.out.println("OOPS! You forgot to include a description!");
                 } catch (BadDescriptionException e) {
-                    System.out.println("OOPS! The number input for " +
-                            "done/delete cannot be " + e.getMessage());
+                    System.out.println("OOPS! " + e.getMessage());
                 } catch (BadDateException e) {
                     System.out.println("Sorry I don't recognise this date format!\n" +
                             "Please make sure the format is: dd mm yy");
