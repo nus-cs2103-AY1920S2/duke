5022df870045763d43b52d0a6ef4f387a161bd8e
LE TRUNG HIEU <lth08091998@gmail.com>
2020-01-26 14:43:07 +0800

    Add commandProcessor for listing

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,30 @@
 import java.util.Scanner;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * A chatbot interface.
  */
 public class Duke {
     static String terminateCommand = "bye";
+    static ArrayList<String> commands = new ArrayList<String>();
+
+    /**
+     * Switching logic for different commands.
+     * @param command user command
+     */
+    private static void commandProcessor(String command) {
+        switch (command) {
+        case "list":
+            for (int i = 0; i < commands.size(); i++) {
+                System.out.println(i + " " + commands.get(i));
+            }
+            break;
+        default:
+            commands.add(command);
+            System.out.println("added: " + command);
+        }
+    }
 
     /**
      * Long-polling for user commands.
@@ -15,9 +34,10 @@ public class Duke {
         String userCommand = scanner.nextLine();
 
         while (!userCommand.equals(terminateCommand)) {
-            System.out.println(userCommand);
+            commandProcessor(userCommand);
             userCommand = scanner.nextLine();
         }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 
     /**
