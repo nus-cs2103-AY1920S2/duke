bf578d2449cf7d25ff507c23ca3ab88c6140a99e
gruntultra <hakwee123@hotmail.com>
2020-01-26 21:16:13 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,20 +11,28 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
-        ArrayList<String> todo = new ArrayList<>();
+        ArrayList<Task> todo = new ArrayList<>();
         while(true) {
             String readtext = scanner.nextLine();
             if(readtext.equals("bye")){
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             } else if (readtext.equals("list")) {
+                System.out.println("Here are the task in your list:");
                 for(int i=0;i <todo.size(); i++){
-                    System.out.println((i + 1) + ". " + todo.get(i));
+                    System.out.println((i + 1) + ". " + "[" + todo.get(i).getStatusIcon() +"] " + todo.get(i).getDescription());
                 }
             } else {
-
-                todo.add(readtext);
-                System.out.println("added: "  + readtext);
+                if (readtext.contains("done")) {
+                    String num = readtext.split(" ")[1];
+                    System.out.println("Nice! I've marked this task as done:\n");
+                    todo.get(Integer.parseInt(num)-1).markAsDone();
+                    System.out.println(num + ". " + "[" + todo.get(Integer.parseInt(num)-1).getStatusIcon() +"] " + todo.get(Integer.parseInt(num)-1).getDescription());
+                } else {
+                    Task t = new Task(readtext);
+                    todo.add(t);
+                    System.out.println("added: "  + readtext);
+                }
             }
         }
 
