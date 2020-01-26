87c56284f738a48b3268c6308f7b2a2c069951f7
Kevin Putera <kevintiosanna@gmail.com>
2020-01-26 21:42:26 +0700

    Duke level-5

--- src/main/java/com/duke/bot/Duke.java
+++ src/main/java/com/duke/bot/Duke.java
@@ -19,63 +19,101 @@ public class Duke {
         print(List.of("Hello! I'm Duke", "What can I do for you?"));
     }
 
-    public void start() {
+    public void run() {
         String input = scanner.nextLine();
         while (!input.equalsIgnoreCase("bye")) {
-            if (input.equalsIgnoreCase("list")) {
-                print(Stream.concat(
-                        Stream.of("Here are the tasks in your list:"),
-                        IntStream.range(0, tasks.size())
-                                .boxed()
-                                .map(index -> String.format("%d.%s", index + 1, tasks.get(index)))
-                )
-                .collect(Collectors.toUnmodifiableList()));
-            } else if (input.matches("done\\s+\\d+")) {
-                int setDoneIndex = Integer.parseInt(input.split("\\s+")[1]) - 1;
-                Task doneTask = tasks.get(setDoneIndex);
-                doneTask.setDone(true);
-                print(List.of(
-                        "Nice! I've marked this task as done:",
-                        "  " + doneTask
-                ));
-            } else if (input.startsWith("todo")) {
-                Pattern todoPattern = Pattern.compile("^todo\\s+(.+)$");
-                Matcher todoMatcher = todoPattern.matcher(input);
-                if (todoMatcher.matches()) {
-                    Todo newTodo = new Todo(todoMatcher.group(1));
-                    tasks.add(newTodo);
-                    reportNewTask(newTodo);
-                } else {
-                    print(List.of("Invalid todo! Try again."));
-                }
-            } else if (input.startsWith("event")) {
-                Pattern eventPattern = Pattern.compile("^event\\s+(.+)\\s+/at\\s+(.+)$");
-                Matcher eventMatcher = eventPattern.matcher(input);
-                if (eventMatcher.matches()) {
-                    Event newEvent = new Event(eventMatcher.group(1), eventMatcher.group(2));
-                    tasks.add(newEvent);
-                    reportNewTask(newEvent);
-                } else {
-                    print(List.of("Invalid event! Try again."));
-                }
-            } else if (input.startsWith("deadline")) {
-                Pattern deadlinePattern = Pattern.compile("^deadline\\s+(.+)\\s+/by\\s+(.+)$");
-                Matcher deadlineMatcher = deadlinePattern.matcher(input);
-                if (deadlineMatcher.matches()) {
-                    Deadline newDeadline = new Deadline(deadlineMatcher.group(1), deadlineMatcher.group(2));
-                    tasks.add(newDeadline);
-                    reportNewTask(newDeadline);
-                } else {
-                    print(List.of("Invalid deadline! Try again."));
-                }
-            } else {
-                print(List.of(String.format("'%s' is an invalid input! Try again.", input)));
+            try {
+                parseInput(input);
+            } catch(DukeException exception) {
+                print(List.of(exception.getMessage()));
             }
             input = scanner.nextLine();
         }
 
         if (input.equalsIgnoreCase("bye")) {
             print(List.of("Bye. Hope to see you again soon!"));
+        }
+    }
+
+    private void parseInput(String input) throws DukeException {
+        if (input.equalsIgnoreCase("list")) {
+            handleListInput(); 
+        } else if (input.startsWith("done")) {
+            handleDoneInput(input); 
+        } else if (input.startsWith("todo")) {
+            handleTodoInput(input); 
+        } else if (input.startsWith("event")) {
+            handleEventInput(input); 
+        } else if (input.startsWith("deadline")) {
+            handleDeadlineInput(input);
+        } else {
+            throw new DukeException(String.format("☹ OOPS!!! '%s' is an invalid input.", input));
+        }
+    }
+    
+    private void handleListInput() {
+        print(Stream.concat(
+                Stream.of("Here are the tasks in your list:"),
+                IntStream.range(0, tasks.size())
+                        .boxed()
+                        .map(index -> String.format("%d.%s", index + 1, tasks.get(index)))
+        )
+        .collect(Collectors.toUnmodifiableList()));
+    }
+
+    private void handleDoneInput(String input) throws DukeException {
+        Pattern donePattern = Pattern.compile("^done\\s+(\\d+)$");
+        Matcher doneMatcher = donePattern.matcher(input);
+        if (doneMatcher.matches()) {
+            int setDoneIndex = Integer.parseInt(doneMatcher.group(1)) - 1;
+            if (setDoneIndex >= 0 && setDoneIndex < tasks.size()) {
+                Task doneTask = tasks.get(setDoneIndex);
+                doneTask.setDone(true);
+                print(List.of(
+                        "Nice! I've marked this task as done:",
+                        "  " + doneTask
+                ));
+            } else {
+                throw new DukeException("☹ OOPS!!! Invalid done index.");
+            }
+        } else {
+            throw new DukeException("☹ OOPS!!! Invalid done index.");
+        }
+    }
+
+    private void handleTodoInput(String input) throws DukeException {
+        Pattern todoPattern = Pattern.compile("^todo\\s+(.+)$");
+        Matcher todoMatcher = todoPattern.matcher(input);
+        if (todoMatcher.matches()) {
+            Todo newTodo = new Todo(todoMatcher.group(1));
+            tasks.add(newTodo);
+            reportNewTask(newTodo);
+        } else {
+            throw new DukeException("☹ OOPS!!! Invalid todo.");
+        }
+    }
+
+    private void handleEventInput(String input) throws DukeException {
+        Pattern eventPattern = Pattern.compile("^event\\s+(.+)\\s+/at\\s+(.+)$");
+        Matcher eventMatcher = eventPattern.matcher(input);
+        if (eventMatcher.matches()) {
+            Event newEvent = new Event(eventMatcher.group(1), eventMatcher.group(2));
+            tasks.add(newEvent);
+            reportNewTask(newEvent);
+        } else {
+            throw new DukeException("☹ OOPS!!! Invalid event.");
+        }
+    }
+
+    private void handleDeadlineInput(String input) throws DukeException {
+        Pattern deadlinePattern = Pattern.compile("^deadline\\s+(.+)\\s+/by\\s+(.+)$");
+        Matcher deadlineMatcher = deadlinePattern.matcher(input);
+        if (deadlineMatcher.matches()) {
+            Deadline newDeadline = new Deadline(deadlineMatcher.group(1), deadlineMatcher.group(2));
+            tasks.add(newDeadline);
+            reportNewTask(newDeadline);
+        } else {
+            throw new DukeException("☹ OOPS!!! Invalid deadline.");
         }
     }
 
@@ -95,6 +133,6 @@ public class Duke {
 
     public static void main(String[] args) {
         Duke duke = new Duke();
-        duke.start();
+        duke.run();
     }
 }
---
+++ src/main/java/com/duke/bot/DukeException.java
@@ -0,0 +1,7 @@
+package com.duke.bot;
+
+public class DukeException extends Exception {
+    public DukeException(String message) {
+        super(message);
+    }
+}
