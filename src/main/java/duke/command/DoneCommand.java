517f762d7c9f0ade796aaf0a97a86d906d0877f2
JeremyLoh <jeremyloh03@gmail.com>
2020-01-27 22:47:25 +0800

    DoneCommand: Added JavaDoc and modified execute function to save changes to save file

--- src/main/java/duke/command/DoneCommand.java
+++ src/main/java/duke/command/DoneCommand.java
@@ -6,6 +6,7 @@ import duke.Ui;
 import duke.task.Task;
 import duke.task.TaskList;
 
+/** Represents a command marking a given task number in task list as done */
 public class DoneCommand extends Command {
     protected int taskNumber;
 
@@ -14,12 +15,19 @@ public class DoneCommand extends Command {
         this.taskNumber = taskNumber;
     }
 
+    /**
+     * Marks a given task number in task list as done, prints out task summary.
+     *
+     * @param tasks list of tasks
+     * @param ui used to display information to user
+     * @param storage used to update save file
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) {
         try {
             Task task = tasks.get(taskNumber - 1);
-            task.markAsDone();
             ui.markTaskAsDone(task);
+            storage.updateSaveFile(tasks);
         } catch (IndexOutOfBoundsException e) {
             ui.showExceptionMessage(new DukeException("Invalid Task Number given!"));
         }
