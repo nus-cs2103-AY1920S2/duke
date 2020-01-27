7c988c061b493fe00dcf3af7ffb3440dcb537d53
zixinn <ng.zixin99@gmail.com>
2020-01-27 12:49:29 +0800

    Format date and time

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,31 @@
-public class Deadline extends Task {
-    protected String by;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
-    public Deadline(String description, String by) {
+public class Deadline extends Task {
+    protected LocalDate date;
+    protected LocalTime time;
+
+    public Deadline(String description, String by) throws DukeException {
         super(description);
-        this.by = by;
+        try {
+            String[] dateTimeArr = by.split(" ");
+            this.date = LocalDate.parse(dateTimeArr[0]);
+            this.time = LocalTime.parse(dateTimeArr[1]);
+        } catch (DateTimeParseException e) {
+            numOfTasks--;
+            throw new DukeException("Incorrect date or time format. Format required: yyyy-mm-dd hh:mm");
+        }
+    }
+
+    public LocalDate getDate() {
+        return this.date;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " +
+                date.format(DateTimeFormatter.ofPattern("d MMM yyyy")) + " " + time + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,13 +1,38 @@
-public class Event extends Task {
-    protected String at;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
-    public Event(String description, String at) {
+public class Event extends Task {
+    protected LocalDate date;
+    protected LocalTime startTime;
+    protected LocalTime endTime;
+
+    public Event(String description, String at) throws DukeException {
         super(description);
-        this.at = at;
+        try {
+            String[] dateTimeArr = at.split(" ");
+            this.date = LocalDate.parse(dateTimeArr[0]);
+            String[] timeArr = dateTimeArr[1].split("-");
+            this.startTime = LocalTime.parse(timeArr[0]);
+            this.endTime = LocalTime.parse(timeArr[1]);
+            if (startTime.isAfter(endTime)) {
+                numOfTasks--;
+                throw new DukeException("Start time cannot be after end time.");
+            }
+        } catch (DateTimeParseException | ArrayIndexOutOfBoundsException e) {
+            numOfTasks--;
+            throw new DukeException("Incorrect date or time format. Format required: yyyy-mm-dd hh:mm-hh:mm");
+        }
+    }
+
+    public LocalDate getDate() {
+        return this.date;
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + at + ")";
+        return "[E]" + super.toString() + " (at: " +
+                date.format(DateTimeFormatter.ofPattern("d MMM yyyy")) + " " + startTime + "-" + endTime + ")";
     }
 }
