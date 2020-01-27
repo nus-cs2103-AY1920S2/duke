f949f68e18cf6b7977f529f6c8fc937f749206bd
seanlowcy77 <seanlowcy77@Seans-MacBook-Pro-4.local>
2020-01-28 00:21:02 +0800

    Add Functionality for Date and Time for 'event' and 'deadline' commands

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,3 +1,7 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 public class Deadline extends Task {
 
     protected String type = "D";
@@ -5,12 +9,26 @@ public class Deadline extends Task {
 
     public Deadline(String description, String time) {
         super(description);
+        time = time.trim();
         this.time = time;
+
+        try {
+            format_Date();
+        } catch (Exception e) {
+            System.out.println("PS! Timing is not recorded... Follow the format dd/MM/YYYY HHMM!");
+        }
     }
+
+
+    public void format_Date() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy HHmm").withLocale(Locale.ENGLISH);
+        LocalDate date = LocalDate.parse(time,formatter);
+    }
+
 
     @Override
     public String toString() {
-        String temp = "[" + type + "]" + "[" + getStatusIcon() + "] " + description + " (by:" +  time + ")";
+        String temp = "[" + type + "]" + "[" + getStatusIcon() + "] " + description + " (by: " +  time + ")";
         return temp;
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,3 +1,8 @@
+import java.text.*;
+import java.time.*;
+import java.time.format.*;
+import java.util.*;
+
 public class Event extends Task {
 
     protected String type = "E";
@@ -5,12 +10,25 @@ public class Event extends Task {
 
     public Event(String description, String time) {
         super(description);
+        time = time.trim();
         this.time = time;
+
+        try {
+            format_Date();
+        } catch (Exception e) {
+            System.out.println("PS! Timing is not recorded... Follow the format dd/MM/YYYY HHMM!");
+        }
+    }
+
+    public void format_Date() {
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy HHmm").withLocale(Locale.ENGLISH);
+        LocalDate date = LocalDate.parse(time,formatter);
     }
 
     @Override
     public String toString() {
-        String temp = "[" + type + "]" + "[" + getStatusIcon() + "] " + description + " (at:" +  time + ")";
+        String temp = "[" + type + "]" + "[" + getStatusIcon() + "] " + description + " (at: " +  time + ")";
         return temp;
     }
 
