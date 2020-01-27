c3f15409521a2eb686294ee07127a5f6838ec90e
Loh Sze Ying <bloom9334@hotmail.com>
2020-01-27 19:34:01 +0800

    Level 8 added

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,14 +1,25 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
 public class Deadline extends Task {
 
     protected String by;
+    String dateAsString = "";
+    LocalDate d;
+    String statement = "";
 
     public Deadline(String description, String by) {
         super(description);
         this.by = by;
+        String[] tmp = this.by.split(" ");
+        statement = tmp[0];
+        dateAsString = dateAsString + tmp[1];
+        d = LocalDate.parse(dateAsString);
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + "(" + by + ")";
+        return "[D]" + super.toString() + "(" + statement + " " + d.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,14 +1,25 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
 public class Event extends Task {
 
     protected String by;
+    String dateAsString = "";
+    LocalDate d;
+    String statement = "";
 
     public Event(String description, String by) {
         super(description);
         this.by = by;
+        String[] tmp = this.by.split(" ");
+        statement = tmp[0];
+        dateAsString = dateAsString + tmp[1];
+        d = LocalDate.parse(dateAsString);
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + "(" + by + ")";
+        return "[E]" + super.toString() + "(" + statement + " " + d.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 }
