8176033ffa49d84059f450c3bb5f71d9df72b767
gohkh <gohkh2011@gmail.com>
2020-01-27 18:08:44 +0800

    Improve clarity of error message

--- src/main/java/InvalidTaskNumberException.java
+++ src/main/java/InvalidTaskNumberException.java
@@ -1,5 +1,5 @@
 class InvalidTaskNumberException extends InvalidTaskException {
     InvalidTaskNumberException(int invalidTaskNumber) {
-        super("Oops! " + invalidTaskNumber + " is not a valid task number.");
+        super("Oops! " + invalidTaskNumber + " doesn't correspond to any task.");
     }
 }
