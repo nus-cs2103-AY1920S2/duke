d5ad3a667087621226c9a01e3025ac85761024c7
KwanXinJie <xjkwan1998@gmail.com>
2020-01-28 03:27:30 +0800

    DateTime

---
+++ src/main/java/DateTime.java
@@ -0,0 +1,22 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+public class DateTime {
+    protected String date;
+    protected String time;
+
+    public DateTime() {
+    }
+
+    public String convertDate(String date) {
+        LocalDate d = LocalDate.parse(date);
+        return d.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+    }
+
+    public String convertTime(String time) {
+        LocalTime l = LocalTime.parse(time);
+        return l.format(DateTimeFormatter.ofPattern("HH:mm"));
+    }
+}
