b20c3f5eef31fba059a4b2af8b3a8432afb62842
Loh Sze Ying <bloom9334@hotmail.com>
2020-01-27 16:26:09 +0800

    Level 1 added

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("What can I do for you?");
+
+        String nxt;
+        nxt = sc.next();
+        while (nxt.equals("bye") == false) {
+            System.out.println("You said: " + nxt);
+            nxt = sc.next();
+        }
+
+        System.out.println("Bye. Hope to see you again soon!");
+
     }
 }
