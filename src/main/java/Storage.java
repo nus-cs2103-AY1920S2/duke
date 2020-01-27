7715f6855a3990733a0faca21be952a80460008e
Dban1 <huggavamp@gmail.com>
2020-01-27 19:35:56 +0800

    -implemented Phase 1 untested code refactoring for More-OOP -created the Parser, Storage, TaskList and Ui class -To-dos (Parser.java)

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,4 +1,56 @@
-package PACKAGE_NAME;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Storage {
+    private File data;
+
+    public Storage(String s) {
+        try {
+            this.data = new File(s);
+        } catch(Exception e) {
+            System.err.println("Error retrieving file!");
+        }
+    }
+
+    public List<Task> buildTaskList() {
+        List<Task> taskList = new ArrayList<>();
+        Scanner scan = new Scanner(data);
+        while(scan.hasNext()) {
+            String elementLine = scan.nextLine();
+            String[] elements = elementLine.split("\|");
+            switch(elements[0]) {
+            case "T":
+                taskList.add(Todo.create(elements));
+                break;
+            case "E":
+                taskList.add(Event.create(elements));
+                break;
+            case "D":
+                taskList.add(Deadline.create(elements));
+                break;
+            default:
+                taskList.add(Task.create(elements));
+                break;
+            }
+            
+            return taskList;
+        }
+    }
+
+    public void updateStorage(List<Task> taskList) {
+        try {
+            FileWriter writer = new FileWriter(data);
+            for (Task t: taskList) {
+                writer.write(t.store() + "\n");
+            }
+            writer.close();
+        } catch(Exception e) {
+            System.err.println("Error writing to save file, message: " + e.toString());
+        } finally {
+            return;
+        }
+    }
 }
--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
-
 public class Ui {
+    private String dukeLogo = " ____        _        \n" + "|  _ \\ _   _| | _____ \n" + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n" + "|____/ \\__,_|_|\\_\\___|\n";
+
+    public void welcomeUser() {
+        System.out.println("Hello, I am \n" + logo + "your personal buddy. What's up?\n"
+        + "____________________________________________________________\n");
+    }
+
+    public void farewellUser() {
+        divider("Aww, see you again soon!");
+        return;
+    }
+
+    public void divider(String s) {
+        if(s.length() > 0) {
+            System.out.println("____________________________________________________________\n");
+        } else {
+            System.out.println("____________________________________________________________\n" + s + "\n"
+                    + "____________________________________________________________\n");
+            return;
+        }
+    }
+
+
 }
