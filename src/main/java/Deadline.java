e8b3c6eda0ebcdb496a4780e9455186e8a1cc03b
Dban1 <huggavamp@gmail.com>
2020-01-27 11:15:15 +0800

    -implemented date objects for Deadline and Event types

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,25 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.format.FormatStyle;
+
 public class Deadline extends Task {
-    protected String by;
+    protected String by = "";
+    protected LocalDate date;
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        try {
+            this.date = LocalDate.parse(by);
+        } catch(DateTimeParseException de) {
+            this.by = by;
+        }
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + "(by: " + this.by + ")";
+        return "[D]" + super.toString() + "(by: "
+                + (by.isEmpty()?date.format(DateTimeFormatter
+                .ofLocalizedDate(FormatStyle.LONG)):this.by) + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,13 +1,26 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.format.FormatStyle;
+
 public class Event extends Task {
-    protected String at;
+    protected String at = "";
+    protected LocalDate date;
 
     public Event(String description, String at) {
         super(description);
-        this.at = at;
+        try {
+            date = LocalDate.parse(at);
+            System.out.println("Parsed date successfully");
+        } catch(DateTimeParseException de) {
+            this.at = at;
+        }
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + "(at: " + this.at + ")";
+        return "[E]" + super.toString() + "(at: "
+                + (at.isEmpty()?date.format(DateTimeFormatter
+                    .ofLocalizedDate(FormatStyle.LONG)):this.at) + ")";
     }
 }
