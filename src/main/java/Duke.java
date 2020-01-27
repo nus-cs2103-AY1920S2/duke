6f7b7c06f5ef984531a56ad56ab56adad897292b
Yonggiee <fongyongjie27@gmail.com>
2020-01-27 15:16:32 +0800

    completed OOP(still require testing)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,8 +2,9 @@ import java.time.format.DateTimeParseException;
 import java.io.IOException;
 
 public class Duke {
-    private static final String space = "    ";
-    private static final String home = System.getProperty("user.dir");
+    private static final String SPACE = "    ";
+    private static final String HOME = System.getProperty("user.dir");
+
     private Storage storage;
     private TaskList tasks;
     private Ui ui;
@@ -29,7 +30,6 @@ public class Duke {
         String input = ui.getCommand();
 
         while (!input.equals("bye")) {
-            String reply = "";
             String[] inputArr = input.split(" ");
             Command command = parser.parse(input);
             try {
@@ -38,32 +38,31 @@ public class Duke {
                     ui.goodBye();
                     break;
                 case LIST:
-                    reply = tasks.list(inputArr);
+                    ui.reply(tasks.list(inputArr));
                     break;
                 case DONE:
-                    reply += tasks.checkDone(inputArr);
+                    ui.reply(tasks.checkDone(inputArr));
                     storage.saveToSave(tasks);
                     break;
                 case DELETE:
-                    reply += tasks.removeTask(inputArr);
+                    ui.reply(tasks.removeTask(inputArr));
+                    storage.saveToSave(tasks);
                     break;
                 case CREATETODO:
-                    reply = createNew(inputArr, tasks);
+                    ui.reply(tasks.addTodo(inputArr));
                     storage.saveToSave(tasks);
                     break;
                 case CREATEEVENT:
-                    reply = createNew(inputArr, tasks);
+                    ui.reply(tasks.addEvent(inputArr));
                     storage.saveToSave(tasks);
                     break;
                 case CREATEDEADLINE:
-                    reply = createNew(inputArr, tasks);
+                    ui.reply(tasks.addDeadline(inputArr));
                     storage.saveToSave(tasks);
                     break;
                 default:
                     throw new UnknownCommandException();
                 }
-                // printing replies
-                ui.reply(reply);
             } catch (DukeException e) {
                 ui.showDukeError(e);
             } catch (IOException e) {
@@ -77,62 +76,6 @@ public class Duke {
 
     }
     public static void main(String[] args) {
-        new Duke(home).run();       
-    }
-
-    private static String createNew(String[] inputArr, TaskList tasks) throws DukeException {
-        int pointer;
-        String nameOfEvent;
-        String dateTime;
-        String date;
-        
-        int arrLength = inputArr.length;
-
-        String saveReply = "Saving now....:\n     ";
-        
-        if (inputArr[0].equals("event")) {
-            pointer = findIndex("/at", inputArr);
-            if (pointer == -1 || pointer == arrLength - 1) {
-                throw new NoDateTimeException();
-            }
-            nameOfEvent = combineString(inputArr, 1, pointer);
-            dateTime = combineString(inputArr, pointer + 1, arrLength);
-            saveReply += tasks.addEvent(nameOfEvent, dateTime);
-        } else if (inputArr[0].equals("deadline")) {
-            pointer = findIndex("/by", inputArr);
-            if (pointer == -1 || pointer == arrLength - 1) {
-                throw new NoDateException();
-            }
-            nameOfEvent = combineString(inputArr, 1, pointer);
-            date = combineString(inputArr, pointer + 1, arrLength);
-            saveReply += tasks.addDeadline(nameOfEvent, date);
-        } else if (inputArr[0].equals("todo")) {
-            nameOfEvent = combineString(inputArr, 1, arrLength);
-            saveReply += tasks.addTodo(nameOfEvent);
-        } else {
-            throw new UnknownCommandException();
-         }
-
-        return saveReply  + "\n" + space + "Aiyo still got " + tasks.size() + " task(s), what you doing sia";
-    }
-
-    private static int findIndex(String s, String[] arr){
-        for (int i = 0; i < arr.length; i++){
-            if (arr[i].equals(s)){
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    private static String combineString(String[] arr, int start, int end){
-        String ans = "";
-        for(int i = start; i < end; i++){
-            ans += arr[i];
-            if (i != end - 1){
-                ans += " ";
-            }
-        }
-        return ans;
+        new Duke(HOME).run();       
     }
 }
--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -5,7 +5,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class TaskList {
-    private static final String space = "    ";
+    private static final String SPACE = "    ";
     public static final DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
     public List<Task> tasks;
 
@@ -21,22 +21,49 @@ public class TaskList {
         return tasks.get(num);
     }
 
-    public String addTodo(String name) {
-        Task newT = new Todo(name);
-        this.tasks.add(newT);
-        return newT.toString();
+    public String addTodo(String[] inputArr) throws NoDescriptionException {
+        int arrLength = inputArr.length;
+        String saveReply = "Saving now....:\n     ";
+        String nameOfEvent = combineString(inputArr, 1, arrLength);
+        Task newT = new Todo(nameOfEvent);
+        tasks.add(newT);
+        saveReply += newT.toString();
+        return saveReply + "\n" + SPACE + "Aiyo still got " + tasks.size() + " task(s), what you doing sia";
     }
 
-    public String addEvent(String name, String dateTime) {
-        Task newE = new Event(name, dateTime);
-        this.tasks.add(newE);
-        return newE.toString();
+    public String addEvent(String[] inputArr) throws NoDateTimeException {
+        int arrLength = inputArr.length;
+        String saveReply = "Saving now....:\n     ";
+        int pointer = findIndex("/at", inputArr);
+
+        if (pointer == -1 || pointer == arrLength - 1) {
+            throw new NoDateTimeException();
+        }
+
+        String nameOfEvent = combineString(inputArr, 1, pointer);
+        String dateTime = combineString(inputArr, pointer + 1, arrLength);
+        Task newE = new Event(nameOfEvent, dateTime);
+        tasks.add(newE);
+        saveReply += newE.toString();
+        return saveReply + "\n" + SPACE + "Aiyo still got " + tasks.size() + " task(s), what you doing sia";
     }
 
-    public String addDeadline(String name, String date) {
-        Task newD = new Deadline(name, date);
+    public String addDeadline(String[] inputArr) throws DukeException {
+        int arrLength = inputArr.length;
+        String saveReply = "Saving now....:\n     ";
+        int pointer = findIndex("/by", inputArr);
+
+        if (pointer == -1 || pointer == arrLength - 1) {
+            throw new NoDateException();
+        }
+
+        String nameOfEvent = combineString(inputArr, 1, pointer);
+        String date = combineString(inputArr, pointer + 1, arrLength);
+        Task newD = new Deadline(nameOfEvent, date);
         this.tasks.add(newD);
-        return newD.toString();
+        saveReply += newD.toString();
+        
+        return saveReply + "\n" + SPACE + "Aiyo still got " + tasks.size() + " task(s), what you doing sia";
     }
 
     public String removeTask(String[] inputArr) throws DukeException{
@@ -49,7 +76,7 @@ public class TaskList {
             } else {
                 String whichTaskDelete = tasks.get(taskToDelete - 1).toString();
                 tasks.remove(taskToDelete - 1);
-                return "Okcan. I will remove this task:\n" + space + "  " + whichTaskDelete + "\n" + space
+                return "Okcan. I will remove this task:\n" + SPACE + "  " + whichTaskDelete + "\n" + SPACE
                         + "But you still have " + tasks.size() + " task(s) in the list.";
             }
         }
@@ -62,9 +89,9 @@ public class TaskList {
             for (int i = 0; i < tasks.size(); i++) {
                 int numbering = i + 1;
                 reply += (numbering + ".");
-                reply += (tasks.get(i) + "\n" + space);
+                reply += (tasks.get(i) + "\n" + SPACE);
             }
-            reply += "\n" + space + "I told you save liao loh........";
+            reply += "\n" + SPACE + "I told you save liao loh........";
         } else {
             String dateS = arr[1];
             LocalDate date = LocalDate.parse(dateS, inputFormatter);
@@ -74,11 +101,11 @@ public class TaskList {
                 if (currentTask instanceof Deadline || currentTask instanceof Event) {
                     if (currentTask.compareDate(date)) {
                         reply += (numbering++ + ".");
-                        reply += (currentTask + "\n" + space);
+                        reply += (currentTask + "\n" + SPACE);
                     }
                 }
             }
-            reply += ("\n" + space + "This are all the tasks with that date");
+            reply += ("\n" + SPACE + "This are all the tasks with that date");
         }
         return reply;
     }
@@ -86,10 +113,30 @@ public class TaskList {
     public String checkDone(String[] inputArr) {
         int taskNo = Integer.parseInt(inputArr[1]) - 1;
         this.tasks.set(taskNo, tasks.get(taskNo).complete());
-        return "Okcan, I mark this task as done:\n" + space + tasks.get(taskNo);
+        return "Okcan, I mark this task as done:\n" + SPACE + tasks.get(taskNo);
     }
 
     public int size() {
         return this.tasks.size();
+    }
+
+    private static int findIndex(String s, String[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals(s)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private static String combineString(String[] arr, int start, int end) {
+        String ans = "";
+        for (int i = start; i < end; i++) {
+            ans += arr[i];
+            if (i != end - 1) {
+                ans += " ";
+            }
+        }
+        return ans;
     }
 }
