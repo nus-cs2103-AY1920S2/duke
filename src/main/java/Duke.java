172a66c76d2f4d3b6161761aa89154a92fa0a4bf
Loh Sze Ying <bloom9334@hotmail.com>
2020-01-27 16:30:05 +0800

    Level 2 added

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String[] dukeList = new String[100];
+        int counter = 0;
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -12,11 +15,21 @@ public class Duke {
         System.out.println("What can I do for you?");
 
         String nxt;
-        nxt = sc.next();
+        nxt = sc.nextLine();
         while (nxt.equals("bye") == false) {
-            System.out.println("You said: " + nxt);
-            nxt = sc.next();
+            if (nxt.equals("list") == false) {
+                System.out.println("I have added: " + nxt + " to your list.");
+                dukeList[counter] = nxt;
+                counter++;
+                nxt = sc.nextLine();
+            } else {
+                for (int i = 0; i < counter; i++) {
+                    System.out.println((i+1) + ". " + dukeList[i]);
+                }
+                nxt = sc.nextLine();
+            }
         }
+
 
         System.out.println("Bye. Hope to see you again soon!");
 
