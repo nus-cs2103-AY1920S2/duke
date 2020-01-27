a329cbf31ddb47c4044b4f909af226db5dcf2dd1
OngWeiXiang <e0319124@u.nus.edu>
2020-01-27 23:07:26 +0800

    minor changes

--- src/main/java/UI.java
+++ src/main/java/UI.java
@@ -2,10 +2,7 @@
 public class UI {
 
     protected static void printIntro() {
-        String intro =
-                "Hello! I'm Duke\n" +
-                        "What can I do for you?";
-
+        String intro = "Hello! I'm Duke\n" + "What can I do for you?";
         System.out.println(wrapper(intro));
     }
 
