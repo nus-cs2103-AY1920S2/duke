a10f1d8bccc935468559923f523ad0ce58b3c6a4
JeremyLoh <jeremyloh03@gmail.com>
2020-01-27 22:56:18 +0800

    Command package: Updated JavaDoc for all classes in package

--- src/main/java/duke/command/EmptyInputCommand.java
+++ src/main/java/duke/command/EmptyInputCommand.java
@@ -4,12 +4,20 @@ import duke.Storage;
 import duke.Ui;
 import duke.task.TaskList;
 
+/** Represents a command that has empty user input */
 public class EmptyInputCommand extends Command {
 
     public EmptyInputCommand() {
         super(false);
     }
 
+    /**
+     * Displays command not found message to user
+     *
+     * @param tasks list of tasks
+     * @param ui prints output to user
+     * @param storage manages save file
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) {
         ui.commandNotFound();
--- src/main/java/duke/command/ListCommand.java
+++ src/main/java/duke/command/ListCommand.java
@@ -4,11 +4,19 @@ import duke.Storage;
 import duke.Ui;
 import duke.task.TaskList;
 
+/** Represents a command to list out all tasks in task list */
 public class ListCommand extends Command {
     public ListCommand() {
         super(false);
     }
 
+    /**
+     * Prints out list of tasks.
+     *
+     * @param tasks list of tasks
+     * @param ui prints information to user
+     * @param storage manages user save file
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) {
         // Print out all tasks
