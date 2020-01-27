52b9ec39316f9ae2c25d27d86579c8172b803a3e
XinPei-ng <elainengxp@gmail.com>
2020-01-27 14:10:24 +0800

    Edit ToDo class

--- src/main/java/ToDo.java
+++ src/main/java/ToDo.java
@@ -12,7 +12,7 @@ public class ToDo extends Task {
 
         return "[" + Types.ToDo + "]"
                 + "[" + super.getStatus() + "]"
-                + " " + taskDescription;
+                + " " + super.getTaskDescription();
     }
 
     @Override
