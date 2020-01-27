2a6d64df331aee5ae03ea8bf9fd51d50981653be
Zhang Yuanyu <e0322803@u.nus.edu>
2020-01-28 01:55:36 +0800

    Added a new FindCommand in command package.

---
+++ src/main/java/command/FindCommand.java
@@ -0,0 +1,32 @@
+package command;
+import java.io.IOException;
+
+import storage.Storage;
+
+import task.TaskList;
+
+import ui.Ui;
+
+public class FindCommand extends Command{
+    String keyword;
+    public FindCommand(String keyword){
+        this.keyword = keyword;
+    }
+    /**
+     * Displays the list of tasks that have the keyword.
+     * @param tasks The current task list.
+     * @param ui The user interface to be updated.
+     * @param storage The storage file to be updated.
+     * @throws IOException If the storage file is not found.
+     */
+    public void execute(TaskList tasks, Ui ui, Storage storage) throws IOException{
+        ui.printList(tasks.findTasks(keyword));
+    }
+    /**
+     * Returns the result of whether this is an exit program command.
+     * @return The result of whether this command can exit the program.
+     */
+    public boolean isExit(){
+        return false;
+    }
+}
