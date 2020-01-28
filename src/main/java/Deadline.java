4cf7a221f60b7adff9da7418988e2986d2a1126a
Aakanksha Rai <rai.aakanksha@gmail.com>
2020-01-28 18:48:34 +0800

    Add date formatting

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,12 +1,27 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Locale;
+
 public class Deadline extends Task {
     String due;
+    String parsedDue;
+
     public Deadline(String[] description) {
         super(description[0]);
         due = (description[1].split(" ", 2))[1];
+        modifyDateFormat();
+    }
+
+    public void modifyDateFormat() {
+        DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HHmm");
+        LocalDateTime parsedDate = LocalDateTime.parse(this.due, inputFormatter);
+        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("dd MMM yyyy HHmm");
+        this.parsedDue = parsedDate.format(outputFormatter);
     }
 
     @Override
     public String toString() {
-        return "[D] " + super.toString() + "(by: " + due + ")";
+        return "[D] " + super.toString() + "(by: " + this.parsedDue + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,12 +1,25 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task{
     String at;
+    String parsedAt;
+
     public Event(String[] description) {
         super(description[0]);
         at = (description[1].split(" ", 2))[1];
+        modifyDateFormat();
+    }
+
+    public void modifyDateFormat() {
+        DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HHmm");
+        LocalDateTime parsedDate = LocalDateTime.parse(this.at, inputFormatter);
+        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("dd MMM yyyy HHmm");
+        this.parsedAt = parsedDate.format(outputFormatter);
     }
 
     @Override
     public String toString() {
-        return "[E] " + super.toString() + "(at: " + at + ")";
+        return "[E] " + super.toString() + "(at: " + parsedAt + ")";
     }
 }
