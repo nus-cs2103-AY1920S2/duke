0840ab333b005516ceb873c0cd42cbf74b914b9e
chifa3 <foochifa@gmail.com>
2020-01-28 13:14:45 +0800

    Added javadoc comments to all tasks classes

--- src/main/java/duke/tasks/Deadline.java
+++ src/main/java/duke/tasks/Deadline.java
@@ -6,27 +6,53 @@ import duke.exception.BadDateException;
 
 import java.time.LocalDate;
 
+/**
+ * Represents a task with a deadline.
+ */
 public class Deadline extends Task {
+    /** Date to finish task by, in LocalDate format. */
     private LocalDate date;
 
+    /**
+     * @return date in LocalDate format.
+     */
     public LocalDate getDate() {
         return date;
     }
 
+    /**
+     * @return type of task in TType format.
+     */
     public TType getType() {
         return TType.DEADLINE;
     }
 
+    /**
+     * Constructs a deadline task with default false isDone.
+     * @param task Description of task.
+     * @param date Date to be done by, in String form.
+     * @throws BadDateException If date format is wrong.
+     */
     public Deadline(String task, String date) throws BadDateException {
         super(task);
         this.date = Parser.dateParser(date);
     }
 
+    /**
+     * Constructs a deadline task with user defined isDone.
+     * @param done Whether the task is done.
+     * @param task Description of task.
+     * @param date Date to be done by, in String form.
+     * @throws BadDateException If date format is wrong.
+     */
     public Deadline(boolean done, String task, String date) throws BadDateException {
         super(done, task);
         this.date = Parser.dateParser(date);
     }
 
+    /**
+     * @return String form of task, will show ✓ if done and ✗ if not.
+     */
     @Override
     public String toString() {
         if (done) return  "[D][✓] " + task + " (by: " +
--- src/main/java/duke/tasks/Event.java
+++ src/main/java/duke/tasks/Event.java
@@ -6,27 +6,53 @@ import duke.exception.BadDateException;
 
 import java.time.LocalDate;
 
+/**
+ * Represents an event, which is a task to attend to on a certain date.
+ */
 public class Event extends Task {
+    /** Date of event, in LocalDate format. */
     private LocalDate date;
 
+    /**
+     * @return date in LocalDate format.
+     */
     public LocalDate getDate() {
         return date;
     }
 
+    /**
+     * @return type of task in TType format.
+     */
     public TType getType() {
         return TType.EVENT;
     }
 
+    /**
+     * Constructs an event with default false isDone.
+     * @param task Description of event.
+     * @param date Date of event, in String form.
+     * @throws BadDateException If date format is wrong.
+     */
     public Event(String task, String date) throws BadDateException {
         super(task);
         this.date = Parser.dateParser(date);
     }
 
+    /**
+     * Constructs an event with user defined isDone.
+     * @param done Whether the event is done.
+     * @param task Description of event.
+     * @param date Date of event, in String form.
+     * @throws BadDateException If date format is wrong.
+     */
     public Event(boolean done, String task, String date) throws BadDateException {
         super(done, task);
         this.date = Parser.dateParser(date);
     }
 
+    /**
+     * @return String form of task, will show ✓ if done and ✗ if not.
+     */
     @Override
     public String toString() {
         if (done) return  "[E][✓] " + task + " (at: " +
