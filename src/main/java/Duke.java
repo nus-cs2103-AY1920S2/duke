9fc9cfdd037f2d880efe72ec333d038115b02e18
aaroncql <choozy.aaron@gmail.com>
2020-01-28 14:51:48 +0800

    Refactor to use common variables as class member

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,10 @@
 import java.util.Scanner;
 
 public class Duke {
-    private void initialise() {
+    private final Scanner sc = new Scanner(System.in);
+    private final TaskList taskList = new TaskList();
+
+    private void greet() {
         String greeting = "Hello, I am Duke " + new String(Character.toChars(0x1F481)) +", your personal assistant.";
         this.print(greeting);
     }
@@ -11,11 +14,8 @@ public class Duke {
     }
 
     private void getCommands() {
-        Scanner sc = new Scanner(System.in);
-        TaskList taskList = new TaskList();
-
         while (true) {
-            String userInput = sc.nextLine();
+            String userInput = this.sc.nextLine().trim();
             try {
                 Parser parser = new Parser(userInput);
                 Command command = parser.getCommand();
@@ -23,25 +23,25 @@ public class Duke {
                 if (command == Command.EXIT_DUKE) {
                     break;
                 } else if (command == Command.LIST_TASKS) {
-                    this.print(taskList.listTasks());
+                    this.print(this.taskList.listTasks());
                 } else if (command == Command.ADD_TODO) {
                     Todo todo = new Todo(parser.getDescription());
-                    taskList.addTask(todo);
-                    this.print("Added: " + todo.getFullDescription() + "\n    " + taskList.printNumTasks());
+                    this.taskList.addTask(todo);
+                    this.print("Added: " + todo.getFullDescription() + "\n    " + this.taskList.printNumTasks());
                 } else if (command == Command.ADD_DEADLINE) {
                     Deadline deadline = new Deadline(parser.getDescription(), parser.getBy());
-                    taskList.addTask(deadline);
-                    this.print("Added: " + deadline.getFullDescription() + "\n    " + taskList.printNumTasks());
+                    this.taskList.addTask(deadline);
+                    this.print("Added: " + deadline.getFullDescription() + "\n    " + this.taskList.printNumTasks());
                 } else if (command == Command.ADD_EVENT) {
                     Event event = new Event(parser.getDescription(), parser.getAt());
-                    taskList.addTask(event);
-                    this.print("Added: " + event.getFullDescription() + "\n    " + taskList.printNumTasks());
+                    this.taskList.addTask(event);
+                    this.print("Added: " + event.getFullDescription() + "\n    " + this.taskList.printNumTasks());
                 } else if (command == Command.MARK_TASK_AS_DONE) {
-                    Task task = taskList.markAsDone(parser.getTaskIndex());
-                    this.print("Marked as done: " + task.getFullDescription() + "\n    " + taskList.printNumTasks());
+                    Task task = this.taskList.markAsDone(parser.getTaskIndex());
+                    this.print("Marked as done: " + task.getFullDescription() + "\n    " + this.taskList.printNumTasks());
                 } else if (command == Command.DELETE_TASK) {
-                    Task task = taskList.removeTask(parser.getTaskIndex());
-                    this.print("Deleted: " + task.getFullDescription() + "\n    " + taskList.printNumTasks());
+                    Task task = this.taskList.removeTask(parser.getTaskIndex());
+                    this.print("Deleted: " + task.getFullDescription() + "\n    " + this.taskList.printNumTasks());
                 }
             } catch (DukeException e) {
                 this.print(e.getMessage());
@@ -58,7 +58,7 @@ public class Duke {
     public static void main(String[] args) {
         Duke duke = new Duke();
 
-        duke.initialise();
+        duke.greet();
 
         duke.getCommands();
 
