64bf552a9b78428270721bb4e4092736359f5d6e
bscrow <boshen@live.com>
2020-01-28 23:58:05 +0800

    Completed branch-A-JavaDoc

--- src/main/java/cathulhu/Storage.java
+++ src/main/java/cathulhu/Storage.java
@@ -10,6 +10,12 @@ import cathulhu.tasks.*;
 
 public class Storage {
 
+    /**
+     * Loads data from Input task file location, or creates an empty .txt file if the specified file do not exist yet
+     * @param TASKS_FILE File. file path to the data file
+     * @return TaskList. A TaskList object containing all parseable Task objects in the data file
+     * @throws IOException
+     */
     public TaskList loadTasksFile(File TASKS_FILE) throws IOException {
 
         boolean isCreated = TASKS_FILE.createNewFile();
@@ -63,6 +69,12 @@ public class Storage {
         return taskData;
     }
 
+    /**
+     * Writes all Tasks in the TaskList object to the file at the specified file location
+     * @param TASKS_FILE File. path to the specified data file where Tasks are recorded
+     * @param tasks TaskList. TaskList object storing all current Tasks.
+     * @throws IOException
+     */
     public void writeTasksFile(File TASKS_FILE, TaskList tasks) throws IOException {
         FileWriter fw = new FileWriter(TASKS_FILE);
         for (int i = 0; i < tasks.size(); i++) {
