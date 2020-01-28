4d10b730ffc628b632b9486417b84c9829d0da1c
Guofeng-Tang <tgf.tang.guofeng@gmail.com>
2020-01-29 00:09:35 +0800

    Refactor common methods in switch case

--- src/main/java/seedu/duke/Parser.java
+++ src/main/java/seedu/duke/Parser.java
@@ -1,6 +1,11 @@
 package seedu.duke;
 
-import seedu.duke.command.*;
+import seedu.duke.command.AddCommand;
+import seedu.duke.command.Command;
+import seedu.duke.command.DeleteCommand;
+import seedu.duke.command.DoneCommand;
+import seedu.duke.command.ExitCommand;
+import seedu.duke.command.ListCommand;
 import seedu.duke.exception.DukeEmptyDescriptionException;
 import seedu.duke.exception.DukeException;
 import seedu.duke.exception.DukeNoKeywordException;
@@ -21,64 +26,72 @@ class Parser {
 
         try {
             switch (Command.Type.valueOf(commandString)) {
-                case bye:
-                    toReturn = new ExitCommand();
-                    break;
-                case list:
-                    toReturn = new ListCommand();
-                    break;
-                case done:
-                    selectedTaskIndex = Integer.parseInt(splitInput[1]) - 1;
-                    toReturn = new DoneCommand(selectedTaskIndex);
-                    break;
-                case delete:
-                    selectedTaskIndex = Integer.parseInt(splitInput[1]) - 1;
-                    toReturn = new DeleteCommand(selectedTaskIndex);
-                    break;
-                case todo:
-                case deadline:
-                case event:
-                    String taskDescription;
-
-                    if (splitInput.length == 1) {
-                        throw new DukeEmptyDescriptionException("OOPS!!! The description of a task cannot be empty.");
-                    }
-                    String[] inputWithoutCommand = Arrays.copyOfRange(splitInput, 1, splitInput.length);
-
-                    switch (Command.Type.valueOf(commandString)) {
-                        case todo:
-                            // empty string array would become empty string
-                            taskDescription = String.join(" ", inputWithoutCommand);
-                            toReturn = new AddCommand(Command.Type.todo, taskDescription);
-                            break;
-                        case deadline:
-                        case event:
-                            String keyword = commandString.equals(Command.Type.deadline.getCommand())
-                                    ? Command.Type.deadline_by.getCommand()
-                                    : Command.Type.event_at.getCommand();
-                            int keywordIndex = Arrays.asList(splitInput).indexOf(keyword);
-                            if (keywordIndex == -1) {
-                                throw new DukeNoKeywordException("OOPS!!! The description must contain a keyword.");
-                            }
-
-                            taskDescription = String.join(" ",
-                                    Arrays.copyOfRange(splitInput, 1, keywordIndex));
-                            String deadlineOrTime = String.join(" ",
-                                    Arrays.copyOfRange(splitInput, keywordIndex + 1, splitInput.length));
-                            LocalDate date = LocalDate.parse(deadlineOrTime);
-
-                            toReturn = commandString.equals(Command.Type.deadline.getCommand())
-                                    ? new AddCommand(Command.Type.deadline, taskDescription, date)
-                                    : new AddCommand(Command.Type.event, taskDescription, date);
-                    }
-                    break;
-                default:
-                    break;
+            case bye:
+                toReturn = new ExitCommand();
+                break;
+            case list:
+                toReturn = new ListCommand();
+                break;
+            case done:
+                selectedTaskIndex = Integer.parseInt(splitInput[1]) - 1;
+                toReturn = new DoneCommand(selectedTaskIndex);
+                break;
+            case delete:
+                selectedTaskIndex = Integer.parseInt(splitInput[1]) - 1;
+                toReturn = new DeleteCommand(selectedTaskIndex);
+                break;
+            case todo:
+                toReturn = createTodoCommand(splitInput);
+                break;
+            case deadline:
+            case event:
+                toReturn = createDeadLineOrEventCommand(splitInput, commandString);
+                break;
+            default:
+                break;
             }
         } catch (IllegalArgumentException e) {
             throw new DukeWrongCommandException("OOPS!!! I'm sorry, but I don't know what that means :-(");
         }
 
         return toReturn;
+    }
+
+    private static void isSplitInputLengthLargerThanOne(String[] splitInput) throws DukeEmptyDescriptionException {
+        if (splitInput.length == 1) {
+            throw new DukeEmptyDescriptionException("OOPS!!! The description of a task cannot be empty.");
+        }
+    }
+
+    private static Command createTodoCommand(String[] splitInput) throws DukeEmptyDescriptionException {
+        isSplitInputLengthLargerThanOne(splitInput);
+
+        String[] inputWithoutCommand = Arrays.copyOfRange(splitInput, 1, splitInput.length);
+        // empty string array would become empty string
+        String taskDescription = String.join(" ", inputWithoutCommand);
+        return new AddCommand(Command.Type.todo, taskDescription);
+    }
+
+    private static Command createDeadLineOrEventCommand(String[] splitInput, String commandString)
+            throws DukeNoKeywordException, DukeEmptyDescriptionException {
+        isSplitInputLengthLargerThanOne(splitInput);
+
+        String keyword = commandString.equals(Command.Type.deadline.getCommand())
+                ? Command.Type.deadline_by.getCommand()
+                : Command.Type.event_at.getCommand();
+        int keywordIndex = Arrays.asList(splitInput).indexOf(keyword);
+        if (keywordIndex == -1) {
+            throw new DukeNoKeywordException("OOPS!!! The description must contain a keyword.");
+        }
+
+        String taskDescription = String.join(" ",
+                Arrays.copyOfRange(splitInput, 1, keywordIndex));
+        String deadlineOrTime = String.join(" ",
+                Arrays.copyOfRange(splitInput, keywordIndex + 1, splitInput.length));
+        LocalDate date = LocalDate.parse(deadlineOrTime);
+
+        return commandString.equals(Command.Type.deadline.getCommand())
+                ? new AddCommand(Command.Type.deadline, taskDescription, date)
+                : new AddCommand(Command.Type.event, taskDescription, date);
     }
 }
