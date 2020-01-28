c05db2bde9eebb1362069375cecdb82485c45c8f
WillySeahh <willy.seah@hotmail.com>
2020-01-29 00:03:34 +0800

    Level-9 Version 2 (updated with more test cases)

--- src/test/java/DukeTest.java
+++ src/test/java/DukeTest.java
@@ -1,13 +1,23 @@
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DukeTest {
     @Test
-    public void dummyTest(){
-        assertEquals(2, 2);
+    public void testTaskName() {
+        Deadline deadlineTest = new Deadline("test", LocalDate.parse("2020-01-01"));
+        assertEquals("test", deadlineTest.description);
     }
+
+    @Test
+    public void testDoneStatus() {
+        Deadline deadlineTest = new Deadline("test", LocalDate.parse("2020-01-01"));
+        assertEquals(false, deadlineTest.isDone);
+    }
+
 
 
 }
