5b140c9f8b95f9f060969a51343eee2d8223db97
xuanxinng <ngxuanxin@gmail.com>
2020-01-28 19:01:30 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
-import java.util.Scanner;
+import java.lang.reflect.Array;
+import java.util.*;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -9,17 +10,31 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
 
         String line = "     -----------------------------------------------------------------\n";
+        String tab = "      ";
         String greet = line
-                + "     Hello! I'm Duke\n"
-                + "     What can I do for you?\n"
+                + tab + "Hello! I'm Duke\n"
+                + tab + "What can I do for you?\n"
                 + line;
         System.out.println(greet);
 
+        ArrayList list = new ArrayList();
+
         Scanner input = new Scanner(System.in);
 
-        while(!input.hasNext("bye")) {
+        while (!input.hasNext("bye")) {
             String next = input.nextLine();
-            System.out.println(line + "     " + next + "\n" + line);
+
+            if (next.equals("list")) {
+                System.out.print(line);
+                for (int i = 0; i < list.size(); i++) {
+                    int num = i + 1;
+                    System.out.println(tab + num + ": " + list.get(i));
+                }
+                System.out.print(line);
+            } else {
+                System.out.println(line + "     added: " + next + "\n" + line);
+                list.add(next);
+            }
         }
         System.out.println(line + "     Bye. Hope to see you again soon!\n" + line);
     }
