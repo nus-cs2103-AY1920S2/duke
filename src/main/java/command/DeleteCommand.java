8d926cbafe261207d41ecd2d14e95647998c8aa4
TheElectricBlender <yodudee97@gmail.com>
2020-01-29 20:53:31 +0800

    Revert "Implemented Javadocs for command package for all command classes."
    This reverts commit b6e18d27f25b7c6b4ec8ec3d1a2201855131009c.

--- src/main/java/command/DeleteCommand.java
+++ src/main/java/command/DeleteCommand.java
@@ -5,10 +5,6 @@ import task.*;
 import ui.*;
 import java.io.IOException;
 
-/**
- * Represents the command when delete is called with the id of task as argument. A <code>DeleteCommand</code>
- * object represents the command when deleting the task with id e.g. <code>5</code>
- */
 public class DeleteCommand extends Command {
     protected int id;
 
@@ -16,15 +12,6 @@ public class DeleteCommand extends Command {
         this.id = id;
     }
 
-
-    /**
-     * Executes the command, adding the task to the task list and also updates UI and storage file.
-     * If storage file invalid, exception thrown and error message is returned.
-     * @param taskList task list to be updated
-     * @param ui User interface displayed
-     * @param storage Save file to be updated
-     * @throws IOException If storage file not found
-     */
     @Override
     public void execute(TaskList taskList, Ui ui, Storage storage) throws IOException {
         Task task = taskList.get(id - 1);
@@ -33,10 +20,6 @@ public class DeleteCommand extends Command {
         storage.save(taskList);
     }
 
-    /**
-     * Returns whether it is an exit command
-     * @return The result of whether this command causes an exit.
-     */
     @Override
     public boolean isExit() {
         return false;
--- src/main/java/command/ListCommand.java
+++ src/main/java/command/ListCommand.java
@@ -5,28 +5,13 @@ import task.*;
 import ui.*;
 import java.io.IOException;
 
-/**
- * Represents a command which shows the list of tasks left. A <code>ListCommand</code> object corresponds to
- * an ArrayList which saves the tasks.
- */
 public class ListCommand extends Command {
 
-    /**
-     * Executes the command and triggers UI to display the list
-     * @param taskList task list to be updated
-     * @param ui User interface displayed
-     * @param storage Save file to be updated
-     * @throws IOException
-     */
     @Override
     public void execute(TaskList taskList, Ui ui, Storage storage) throws IOException {
         ui.displayTasks(taskList);
     }
 
-    /**
-     * Returns whether it is an exit command
-     * @return The result of whether this command causes an exit.
-     */
     @Override
     public boolean isExit() {
         return false;
