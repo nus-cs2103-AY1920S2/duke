2194804be572d80f53a176b5dcceb70ddba9d27e
Hanming Zhu <hanming@huawen.com>
2020-01-29 19:18:28 +0800

    feat: change deadline implementation to use localdatetime
    Change Deadline to accept LocalDateTime instead of String for deadline. Changed the appropriate toString() method as well.
    
    To do: change Duchess to utilise new implementation of Deadline.

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,17 @@
-public class Deadline extends Task {
-    private String deadline;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-    Deadline(String description, String deadline) {
+public class Deadline extends Task {
+    private LocalDateTime deadline;
+
+    Deadline(String description, LocalDateTime deadline) {
         super(description);
         this.deadline = deadline;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + this.deadline + ")";
+        return "[D]" + super.toString() + " (by: "
+                + this.deadline.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 }
