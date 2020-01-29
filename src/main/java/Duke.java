3c01239812e27dff676104fc5ba91a669105ac49
Joycelyn Teo <joycelyn.teo99@gmail.com>
2020-01-29 18:20:12 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -15,22 +15,33 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
 
         //create list
-        List<String> list = new ArrayList<String>();
+        List<Task> list = new ArrayList<Task>();
 
         while (true) {
             String command = sc.nextLine();
+            String arr[] = command.split(" ");
+            String firstWord = arr[0];
 
             if (command.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             } else if (command.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 if (!list.isEmpty()) {
                     for (int i=0; i<list.size(); i++) {
-                        System.out.println((i+1) + ". " + list.get(i));
+                        Task t = list.get(i);
+                        System.out.println((i+1) + ".[" + t.getStatusIcon() + "] " + t.getDescription());
                     }
                 }
+            } else if (firstWord.equals("done")){
+                String secNum = arr[1];
+                System.out.println("Nice! I've marked this task as done:");
+                Task t = list.get(Integer.parseInt(secNum) - 1);
+                t.setDone();
+                System.out.println("[" + t.getStatusIcon() + "] " + t.getDescription());
             } else {
-                list.add(command);
+                Task t = new Task(command);
+                list.add(t);
                 System.out.println("added: " + command);
             }
         }
