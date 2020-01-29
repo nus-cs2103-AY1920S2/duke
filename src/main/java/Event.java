b57cfe83199fec1e2671c2716ea1a3d63f02bbdb
Cherweijie <44356103+Cherweijie@users.noreply.github.com>
2020-01-29 16:20:03 +0800

    Included formatting output for level-8

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,7 +1,10 @@
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Event extends Task {
     protected LocalDate dateAt;
+    DateTimeFormatter formattedOutput = DateTimeFormatter.ofPattern("MMM d yyyy");
+    DateTimeFormatter input = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     Event(String taskName, LocalDate dateAt) {
         super(taskName);
@@ -10,6 +13,6 @@ public class Event extends Task {
 
     @Override
     public String toString() {
-            return "[E]" + super.toString() + "(at:" + dateAt.toString() + ")";
+            return "[E]" + super.toString() + "(at:" + dateAt.format(formattedOutput) + ")";
     }
 }
