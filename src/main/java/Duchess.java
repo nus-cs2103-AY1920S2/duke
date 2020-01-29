7ed37538858afe98aa1182e519b81776a8213f38
Hanming Zhu <hanming@huawen.com>
2020-01-29 22:21:00 +0800

    feat: add better input handling for deadlines
    Allows creation of Deadline via timings such as today, tonight, tomorrow, day of the week, and two different formats of datetimes.
    
    Meets the Level-8 requirements.

--- src/main/java/Duchess.java
+++ src/main/java/Duchess.java
@@ -1,7 +1,9 @@
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
+import java.time.temporal.TemporalAdjusters;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
@@ -126,14 +128,47 @@ class Duchess {
                         "I don't know when is your deadline! Please use /by [deadline here].");
             }
             LocalDateTime deadline;
-            try {
-                deadline = LocalDateTime.parse(details.get(1).trim(), dateTimeFormatter);
-            } catch (DateTimeParseException e1) {
+            String timeDetails = details.get(1).trim().toLowerCase();
+            switch (timeDetails) {
+            case "today":
+                deadline = LocalDate.now().atTime(17, 00);
+                break;
+            case "tonight":
+                deadline = LocalDate.now().atTime(21, 00);
+                break;
+            case "tomorrow":
+                deadline = LocalDate.now().plusDays(1).atTime(17, 00);
+                break;
+            case "monday":
+                // Fallthrough
+            case "tuesday":
+                // Fallthrough
+            case "wednesday":
+                // Fallthrough
+            case "thursday":
+                // Fallthrough
+            case "friday":
+                // Fallthrough
+            case "saturday":
+                // Fallthrough
+            case "sunday":
+                deadline = LocalDate.now()
+                        .with(TemporalAdjusters.next(DayOfWeek.valueOf(timeDetails.toUpperCase())))
+                        .atTime(17, 00);
+                break;
+            default:
                 try {
-                    deadline = LocalDate.parse(details.get(1).trim(), dateFormatter).atStartOfDay();
-                } catch (DateTimeParseException e2) {
-                    throw new DuchessException("Please use the following format for the datetime instead:\n" +
-                            "\td/M/yyyy HHmm OR d/M/yyyy");
+                    deadline = LocalDateTime.parse(timeDetails, dateTimeFormatter);
+                } catch (DateTimeParseException e1) {
+                    try {
+                        deadline = LocalDate.parse(timeDetails, dateFormatter).atStartOfDay();
+                    } catch (DateTimeParseException e2) {
+                        throw new DuchessException(
+                                "Please use the following format for the datetime instead:\n"
+                                        + "\td/M/yyyy HHmm OR d/M/yyyy\n"
+                                        + "\tOR today/tonight/tomorrow\n"
+                                        + "\tOR day of the week (e.g. Monday, Tuesday)");
+                    }
                 }
             }
             newTask = new Deadline(details.get(0).trim(), deadline);
