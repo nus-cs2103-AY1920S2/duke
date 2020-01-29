39445250a36ef32b4a808a84e57a198cbcb9af77
nhamhung <nhamhung.gttn@gmail.com>
2020-01-30 07:42:05 +0800

    Abstract out the bot commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,8 @@ import java.util.ArrayList;
 import java.util.NoSuchElementException;
 
 public class Duke {
+    public static ArrayList<Task> tasks = new ArrayList<>();
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -13,73 +15,55 @@ public class Duke {
         System.out.println("What can I do for you?");
 
         Scanner sc = new Scanner(System.in);
-        ArrayList<Task> tasks = new ArrayList<>();
 
         while (sc.hasNext()) {
             try {
                 String t = sc.nextLine();
                 String input[] = t.split(" ", 2);
-                if (input[0].equals("bye")) {
+                String userCommand = input[0];
+                if (userCommand.equals("bye")) {
                     System.out.println("Bye. Hope to see you again soon!");
                     return;
-                } else if (input[0].equals("list")) {
+                } else if (userCommand.equals("list")) {
                     if (tasks.size() == 0) {
                         System.out.println("There is no task in your list! Please continue...");
                         continue;
                     }
-                    System.out.println("Here are the tasks in your list:");
-                    for (int i = 1; i <= tasks.size(); i++) {
-                        System.out.println(i + ".  " + tasks.get(i - 1));
-                    }
-                    continue;
-                } else if (input[0].equals("done")) {
+                    printTasks();
+                } else if (userCommand.equals("done")) {
                     int idx = Integer.parseInt(input[1]) - 1;
-                    if (tasks.size() <= idx || idx < 0) {
-                        throw new InvalidIndexException("This index does not match any task in our list!! Try again...");
-                    }
-                    tasks.get(idx).finishTask();
-                    System.out.println("Nice! I've marked this task as done:");
-                    System.out.println(tasks.get(idx));
-                } else if (input[0].equals("todo")) {
+
+                    done(idx);
+                } else if (userCommand.equals("todo")) {
                     if (input.length == 1) {
                         throw new EmptyDescriptionException("☹ OOPS!!! The description of a todo cannot be empty.");
                     }
-                    System.out.println("Got it. I've added this task:");
-                    Todo todo = new Todo(t.substring(5));
-                    System.out.println("   " + todo);
-                    tasks.add(todo);
-                    System.out.println("Now you have " + tasks.size() + " task(s) in the list.");
-                } else if (input[0].equals("deadline")) {
+                    String task = t.substring(5);
+
+                    toDo(task);
+                } else if (userCommand.equals("deadline")) {
                     if (input.length == 1) {
                         throw new EmptyDescriptionException("☹ OOPS!!! The description of a deadline cannot be empty.");
                     }
-                    System.out.println("Got it. I've added this task:");
                     String deadline = t.split("/")[1].substring(3);
                     String task = t.split("/")[0].split(" ", 2)[1];
-                    Deadline newDeadline = new Deadline(task, deadline);
-                    System.out.println("   " + newDeadline);
-                    tasks.add(newDeadline);
-                    System.out.println("Now you have " + tasks.size() + " task(s) in the list.");
-                } else if (input[0].equals("event")) {
+
+                    deadLine(task, deadline);
+                } else if (userCommand.equals("event")) {
                     if (input.length == 1) {
                         throw new EmptyDescriptionException("☹ OOPS!!! The description of an event cannot be empty.");
                     }
-                    System.out.println("Got it. I've added this task:");
                     String event = t.split("/")[1].substring(3);
                     String task = t.split("/")[0].split(" ", 2)[1];
-                    Event newEvent = new Event(task, event);
-                    System.out.println("   " + newEvent);
-                    tasks.add(newEvent);
-                    System.out.println("Now you have " + tasks.size() + " task(s) in the list.");
-                } else if (input[0].equals("delete")) {
+
+                    event(task, event);
+                } else if (userCommand.equals("delete")) {
                     int idx = Integer.parseInt(input[1]) - 1;
                     if (tasks.size() <= idx || idx < 0) {
                         throw new InvalidIndexException("This index does not match any task in our list!! Try again...");
                     }
-                    System.out.println("Noted. I've removed this task:");
-                    System.out.println(tasks.get(idx));
-                    tasks.remove(tasks.get(idx));
-                    System.out.println("Now you have " + tasks.size() + " tasks in the list");
+
+                    delete(idx);
                 }   else {
                     throw new CommandNotFoundException("Sorry I don't recognize this command SIA!");
                 }
@@ -88,5 +72,52 @@ public class Duke {
                 continue;
             }
         }
+    }
+
+    public static void printTasks() {
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 1; i <= tasks.size(); i++) {
+            System.out.println(i + ".  " + tasks.get(i - 1));
+        }
+    }
+
+    public static void done(int idx) throws InvalidIndexException {
+        if (tasks.size() <= idx || idx < 0) {
+            throw new InvalidIndexException("This index does not match any task in our list!! Try again...");
+        }
+        tasks.get(idx).finishTask();
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println(tasks.get(idx));
+    }
+
+    public static void toDo(String task) {
+        System.out.println("Got it. I've added this task:");
+        Todo todo = new Todo(task);
+        System.out.println("   " + todo);
+        tasks.add(todo);
+        System.out.println("Now you have " + tasks.size() + " task(s) in the list.");
+    }
+
+    public static void deadLine(String task, String deadline) {
+        System.out.println("Got it. I've added this task:");
+        Deadline newDeadline = new Deadline(task, deadline);
+        System.out.println("   " + newDeadline);
+        tasks.add(newDeadline);
+        System.out.println("Now you have " + tasks.size() + " task(s) in the list.");
+    }
+
+    public static void event(String task, String event) {
+        System.out.println("Got it. I've added this task:");
+        Event newEvent = new Event(task, event);
+        System.out.println("   " + newEvent);
+        tasks.add(newEvent);
+        System.out.println("Now you have " + tasks.size() + " task(s) in the list.");
+    }
+
+    public static void delete(int idx) {
+        System.out.println("Noted. I've removed this task:");
+        System.out.println(tasks.get(idx));
+        tasks.remove(tasks.get(idx));
+        System.out.println("Now you have " + tasks.size() + " tasks in the list");
     }
 }
