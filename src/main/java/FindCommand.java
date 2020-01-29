bd4ba385ac56bbe3c1426c38f926200771b5a0f0
zhouxinwei97 <45256284+zhouxinwei97@users.noreply.github.com>
2020-01-29 23:39:00 +0800

    Merge all levels

--- src/main/java/FindCommand.java
+++ src/main/java/FindCommand.java
@@ -1,10 +1,17 @@
-
 import java.util.ArrayList;
 
+/**
+ * Find Command allows a user to find tasks that are similar to the keyword specified.
+ */
 public class FindCommand extends Command {
 
     private String keyword;
 
+    /**
+     * Creates an instance find command to be executed.
+     * @param response full instruction given by the user.
+     * @param keyword keyword that was parsed by Parser class.
+     */
     public FindCommand(String response, String keyword) {
 
         super(response);
@@ -12,6 +19,13 @@ public class FindCommand extends Command {
 
     }
 
+    /**
+     * Executes the find command, allowing the program find the task with the keyword specified.
+     * @param tasksStorage storage for the task, deals with storing data and retrieving data from hard disk.
+     * @param taskList the list where all the tasks is being stored.
+     * @param ui ui that is responsible for interaction with the user.
+     * @throws DukeException if no item matching the keyword is found.
+     */
     public void execute(Storage tasksStorage, TaskList taskList, Ui ui) throws DukeException {
 
         ArrayList<Task> foundTask = new ArrayList<>();
