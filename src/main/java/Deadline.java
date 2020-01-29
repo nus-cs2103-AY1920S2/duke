5f07a238922e9c7c6278f7edc282e3933c24f24f
NNpanpan <dpnhat.nguyen@gmail.com>
2020-01-29 23:33:09 +0800

    Used LocalDate

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,20 +1,33 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
     private final String bulletin = "[D]";
 
     private String byTime;
     private TaskType type;
+    private LocalDate localDate;
+    private LocalTime localTime;
 
     public Deadline(String name, String byTime) {
         super(name);
         this.byTime = byTime;
         type = TaskType.deadline;
+        convertDateTime(byTime);
+    }
+
+    private void convertDateTime(String byTime) {
+        localDate = LocalDate.parse(byTime.split(" ")[1]);
     }
 
     @Override
     public String toString() {
         if (super.isDone)
-            return bulletin + " [" + doneSymbol + "] " + name;
+            return bulletin + " [" + doneSymbol + "] " + name + "by "
+                    + localDate.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
         else
-            return bulletin + " [" + notDoneYetSymbol + "] " + name;
+            return bulletin + " [" + notDoneYetSymbol + "] " + name + "by "
+                    + localDate.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,20 +1,33 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
     private final String bulletin = "[E]";
 
     private String atTime;
     private TaskType type;
+    private LocalDate localDate;
+    private LocalTime localTime;
 
     public Event(String name, String atTime) {
         super(name);
         this.atTime = atTime;
         type = TaskType.event;
+        convertDateTime(atTime);
+    }
+
+    private void convertDateTime(String atTime) {
+        localDate = LocalDate.parse(atTime.split(" ")[1]);
     }
 
     @Override
     public String toString() {
         if (super.isDone)
-            return bulletin + " [" + doneSymbol + "] " + name;
+            return bulletin + " [" + doneSymbol + "] " + name + "at "
+                    + localDate.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
         else
-            return bulletin + " [" + notDoneYetSymbol + "] " + name;
+            return bulletin + " [" + notDoneYetSymbol + "] " + name + "at "
+                    + localDate.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
     }
 }
