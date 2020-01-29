1006366989b2df51e2613cc88d78a6995af127e8
xCelestialPhoenix <terenceng1996@gmail.com>
2020-01-29 15:11:51 +0800

    Updated testing

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -19,7 +19,7 @@ public class Task {
     }
 
     public String getStatusIcon() {
-        return (isDone ? "✓" : "✘");
+        return (isDone ? "\u2713" : "\u2718");
     }
 
     public void markAsDone() throws DuplicateMarkAelitaException {
--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -5,80 +5,100 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class TaskTest {
 
+    //Class variables does not reset after a test ends
+
     @Test
     void getTotalTaskCount() {
         Task task1 = new Task("test1");
         Task task2 = new Task("test2");
         assertEquals(2, Task.getTotalTaskCount());
+        Task.totalTaskCount = 0;
     }
 
     @Test
     void getDescription() {
         Task task = new Task("test");
         assertEquals("test", task.getDescription());
+        Task.totalTaskCount = 0;
     }
 
     @Test
     void getStatusIcon_notDone() {
+        assertEquals(0, Task.totalTaskCount);
         Task task = new Task("test");
-        assertEquals("✘", task.getDescription());
+        assertEquals("\u2718", task.getStatusIcon());
+        Task.totalTaskCount = 0;
     }
 
     @Test
     void getStatusIcon_Done() {
+        assertEquals(0, Task.totalTaskCount);
         Task task = new Task("test");
         task.isDone = true;
-        assertEquals("✓", task.getStatusIcon());
+        assertEquals("\u2713", task.getStatusIcon());
+        Task.totalTaskCount = 0;
     }
 
     @Test
     void markAsDone() {
+        assertEquals(0, Task.totalTaskCount);
         Task task = new Task("test");
         try {
             task.markAsDone();
-            assertEquals("✓", task.getStatusIcon());
+            assertEquals("\u2713", task.getStatusIcon());
         } catch (DuplicateMarkAelitaException e) {
             Assertions.fail();
+        } finally {
+            Task.totalTaskCount = 0;
         }
     }
 
     @Test
     void markAsDone_duplicateDone_exceptionThrown() {
+        assertEquals(0, Task.totalTaskCount);
         Task task = new Task("test");
         task.isDone = true;
         try {
             task.markAsDone();
             Assertions.fail();
         } catch (DuplicateMarkAelitaException e) {
-            assertEquals("✓", task.getStatusIcon());
+            assertEquals("\u2713", task.getStatusIcon());
+        } finally {
+            Task.totalTaskCount = 0;
         }
     }
 
     @Test
     void setTotalTaskCount() {
-
+        assertEquals(0, Task.totalTaskCount);
         try {
             Task.setTotalTaskCount(2);
             assertEquals(2, Task.totalTaskCount);
         } catch (InvalidArgumentAelitaException e) {
             Assertions.fail();
+        } finally {
+            Task.totalTaskCount = 0;
         }
     }
 
     @Test
     void setTotalTaskCount_negativeArgument_exceptionThrown() {
-
+        assertEquals(0, Task.totalTaskCount);
         try {
             Task.setTotalTaskCount(-2);
             Assertions.fail();
         } catch (InvalidArgumentAelitaException e) {
             assertEquals(0, Task.totalTaskCount);
+        } finally {
+            Task.totalTaskCount = 0;
         }
     }
 
     @Test
     void testToString() {
+        assertEquals(0, Task.totalTaskCount);
         Task test = new Task("Test 1");
-        assertEquals("[✘] Test 1", test.toString());
+        assertEquals("[\u2718] Test 1", test.toString());
+        Task.totalTaskCount = 0;
     }
 }
