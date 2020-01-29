06f7e88bc3830c97c5a971fea5c5faee9cf320a0
Hanming Zhu <hanming@huawen.com>
2020-01-30 01:57:15 +0800

    refactor: shift filePath entry point to be in Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Duchess duchess = new Duchess();
+        Duchess duchess = new Duchess("data/tasks.json");
         duchess.run();
     }
 }
