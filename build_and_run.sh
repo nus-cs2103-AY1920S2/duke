81a24de7ad4c751363d703245153cd20bf90ccf5
Aloysius Chan <alloystory@gmail.com>
2020-01-29 20:54:48 +0800

    added script to test app

---
+++ build_and_run.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# -----------------------------------------------------------------------------------------
+# Step 1: Compiling
+# Create bin directory if it doesn't exist
+if [ ! -d "./bin" ]
+then
+    mkdir ./bin
+fi
+
+# Compile the code into the bin folder, terminates if error occurred
+if ! javac -cp ./src/main/java -Xlint:none -d ./bin ./src/main/java/Duke.java
+then
+    echo "********** BUILD FAILURE **********"
+    exit 1
+fi
+
+# -----------------------------------------------------------------------------------------
+# Step 2: Testing
+java -cp ./bin Duke
