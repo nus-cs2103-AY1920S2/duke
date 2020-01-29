fac4f210552364eed7c891f09c2b7d78459f6d06
Alden <49450743+alcen@users.noreply.github.com>
2020-01-30 00:19:28 +0800

    JUnit tests for Todo, CommandParser (A-JUnit)

---
+++ src/test/java/CommandParserTest.java
@@ -0,0 +1,26 @@
+import bot.command.CommandParser;
+import bot.command.Instruction;
+import bot.command.exception.UnknownInstructionException;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CommandParserTest {
+    @Test(expected = UnknownInstructionException.class)
+    public void parse_randomWords_exceptionThrown() throws UnknownInstructionException {
+        new CommandParser().parse("beehive valley spring darkness");
+    }
+
+    @Test
+    public void parse_event_success() throws UnknownInstructionException {
+        assertEquals(new CommandParser().parse("event party /at my place"),
+                Instruction.STORE_EVENT);
+    }
+
+    @Test
+    public void parse_deadline_success() throws UnknownInstructionException {
+        assertEquals(new CommandParser().parse("deadline project"),
+                Instruction.STORE_DDL);
+    }
+}
---
+++ src/test/java/TodoTest.java
@@ -0,0 +1,29 @@
+import bot.command.exception.InadequateArgumentsException;
+import bot.task.Todo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class TodoTest {
+    @Test(expected = InadequateArgumentsException.class)
+    public void createTodo_noArguments_exceptionThrown() throws InadequateArgumentsException {
+        new Todo("todo");
+    }
+
+    @Test
+    public void createTodo_withDescription_success() throws InadequateArgumentsException {
+        Todo td = new Todo("todo return book");
+        assertEquals(td.getTaskDetails(), "return book");
+    }
+
+    @Test
+    public void markAsDone_newTodo_success() throws InadequateArgumentsException {
+        Todo td = new Todo("todo buy groceries");
+        assertFalse(td.isDone());
+        td.markAsDone();
+        assertTrue(td.isDone());
+    }
+}
