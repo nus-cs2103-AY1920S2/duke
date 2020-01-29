d8a61c2def79a0735a6d5d2f8013cc94bb874d38
alam8064 <59945273+alam8064@users.noreply.github.com>
2020-01-29 22:39:39 +0800

    Level 8

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,15 +1,19 @@
 import java.util.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Event extends Task {
     protected String at;
+    protected LocalDate date;
 
     public Event(String task, String at) {
         super(task);
         this.at = at;
+        this.date = LocalDate.parse(at);
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + at + ")";
+        return "[E]" + super.toString() + " (at: " + date.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 }
