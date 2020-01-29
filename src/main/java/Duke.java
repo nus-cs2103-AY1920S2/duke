d0bd53d749522e3a05fbecee81fa8f68a3315e32
Joycelyn Teo <joycelyn.teo99@gmail.com>
2020-01-29 19:14:51 +0800

    Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -19,7 +19,7 @@ public class Duke {
 
         while (true) {
             String command = sc.nextLine();
-            String arr[] = command.split(" ");
+            String arr[] = command.split(" ", 2);
             String firstWord = arr[0];
 
             if (command.equals("bye")) {
@@ -30,19 +30,48 @@ public class Duke {
                 if (!list.isEmpty()) {
                     for (int i=0; i<list.size(); i++) {
                         Task t = list.get(i);
-                        System.out.println((i+1) + ".[" + t.getStatusIcon() + "] " + t.getDescription());
+                        System.out.println((i+1) + "." + t.toString());
                     }
                 }
             } else if (firstWord.equals("done")){
+                //which task done
                 String secNum = arr[1];
-                System.out.println("Nice! I've marked this task as done:");
+                //set that task to done
                 Task t = list.get(Integer.parseInt(secNum) - 1);
                 t.setDone();
+                //print output
+                System.out.println("Nice! I've marked this task as done:");
                 System.out.println("[" + t.getStatusIcon() + "] " + t.getDescription());
-            } else {
-                Task t = new Task(command);
+            } else if (firstWord.equals("todo")) {
+                //add task to list
+                ToDos t = new ToDos(arr[1]);
                 list.add(t);
-                System.out.println("added: " + command);
+                //print output
+                System.out.println("Got it. I've added this task:");
+                System.out.println(t.toString());
+                System.out.println("Now you have " + list.size() + " tasks in the list");
+            } else if (firstWord.equals("deadline")) {
+                //separate task and deadline
+                String arr2[] = arr[1].split("/", 2);
+                String arr3[] = arr2[1].split(" ", 2);
+                //add task and deadline to list
+                Deadline d = new Deadline(arr2[0], arr3[1]);
+                list.add(d);
+                //print output
+                System.out.println("Got it. I've added this task:");
+                System.out.println(d.toString());
+                System.out.println("Now you have " + list.size() + " tasks in the list");
+            } else if (firstWord.equals("event")) {
+                //separate task and time
+                String arr2[] = arr[1].split("/", 2);
+                String arr3[] = arr2[1].split(" ", 2);
+                //add task and deadline to list
+                Events e = new Events(arr2[0], arr3[1]);
+                list.add(e);
+                //print output
+                System.out.println("Got it. I've added this task:");
+                System.out.println(e.toString());
+                System.out.println("Now you have " + list.size() + " tasks in the list");
             }
         }
     }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -18,4 +18,8 @@ public class Task {
     public String getDescription() {
         return description;
     }
+
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
+    }
 }
