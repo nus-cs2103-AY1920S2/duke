a07a99900ec415027c7d944f26a261e8771c0464
muhd97 <zain.sma.mz@gmail.com>
2020-01-29 15:36:36 +0800

    Fix: replace tick and cross with / and X

--- src/test/java/DukeTest.java
+++ src/test/java/DukeTest.java
@@ -10,14 +10,14 @@ public class DukeTest {
     public void test1() {
         LocalDate date = LocalDate.parse("2020-02-02", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
         Task deadLine = new Deadline(" return book ", date, "1800");
-        String expectedFormat = "[D][\u2718] return book (by: Feb 2 2020 1800)";
+        String expectedFormat = "[D][X] return book (by: Feb 2 2020 1800)";
         assertEquals(deadLine.toString(), expectedFormat);
     }
 
     @Test
     public void test2() {
         Task toDo = new ToDo(" read book");
-        String expectedFormat = "[T][\u2718] read book";
+        String expectedFormat = "[T][X] read book";
         assertEquals(toDo.toString(), expectedFormat);
     }
 
@@ -25,7 +25,7 @@ public class DukeTest {
     public void test3() {
         LocalDate date = LocalDate.parse("2020-02-05", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
         Task event = new Event(" project meeting ", date, "2-4pm");
-        String expectedFormat = "[E][\u2718] project meeting (at: Feb 5 2020 2-4pm)";
+        String expectedFormat = "[E][X] project meeting (at: Feb 5 2020 2-4pm)";
         assertEquals(event.toString(), expectedFormat);
     }
 
