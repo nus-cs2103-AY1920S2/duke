691485339b8f8d09cbafc932504efcfa10d05162
Hanming Zhu <hanming@huawen.com>
2020-01-30 10:38:46 +0800

    docs: add javadoc for command enum

--- src/main/java/duchess/command/Command.java
+++ src/main/java/duchess/command/Command.java
@@ -5,6 +5,10 @@ import duchess.task.TaskList;
 import duchess.ui.Ui;
 import duchess.util.QuadFunction;
 
+/**
+ * The {@code Command} enum contains all of the different types of
+ * commands recognised by {@code Duchess}.
+ */
 public enum Command {
     TODO(CommandHandler::handleTodoCommand),
     EVENT(CommandHandler::handleEventCommand),
@@ -14,8 +18,17 @@ public enum Command {
     DELETE(CommandHandler::handleDeleteCommand),
     BYE(CommandHandler::handleByeCommand);
 
+    /**
+     * Executes the command. Use {@code execute.apply} to run the function.
+     */
     public final QuadFunction<String, TaskList, Ui, Storage, Void> execute;
 
+    /**
+     * Initialises the Command enum type with the appropriate {@code execute}
+     * function.
+     *
+     * @param execute The {@code QuadFunction} for the Command type.
+     */
     Command(QuadFunction<String, TaskList, Ui, Storage, Void> execute) {
         this.execute = execute;
     }
