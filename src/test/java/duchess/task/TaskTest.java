d707ef3cf5bf5d3de428c25f43795a872749532e
Hanming Zhu <hanming@huawen.com>
2020-01-31 01:08:43 +0800

    docs: add javadoc for tasktest class

--- src/test/java/duchess/task/TaskTest.java
+++ src/test/java/duchess/task/TaskTest.java
@@ -6,21 +6,33 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+/**
+ * JUnit test class for {@code Task}.
+ */
 public class TaskTest {
+    /**
+     * Tests the {@code getDescription()} method.
+     */
     @Test
     public void testGetDescription() {
         assertEquals("This is a test description", new Task("This is a test description").getDescription());
         assertEquals("Same here", new Task("Same here", true).getDescription());
     }
 
+    /**
+     * Tests the {@code isCompleted()} method.
+     */
     @Test
     public void testIsCompleted() {
         assertFalse(new Task("Test description").isCompleted());
         assertTrue(new Task("Hello world", true).isCompleted());
     }
 
+    /**
+     * Tests the {@code toggleIsCompleted()} method.
+     */
     @Test
-    public void testToggleCompleted() {
+    public void testToggleIsCompleted() {
         Task testTask = new Task("Task description");
         assertFalse(testTask.isCompleted());
         testTask.toggleIsCompleted();
@@ -29,6 +41,9 @@ public class TaskTest {
         assertFalse(testTask.isCompleted());
     }
 
+    /**
+     * Tests the overridden {@code toString()} method.
+     */
     @Test
     public void testToString() {
         Task testTask = new Task("What's up world!");
