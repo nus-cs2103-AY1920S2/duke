12eeb2aa44d3b3b99d60fc5982fe2719c8b1ae95
NicholasCF <nicholas.cristian@yahoo.com>
2020-01-30 17:08:58 +0800

    Add command to show errors

---
+++ src/main/java/InvalidFormatCommand.java
@@ -0,0 +1,6 @@
+public class InvalidFormatCommand extends Command {
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) {
+        ui.showInvalidFormatMessage();
+    }
+}
