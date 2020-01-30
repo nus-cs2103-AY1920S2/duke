ec5e6c125ef9185e052a5516b309789eb21bbecb
NicholasCF <nicholas.cristian@yahoo.com>
2020-01-30 12:15:49 +0800

    Modify date processing

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -9,6 +9,6 @@ public class Deadline extends Task{
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (" + time.replace("by", "by:") + ")";
+        return "[D]" + super.toString() + " (by: " + time + ")";
     }
 }
