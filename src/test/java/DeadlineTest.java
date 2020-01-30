d927da38b3d248edb0d8ec0d8ae664a7c8a2f41e
ZhangYiping126 <42378828+ZhangYiping126@users.noreply.github.com>
2020-01-30 10:18:19 +0800

    Update JUnit

--- src/test/java/DeadlineTest.java
+++ src/test/java/DeadlineTest.java
@@ -1,8 +1,6 @@
 import java.time.LocalDate;
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 class DeadlineTest {
-    @Test
     public void test() {
         Deadline deadline = new Deadline("return book", LocalDate.parse("2020-02-02"));
         assertEquals(deadline.toString().trim(),"[D][✗] return book (by: Feb 2 2020)");
