96118307a129b7c86aa23f407089d3b97c40e769
NicholasCF <nicholas.cristian@yahoo.com>
2020-01-30 17:12:41 +0800

    Change tasklist abstraction

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,10 +1,11 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class TaskList {
     private List<Task> tasks;
 
-    public TaskList(List<Task> tasks) {
-        this.tasks = tasks;
+    public TaskList() {
+        this.tasks = new ArrayList<>();
     }
 
     public void addTask(Task task) {
