caf8733b8ae4ca90ce4043409f91256b5ffa01da
Hanming Zhu <hanming@huawen.com>
2020-01-30 10:39:25 +0800

    docs: add javadoc for deadline class

--- src/main/java/duchess/task/Deadline.java
+++ src/main/java/duchess/task/Deadline.java
@@ -4,14 +4,33 @@ import duchess.util.DateTimeStringFormatter;
 
 import java.time.LocalDateTime;
 
+/**
+ * The {@code Deadline} class extends from {@code Task} to allow
+ * the setting of a {@code LocalDateTime deadline}.
+ */
 public class Deadline extends Task {
     private LocalDateTime deadline;
 
+    /**
+     * Initialises the {@code Deadline} instance with its description
+     * and deadline.
+     *
+     * @param description Written description of the task.
+     * @param deadline    {@code LocalDateTime} object indicating the deadline of the task.
+     */
     public Deadline(String description, LocalDateTime deadline) {
         super(description);
         this.deadline = deadline;
     }
 
+    /**
+     * Initialises the {@code Deadline} instance with its description, deadline
+     * and completion status.
+     *
+     * @param description Written description of the task.
+     * @param deadline    {@code LocalDateTime} object indicating the deadline of the task.
+     * @param isCompleted {@code boolean} value indicating whether the task is completed.
+     */
     public Deadline(String description, LocalDateTime deadline, boolean isCompleted) {
         super(description, isCompleted);
         this.deadline = deadline;
