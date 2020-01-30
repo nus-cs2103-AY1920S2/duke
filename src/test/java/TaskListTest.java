467d966fd120162f5d74b79a2f86105abe8bd410
khsc96 <khsc96@gmail.com>
2020-01-30 20:04:26 +0800

    TaskListTest: Added JUnit test method testing findTask

--- src/test/java/TaskListTest.java
+++ src/test/java/TaskListTest.java
@@ -1,5 +1,27 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class TaskListTest {
 
+    @Test
+    void findTaskTest_keywordBook_listOfTaskWithBookInDescription() {
+        List<Task> listOfTasks = new ArrayList<>();
+        List<Task> expectedList = new ArrayList<>();
+        ToDo toDo = new ToDo("read book");
+        Deadline deadline = new Deadline("return book", LocalDate.parse("2020-01-30"));
+        Event event = new Event("abcdef", LocalDate.parse("2020-01-29"));
+        listOfTasks.add(toDo);
+        listOfTasks.add(deadline);
+        listOfTasks.add(event);
+        expectedList.add(toDo);
+        expectedList.add(deadline);
+        TaskList taskListTest = new TaskList(listOfTasks);
+        List<Task> actualResult = taskListTest.findTask("book");
+        assertArrayEquals(expectedList.toArray(), actualResult.toArray());
+    }
 }
