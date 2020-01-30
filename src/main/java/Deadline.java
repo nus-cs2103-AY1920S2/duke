17b3303ce24ff84d781f88af47a5c60d6a63e859
Christian James Welly <39089453+chrisjwelly@users.noreply.github.com>
2020-01-30 10:20:04 +0800

    Convert Strings to LocalDate object

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,21 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-    protected String by;
+    protected LocalDate by;
 
     public Deadline(String desc, String by) {
         super(desc);
-        this.by = by;
+        // by variable must be in YYYY-mm-dd
+        this.by = LocalDate.parse(by);
+    }
+
+    private String formatDate() {
+        return by.format(DateTimeFormatter.ofPattern("dd MMM yy"));
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + formatDate() + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,13 +1,21 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
-    protected String at;
+    protected LocalDate at;
 
     public Event(String desc, String at) {
         super(desc);
-        this.at = at;
+        // at variable must be in YYYY-mm-dd
+        this.at = LocalDate.parse(at);
+    }
+
+    private String formatDate() {
+        return at.format(DateTimeFormatter.ofPattern("dd MMM yy"));
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + at + ")";
+        return "[E]" + super.toString() + " (at: " + formatDate() + ")";
     }
 }
