526d9dc6ac5750f84090b98e6be19827cb4ce5b2
NicholasCF <nicholas.cristian@yahoo.com>
2020-01-30 14:54:41 +0800

    Add TaskList class

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,29 @@
+import java.util.List;
+
+public class TaskList {
+    private List<Task> tasks;
+
+    public TaskList(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+
+    public Task deleteTask(int index) {
+        return tasks.remove(index);
+    }
+
+    @Override
+    public String toString() {
+        String result = "";
+
+        for (int i = 0; i < tasks.size(); i++) {
+            result += i + "."
+                    + tasks.get(i) + "\n";
+        }
+
+        return result;
+    }
+}
