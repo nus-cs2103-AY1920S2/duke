fe4b142b227da5b9a7ebc209950556fbfab9a9a2
xuanxinng <ngxuanxin@gmail.com>
2020-01-30 15:13:38 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,7 +11,6 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-
         String line = "     -----------------------------------------------------------------\n";
         String tab = "      ";
         String greet = line
