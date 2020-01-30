c86ab0fd2a4da8cc4800d796da2ac3783c9a3f81
FyonnOh <>
2020-01-30 23:10:38 +0800

    Data

--- src/test/java/runTest.sh
+++ src/test/java/runTest.sh
@@ -13,7 +13,7 @@ then
 fi
 
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src -Xlint:none -d ../bin ../src/main/java/*.java
+if ! javac -cp ../../ -Xlint:none -d ../bin ../../main/java/*.java
 then
     echo "********** BUILD FAILURE **********"
     exit 1
@@ -22,7 +22,7 @@ fi
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
 java -classpath ../bin Duke < input.txt > output.txt
 
-# compare the output to the expected output
+# compare the output to the expected outputr
 diff output.txt expected.txt
 if [ $? -eq 0 ]
 then
