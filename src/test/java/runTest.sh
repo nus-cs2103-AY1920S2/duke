55e55b56fd23b1c07ecd436ec6bf6c3b11f836f7
FyonnOh <>
2020-01-30 16:46:14 +0800

    Adding test cases 1.0

---
+++ src/test/java/runTest.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# create bin directory if it doesn't exist
+if [ ! -d "../bin" ]
+then
+    mkdir ../bin
+fi
+
+# delete output from previous run
+if [ -e "./output.TXT" ]
+then
+    rm output.TXT
+fi
+
+# compile the code into the bin folder, terminates if error occurred
+if ! javac -cp ../src -Xlint:none -d ../bin ../src/main/java/*.java
+then
+    echo "********** BUILD FAILURE **********"
+    exit 1
+fi
+
+# run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
+java -classpath ../bin Duke < input.txt > output.txt
+
+# compare the output to the expected output
+diff output.txt expected.txt
+if [ $? -eq 0 ]
+then
+    echo "Test result: PASSED"
+    exit 0
+else
+    echo "Test result: FAILED"
+    exit 1
+fi
