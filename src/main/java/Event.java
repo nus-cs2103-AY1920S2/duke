8c985fc73d2f33ee921bb9fcb4f97ece941cba6e
akhilvuputuri <akhil.vuputuri@temasek.nus.edu.sg>
2020-01-31 03:41:01 +0800

    Fix Event toString format

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -16,6 +16,6 @@ public class Event extends Task {
 
     @Override
     public String toString() {
-        return "[E] " + super.toString() + "(at: " + dateString + ")";
+        return "[E]" + super.toString() + "(at: " + dateString + ")";
     }
 }
