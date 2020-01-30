f566f7c8cde663bb8bd8ff039d733d41b5448774
Christian James Welly <39089453+chrisjwelly@users.noreply.github.com>
2020-01-30 18:03:13 +0800

    Add JavaDoc for Parser, Storage, and TaskList

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -3,36 +3,67 @@ import java.util.Scanner;
 public class Parser {
     static Scanner sc = new Scanner(System.in);
 
-    public Parser() {
-    }
-
+    /**
+     * Reads the next line of input.
+     * @return the String that has been read
+     */
     public static String readLine() {
         return sc.nextLine();
     }
 
+    /**
+     * Checks if there is still input in the next line
+     * @return true if there is, false if not
+     */
     public static boolean stillHasInputs() {
         return sc.hasNext();
     }
 
+    /**
+     * Extracts and returns the command from the input line
+     * @param input A string of a single command, or a command with arguments
+     * @return the command represented as a String
+     */
     public static String getCommand(String input) {
         String[] inputs = input.split(" ", 2);
         return inputs[0];
     }
 
+    /**
+     * Extracts and returns the arguments from the input line
+     * @param input A String of command with arguments
+     * @return the arguments represented as a String
+     */
     public static String getArgs(String input) {
         String[] inputs = input.split(" ", 2);
         return inputs[1];
     }
 
+    /**
+     * Checks if the command in the input line is the same as the command expected
+     * @param command The command expected represented as a string
+     * @param input A String of a single command or a command with arguments
+     * @return true if the expected command matches, false otherwise
+     */
     public static boolean commandEquals(String command, String input) {
         return getCommand(input).equals(command);
     }
 
+    /**
+     * Checks if the command does not have any arguments. I.e. Input is a single command or a command with arguments
+     * @param input
+     * @return true if it is a single command, false otherwise
+     */
     public static boolean hasNoArgs(String input) {
         String[] inputs = input.split(" ", 2);
         return inputs.length == 1;
     }
 
+    /**
+     * Obtains the task number from the input line
+     * @param input A command with a number as an argument
+     * @return an integer of the argument
+     */
     public static int getTaskNo(String input) {
         return Integer.parseInt(getArgs(input));
     }
--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -4,6 +4,10 @@ import java.util.List;
 public class TaskList {
     private List<Task> tasks;
 
+    /**
+     * Initialises the TaskList according to a list of tasks that have been saved
+     * @param savedTasks Tasks that have been saved represented as a List<List<String>>
+     */
     public TaskList(List<List<String>> savedTasks) {
         tasks = new ArrayList<>();
         for (List<String> savedTask : savedTasks) {
@@ -30,10 +34,18 @@ public class TaskList {
         tasks = new ArrayList<>();
     }
 
+    /**
+     * Gets the concrete representation of the tasks
+     * @return A List of Tasks
+     */
     public List<Task> getTasks() {
         return tasks;
     }
 
+    /**
+     * Adds a deadline object to the task list
+     * @param args A String in the format of: "<desc> /by <date>"
+     */
     public void addDeadline(String args) {
         String[] descAndBy = args.split(" /by ");
         Deadline deadline = new Deadline(descAndBy[0], descAndBy[1]);
@@ -46,6 +58,10 @@ public class TaskList {
         Ui.printLine();
     }
 
+    /**
+     * Adds an Event object to the task list
+     * @param args A String in the format of: "<desc> /at <date>"
+     */
     public void addEvent(String args) {
         String[] descAndAt = args.split(" /at ");
         Event event = new Event(descAndAt[0], descAndAt[1]);
@@ -58,6 +74,10 @@ public class TaskList {
         Ui.printLine();
     }
 
+    /**
+     * Add a Todo object to the task list
+     * @param args A String in the format of: "<desc>"
+     */
     public void addTodo(String args) {
         Todo todo = new Todo(args);
         tasks.add(todo);
@@ -68,14 +88,28 @@ public class TaskList {
         Ui.printLine();
     }
 
+    /**
+     * The number of tasks in the TaskList
+     * @return The number of tasks in the TaskList
+     */
     public int size() {
         return tasks.size();
     }
 
+    /**
+     * The Task at a the index specified by the param
+     * @param idx 0-based indexing to get the Task object
+     * @return
+     */
     public Task get(int idx) {
         return tasks.get(idx);
     }
 
+    /**
+     * Mark a Task (specified by the index) as done
+     * @param taskNo A 1-based indexing of the position of the Task in the list
+     * @throws InvalidIndexException
+     */
     public void markTaskAsDone(int taskNo) throws InvalidIndexException {
         if (taskNo < 1 || taskNo > tasks.size()) {
             throw new InvalidIndexException();
@@ -89,6 +123,11 @@ public class TaskList {
         Ui.printLine();
     }
 
+    /**
+     * Delete a Task (specified by the index)
+     * @param taskNo A 1-based indexing of the position of the Task in the list
+     * @throws InvalidIndexException
+     */
     public void deleteTask(int taskNo) throws InvalidIndexException {
         if (taskNo < 1 || taskNo > tasks.size()) {
             throw new InvalidIndexException();
@@ -103,6 +142,10 @@ public class TaskList {
         Ui.printLine();
     }
 
+    /**
+     * Checks if the TaskList is full (full is defined by 100 tasks)
+     * @return true if TaskList is full
+     */
     public boolean isFull() {
         return tasks.size() == 100;
     }
