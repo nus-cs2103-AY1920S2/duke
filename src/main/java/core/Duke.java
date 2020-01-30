959d92f9c68f7cb142287ac7f4c59e1c0d1a9348
Daythehangedman <59592689+Daythehangedman@users.noreply.github.com>
2020-01-30 20:03:50 +0800

    remove module on test, and added javafx tut part 1

--- src/main/java/core/Duke.java
+++ src/main/java/core/Duke.java
@@ -3,10 +3,15 @@ package core;
 import command.Command;
 import dukexception.DukeException;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
 /**
  * The main driver of the program.
  */
-public class Duke {
+public class Duke extends Application {
 
     private Ui ui;
     private Common common;
@@ -50,4 +55,15 @@ public class Duke {
         Duke duke = new Duke();
         duke.run();
     }
+
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
+    }
+
+
 }
