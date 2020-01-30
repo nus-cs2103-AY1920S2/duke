66b37ecc2fd75b8298e6a0319e8b7c47376358c7
FyonnOh <>
2020-01-30 16:41:14 +0800

    week 2: touch-up

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.sql.SQLOutput;
 import java.util.Scanner;
 import java.util.ArrayList;
 
@@ -10,34 +11,40 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println("Konnichiwa! Watashi no namaeha Duke desu! Whatchu can i duwu for yuwu? (^.___.^)");
+        System.out.println("Konnichiwa! I am Duke the cat! What can I do for you? meow~ (^.___.^)");
         Scanner sc = new Scanner(System.in);
         ArrayList<Task> tasksArr = new ArrayList<>();
         while(sc.hasNext()) {
             try {
                 String command = sc.next();
                 if (command.equals("list")) {
-                    System.out.println("Hewe awu de tasku in ywour listu! uwu \n");
+                    System.out.println("Here are the tasks in your list!");
+                    System.out.println("____________________________________________________________");
                     for(int i=0; i<tasksArr.size(); i++) {
-                        System.out.println(i+1 + " "+ tasksArr.get(i));
+                        System.out.println("        "+ i+1 + " "+ tasksArr.get(i));
                     }
+                    System.out.println("____________________________________________________________");
                 } else if (command.equals("done")) {
                     int index = sc.nextInt()-1;
                     if (index < 0 || index > tasksArr.size()-1) {
                         throw new DukeException("done");
                     } else {
                         tasksArr.get(index).markAsDone();
-                        System.out.println("Nicesu! watashi hasu marku the tasku as done desu ʕ ᵒ ᴥ ᵒʔ: \n");
-                        System.out.println("        " + tasksArr.get(index) + "\n");
+                        System.out.println("____________________________________________________________");
+                        System.out.println("Good job! I have marked the task as done! meow~ \n");
+                        System.out.println("        " + tasksArr.get(index));
+                        System.out.println("____________________________________________________________");
                     }
                 } else if (command.equals("todo")) {
                     String str = sc.nextLine();
                     if (str.split("").length > 1) {
                         ToDo t = new ToDo(str);
                         tasksArr.add(t);
-                        System.out.println("Got it desu! uwu Watashi hasu added des tasku (´・(oo)・｀): \n");
+                        System.out.println("____________________________________________________________");
+                        System.out.println("Got it! I have added this task: \n");
                         System.out.println("        " + t + "\n");
-                        System.out.println("nuw omae has " + tasksArr.size()+ " tasks in the list!");
+                        System.out.println("Now you have " + tasksArr.size()+ " tasks in the list! [^._.^]ﾉ");
+                        System.out.println("____________________________________________________________");
                     } else {
                         throw new DukeException("todo");
                     }
@@ -47,11 +54,13 @@ public class Duke {
                         String[] splitStr = str.split("/by");
                         DeadLine t = new DeadLine(splitStr[0], splitStr[1]);
                         tasksArr.add(t);
-                        System.out.println("Gotcha! Watashi hasu added des tasku [^._.^]ﾉ: \n");
+                        System.out.println("____________________________________________________________");
+                        System.out.println("Got it! I have added this task: \n");
                         System.out.println("        " + t + "\n");
-                        System.out.println("nuw omae has " + tasksArr.size()+ " tasks in the list!");
+                        System.out.println("Now you have " + tasksArr.size()+ " tasks in the list! [^._.^]ﾉ");
+                        System.out.println("____________________________________________________________");
                     } else {
-                        throw new DukeException("todo");
+                        throw new DukeException("deadline");
                     }
                 } else if (command.equals("event")) {
                     String str = sc.nextLine();
@@ -59,9 +68,11 @@ public class Duke {
                         String[] splitStr = str.split("/at");
                         Event t = new Event(splitStr[0], splitStr[1]);
                         tasksArr.add(t);
-                        System.out.println("Gotcha! Watashi hasu added des tasku (=＾● ⋏ ●＾=): \n");
+                        System.out.println("____________________________________________________________");
+                        System.out.println("Got it! I have added this task: \n");
                         System.out.println("        " + t + "\n");
-                        System.out.println("nuw omae has " + tasksArr.size()+ " tasks in the list!");
+                        System.out.println("Now you have " + tasksArr.size()+ " tasks in the list! [^._.^]ﾉ");
+                        System.out.println("____________________________________________________________");
                     } else {
                         throw new DukeException("event");
                     }
@@ -72,10 +83,30 @@ public class Duke {
                     } else {
                         Task t = tasksArr.get(index);
                         tasksArr.remove(index);
-                        System.out.println("Noted desu! Watashi has removed this task! ʕ ᵒ ᴥ ᵒʔ: \n");
+                        System.out.println("____________________________________________________________");
+                        System.out.println("Noted! I have removed this task: \n");
                         System.out.println("        " + t + "\n");
-                        System.out.println("nuw omae has " + tasksArr.size()+ " tasks in the list!");
+                        System.out.println("Now you have " + tasksArr.size()+ " tasks in the list![^._.^]ﾉ");
+                        System.out.println("____________________________________________________________");
                     }
+                } else if (command.equals("bye")) {
+                    System.out.println("Sayonara~");
+                    System.out.println(" (_＼ヽ\n" +
+                            "　 ＼＼ .Λ＿Λ.\n" +
+                            "　　 ＼(　ˇωˇ)　\n" +
+                            "　　　 >　⌒ヽ\n" +
+                            "　　　/ 　 へ＼\n" +
+                            "　　 /　　/　＼＼\n" +
+                            "　　 ﾚ　ノ　　 ヽ_つ\n" +
+                            "　　/　/\n" +
+                            "　 /　/|\n" +
+                            "　(　(ヽ\n" +
+                            "　|　|、＼\n" +
+                            "　| 丿 ＼ ⌒)\n" +
+                            "　| |　　) /\n" +
+                            "`ノ ) 　 Lﾉ\n" +
+                            "(_／");
+                    break;
                 } else {
                     throw new DukeException("invalid");
                 }
