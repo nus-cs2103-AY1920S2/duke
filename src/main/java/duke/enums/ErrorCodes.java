6e8f296307358be429f3bea00ad2769f228d280d
Firzan Armani <mrfirzanarmani@gmail.com>
2020-01-30 22:34:33 +0800

    Create ErrorCode and TaskTypes enum classes

---
+++ src/main/java/duke/enums/ErrorCodes.java
@@ -0,0 +1,13 @@
+package duke.enums;
+
+public enum ErrorCodes {
+    UNKNOWN_COMMAND,
+    MISSING_TASK_NAME,
+    MISSING_EVENT_DATE,
+    MISSING_DEADLINE_DATE,
+    INVALID_TASK_INDEX,
+    INVALID_DATE_FORMAT,
+    FILE_NOT_FOUND,
+    LOADING_ERROR,
+    SAVING_ERROR
+}
---
+++ src/main/java/duke/enums/TaskTypes.java
@@ -0,0 +1,7 @@
+package duke.enums;
+
+public enum TaskTypes {
+    DEADLINE,
+    TODO,
+    EVENT
+}
