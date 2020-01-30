1fa0e76e90c2a4449718741157f2c8a4766b8b4a
Hanming Zhu <hanming@huawen.com>
2020-01-30 10:39:13 +0800

    docs: add javadoc for parser class

--- src/main/java/duchess/io/Parser.java
+++ src/main/java/duchess/io/Parser.java
@@ -3,7 +3,19 @@ package duchess.io;
 import duchess.command.Command;
 import duchess.exception.DuchessException;
 
+/**
+ * The {@code Parser} class helps to parse given user inputs
+ * into a {@code Command} of the appropriate type.
+ */
 public class Parser {
+    /**
+     * Parses a given {@code String} into a {@code Command} of the
+     * appropriate type.
+     *
+     * @param command Entire command to be processed.
+     * @return Command type of the given command.
+     * @throws DuchessException If the command is not recognised.
+     */
     public static Command parse(String command) throws DuchessException {
         try {
             return Command.valueOf(command.split("\\s", 2)[0].toUpperCase());
