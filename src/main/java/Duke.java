8fdf6e6b1dff30792b9a38f1ff651a698f81aaca
NicholasCF <nicholas.cristian@yahoo.com>
2020-01-30 17:20:30 +0800

    Refactor Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,193 +1,35 @@
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
-import java.io.*;
-import java.nio.file.Path;
+import java.io.FileNotFoundException;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
 
 public class Duke {
-    ArrayList<Task> tasks;
+    private Storage storage;
+    private TaskList tasks;
+    private Ui ui;
 
-    public Duke() {
-        tasks = new ArrayList<>();
-        loadList();
+    public Duke(String filePath) {
+        ui = new Ui();
+        storage = new Storage(filePath);
+        try {
+            tasks = storage.load();
+        } catch (FileNotFoundException e) {
+            ui.showLoadingError();
+            tasks = new TaskList();
+        }
+    }
+
+    public void run() {
+        ui.showWelcome();
+        boolean isExit = false;
+        while (!isExit) {
+            String fullCommand = ui.getCommand();
+            Command c = Parser.parse(fullCommand);
+            c.execute(tasks, ui, storage);
+            isExit = c.isExit();
+        }
     }
 
     public static void main(String[] args) {
-        Duke chatbot = new Duke();
-
-        chatbot.greet();
-
-        Scanner sc = new Scanner(System.in);
-        while (sc.hasNextLine()) {
-            String command = sc.nextLine();
-
-            if (command.equals("bye")) {
-                chatbot.bye();
-                break;
-            } else {
-                chatbot.process(command);
-            }
-        }
+        new Duke(Paths.get("data", "duke.txt").toString()).run();
     }
 
-    private void greet() {
-        String greeting = "Hello. I'm Duke.\n"
-                + "Oh, it's you, can you please stop coming?";
-
-        say(greeting);
-    }
-
-    private void process(String speech) {
-        String[] words = speech.split(" ");
-        String firstWord = words[0];
-
-        switch (firstWord) {
-            case "todo":
-                if (words.length > 1) {
-                    String todo = String.join(" ", Arrays.copyOfRange(words, 1, words.length));
-                    addTodo(todo);
-                    say("Another task? Oh well, here's the task:\n"
-                            + "\t" + getTask(getTaskCount()) + "\n"
-                            + "Now you have " + getTaskCount() + " tasks in the list.");
-                } else {
-                    say("Your todo is as empty as your brain. Try again properly.");
-                }
-                saveList();
-                break;
-            case "deadline":
-                String entry = String.join(" ", Arrays.copyOfRange(words, 1, words.length));
-                String description = entry.split(" /by ")[0].strip();
-                String time = entry.split(" /by ")[1];
-
-                addDeadline(description, time);
-                say("Another task? Oh well, here's the task:\n"
-                        + "\t" + getTask(getTaskCount()) + "\n"
-                        + "Now you have " + getTaskCount() + " tasks in the list.");
-                saveList();
-                break;
-            case "event":
-                String input = String.join(" ", Arrays.copyOfRange(words, 1, words.length));
-                String event = input.split(" /at ")[0].strip();
-                String timeRange = input.split(" /at ")[1];
-
-                addEvent(event, timeRange);
-                say("Another task? Oh well, here's the task:\n"
-                        + "\t" + getTask(getTaskCount()) + "\n"
-                        + "Now you have " + getTaskCount() + " tasks in the list.");
-                saveList();
-                break;
-            case "list":
-                say("Here are your procrastinated tasks:\n"
-                        + getTasks());
-                break;
-            case "done":
-                int index = Integer.parseInt(words[1]);
-                tickTask(index);
-
-                say("Yay. You've finally done this task:\n\t"
-                        + getTask(index));
-                break;
-            case "delete":
-                int number = Integer.parseInt(words[1]);
-
-                say("Giving up already? Removed this:\n\t"
-                        + popTask(number));
-
-                saveList();
-                break;
-            default:
-                say("Unknown command. Program it yourself or get a dictionary.");
-                break;
-        }
-    }
-
-    private void bye() {
-        String parting = "Bye. Please don't come back again or I'll call the cops.";
-
-        say(parting);
-    }
-
-    private void say(String speech) {
-        String separator = "\t============================================================\n";
-
-        String[] lines = speech.split("\n");
-        String indented = "\t " + String.join("\n\t ", lines) + "\n";
-
-        String result = separator + indented + separator;
-
-        System.out.println(result);
-    }
-
-    private void saveList() {
-        try {
-            File f = new File(Paths.get("data", "duke.txt").toString());
-            f.getParentFile().mkdirs();
-
-            Gson gson = new Gson();
-
-            FileWriter fw = new FileWriter(f);
-            fw.append(gson.toJson(tasks));
-
-            fw.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void loadList() {
-        try {
-            File f = new File(Paths.get("data", "duke.txt").toString());
-            FileReader fr = new FileReader(f);
-            BufferedReader br = new BufferedReader(fr);
-            Gson gson = new Gson();
-
-            tasks = gson.fromJson(br, TypeToken.getParameterized(tasks.getClass(), Task.class).getType());
-        } catch (FileNotFoundException e) {
-            return;
-        }
-    }
-
-    private void addTodo(String todo) {
-        tasks.add(new Todo(todo));
-    }
-
-    private void addDeadline(String description, String time) {
-        tasks.add(new Deadline(description, time));
-    }
-
-    private void addEvent(String description, String timeRange) {
-        tasks.add(new Event(description, timeRange));
-    }
-
-    private void tickTask(int index) {
-        tasks.get(index - 1).tick();
-    }
-
-    private String popTask(int index) {
-        return tasks.remove(index - 1).toString();
-    }
-
-    private String getTask(int index) {
-        return tasks.get(index - 1).toString();
-    }
-
-    private int getTaskCount() {
-        return tasks.size();
-    }
-
-    private String getTasks() {
-        String result = "";
-
-        int index = 1;
-        for (Task t : tasks) {
-            result += index++
-                    + "." + t + "\n";
-        }
-
-        return result;
-    }
 }
