33c28cc96561e435e438ba47715a08fb31eeb767
Hanming Zhu <hanming@huawen.com>
2020-01-31 01:09:15 +0800

    docs: add javadoc for duchessexceptiontest class

--- src/test/java/duchess/exception/DuchessExceptionTest.java
+++ src/test/java/duchess/exception/DuchessExceptionTest.java
@@ -4,7 +4,13 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * JUnit test class for {@code DuchessException}.
+ */
 public class DuchessExceptionTest {
+    /**
+     * Tests the constructor of {@code DuchessException}.
+     */
     @Test
     public void testConstructor() {
         assertEquals("Exception! Failed to succeed.",
