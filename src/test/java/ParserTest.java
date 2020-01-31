37f0202c72473daa7f779b9008816daa86d784f3
kusnim <minschoi97@gmail.com>
2020-02-01 00:43:05 +0800

    ParserTest Done

--- src/test/java/ParserTest.java
+++ src/test/java/ParserTest.java
@@ -3,11 +3,25 @@ import DukeException.*;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ParserTest {
+
     @Test
     void todoDescriptionCorrectIn() {
         String in = "todo drink";
-        assertEquals();
+        assertEquals("drink", Parser.todoDescription(in));
+    }
+
+    @Test
+    void deadlineParamsWrongIn() {
+        String in = "deadline buy chocolate by 9pm";
+        assertThrows(DukeUnknownInputException.class, () -> Parser.deadlineParams(in));
+    }
+
+    @Test
+    void eventParamsMissingDescription() {
+        String in = "event ";
+        assertThrows(DukeMissingDescriptionException.class, () -> Parser.eventParams(in));
     }
 }
