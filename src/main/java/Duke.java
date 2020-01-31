8cda2a8117cd3cbaaf2266437062aec4ed6f2aaa
sigmund-c <sigmund@u.nus.edu>
2020-02-01 01:20:29 +0800

    Level 7. Save

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -13,7 +14,68 @@ enum Command {
 }
 
 public class Duke {
-    public static void main(String[] args) {
+
+    public static final String SAVE_FILE = "save.txt";
+
+    public static Task stringToTask(String string) {
+        char type = string.charAt(1);
+        boolean isDone = string.charAt(4) == '\u2713' ? true: false;
+        Task task;
+        String desc;
+        String date;
+        if (type == 'T') {
+            desc = string.substring(7);
+
+            task = new Todo(desc);
+        } else if(type == 'D') {
+            desc = string.substring(7);
+            date = desc.split("\\(by: ")[1];
+            date = date.substring(0, date.length() - 1); // remove ending bracket
+            desc = desc.split("\\(by: ")[0];
+
+            task = new Deadline(desc, date);
+        } else {
+            desc = string.substring(7);
+            date = desc.split("\\(date: ")[1];
+            date = date.substring(0, date.length() - 1); // remove ending bracket
+            desc = desc.split("\\(date: ")[0];
+
+            task = new Event(desc, date);
+        }
+
+        if (isDone) {
+            task.markAsDone();
+        }
+        return task;
+    }
+
+    public static List<Task> readList() throws FileNotFoundException {
+        List<Task> taskList = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(SAVE_FILE))) {
+            String line = reader.readLine();
+            while(line != null) {
+                Task task = stringToTask(line);
+                taskList.add(task);
+
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+        return taskList;
+    }
+
+    public static void writeList(List<Task> toWrite) throws FileNotFoundException {
+        PrintWriter pw = new PrintWriter(new FileOutputStream(SAVE_FILE));
+        for (Task task: toWrite) {
+            pw.println(task);
+        }
+        pw.close();
+    }
+
+    public static void main(String[] args) throws FileNotFoundException{
         Scanner sc = new Scanner(System.in);
 
         String charmander = "              _.--\"\"`-..\n" +
@@ -54,7 +116,7 @@ public class Duke {
         System.out.println(charmander);
         System.out.println("Char! Charmander!");
 
-        List<Task> charmanderList = new ArrayList<>();
+        List<Task> charmanderList = readList();
 
         boolean isLooping = true;
         while (isLooping) {
@@ -75,6 +137,9 @@ public class Duke {
 
                 switch (comm) {
 
+                    case BYE:
+                        isLooping = false;
+                        break;
                     case LIST:
                         int listNo = 1;
 
@@ -94,6 +159,8 @@ public class Duke {
                         } catch (IndexOutOfBoundsException | NumberFormatException err) {
                             throw new DukeException("Charmander needs a valid number from the list!");
                         }
+
+                        writeList(charmanderList);
                         break;
                     case DELETE:
                         try {       // check if value is given and correct
@@ -105,6 +172,8 @@ public class Duke {
                         } catch (IndexOutOfBoundsException | NumberFormatException err) {
                             throw new DukeException("Charmander needs a valid number from the list!");
                         }
+
+                        writeList(charmanderList);
                         break;
                     case TODO:
                         try {       // check if desc is given
@@ -117,6 +186,8 @@ public class Duke {
                         System.out.println("Charmander writes a Todo. You peek over and it says:");
                         System.out.println(newTodo);
                         System.out.println("Charmander holds out " + charmanderList.size() + " finger(s).");
+
+                        writeList(charmanderList);
                         break;
                     case DEADLINE:
                         if (commandAndRest.length < 2) {       // check if desc is given
@@ -135,6 +206,8 @@ public class Duke {
                         } catch (ArrayIndexOutOfBoundsException err) {
                             throw new DukeException("Charmander needs a description AND a date to write it down!");
                         }
+
+                        writeList(charmanderList);
                         break;
                     case EVENT:
                         if (commandAndRest.length < 2) {       // check if desc is given
@@ -153,10 +226,13 @@ public class Duke {
                         } catch (ArrayIndexOutOfBoundsException err) {
                             throw new DukeException("Charmander needs a description AND a date to write it down!");
                         }
+
+                        writeList(charmanderList);
                         break;
                     default:
                         throw new DukeException("Charmander hurt itself in its confusion!");
                 }
+
             } catch (DukeException err) {
                 System.out.println(err);
             }
