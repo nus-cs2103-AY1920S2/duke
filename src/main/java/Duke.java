b5c86ca675227c036caa4436952a663cd29c6770
teriaiw <50551345+teriaiw@users.noreply.github.com>
2020-01-31 15:52:09 +0800

    add Level-8

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,8 @@
-import java.io.PrintStream;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class Duke {
     public static void main(String[] args) {
@@ -10,10 +12,10 @@ public class Duke {
                 + "     | | | | | | | |/ / _ \\\n"
                 + "     | |_| | |_| |   <  __/\n"
                 + "     |____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("    -------------------------------------------------\n"
-                + "      Hello! I'm\n" + logo);
-        System.out.println("      What can I do for you? :)\n"
-                + "    -------------------------------------------------");
+
+        String format = "    -----------------------------------------------------------------\n";
+        System.out.println(format + "      Hello! I'm\n" + logo);
+        System.out.println("      What can I do for you? :)\n" + format);
 
         Scanner sc = new Scanner(System.in);
         ArrayList<Task> taskList = new ArrayList<>();
@@ -21,40 +23,36 @@ public class Duke {
         while (sc.hasNext()) {
             String input = sc.nextLine();
             String arr[] = input.split(" ", 2);
-            //Task t = new Task(input);
 
             if (input.equals("bye")) {
-                System.out.println("    -------------------------------------------------\n"
-                        + "      Bye. Hope to see you again soon! :)\n"
-                        + "    -------------------------------------------------");
+                System.out.println(format + "      Bye. Hope to see you again soon! :)\n" + format);
                 System.exit(0);
 
             } else if (input.equals("list")) {
-                System.out.println("    -------------------------------------------------\n"
-                        + "      Here are the tasks in your list:");
+                System.out.println(format + "      Here are the tasks in your list:");
+
                 for (int i = 1; i < taskList.size() + 1; i++) {
                     Task current = taskList.get(i - 1);
                     System.out.println("      " + i + ". " + current);
                 }
-                System.out.println("    -------------------------------------------------");
+                System.out.println(format);
 
             } else if (arr[0].equals("done")) {
 
                 try {
                     checkNum(arr.length);
-
+                    checkValid(arr[1]);
                     int taskNum = Integer.parseInt(arr[1]) - 1;
+
                     if (taskList.size() > taskNum) {
                         Task current = taskList.get(taskNum);
                         current.markDone();
-                        System.out.println("    -------------------------------------------------\n"
+                        System.out.println(format
                                 + "      Nice! I've marked this task as done:\n "
                                 + "        " + current + "\n"
-                                + "    -------------------------------------------------");
+                                + format);
                     } else {
-                        System.out.println("    -------------------------------------------------\n"
-                                + "      Sorry, this task does not exist :(\n"
-                                + "    -------------------------------------------------");
+                        System.out.println(format + "      Sorry, this task does not exist :(\n" + format);
                     }
 
                 } catch (DukeException ex) {
@@ -65,20 +63,21 @@ public class Duke {
 
                 try {
                     checkNum(arr.length);
-
+                    checkValid(arr[1]);
                     int taskNum = Integer.parseInt(arr[1]) - 1;
+
                     if (taskList.size() > taskNum) {
                         Task current = taskList.get(taskNum);
                         taskList.remove(taskNum);
-                        System.out.println("    -------------------------------------------------\n"
+                        System.out.println(format
                                 + "      I've removed this task:\n "
                                 + "        " + current + "\n"
                                 + "      Now you have " + taskList.size() + " tasks in the list.\n"
-                                + "    -------------------------------------------------");
+                                + format);
                     } else {
-                        System.out.println("    -------------------------------------------------\n"
+                        System.out.println(format
                                 + "      Sorry, this task does not exist :(\n"
-                                + "    -------------------------------------------------");
+                                + format);
                     }
 
                 } catch (DukeException ex) {
@@ -87,34 +86,57 @@ public class Duke {
 
             } else {
                 try {
+                    String taskType = arr[0];
+                    String[] temp;
+                    String task = "";
                     checkAction(arr[0]);
                     checkDescription(arr.length);
-                    Task newTask = new Task("task");
+                    Task newTask = new Task("");
 
-                    if (arr[0].equals("todo")) {
+                    switch (taskType) {
+                    case "todo":
                         newTask = new ToDo(arr[1]);
                         taskList.add(newTask);
+                        break;
 
-                    } else if (arr[0].equals("deadline")) {
-                        String[] temp = arr[1].split("/by");
-                        String task = temp[0];
+                    case "deadline":
+                        temp = arr[1].split("/by ");
+                        task = temp[0];
                         String date = temp[1];
-                        newTask = new Deadline(task, date);
-                        taskList.add(newTask);
+                        try {
+                            LocalDate localDate = LocalDate.parse(date);
+                            newTask = new Deadline(task, localDate);
+                            taskList.add(newTask);
+                        } catch (DateTimeParseException ex) {
+                            System.out.println(format
+                                    + "      Please enter a date in this format: YYYY-MM-DD !\n"
+                                    + format);
+                        }
+                        break;
 
-                    } else if (arr[0].equals("event")) {
-                        String[] temp = arr[1].split("/at");
-                        String task = temp[0];
+                    case "event":
+                        temp = arr[1].split("/at ");
+                        task = temp[0];
                         String time = temp[1];
-                        newTask = new Event(task, time);
-                        taskList.add(newTask);
+                        try {
+                            LocalDateTime localDateTime = LocalDateTime.parse(time);
+                            newTask = new Event(task, localDateTime);
+                            taskList.add(newTask);
+                        } catch (DateTimeParseException ex) {
+                            System.out.println(format
+                                    + "      Please enter a date & time in this format: YYYY-MM-DDTHH:MM !\n"
+                                    + format);
+                        }
+
                     }
 
-                    System.out.println("    -------------------------------------------------\n"
-                            + "      Got it. I've added this task:\n"
-                            + "        " + newTask + "\n"
-                            + "      Now you have " + taskList.size() + " tasks in the list.\n"
-                            + "    -------------------------------------------------");
+                    if (!newTask.description.equals("")) {
+                        System.out.println(format
+                                + "      Got it. I've added this task:\n"
+                                + "        " + newTask + "\n"
+                                + "      Now you have " + taskList.size() + " tasks in the list.\n"
+                                + format);
+                    }
 
                 } catch (DukeException ex) {
                     System.out.println(ex.getMessage());
@@ -126,25 +148,35 @@ public class Duke {
 
     public static void checkDescription(int size) throws DukeException {
         if (size < 2) {
-            throw new DukeException("    -------------------------------------------------\n"
+            throw new DukeException("    -----------------------------------------------------------------\n"
                     + "      Please enter a Task description!\n"
-                    + "    -------------------------------------------------");
+                    + "    -----------------------------------------------------------------");
+        }
+    }
+
+    public static void checkValid(String input) throws DukeException {
+        String[] arr = input.split(" ");
+
+        if (arr.length > 1) {
+            throw new DukeException("    -----------------------------------------------------------------\n"
+                    + "      Please enter a only one Task number!\n"
+                    + "    -----------------------------------------------------------------");
         }
     }
 
     public static void checkNum(int size) throws DukeException {
         if (size < 2) {
-            throw new DukeException("    -------------------------------------------------\n"
+            throw new DukeException("    -----------------------------------------------------------------\n"
                     + "      Please enter a Task number!\n"
-                    + "    -------------------------------------------------");
+                    + "    -----------------------------------------------------------------");
         }
     }
 
     public static void checkAction(String action) throws DukeException {
         if (!action.equals("todo") && !action.equals("deadline") && !action.equals("event")) {
-            throw new DukeException("    -------------------------------------------------\n"
+            throw new DukeException("    -----------------------------------------------------------------\n"
                     + "      Sorry, I didn't understand that :( Please try again.\n"
-                    + "    -------------------------------------------------");
+                    + "    -----------------------------------------------------------------");
         }
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,14 +1,17 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
 
-    protected String at;
+    protected LocalDateTime at;
 
-    public Event(String description, String at) {
+    public Event(String description, LocalDateTime at) {
         super(description);
         this.at = at;
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (by: " + at + ")";
+        return "[E]" + super.toString() + " (by: " + at.format(DateTimeFormatter.ofPattern("MMM d yyyy h:mma")) + ")";
     }
 }
