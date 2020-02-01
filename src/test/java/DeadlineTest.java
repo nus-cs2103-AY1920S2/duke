d4eab545746f6d61b249453a048c56d812d98e9a
Kangwkk <846199604@qq.com>
2020-02-02 00:51:41 +0800

    add JUnit tests

---
+++ src/test/java/DeadlineTest.java
@@ -0,0 +1,21 @@
+import Task.Deadline;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeadlineTest {
+
+    @Test
+    void getBy() {
+        Assertions.assertEquals(LocalDate.parse("2020-03-19"), new Deadline("read book", "2020-03-19").getBy());
+    }
+
+    @Test
+    void testToString() {
+        assertEquals("[D][\u2718] finish homework (by: Feb 10 2020)",
+                new Deadline("finish homework", "2020-02-10").toString());
+    }
+}
