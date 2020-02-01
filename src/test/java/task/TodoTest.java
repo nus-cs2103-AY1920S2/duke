1c68ccefea434d0a74a21bb66eae6917931a6e72
Jade Tay <jadet.hx@gmail.com>
2020-02-01 13:44:20 +0800

    A-JUnit

---
+++ src/test/java/task/TodoTest.java
@@ -0,0 +1,15 @@
+package task;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TodoTest {
+
+    @Test
+    public void testTodo() {
+        Task todo = new Todo("Task");
+        String expectedText = "[T][\u2718] Task";
+        String actualText = todo.toString();
+        assertEquals(expectedText, actualText);
+    }
+}
