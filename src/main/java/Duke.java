6c86f3fcf71b76cc8eba8910491a4acc17a8ce1a
TheElectricBlender <yodudee97@gmail.com>
2020-02-02 01:03:16 +0800

    Enabled gradle and javafx

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,9 +6,12 @@ import task.*;
 import ui.*;
 import java.io.IOException;
 import java.util.ArrayList;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
-public class Duke {
-    String filePath = "/Users/Simon/Desktop/Y2S2/CS2103T/duke/src/main/java/duke.txt";
+public class Duke extends Application{
     private Storage storage;
     private TaskList tasks;
     private Ui ui;
@@ -44,9 +47,16 @@ public class Duke {
         }
     }
 
-    static ArrayList<Task> list = new ArrayList<>();
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
 
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
+    }
 
+    
     public static void main(String[] args) throws IOException{
         new Duke("/Users/Simon/Desktop/Y2S2/CS2103T/duke/src/main/java/duke.txt").run();
     }
