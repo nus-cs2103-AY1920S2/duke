826fabae71ec99634a9499afecee4203f66ebf0c
Hanming Zhu <hanming@huawen.com>
2020-02-01 10:06:02 +0800

    test: fix up text-ui-test to work with new changes
    Previous text-ui-test was designed to work with the old Duchess. It would not work for the current Duchess.
    
    Updated to clear data from previous tests, compile and run.

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -10,11 +10,15 @@ if [ -e "./ACTUAL.TXT" ]; then
   rm ACTUAL.TXT
 fi
 
+if [ -e "./data" ]; then
+  rm -r data
+fi
+
 # compile the code into the bin folder, terminates if error occurred
 if
   ! (
-    find ../src/main/java -name "*.java" >sources.txt
-    javac -cp ../src -Xlint:none -d ../bin @sources.txt
+    find ../src/main/java -name "*.java" >./sources.txt
+    javac -cp "../src:./gson-2.8.6.jar" -Xlint:none -d ../bin @./sources.txt
   )
 then
   echo "********** BUILD FAILURE **********"
@@ -22,11 +26,10 @@ then
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Main <input.txt >ACTUAL.TXT
+java -cp "../bin:./gson-2.8.6.jar" duchess.Duke <input.txt >ACTUAL.TXT
 
 # compare the output to the expected output
-diff ACTUAL.TXT EXPECTED.TXT
-if [ $? -eq 0 ]; then
+if diff ACTUAL.TXT EXPECTED.TXT; then
   echo "Test result: PASSED"
   exit 0
 else
