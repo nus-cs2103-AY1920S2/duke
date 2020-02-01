91285389036bc70af61996421119dc0939a4738e
JJiahui <jiangjiahui2048@gmail.com>
2020-02-01 16:49:28 +0800

    A-JavaDoc

--- src/main/java/ip/Storage.java
+++ src/main/java/ip/Storage.java
@@ -11,12 +11,22 @@ import java.io.ObjectInputStream;
 
 public class Storage {
     private final String FILENAME = "yourfile.txt";
+
+    /**
+     * Write all the user's tasks to local storage
+     * @param tasks the user's tasks
+     */
     public void writeToFile(TaskList tasks) {
         try (FileOutputStream fileOutputStream = new FileOutputStream(new File(FILENAME));
              ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream)) {
             objectOutputStream.writeObject(tasks);
         } catch (IOException e) {}
     }
+
+    /**
+     * Reads the user's tasks from local storage.
+     * @return the user's tasks
+     */
     public TaskList readFromFile() {
         try (FileInputStream fileInputStream = new FileInputStream(new File(FILENAME));
             ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream)) {
--- src/main/java/ip/Ui.java
+++ src/main/java/ip/Ui.java
@@ -14,13 +14,26 @@ public class Ui {
         }
         System.out.println(LINE);
     }
+
+    /**
+     * Reads one line from standard input
+     * @return the line from standard input
+     */
     public String getInput() {
         return sc.nextLine().trim();
     }
+
+    /**
+     * Displays the error message
+     * @param msg the error message
+     */
     public void displayError(String msg) {
         printResponse(":( OOPS!!! ", msg);
-//        printResponse("☹ OOPS!!! ", msg);
     }
+
+    /**
+     * Displays the initial greeting when the program starts
+     */
     public void initialGreeting() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -29,9 +42,20 @@ public class Ui {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
     }
+
+    /**
+     * Displays the response when a new task is added
+     * @param t the task added
+     * @param numTasks the no. of tasks in the list currently
+     */
     public void displayTaskAdded(Task t, int numTasks) {
         printResponse("Got it, I've added this task:", t.toString(), "Now you have " + numTasks + " task(s) in the list.");
     }
+
+    /**
+     * Displays all tasks in the list
+     * @param tasks the list of tasks
+     */
     public void displayAllTasks(TaskList tasks) {
         if (tasks.size() == 0) {
             printResponse("There are no tasks in the list! Please add some :)");
@@ -43,12 +67,27 @@ public class Ui {
         }
         printResponse(strList);
     }
+
+    /**
+     * Displays a goodbye greeting when the user exits
+     */
     public void displayGoodbye() {
         printResponse("Goodbye! Hope to hear from you soon :)");
     }
+
+    /**
+     * Displays the response when a task is marked as done by the user
+     * @param t the task marked as done
+     */
     public void displayTaskMarkedDone(Task t) {
         printResponse("Awesome! I've marked this task as done: ", t.toString());
     }
+
+    /**
+     * Displays the response when a task is deleted by the user
+     * @param t the task deleted
+     * @param numTasks the no. of tasks in the list currently
+     */
     public void displayTaskDeleted(Task t, int numTasks) {
         printResponse("Noted, I've removed this task: ", t.toString(), "Now you have " + numTasks + " task(s) in the list.");
     }
