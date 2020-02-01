9b81edb4c3a86530cc007ee53fe84e5f9a5fe389
JeremyLoh <jeremyloh03@gmail.com>
2020-02-01 16:24:54 +0800

    Duke: Add GUI layout based on javaFxTutorialPart2 Launcher: Add GUI launcher

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -2,17 +2,39 @@ package duke;
 
 import duke.command.Command;
 import duke.task.TaskList;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 /** Driver for duke project. */
-public class Duke {
+public class Duke extends Application {
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+
     private Storage storage;
     protected TaskList tasks;
     private Ui ui;
     protected static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    /**
+     * Returns a new Duke instance, uses duke.txt for save file.
+     */
+    public Duke() {
+        this("duke.txt");
+    }
 
     /**
      * Return a new instance of Duke class.
@@ -57,5 +79,64 @@ public class Duke {
             }
         }
         ui.goodbye();
+    }
+
+    /**
+     * The main entry point for all JavaFX applications.
+     * The start method is called after the init method has returned,
+     * and after the system is ready for the application to begin running.
+     *
+     * <p>
+     * NOTE: This method is called on the JavaFX Application Thread.
+     * </p>
+     *
+     * @param primaryStage the primary stage for this application, onto which
+     *                     the application scene can be set.
+     *                     Applications may create other stages, if needed, but they will not be
+     *                     primary stages.
+     * @throws Exception if something goes wrong
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        //The container for the content of the chat to scroll.
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        // Formatting the window to look as expected
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.setPrefSize(400.0, 600.0);
+        primaryStage.setTitle("Duke");
+        primaryStage.setResizable(false);
+        primaryStage.setMinHeight(600.0);
+        primaryStage.setMinWidth(400.0);
+
+        scrollPane.setPrefSize(400.0, 550.0);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+        // Set user input and send button size
+        userInput.setPrefWidth(330.0);
+        userInput.setPrefHeight(45.0);
+        sendButton.setPrefWidth(60.0);
+        sendButton.setPrefHeight(45.0);
+        // Place scroll pane at top
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+        // Place send button at bottom right
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+        // Place user input at bottom left
+        AnchorPane.setLeftAnchor(userInput, 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+        // Add nodes to layout
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+        scene = new Scene(mainLayout);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
