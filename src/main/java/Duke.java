3d755c2267be36e333c07f0f1de4b470af37f0ae
Jade Tay <jadet.hx@gmail.com>
2020-02-02 15:02:44 +0800

    Revert "GUI Implementation"
    This reverts commit 3017e7fc8e1911d468d94ea05b49fa01749ea84e.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,46 +5,22 @@ import storage.Storage;
 import tasklist.TaskList;
 import ui.Ui;
 
-import javafx.scene.control.Label;
-import javafx.scene.layout.Region;
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-
 /**
  * This programme implements an application that simulates like a chat bot.
  * Features include adding/deleting/searching Tasks, mark Tasks as done, listing Tasks
  * and exiting chat bot.
  */
 
-public class Duke extends Application {
+public class Duke {
 
     private Storage storage;
     private TaskList tasks;
     private Ui ui;
 
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Scene scene;
-    private Image user = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image duke = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
-
-    public Duke() {
-    }
-
     /**
      * Constructor for Duke class.
      *
-     * @param filePath File path of the text file to be loaded/saved.
+     * @param filePath  File path of the text file to be loaded/saved.
      */
     public Duke(String filePath) {
         ui = new Ui();
@@ -78,7 +54,6 @@ public class Duke extends Application {
         }
     }
 
-
     /**
      * This is the main method which makes use of static run method.
      *
@@ -87,21 +62,4 @@ public class Duke extends Application {
     public static void main(String[] args) {
         new Duke("/Users/jadetay/duke/data/tasks.txt").run();
     }
-
-
-
-    /**
-     * You should have your own function to generate a response to user input.
-     * Replace this stub with your completed method.
-     */
-    public String processCommand(String input) {
-        try {
-            Command c = Parser.parse(input);
-            c.execute(tasks, ui, storage);
-        } catch (DukeException e ) {
-            e.getMessage();
-        }
-    }
-
-
 }
