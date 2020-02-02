f63e4f6bddf90b6cad34d949cbcd01fbd4ba6fb2
aaroncql <choozy.aaron@gmail.com>
2020-02-02 11:09:00 +0800

    Add javafx tutorial 2 code

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,8 +3,13 @@ import java.util.Scanner;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.layout.Region;
 
 /**
  * This class is the main point of entry for this project.
@@ -106,10 +111,48 @@ public class Duke extends Application {
 
     @Override
     public void start(Stage stage) {
-        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
-        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+        ScrollPane scrollPane = new ScrollPane();
+        VBox dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        TextField userInput = new TextField();
+        Button sendButton = new Button("Send");
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        Scene scene = new Scene(mainLayout);
 
         stage.setScene(scene); // Setting the stage to show our screen
         stage.show(); // Render the stage.
+
+        stage.setTitle("Duke");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+        
+        mainLayout.setPrefSize(400.0, 600.0);
+        
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+        
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+        
+        // You will need to import `javafx.scene.layout.Region` for this. 
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+        
+        userInput.setPrefWidth(325.0);
+        
+        sendButton.setPrefWidth(55.0);
+        
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+        
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+        
+        AnchorPane.setLeftAnchor(userInput , 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
     }
 }
