c041448addb7af0b2a9c23c3b7615edb72a39bf1
Aloysius Chan <alloystory@gmail.com>
2020-02-02 18:57:57 +0800

    abstract main functions into other classes

--- src/main/java/app/Duke.java
+++ src/main/java/app/Duke.java
@@ -1,44 +1,38 @@
 package app;
 
-import java.util.Scanner;
-
 import app.exceptions.BaseException;
-import app.exceptions.WrongCommandException;
 import app.core.UserInterface;
+import app.core.commands.Command;
+import app.core.commands.CommandManager;
 import app.core.tasks.TaskManager;
 
 public class Duke {
-    public static final int MAX_TASKS = 100;
     public static final String WELCOME_MESSAGE = "Wussup Dawggg! I'm Dukeee\nWhat you want me do?";
     public static final String GOODBYE_MESSAGE = "Bye!\nStay cool bruh! (((:";
 
     private UserInterface userInterface;
+    private CommandManager commandManager;
+    private TaskManager taskManager;
+
     public Duke() {
         this.userInterface = new UserInterface();
+        this.commandManager = new CommandManager();
+        this.taskManager = new TaskManager();
     }
     
     public void start() {
         this.userInterface.render(WELCOME_MESSAGE);
-
-        TaskManager taskManager = new TaskManager(MAX_TASKS);
           
         while (!this.userInterface.isClosed()) {
             try {
                 String input = this.userInterface.listen();
-
-                if      (command.equals("bye"))                 this.userInterface.close();
-                else if (command.equals("list"))                this.userInterface.render(taskManager.toString());                
-                else if (command.equals("done"))                this.userInterface.render(taskManager.setTaskDone(nextArgs));
-                else if (command.equals("todo"))                this.userInterface.render(taskManager.addTodoTask(nextArgs));
-                else if (command.equals("deadline"))            this.userInterface.render(taskManager.addDeadlineTask(nextArgs));
-                else if (command.equals("event"))               this.userInterface.render(taskManager.addEventTask(nextArgs));
-                else if (command.equals("delete"))              this.userInterface.render(taskManager.deleteTask(nextArgs));
-                else                                            throw new WrongCommandException(String.format("The command '%s' is not supported", command));
+                Command command = this.commandManager.getCommand(input);
+                command.execute(this.taskManager, this.userInterface);
             } catch (BaseException e) {
-                this.userInterface.render(e.getMessage());
+                this.userInterface.renderError(e.getMessage());
             } catch (Exception e) {
-                this.userInterface.render("Caught some other exception! Notify developer!");
-                this.userInterface.render(e.getMessage());
+                this.userInterface.renderError("Caught some other exception! Notify developer!");
+                this.userInterface.renderError(e.getMessage());
             }
         }
 
