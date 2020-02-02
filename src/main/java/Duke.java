878d433189fa3f2a03466bd8f6f1ed108369e1fe
aaroncql <choozy.aaron@gmail.com>
2020-02-02 12:10:05 +0800

    Refactor to use fxml

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,23 +1,10 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-import javafx.scene.layout.Region;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-
 /**
  * This class is the main point of entry for this project.
  */
-public class Duke extends Application {
+public class Duke {
     private final Scanner sc = new Scanner(System.in);
     private final TaskList taskList = new TaskList();
     private final Storage storage = new Storage();
@@ -110,90 +97,5 @@ public class Duke extends Application {
         duke.getCommands();
 
         duke.exit();
-    }
-
-    /**
-     * Iteration 2: Creates two dialog boxes, one echoing user input and the other
-     * containing Duke's reply and then appends them to the dialog container. Clears
-     * the user input after processing.
-     */
-    private void handleUserInput(TextField userInput, VBox dialogContainer, Image duke, Image user) {
-        Label userText = new Label(userInput.getText());
-        Label dukeText = new Label(this.getResponse(userInput.getText()));
-        dialogContainer.getChildren().addAll(DialogBox.getUserDialog(userText, new ImageView(user)),
-                DialogBox.getDukeDialog(dukeText, new ImageView(duke)));
-        userInput.clear();
-    }
-
-    /**
-     * You should have your own function to generate a response to user input.
-     * Replace this stub with your completed method.
-     */
-    private String getResponse(String input) {
-        return "Duke heard: " + input;
-    }
-
-    @Override
-    public void start(Stage stage) {
-        // step 1
-        ScrollPane scrollPane = new ScrollPane();
-        VBox dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
-
-        TextField userInput = new TextField();
-        Button sendButton = new Button("Send");
-
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-
-        Scene scene = new Scene(mainLayout);
-
-        stage.setScene(scene); // Setting the stage to show our screen
-        stage.show(); // Render the stage.
-
-        // step 2
-        stage.setTitle("Duke");
-        stage.setResizable(false);
-        stage.setMinHeight(600.0);
-        stage.setMinWidth(400.0);
-
-        mainLayout.setPrefSize(400.0, 600.0);
-
-        scrollPane.setPrefSize(385, 535);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        // You will need to import `javafx.scene.layout.Region` for this.
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-
-        userInput.setPrefWidth(325.0);
-
-        sendButton.setPrefWidth(55.0);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-
-        AnchorPane.setLeftAnchor(userInput, 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-
-        // step 3
-        Image user = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-        Image duke = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
-
-        sendButton.setOnMouseClicked((event) -> {
-            this.handleUserInput(userInput, dialogContainer, duke, user);
-        });
-
-        userInput.setOnAction((event) -> {
-            this.handleUserInput(userInput, dialogContainer, duke, user);
-        });
-
-        // Scroll down to the end every time dialogContainer's height changes.
-        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
     }
 }
