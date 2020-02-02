eda6cb3c86ba9fbf729b89300abd1fec5536bbd5
Hanming Zhu <hanming@huawen.com>
2020-02-03 00:41:04 +0800

    feat: extend duke from javafx application
    Previously, the project used to run from Duke.
    
    Changes were made to Duke such that it extends from JavaFX Application, and now has the start() method. Implemented Tutorials 1 and 2 of the JavaFX tutorials, but changed the implementation of the window to follow the flexbox concept - using VBox and HBox to allow for responsiveness.
    
    To do: Continue with tutorial to implement everything else.

--- src/main/java/duchess/Duke.java
+++ src/main/java/duchess/Duke.java
@@ -6,6 +6,16 @@ import duchess.io.Parser;
 import duchess.storage.Storage;
 import duchess.task.TaskList;
 import duchess.ui.Ui;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 /**
  * The {@code Duke} class is the entry point of the Duchess program.
@@ -19,7 +29,7 @@ import duchess.ui.Ui;
  *
  * @author Zhu Hanming
  */
-public class Duke {
+public class Duke extends Application {
     private TaskList taskList;
     private Ui ui;
     private Storage storage;
@@ -34,6 +44,20 @@ public class Duke {
     public Duke(String filePath) {
         this.ui = new Ui();
         this.storage = new Storage(filePath);
+        try {
+            this.taskList = new TaskList(this.storage.load());
+        } catch (DuchessException e) {
+            this.ui.printLoadingError(e.getMessage());
+            this.taskList = new TaskList();
+        }
+    }
+
+    /**
+     * Initialises Duke with the default storage location.
+     */
+    public Duke() {
+        this.ui = new Ui();
+        this.storage = new Storage("data/tasks.json");
         try {
             this.taskList = new TaskList(this.storage.load());
         } catch (DuchessException e) {
@@ -74,5 +98,49 @@ public class Duke {
     public static void main(String[] args) {
         Duke duchess = new Duke("data/tasks.json");
         duchess.run();
+    }
+
+    /**
+     * Starts the JavaFX Stage.
+     *
+     * @param stage Stage to show the screen.
+     */
+    @Override
+    public void start(Stage stage) {
+        //Step 1. Setting up required components
+
+        //The container for the content of the chat to scroll.
+        ScrollPane scrollPane = new ScrollPane();
+        VBox dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        TextField userInput = new TextField();
+        Button sendButton = new Button("Send");
+
+        VBox.setVgrow(scrollPane, Priority.ALWAYS);
+        HBox.setHgrow(userInput, Priority.ALWAYS);
+        VBox mainLayout = new VBox();
+        HBox inputLayout = new HBox();
+        inputLayout.getChildren().addAll(userInput, sendButton);
+        mainLayout.getChildren().addAll(scrollPane, inputLayout);
+
+        Scene scene = new Scene(mainLayout);
+
+        stage.setScene(scene);
+
+        //Step 2. Formatting the window to look as expected
+        stage.setTitle("Duke");
+        stage.setResizable(true);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+        scrollPane.setVvalue(1.0);
+
+        // You will need to import `javafx.scene.layout.Region` for this.
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+        sendButton.setPrefWidth(55.0);
+        stage.show();
     }
 }
