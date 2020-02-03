af38dc69dd0cd041887a2df23182ffb89987aafe
Daniel Alfred Widjaja <dalfred2000@gmail.com>
2020-02-04 01:27:53 +0800

    add datetime format

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,18 +1,25 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
 
     protected String connector;
-    protected String datetime;
+    protected LocalDate datetime;
 
     public Deadline(String description, String connector, String datetime) {
         super(description);
         this.description = description;
         this.connector = connector;
-        this.datetime = datetime;
+        this.datetime = LocalDate.parse(datetime);
     }
 
     @Override
     public String toString() {
-        return ("[D][" + getStatusIcon() + "] " + description + " (" + connector + ": " + datetime + ")");
+        return ("[D][" + getStatusIcon() + "] " + description + " (" + connector + ": " + this.getDatetime() + ")");
+    }
+
+    public String getDatetime() {
+        return this.datetime.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
     }
 
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,18 +1,24 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
 
     protected String connector;
-    protected String datetime;
+    protected LocalDate datetime;
 
     public Event(String description, String connector, String datetime) {
         super(description);
         this.description = description;
         this.connector = connector;
-        this.datetime = datetime;
+        this.datetime = LocalDate.parse(datetime);
     }
 
     @Override
     public String toString() {
-        return ("[E][" + getStatusIcon() + "] " + description + " (" + connector + ": " + datetime + ")");
+        return ("[E][" + getStatusIcon() + "] " + description + " (" + connector + ": " + this.getDatetime() + ")");
     }
 
+    public String getDatetime() {
+        return this.datetime.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+    }
 }
