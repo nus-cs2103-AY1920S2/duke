62d65284e13b2db85163795dec267fd4186049bc
Aloysius Chan <alloystory@gmail.com>
2020-02-03 22:44:13 +0800

    add javadoc comments to the app.exceptions package

--- src/main/java/app/exceptions/WrongDateTimeFormatException.java
+++ src/main/java/app/exceptions/WrongDateTimeFormatException.java
@@ -1,6 +1,15 @@
 package app.exceptions;
 
+/**
+ * This class represents the exception thrown when the user
+ * enters a date that does not conform to the supported formats
+ */
 public class WrongDateTimeFormatException extends BaseException {
+    /**
+     * Initialises a new WrongDateTimeFormatException object with a
+     * custom error message
+     * @param message The error message
+     */
     public WrongDateTimeFormatException(String message) {
         super(message);
     }
--- src/main/java/app/exceptions/WrongUsageException.java
+++ src/main/java/app/exceptions/WrongUsageException.java
@@ -1,6 +1,15 @@
 package app.exceptions;
 
+/**
+ * This class represents the exception thrown when the user
+ * fails to provide certain arguments required by the command
+ */
 public class WrongUsageException extends BaseException {
+    /**
+     * Initialises a new WrongUsageException object with a
+     * custom error message
+     * @param message The error message
+     */
     public WrongUsageException(String message) {
         super(message);
     }
