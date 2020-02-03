f1809adcce7e82acc0762258187db00d36a790b2
J-Dan23 <janiceongyw@gmail.com>
2020-02-03 17:06:03 +0800

    feat: add DukeTest with two basic test methods (Todo and Event)

--- src/test/java/DukeTest.java
+++ src/test/java/DukeTest.java
@@ -4,7 +4,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DukeTest {
     @Test
-    public void dummyTest(){
-        assertEquals(2, 2);
+    public void ToDoTest(){
+        Todo task = new Todo("Find Royce on Bracket Towers");
+        assertEquals("[T][\u2718] Find Royce on Bracket Towers", task.toString());
+    }
+
+    @Test
+    public void EventTest() {
+        Event task = new Event("Performance at the Empty Set", "2014-05-20");
+        assertEquals("[E][\u2718] Performance at the Empty Set (May 20 2014)", task.toString());
     }
 }
