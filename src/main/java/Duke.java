be66f163a5cfb5588130901cb23f1d2fd49c443b
J-Dan23 <janiceongyw@gmail.com>
2020-02-04 01:21:14 +0800

    chore: add javadocs

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,29 @@
 import java.io.IOException;
 
+/**
+ * Driver class of Duke chatbot, also saves on startup and loads on exit.
+ */
 public class Duke {
+
+    /**
+     * Storage for loading task list, and to save right before exiting Duke.
+     */
     private Storage storage;
+
+    /**
+     * TaskList to keep track of existing tasks.
+     */
     private TaskList tasks;
+
+    /**
+     * Ui to provide user interface printing.
+     */
     private Ui ui;
 
+    /**
+     * Constructor of Duke.
+     * @param filePath
+     */
     public Duke(String filePath) {
         ui = new Ui();
         storage = new Storage(filePath);
@@ -16,12 +35,19 @@ public class Duke {
         }
     }
 
+    /**
+     * Driver method for Duke.
+     */
     public void run() {
         ui.printLogo();
         ui.greetDuke();
         Parser.handle(tasks, ui);
     }
 
+    /**
+     * Main method of Duke.
+     * @param args
+     */
     public static void main(String[] args) {
         Duke duke = new Duke("savedata.txt");
         duke.run();
