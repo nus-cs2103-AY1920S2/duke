aada13f7d423ab8fc2c0bc7739e5df532da1dc4a
maxiepangz <mpang45456@gmail.com>
2020-02-04 07:29:19 +0800

    Level-8

--- src/main/java/DeadLine.java
+++ src/main/java/DeadLine.java
@@ -1,13 +1,17 @@
+import java.time.LocalDate;
+
 public class DeadLine extends task {
     private String date;
+    private LocalDate ld;
 
     public DeadLine (String name, String end) {
         super(name);
         this.date = end;
+        this.ld = LocalDate.parse(end);
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + date + ")";
+        return "[D]" + super.toString() + " (by: " + ld.getDayOfMonth() + " " + ld.getMonth() + " " + ld.getYear() + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,13 +1,20 @@
+import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Event extends task {
-    private String date;
+    private String from;
+    private LocalDate ld;
 
     public Event (String name, String date) {
         super(name);
-        this.date = date;
+        String[] temp = date.split(" ");
+        this.ld = LocalDate.parse(temp[0]);
+        this.from = temp[1];
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + date + ")";
+        return "[E]" + super.toString() + " (at: " + ld.getDayOfMonth() + " " + ld.getMonth() + " " + ld.getYear() + " " + from + ")";
     }
 }
