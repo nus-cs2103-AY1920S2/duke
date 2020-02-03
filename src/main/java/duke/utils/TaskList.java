28940d94400b87c520a2e3939a66f709b0bab45e
HubertHalim <huberthalim7@gmail.com>
2020-02-03 21:38:01 +0800

    level 9

--- src/main/java/duke/utils/TaskList.java
+++ src/main/java/duke/utils/TaskList.java
@@ -7,6 +7,7 @@ import duke.task.Todos;
 
 import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class TaskList {
@@ -45,6 +46,17 @@ public class TaskList {
 
     public void removeTask(int taskId) {
         tasks.remove(taskId);
+    }
+
+    public List<Task> findTasksByKeyword(String word) {
+        List<Task> searchedTask = new ArrayList<>();
+        for (int i = 0; i < tasks.size(); i++) {
+            String[] tokens = tasks.get(i).getTaskName().split(" ");
+            if(Arrays.asList(tokens).contains(word)) {
+                searchedTask.add(tasks.get(i));
+            }
+        }
+        return searchedTask;
     }
 
     public boolean addToList(String item, String type) throws ParseException {
