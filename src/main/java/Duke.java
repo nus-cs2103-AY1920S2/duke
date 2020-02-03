70b6fa4e64da66649a2913ee57c76ed07d2919f6
khsc96 <khsc96@gmail.com>
2020-02-03 18:30:41 +0800

    Tutorial 2: Style the controls

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,8 +9,10 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.layout.Region;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+
 /**
  * Represents a Personal Assistant Chatbot named EXE that
  * helps a person to keep track of various things.
@@ -103,9 +105,39 @@ public class Duke extends Application {
 
         scene = new Scene(mainLayout);
 
+        // step 2:
+        stage.setTitle("Duke");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        // You will need to import `javafx.scene.layout.Region` for this.
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(325.0);
+
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput , 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+
         stage.setScene(scene);
         stage.show();
-//
+
 //        Scene anotherScene = new Scene(new Label("Another scene here"));
 //        Stage anotherStage = new Stage();
 //        anotherStage.setScene(anotherScene);
