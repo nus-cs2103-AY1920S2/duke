9f41718cbc70ed998fe227ab70ed018965b798d5
braddyH <yingxu.he1998@gmail.com>
2020-02-03 22:21:47 +0800

    add new exceptions

---
+++ src/main/java/exceptions/NoCommandException.java
@@ -0,0 +1,7 @@
+package main.java.exceptions;
+
+public class NoCommandException extends Exception {
+    public NoCommandException(String cause) {
+        super(cause);
+    }
+}
---
+++ src/main/java/exceptions/NoTimeException.java
@@ -0,0 +1,7 @@
+package main.java.exceptions;
+
+public class NoTimeException extends Exception{
+    public NoTimeException(String cause) {
+        super(cause);
+    }
+}
