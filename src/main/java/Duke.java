ef1f2fac0bf55827a942d3874df107c8d18174a9
braddyH <yingxu.he1998@gmail.com>
2020-02-03 22:22:41 +0800

    achieve more oop

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,20 +1,11 @@
 package main.java;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
+import main.java.Parser.Command;
+import main.java.Parser.ExitCommand;
+import main.java.Parser.Parser;
+import main.java.exceptions.NoCommandException;
 import main.java.model.TaskList;
-import main.java.model.Task;
-import main.java.model.ToDoTask;
-import main.java.model.EventTask;
-import main.java.model.DeadLineTask;
+
 
 import main.java.exceptions.NoDescriptionException;
 
@@ -22,172 +13,53 @@ public class Duke {
 
     protected String user_name;
     protected TaskList taskList;
-
-    static String logo = " ____        _        \n"
-            + "|  _ \\ _   _| | _____ \n"
-            + "| | | | | | | |/ / _ \\\n"
-            + "| |_| | |_| |   <  __/\n"
-            + "|____/ \\__,_|_|\\_\\___|\n";
-
-    static String line = "____________________________________________________________\n";
-
-    static String greeting = "I'm Duke.\n What can I do for you?\n";
-    static String viewListGreeting = "Here are the tasks in your list:\n";
-    static String exitGreeting = "Bye. Hope to see you again soon!\n";
-    static String addTaskStart = "Got it. I've added this task:\n";
-    static String deleteTaskStart = "Noted. I have removed this task:\n";
-
-
-    static String exitKey = "bye";
-    static String viewListKey = "list";
-    static String deleteKey = "(delete)(\\s+)(\\d+)";
-    static String finishKey = "(done)(\\s+)(\\d+)";
-    static String todoKey = "(todo)(.*)";
-    static String deadlineKey = "(deadline)(.*)(\\/by)(.*)";
-    static String eventKey = "(event)(.*)(\\/at)(.*)";
-
-    static Pattern deletePattern = Pattern.compile(deleteKey);
-    static Pattern finishPattern = Pattern.compile(finishKey);
-    static Pattern todoPattern = Pattern.compile(todoKey);
-    static Pattern deadlinePattern = Pattern.compile(deadlineKey);
-    static Pattern eventPattern = Pattern.compile(eventKey);
+    protected Parser parser;
+    protected Ui ui;
 
     public Duke () {
         this.user_name = "";
-        this.taskList = new TaskList();
     }
 
     public Duke (String user_name) {
         this.user_name = user_name;
-        this.taskList = new TaskList();
     }
 
     public void setUserName(String user_name) {
         this.user_name = user_name;
     }
 
-    private static boolean isExitKey(String input) { return exitKey.equals(input); }
-
-    private static boolean isViewListKey(String input) {return viewListKey.equals(input); }
-
-    private static boolean isDeleteKey(String input) {
-        Matcher deleteMatcher = deletePattern.matcher(input);
-        return deleteMatcher.find();
-    }
-
-    private static boolean isFinishKey(String input) {
-        Matcher finishMatcher = finishPattern.matcher(input);
-        return finishMatcher.find();
-    }
-
-    private static boolean isTodoKey(String input) {
-        Matcher todoMatcher = todoPattern.matcher(input);
-        return todoMatcher.find();
-    }
-
-    private static boolean isDeadLineKey(String input) {
-        Matcher deadlineMatcher = deadlinePattern.matcher(input);
-        return deadlineMatcher.find();
-    }
-
-    private static boolean isEventKey(String input) {
-        Matcher eventMatcher = eventPattern.matcher(input);
-        return eventMatcher.find();
-    }
-
-    private int findIndex(Pattern pattern, String input) {
-        Matcher matcher = pattern.matcher(input);
-        matcher.find();
-        return Integer.parseInt(matcher.group(3)) - 1;
-    }
-
-    private String findDescription(Pattern pattern, String input) {
-        Matcher matcher = pattern.matcher(input);
-        matcher.find();
-        return matcher.group(2).trim();
-    }
-
-    private String findEndTime(Pattern pattern, String input) {
-        Matcher matcher = pattern.matcher(input);
-        matcher.find();
-        return matcher.group(4).trim();
-    }
-
-    public void greet() {
-        System.out.println(line + "Hello "+ this.user_name + "! " + greeting + line);
+    private void start() {
+        this.taskList = new TaskList();
+        this.parser = new Parser();
+        this.ui = new Ui();
+        ui.askForName();
+        ui.greet();
     }
 
     private void exit() {
-        System.out.println(line + exitGreeting + line);
         System.exit(0);
     }
 
-    public void listen(String input) {
-        if (this.isExitKey(input)) {
-            this.exit();
-        }
-        else if (this.isViewListKey(input)) {
-            System.out.println(this.taskList.toString());
-        }
-        else if (this.isDeleteKey(input)) {
-            int deletedTaskIndex = this.findIndex(deletePattern, input);
-            this.taskList.remove(deletedTaskIndex);
-        }
-        else if (this.isFinishKey(input)) {
-            int finishedTaskIndex = this.findIndex(finishPattern, input);
-            this.taskList.markTaskAsDone(finishedTaskIndex);
-        }
-        else if (this.isTodoKey(input)){
-            String description = this.findDescription(todoPattern, input);
+    public void run() {
+        Command command = new Command();
+        this.start();
+
+        while (!ExitCommand.isExit(command)) {
             try {
-                ToDoTask currentTask = new ToDoTask(description);
-                this.taskList.add(currentTask);
-            } catch (NoDescriptionException e) {
-                System.out.println(e.getMessage());
+                String input = ui.getUserInput();
+                command = parser.parseCommand(input);
+                command.setTaskList(this.taskList);
+                String commandResult = command.execute();
+                ui.printCommandResult(commandResult);
+            } catch (NoDescriptionException | NoCommandException e) {
+                ui.printErrorMessage(e.getMessage());
             }
         }
-        else if (this.isDeadLineKey(input)){
-            String description = this.findDescription(deadlinePattern, input);
-            String by = this.findEndTime(deadlinePattern, input);
-            try {
-                DeadLineTask currentTask = new DeadLineTask(description, by);
-                this.taskList.add(currentTask);
-            } catch (NoDescriptionException e) {
-                System.out.println(e.getMessage());
-            }
-        }
-        else if (this.isEventKey(input)){
-            String description = this.findDescription(eventPattern, input);
-            String at = this.findEndTime(eventPattern, input);
-            try {
-                EventTask currentTask = new EventTask(description, at);
-                this.taskList.add(currentTask);
-            } catch (NoDescriptionException e) {
-                System.out.println(e.getMessage());
-            }
-        }
-        else {
-            System.out.println(line + "OOPS!!! I'm sorry, but I don't know what that means :-(\n" + line);
-        }
+        this.exit();
     }
 
     public static void main(String[] args) {
-        BufferedReader br = null;
         Duke myDuke = new Duke();
-
-        try {
-            br = new BufferedReader(new InputStreamReader(System.in));
-            System.out.println(line + "Please input user name.\n" + line);
-            String input = br.readLine();
-            myDuke.setUserName(input.trim());
-            myDuke.greet();
-
-            while (true) {
-                String next_input = br.readLine();
-                myDuke.listen(next_input);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        myDuke.run();
     }
 }
