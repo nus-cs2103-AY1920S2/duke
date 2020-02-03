7ae505e9a5f83dfe5239d370232c534f3bab9de4
Aloysius Chan <alloystory@gmail.com>
2020-02-03 22:44:53 +0800

    add javadoc comments to Duke.java

--- src/main/java/app/Duke.java
+++ src/main/java/app/Duke.java
@@ -6,20 +6,37 @@ import app.core.commands.Command;
 import app.core.commands.CommandManager;
 import app.core.tasks.TaskManager;
 
+/**
+ * Main class for Duke. The class contains all the main components 
+ * required for Duke to run properly.
+ */
 public final class Duke {
+    /**
+     * Welcome message that is printed upon starting Duke
+     */
     public static final String WELCOME_MESSAGE = "Wussup Dawggg! I'm Dukeee\nWhat you want me do?";
+
+    /**
+     * Goodbye message that is printed right before the program exits
+     */
     public static final String GOODBYE_MESSAGE = "Bye!\nStay cool bruh! (((:";
 
     private UserInterface userInterface;
     private CommandManager commandManager;
     private TaskManager taskManager;
 
+    /**
+     * Initializes a new instance of Duke.
+     */
     public Duke() {
         this.userInterface = new UserInterface();
         this.commandManager = new CommandManager();
         this.taskManager = new TaskManager();
     }
     
+    /**
+     * Starts Duke
+     */
     public void start() {
         this.userInterface.render(WELCOME_MESSAGE);
           
