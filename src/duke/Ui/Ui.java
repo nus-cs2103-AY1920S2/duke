47e394add4a1a99a8ae0108a11d6f8901a1c589b
HemanshuGandhi <hemanshu.gandhi@gmail.com>
2020-02-04 21:04:16 +0800

    Set up gradle so that you can build and run Duke using gradle

--- src/duke/Ui/Ui.java
+++ src/duke/Ui/Ui.java
@@ -11,7 +11,7 @@ public class Ui {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println(logo);
-        System.out.println("Hello! I'm duke.Duke\n" + "What can I do for you?");
+        System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
     }
 
     public String showError() {
