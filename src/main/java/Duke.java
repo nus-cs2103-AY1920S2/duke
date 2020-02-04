4a28d29f86eda20bd5b07b99d15df445e26dfe51
LiuChao93 <>
2020-02-04 11:59:49 +0800

    cleaned code

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,143 +1,170 @@
 import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import static java.lang.Integer.*;
 
 public class Duke {
-    public static void main(String[] args) {
-        Task[] tasks = new Task[100];
-        Scanner myObj = new Scanner(System.in);
-        int count = 0;
-        int count2 = 0;
-        int count3 = 1;
-        int count4 = 0;
-        String content2;
-        String content3;
-        boolean isProblem = false;
+    static String PATH = "./src/main/java/savedTaskList.txt";
+    static Task[] totalTasks = new Task[100];
+    static int totalTasksCount = 0;
+    static boolean isProblem = false;
 
+    public static void testMessage(String userInput) throws DukeException {
+        if (!userInput.contains("bye") && !userInput.contains("list") && !userInput.contains("done 2")
+                && !userInput.contains("delete") && !userInput.contains("todo")
+                && !userInput.contains("deadline") && !userInput.contains("event")) {
+            System.out.println("    ____________________________________________________________\n"
+                    + "     ☹ OOPS!!! I'm sorry, but I don't know what that means :-(\n"
+                    + "    ____________________________________________________________\n");
+            throw new DukeException("I don't know what that means");
+
+        } else if (!userInput.contains(" ") && !userInput.contains("bye") && !userInput.contains("list")){
+            System.out.println("    ____________________________________________________________\n"
+                    + "     ☹ OOPS!!! The description of a " + userInput + " cannot be empty.\n"
+                    + "    ____________________________________________________________\n");
+            throw new DukeException("Empty");
+        }
+    }
+
+    public static void init(){
         System.out.println("    ____________________________________________________________");
         System.out.println("     Hello! I'm Duke");
         System.out.println("     What can I do for you?");
         System.out.println("    ____________________________________________________________\n");
+    }
 
-        String userInput = myObj.nextLine();
+    public static String input(){
+        Scanner myObj = new Scanner(System.in);
+        String inputData = myObj.nextLine();
         try{
-            testMessage(userInput);
+            testMessage(inputData);
         } catch(DukeException ex){
-            System.out.println(ex);
             isProblem = true;
         }
+        return inputData;
+    }
 
-        while(!userInput.equals("bye")){
-            if(isProblem) {
-                isProblem = false;
-
-            } else {
-                if (userInput.equals("list")) {
-                    System.out.println("    ____________________________________________________________");
-                    System.out.println("     Here are the tasks in your list:");
-                    count2 = 0;
-                    count3 = 1;
-                    while (count2 < count) {
-                        System.out.println("     " + count3 + "." + tasks[count2].toString());
-                        count2++;
-                        count3++;
-                    }
-                    System.out.println("    ____________________________________________________________\n");
-
-                } else if (userInput.equals("done 2")) {
-                    System.out.println("    ____________________________________________________________");
-                    System.out.println("     Nice! I've marked this task as done: ");
-                    count2 = 0;
-                    while (count2 < count) {
-                        if (tasks[count2].getDone()) {
-                            System.out.println("       " + tasks[count2].getStatusIcon() + " " + tasks[count2].getDescription());
-                        }
-                        count2++;
-                    }
-                    System.out.println("    ____________________________________________________________");
-
-                } else if (userInput.contains("delete")){
-                    content2 = userInput.substring(7);
-                    count4 = Integer.parseInt(content2) - 1;
-                    if(count4 == count){
-                        count = count - 1;
-                        Task.taskNo = Task.taskNo - 1;
-
-                    } else {
-                        while (count4 <= count) {
-                            tasks[count4] = tasks[count4 + 1];
-                            count4++;
-                        }
-                        count = count - 1;
-                        Task.taskNo = Task.taskNo - 1;
-                    }
-
-                } else {
-                    if (userInput.contains("todo")) {
-                        content2 = userInput.substring(5);
-                        Task t = new Todo(content2);
-                        tasks[count] = t;
-
-                    } else if (userInput.contains("deadline")) {
-                        userInput = userInput.substring(9);
-                        content2 = userInput.substring(0, userInput.indexOf("/by") - 1);
-                        content3 = userInput.substring(userInput.indexOf("/by") + 4);
-                        Task t = new Deadline(content2, content3);
-                        tasks[count] = t;
-                    } else {
-                        userInput = userInput.substring(6);
-                        content2 = userInput.substring(0, userInput.indexOf("/at") - 1);
-                        content3 = userInput.substring(userInput.indexOf("/at") + 4);
-                        Task t = new Event(content2, content3);
-                        tasks[count] = t;
-                    }
-                    count++;
-                }
-            }
-            userInput = myObj.nextLine();
-            try{
-                testMessage(userInput);
-            } catch(DukeException ex){
-                System.out.println(ex);
-                isProblem = true;
-            }
-        }
-
+    public static void bye(){
         System.out.println("    ____________________________________________________________");
         System.out.println("     Bye. Hope to see you again soon!");
         System.out.println("    ____________________________________________________________");
     }
 
-    public static void testMessage(String userInput) throws DukeException {
-        if (!userInput.contains(" ") && !userInput.contains("list")) {
-            if ((userInput.equals("todo")) || (userInput.equals("deadline")) || (userInput.equals("event"))
-                    || (userInput.equals("list")) || (userInput.equals("bye")) || (userInput.equals("delete"))) {
-                System.out.println("    ____________________________________________________________\n"
-                        + "     ☹ OOPS!!! The description of a todo cannot be empty.\n"
-                        + "    ____________________________________________________________\n");
-                throw new DukeException("I don't know what that means");
+    public static void list(){
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Here are the tasks in your list:");
+        int i = 0;
+        int j = 1;
+        while (i < totalTasksCount) {
+            System.out.println("     " + j + "." + totalTasks[i].toString());
+            i++;
+            j++;
+        }
+        System.out.println("    ____________________________________________________________\n");
+    }
 
-            } else {
-                System.out.println("    ____________________________________________________________\n"
-                        + "     ☹ OOPS!!! I'm sorry, but I don't know what that means :-(\n"
-                        + "    ____________________________________________________________\n");
-                throw new DukeException("Empty");
+    public static void done2(){
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Nice! I've marked this task as done: ");
+        int i = 0;
+        while (i < totalTasksCount) {
+            if (totalTasks[i].getDone()) {
+                System.out.println("       " + totalTasks[i].getStatusIcon() + " " + totalTasks[i].getDescription());
             }
+            i++;
+        }
+        System.out.println("    ____________________________________________________________");
+    }
 
-        } else if(!userInput.contains("list")){
-            String content1 = userInput.substring(0, userInput.indexOf(" "));
-
-            if ((!content1.equals("todo")) && (!content1.equals("deadline")) && (!content1.equals("event"))
-                    && (!content1.equals("list")) && (!content1.equals("bye"))  && (userInput.equals("delete"))) {
-                System.out.println("    ____________________________________________________________\n"
-                        + "     ☹ OOPS!!! I'm sorry, but I don't know what that means :-(\n"
-                        + "    ____________________________________________________________\n");
-                throw new DukeException("I don't know what that means");
-
-            } else if (userInput.equals(content1)) {
-                System.out.println("    ____________________________________________________________\n"
-                        + "     ☹ OOPS!!! The description of a todo cannot be empty.\n"
-                        + "    ____________________________________________________________\n");
-                throw new DukeException("Empty");
+    public static void delete(String userInput){
+        String deleteSelect = userInput.substring(7);
+        int deleteSelectNo = parseInt(deleteSelect) - 1;
+        if (deleteSelectNo != totalTasksCount) {
+            while (deleteSelectNo <= totalTasksCount) {
+                totalTasks[deleteSelectNo] = totalTasks[deleteSelectNo + 1];
+                deleteSelectNo++;
             }
         }
+        totalTasksCount = totalTasksCount - 1;
+        Task.taskNo = Task.taskNo - 1;
+    }
+
+    public static void toDo(String userInput){
+        String content = userInput.substring(5);
+        Task t = new Todo(content);
+        totalTasks[totalTasksCount] = t;
+        totalTasksCount++;
+    }
+
+    public static void deadline(String userInput){
+        userInput = userInput.substring(9);
+        String contentTasks = userInput.substring(0, userInput.indexOf("/by") - 1);
+        String taskDeadline = userInput.substring(userInput.indexOf("/by") + 4);
+        Task t = new Deadline(contentTasks, taskDeadline);
+        totalTasks[totalTasksCount] = t;
+        totalTasksCount++;
+    }
+
+    public static void event(String userInput){
+        userInput = userInput.substring(6);
+        String contentTasks = userInput.substring(0, userInput.indexOf("/at") - 1);
+        String taskTime = userInput.substring(userInput.indexOf("/at") + 4);
+        Task t = new Event(contentTasks, taskTime);
+        totalTasks[totalTasksCount] = t;
+        totalTasksCount++;
+    }
+
+   // public static void writeFile(String message, String path) throws IOException {
+       // FileWriter writeFile = new FileWriter(path);
+       // writeFile.write(message);
+       // writeFile.close();
+        //try {
+            //writeFile("testing 123", PATH);
+        //} catch (IOException e) {
+            // could not write file
+        //}
+   // }
+
+    public static void readFile() throws FileNotFoundException {
+        File savedTaskList = new File(PATH);
+        Scanner scanSavedTaskList = new Scanner(savedTaskList);
+        while (scanSavedTaskList.hasNext()){
+            System.out.println(scanSavedTaskList.nextLine());
+        }
+    }
+
+    public static void main(String[] args){
+        try {
+            readFile();
+        } catch (FileNotFoundException e) {
+            // creating new file
+        }
+        init();
+        String userInput = input();
+
+        while(!userInput.equals("bye")){
+            if(isProblem) {
+                isProblem = false;
+            } else {
+                if (userInput.equals("list")) {
+                    list();
+                } else if (userInput.equals("done 2")) {
+                    done2();
+                } else if (userInput.contains("delete")){
+                    delete(userInput);
+                } else if (userInput.contains("todo")) {
+                    toDo(userInput);
+                } else if (userInput.contains("deadline")) {
+                    deadline(userInput);
+                } else {
+                    event(userInput);
+                }
+            }
+            userInput = input();
+        }
+        bye();
     }
 }
