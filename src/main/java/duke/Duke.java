6ca44fc6594979d851ec912e3c1ab9e4cc59a9f3
Samuel Huang <eclmist@gmail.com>
2020-02-04 20:41:01 +0800

    Add support for JavaFX

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -7,7 +7,12 @@ import duke.io.Serializer;
 import duke.io.Ui;
 import duke.task.TaskList;
 
-public class Duke {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class Duke extends Application {
 
     protected TaskList tasks;
     protected Ui ui;
@@ -42,4 +47,14 @@ public class Duke {
             }
         }
     }
+
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
+    }
+
 }
