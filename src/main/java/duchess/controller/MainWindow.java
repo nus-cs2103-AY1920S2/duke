b380724b8fbde99cdcd0a27d73e3b70018544c94
Hanming Zhu <hanming@huawen.com>
2020-02-04 20:14:31 +0800

    feat: update mainwindow fxml and controller
    Change implementation of MainWindow to use VBox instead of AnchorPane.
    
    Change the initialisation of MainWindow to get initial output of Duke and display it out.

--- src/main/java/duchess/controller/MainWindow.java
+++ src/main/java/duchess/controller/MainWindow.java
@@ -8,7 +8,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
@@ -16,7 +15,7 @@ import java.io.IOException;
 /**
  * Controller for MainWindow. Provides the layout for the other controls.
  */
-public class MainWindow extends AnchorPane {
+public class MainWindow extends VBox {
     @FXML
     private ScrollPane scrollPane;
     @FXML
@@ -31,11 +30,19 @@ public class MainWindow extends AnchorPane {
     private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
     private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
 
+    /**
+     * Initialises {@code MainWindow} via FXML.
+     */
     @FXML
     public void initialize() {
         scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
     }
 
+    /**
+     * Initialises a {@code MainWindow} controller instance.
+     *
+     * @param d Duke instance.
+     */
     public MainWindow(Duke d) {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
@@ -45,11 +52,11 @@ public class MainWindow extends AnchorPane {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        this.setDuke(d);
-    }
-
-    public void setDuke(Duke d) {
-        duke = d;
+        this.duke = d;
+        String welcomeMessage = duke.getWelcomeMessage();
+        dialogContainer.getChildren().add(
+                DialogBox.getDukeDialog(welcomeMessage, dukeImage)
+        );
     }
 
     /**
