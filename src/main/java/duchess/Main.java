75ca899fa4970dcea2a82ed4712e497311d5e763
Hanming Zhu <hanming@huawen.com>
2020-02-04 20:20:12 +0800

    feat+docs: change mainwindow to vbox and set min dimensions and add javadoc

--- src/main/java/duchess/Main.java
+++ src/main/java/duchess/Main.java
@@ -3,18 +3,24 @@ package duchess;
 import duchess.controller.MainWindow;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+/**
+ * The {@code Main} class helps to start up the GUI for Duchess.
+ */
 public class Main extends Application {
 
-    private Duke duke = new Duke("data/tasks.json");
+    private Duke duke = new Duke("data/tasks.json", true);
 
     @Override
     public void start(Stage stage) {
-        AnchorPane ap = new MainWindow(duke);
+        VBox ap = new MainWindow(duke);
         Scene scene = new Scene(ap);
         stage.setScene(scene);
+        stage.setMinHeight(600);
+        stage.setHeight(600);
+        stage.setMinWidth(400);
         stage.show();
     }
 }
