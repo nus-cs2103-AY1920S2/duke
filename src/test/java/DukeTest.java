5f9350472576675b80e3ad4ba7458c6caf82be87
Ernest Lian <ernestlian@hotmail.com>
2020-02-04 17:46:30 +0800

    A-JUnit

---
+++ src/test/java/DukeTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DukeTest {
+    @Test
+
+    public void dukeTest() {
+        assertEquals(2, 2);
+    }
+}
