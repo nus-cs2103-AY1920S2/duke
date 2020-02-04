5b916dac90234ab3e63211325b7f48979cee4d5f
Hanming Zhu <hanming@huawen.com>
2020-02-04 20:28:56 +0800

    feat(duke): add ability to support both console and gui mode
    Modified Duke to be able to run on both console and GUI mode. This is done via overloading the constructor and dedicating the run() method for console mode, and the getResponse() method for GUI mode.
    
    This meets the A-JavaFX requirements.

--- src/main/java/duchess/Duke.java
+++ src/main/java/duchess/Duke.java
@@ -23,9 +23,10 @@ public class Duke {
     private TaskList taskList;
     private Ui ui;
     private Storage storage;
+    private String loadingErrorMessage;
 
     /**
-     * Initialises a newly created {@code Duchess} object that uses the
+     * Initialises a newly created {@code Duke} object that uses the
      * provided {@code filePath} as the location of the JSON save file.
      *
      * @param filePath A {@code String} denoting the location of the JSON
@@ -43,126 +44,56 @@ public class Duke {
     }
 
     /**
-     * Begins the Duchess program. Upon calling the run() method, the user
-     * can begin to interact with the program.
+     * Initialises a newly created {@code Duke} object that uses the
+     * provided {@code filePath} as the location of the JSON save file.
+     * This is an overloaded constructor for GUI that stores any error
+     * messages encountered when starting up so as to print subsequently.
+     *
+     * @param filePath A {@code String} denoting the location of the JSON
+     *                 save file.
+     * @param isGui    A {@code boolean} denoting whether the program is in
+     *                 Gui mode.
+     */
+    public Duke(String filePath, boolean isGui) {
+        this.ui = new Ui();
+        this.storage = new Storage(filePath);
+        try {
+            this.taskList = new TaskList(this.storage.load());
+        } catch (DuchessException e) {
+            if (isGui) {
+                this.loadingErrorMessage = this.ui.printLoadingError(e.getMessage());
+            } else {
+                this.ui.printLoadingError(e.getMessage());
+            }
+            this.taskList = new TaskList();
+        }
+    }
+
+    /**
+     * Begins the console version of the Duchess program. Upon calling
+     * the run() method, the user can begin to interact with the program.
      */
     public void run() {
-        this.ui.printWelcome();
+        this.ui.printToConsole(this.ui.printConsoleWelcome());
+        ui.printLine();
         boolean isRunning = true;
         while (isRunning) {
             try {
-                String fullCommand = ui.readCommand();
-                ui.printLine();
+                String fullCommand = this.ui.readCommand();
+                this.ui.printLine();
                 Command command = Parser.parse(fullCommand);
-                command.execute.apply(fullCommand, this.taskList, this.ui, this.storage);
+                String response = command.execute.apply(fullCommand, this.taskList, this.ui, this.storage);
+                this.ui.printToConsole(response);
                 if (command == Command.BYE) {
                     isRunning = false;
                 }
             } catch (DuchessException e) {
-                ui.printError(e.getMessage());
+                this.ui.printToConsole(this.ui.printError(e.getMessage()));
             } finally {
                 ui.printLine();
             }
         }
     }
-
-    /**
-     * Starts up the Duchess program.
-     *
-     * @param args Not used.
-     */
-    public static void main(String[] args) {
-        Duke duchess = new Duke("data/tasks.json");
-        duchess.run();
-    }
-
-//    /**
-//     * Starts the JavaFX Stage.
-//     *
-//     * @param stage Stage to show the screen.
-//     */
-//    @Override
-//    public void start(Stage stage) {
-//        //Step 1. Setting up required components
-//
-//        //The container for the content of the chat to scroll.
-//        scrollPane = new ScrollPane();
-//        dialogContainer = new VBox();
-//        scrollPane.setContent(dialogContainer);
-//
-//        userInput = new TextField();
-//        sendButton = new Button("Send");
-//
-//        VBox.setVgrow(scrollPane, Priority.ALWAYS);
-//        HBox.setHgrow(userInput, Priority.ALWAYS);
-//        VBox mainLayout = new VBox();
-//        HBox inputLayout = new HBox();
-//        inputLayout.getChildren().addAll(userInput, sendButton);
-//        mainLayout.getChildren().addAll(scrollPane, inputLayout);
-//
-//        Scene scene = new Scene(mainLayout);
-//
-//        stage.setScene(scene);
-//
-//        //Step 2. Formatting the window to look as expected
-//        stage.setTitle("Duke");
-//        stage.setResizable(true);
-//        stage.setMinHeight(600.0);
-//        stage.setMinWidth(400.0);
-//
-//        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-//        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-//        scrollPane.setVvalue(1.0);
-//        scrollPane.setFitToWidth(true);
-//
-//        // You will need to import `javafx.scene.layout.Region` for this.
-//        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-//        sendButton.setPrefWidth(55.0);
-//
-//        //Step 3. Add functionality to handle user input.
-//        sendButton.setOnMouseClicked((event) -> {
-//            handleUserInput();
-//        });
-//
-//        userInput.setOnAction((event) -> {
-//            handleUserInput();
-//        });
-//        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
-//
-//
-//        stage.show();
-//    }
-//
-//    /**
-//     * Iteration 1:
-//     * Creates a label with the specified text and adds it to the dialog container.
-//     *
-//     * @param text String containing text to add
-//     * @return a label with the specified text that has word wrap enabled.
-//     */
-//    private Label getDialogLabel(String text) {
-//        // You will need to import `javafx.scene.control.Label`.
-//        Label textToAdd = new Label(text);
-//        textToAdd.setWrapText(true);
-//
-//        return textToAdd;
-//    }
-//
-//    /**
-//     * Iteration 2:
-//     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply
-//     * and then appends them to the dialog container. Clears the user input after processing.
-//     */
-//    private void handleUserInput() {
-//        Label userText = new Label(userInput.getText());
-//        Label dukeText = new Label(getResponse(userInput.getText()));
-//        dialogContainer.getChildren().addAll(
-//                DialogBox.getUserDialog(userText, new ImageView(user)),
-//                DialogBox.getDukeDialog(dukeText, new ImageView(duke))
-//        );
-//        userInput.clear();
-//    }
-//
 
     /**
      * Returns a Duke response to the given input.
@@ -171,6 +102,24 @@ public class Duke {
      * @return Duke response.
      */
     public String getResponse(String input) {
-        return "Duke heard: " + input;
+        try {
+            Command command = Parser.parse(input);
+            return command.execute.apply(input, this.taskList, this.ui, this.storage);
+        } catch (DuchessException e) {
+            return ui.printError(e.getMessage());
+        }
+    }
+
+    /**
+     * Returns the welcome message for GUI mode.
+     *
+     * @return Welcome message {@code String} for GUI mode.
+     */
+    public String getWelcomeMessage() {
+        if (this.loadingErrorMessage != null && !this.loadingErrorMessage.isEmpty()) {
+            return this.ui.printWelcome() + this.loadingErrorMessage;
+        } else {
+            return this.ui.printWelcome();
+        }
     }
 }
