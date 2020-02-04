4cb2570345ecd362cc8047c77c3aad142fc3dd39
ncslzh <ncslzh@gmail.com>
2020-02-04 13:23:09 +0800

    cleaned up tests

--- src/test/java/duke/DeadlineTest.java
+++ src/test/java/duke/DeadlineTest.java
@@ -1,4 +1,8 @@
+package duke;
+
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
@@ -7,12 +11,12 @@ public class DeadlineTest {
     @Test
     public void toString_validInput_success() {
         assertEquals("[D][✘] test description (date: 20 APRIL 2015)",
-                new Deadline("test description /by 20/04/2015").toString());
+                new Deadline("test description", LocalDate.of(2015,04, 20)).toString());
     }
 
     @Test
     public void toFileFormat_validInput_success() {
         assertEquals("D | 0 | test description | 20/04/2015",
-                new Deadline("test description /by 20/04/2015").toFileFormat());
+                new Deadline("test description", LocalDate.of(2015, 04, 20)).toFileFormat());
     }
 }
