24daa15c09ae82c32b185f72eb214560087708d9
HemanshuGandhi <hemanshu.gandhi@gmail.com>
2020-02-04 23:32:45 +0800

    initialise javaFX in duke.Duke

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -6,10 +6,15 @@ import duke.storage.TaskStorage;
 import duke.task.Task;
 import duke.task.TaskDispatch;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class Duke {
+public class Duke extends Application {
     // attributes
     private TaskStorage taskStorage;
     private Ui ui;
@@ -26,6 +31,15 @@ public class Duke {
 
     public static void main(String[] args) {
         new Duke("./data/Storage.txt").runDuke();
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
     }
 
     // Carry out Add, List, Done commands if entered by user
