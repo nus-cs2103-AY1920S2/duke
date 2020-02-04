309224dc9fcb64bd8dbe8de9bcf84f27a31abd11
Daniel Alfred Widjaja <dalfred2000@gmail.com>
2020-02-05 03:07:10 +0800

    fix gradle version

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -20,7 +20,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-@SuppressWarnings("checkstyle:CommentsIndentation")
 public class Duke extends Application {
 
     private ScrollPane scrollPane;
