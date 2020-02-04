ca899a70805334cc876930b15d6128f5f403dd3e
Hanming Zhu <hanming@huawen.com>
2020-02-04 20:21:32 +0800

    feat+docs: add handling of both gui and console mode and add javadoc
    Modified Launcher to be able to allow for console mode. This is done via the -mode console flag.
    
    Added JavaDoc as well for the new class and methods.

--- src/main/java/duchess/Launcher.java
+++ src/main/java/duchess/Launcher.java
@@ -2,11 +2,36 @@ package duchess;
 
 import javafx.application.Application;
 
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * The {@code Launcher} class is the entry point of the Duchess program.
+ * When starting up the program using the command line, use the flag
+ * "-mode console" to run the application in console mode. Else it will
+ * default to "-mode gui" by default.
+ */
 public class Launcher {
     /**
-     * A launcher class to workaround classpath issues.
+     * Starts the Duchess program.
+     *
+     * @param args Settings for the app.
      */
     public static void main(String[] args) {
-        Application.launch(Main.class, args);
+        final List<String> arguments = Arrays.asList(args);
+        final int modeIndex = arguments.indexOf("-mode");
+        final String mode = modeIndex == -1 ? "gui" : arguments.get(modeIndex + 1);
+        if ("console".equals(mode)) {
+            runConsole();
+        } else if ("gui".equals(mode)) {
+            Application.launch(Main.class, args);
+        } else {
+            System.err.println("Bad mode: " + mode);
+        }
+    }
+
+    private static void runConsole() {
+        Duke duchess = new Duke("data/tasks.json");
+        duchess.run();
     }
 }
