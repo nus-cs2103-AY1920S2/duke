2c917c59ed60c062a9606eff84ea512235a71cdd
chifa3 <foochifa@gmail.com>
2020-02-04 14:02:11 +0800

    Add ExitCommand.java

---
+++ src/main/java/dukeproj/command/ExitCommand.java
@@ -0,0 +1,14 @@
+package dukeproj.command;
+
+import dukeproj.Storage;
+import dukeproj.Ui;
+import dukeproj.data.Calender;
+import dukeproj.data.TaskList;
+import dukeproj.enums.SayType;
+
+public class ExitCommand extends Command {
+    @Override
+    public String execute(Ui ui, TaskList taskList, Storage storage, Calender calender) {
+        return ui.say(SayType.EXIT);
+    }
+}
