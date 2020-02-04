2f620868339b84d8e4d2f97a7cf6a1c02ba0c467
don-tay <dontay0209@gmail.com>
2020-02-04 16:32:22 +0800

    Refactor code to meet checkstyle requirements

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -6,12 +6,25 @@ public class Event extends AbstractTask {
     String time;
     String preposition;
 
+    /**
+     * Constructor for Event class.
+     * @param taskName Name or description of the event
+     * @param preposition English word string to indicate the date clearly (eg. at, by, before, between)
+     * @param date Date of event
+     */
     public Event(String taskName, String preposition, LocalDate date) {
         super(taskName);
         this.preposition = preposition;
         this.date = date;
     }
 
+    /**
+     * Constructor for Event class with additional info provided after date taken to be time.
+     * @param taskName Name or description of the event
+     * @param preposition English word string to indicate the date clearly (eg. at, by, before, between)
+     * @param date Date of event
+     * @param time Time of event
+     */
     public Event(String taskName, String preposition, LocalDate date, String time) {
         super(taskName);
         this.preposition = preposition;
@@ -25,7 +38,8 @@ public class Event extends AbstractTask {
 
     @Override
     public String toString() {
-        return (this.time != null) ? "[E]" + taskStateString() + " " + this.taskName + " " + preposition + " " + formattedDate() + " " + this.time
+        return (this.time != null) ? "[E]" + taskStateString() + " " + this.taskName + " " + preposition + " "
+                + formattedDate() + " " + this.time
                 : "[E]" + taskStateString() + " " + this.taskName + " " + this.preposition + " " + formattedDate();
     }
 
