23cce94307002ab66e11419284273a7509b18320
NNpanpan <dpnhat.nguyen@gmail.com>
2020-02-04 21:03:05 +0800

    JavaFX started

--- src/main/java/duke/main/Main.java
+++ src/main/java/duke/main/Main.java
@@ -2,12 +2,29 @@ package duke.main;
 import duke.exception.InvalidCommandException;
 import duke.exception.OutOfBoundMarkingRequestException;
 import duke.exception.TaskErrorException;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 import java.util.Scanner;
 
-public class Main {
+public class Main extends Application {
     static final Scanner scanner = new Scanner(System.in);
     static final Duke bot = new Duke();
+
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+
+    public Main() {
+    }
 
     /**
      * The main driver for the program
@@ -43,5 +60,28 @@ public class Main {
 
         bot.byeBye();
         scanner.close();
+    }
+
+    @Override
+    public void start(Stage stage) {
+        //Step 1. Setting up required components
+
+        //The container for the content of the chat to scroll.
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);
+
+        stage.setScene(scene);
+        stage.show();
+
+        // more code to be added here later
     }
 }
