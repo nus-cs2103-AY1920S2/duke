da3a1b5ac0a5c40861bc1010c27d0288228e1017
PotatoCombat <39809238+PotatoCombat@users.noreply.github.com>
2020-02-04 14:09:52 +0800

    Add JavaFX extension (part 3.3)
    Changes:
    + add dialog boxes for user and Duke
    + flip Duke dialog boxes

--- src/main/java/duke/DialogBox.java
+++ src/main/java/duke/DialogBox.java
@@ -1,7 +1,11 @@
 package duke;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 import javafx.geometry.Pos;
 
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
@@ -28,5 +32,35 @@ public class DialogBox extends HBox {
 
         this.setAlignment(Pos.TOP_RIGHT);
         this.getChildren().addAll(text, displayPicture);
+    }
+
+    /**
+     * Flips the dialog box such that the ImageView is on the left and text on the right.
+     */
+    private void flip() {
+        this.setAlignment(Pos.TOP_LEFT);
+        ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
+        FXCollections.reverse(tmp);
+        this.getChildren().setAll(tmp);
+    }
+
+    /**
+     * Constructs a dialog box for the user.
+     *
+     * @return a dialog box for the user.
+     */
+    public static DialogBox getUserDialog(Label l, ImageView iv) {
+        return new DialogBox(l, iv);
+    }
+
+    /**
+     * Constructs a dialog box for Duke.
+     *
+     * @return a dialog box for Duke.
+     */
+    public static DialogBox getDukeDialog(Label l, ImageView iv) {
+        DialogBox db = new DialogBox(l, iv);
+        db.flip();
+        return db;
     }
 }
