c8733575afca4a061573b8ef40d0fb8b61614d4e
Li Zi Ying <ziying338@gmail.com>
2020-02-04 16:21:21 +0800

    Complete Level 10

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,67 +1,61 @@
-import java.util.Scanner;
 import java.io.IOException;
 import java.time.LocalDate;
-import java.util.ArrayList;
 
 /**
  * Represents the entire Duke program, basically an interactive To-do List.
  */
 public class Duke {
     /**
-     * Main logic of Duke, handles all possible commands and certain input errors.
+     * Handles the input and produces the response to be displayed back to the user.
      * 
-     * @param sc       Scanner object to scan for user input.
-     * @param taskList TaskList of Task objects.
-     * @param tasks    String object of tasks, read from disk memory.
-     * @throws DukeException Handles invalid input.
-     * @throws IOException   Handles file reading and writing errors.
+     * @param input User input
+     * @return Response to user input in String format
+     * @throws IOException Handles IO exceptions
      */
-    public static void run(Scanner sc, TaskList taskList, String tasks) throws DukeException, IOException {
+    public String getResponse(String input) throws IOException {
 
-        String input = sc.nextLine();
         String[] split = input.split("\\s+");
         String command = split[0];
         int numOfTask;
         Storage storage = new Storage();
-        Ui ui = new Ui();
+        String tasks = storage.readFromFile();
+        TaskList taskList = Parser.storageToTaskList(tasks);
 
         if (command.equals("bye")) {
-            ui.print("Bye. Hope to see you again soon!");
-            System.exit(0);
+            return ("Bye. Hope to see you again soon!");
 
         } else if (command.equals("list")) {
-            ui.print(taskList.toString());
+            return (taskList.toString());
 
         } else if (command.equals("done")) {
             int index = Integer.parseInt(split[1]) - 1;
             Task done = taskList.list.get(index);
             done.markAsDone();
-            ui.print("Nice! I've marked this task as done: \n" + done);
-
             tasks = storage.readFromFile();
             storage.changeToDone(tasks, index);
+
+            return ("Nice! I've marked this task as done: \n" + done);
 
         } else if (command.equals("delete")) {
             int index = Integer.parseInt(split[1]) - 1;
             Task remove = taskList.list.remove(index);
             numOfTask = taskList.list.size();
-            ui.print("Noted. I've removed this task: \n" + remove + "\nNow you have " + numOfTask
-                    + " task(s) in the list");
-
             tasks = storage.readFromFile();
             storage.removeLine(tasks, index);
 
+            return ("Noted. I've removed this task: \n" + remove + "\nNow you have " + numOfTask
+                    + " task(s) in the list");
+
         } else if (command.equals("todo")) {
             if (split.length == 1) {
-                throw new DukeException("OOPS!!!" + " The description of a todo cannot be empty. \n");
+                return ("OOPS!!!" + " The description of a todo cannot be empty. \n");
             }
             String desc = input.substring(5);
             Task t = new ToDoTask(desc);
             taskList.list.add(t);
             numOfTask = taskList.list.size();
-            ui.print("Got it. I've added this task: \n" + t + "\nNow you have " + numOfTask + " task(s) in the list.");
-
             storage.writeToFile("T | 0 | " + desc);
+            return ("Got it. I've added this task: \n" + t + "\nNow you have " + numOfTask + " task(s) in the list.");
 
         } else if (command.equals("deadline")) {
             String[] arr = input.split(" /by ");
@@ -69,10 +63,9 @@ public class Duke {
             String time = arr[1];
             Task t = new DeadlineTask(desc, LocalDate.parse(time));
             taskList.list.add(t);
-            numOfTask = taskList.list.size();
-            ui.print("Got it. I've added this task: \n" + t + "\nNow you have " + numOfTask + " task(s) in the list.");
-
             storage.writeToFile("D | 0 | " + desc + " | " + time);
+            numOfTask = taskList.list.size();
+            return ("Got it. I've added this task: \n" + t + "\nNow you have " + numOfTask + " task(s) in the list.");
 
         } else if (command.equals("event")) {
             String[] arr = input.split(" /at ");
@@ -81,47 +74,18 @@ public class Duke {
             Task t = new EventTask(desc, LocalDate.parse(time));
             taskList.list.add(t);
             numOfTask = taskList.list.size();
-            ui.print("Got it. I've added this task: \n" + t + "\nNow you have " + numOfTask + " task(s) in the list.");
-
             storage.writeToFile("E | 0 | " + desc + " | " + time);
+            return ("Got it. I've added this task: \n" + t + "\nNow you have " + numOfTask + " task(s) in the list.");
 
         } else if (command.equals("find")) {
             String keyword = split[1];
             tasks = storage.readFromFile();
             String found = storage.findTask(tasks, keyword);
             TaskList foundList = Parser.storageToTaskList(found);
-            ui.print("Here are the matching tasks in your list: \n" + foundList.toString());
+            return ("Here are the matching tasks in your list: \n" + foundList.toString());
 
         } else {
-            throw new DukeException("OOPS!!!" + " I'm sorry, but I don't know what that means :-( \n");
+            return "OOPS!!! I'm sorry, but I don't know what that means :-( \n";
         }
-    }
-
-    /**
-     * Main driver of Duke program.
-     * 
-     * @param args Arguments for main method.
-     * @throws IOException Handles file reading and writing errors.
-     */
-    public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        Ui ui = new Ui();
-        TaskList taskList = new TaskList(new ArrayList<Task>());
-
-        ui.print("Hello! I'm Duke \nWhat can I do for you?");
-        Storage storage = new Storage();
-        String tasks = storage.readFromFile();
-        taskList = Parser.storageToTaskList(tasks);
-
-        while (sc.hasNext()) {
-            try {
-                run(sc, taskList, tasks);
-            } catch (DukeException ex) {
-                ui.print(ex.getMessage());
-            } catch (IOException e) {
-                ui.print(e.getMessage());
-            }
-        }
-        sc.close();
     }
 }
