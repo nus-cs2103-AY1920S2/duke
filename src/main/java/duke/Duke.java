135e0afcdb97bd9ce95032caf7f362a55df8abd8
Thng Yao Jie <yaoz1337@gmail.com>
2020-02-05 10:53:35 +0800

    Partial Implementation of JavaFX

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -4,12 +4,13 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.stage.Stage;
+
 import duke.exceptions.IncorrectArgumentException;
 import duke.exceptions.InvalidCommandException;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Region;
+import javafx.stage.Stage;
 
 /**
  * Duke class represents the main chatbot entity; it contains classes that manage different components of the chatbot.
@@ -17,6 +18,8 @@ import duke.exceptions.InvalidCommandException;
 public class Duke extends Application {
     /** the path where the saved Tasklist file is stored. */
     private static final Path filePath = Paths.get(System.getProperty("user.dir"), "data.duke");
+
+    private View view;
     private Storage storage;
     private TaskList tasks = new TaskList();
     private Ui ui;
@@ -32,18 +35,20 @@ public class Duke extends Application {
      */
     @Override
     public void start(Stage stage) {
-        // Creating a new Label to contain the text that we want to show.
-        Label helloWorld = new Label("Hello World!"); 
-        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
-
+        Region mainLayout = view.initialize();
+        Scene scene = new Scene(mainLayout); // Set the scene to be the mainLayout
+    
         stage.setScene(scene); // Setting the stage to show our screen
         stage.show(); // Render the stage.
+
+        view.format(stage, mainLayout);
     }
 
     /**
      * Initializes Duke and loads the TaskList from the files in the save directory.
      */
     public Duke() {
+        view = new View();
         ui = new Ui();
         storage = new Storage(filePath);
         parser = new Parser(storage, ui, tasks);
