9a7338fdcad78c75bbb466f85b44529e55e1b052
don-tay <dontay0209@gmail.com>
2020-02-05 11:57:07 +0800

    Added simple Hello World GUI with javaFX

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,9 @@
-public class Duke {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class Duke extends Application {
 
     private Storage storage;
     private TaskList tasks;
@@ -15,12 +20,29 @@ public class Duke {
         ui.setTaskList(tasks);
     }
 
+    public Duke() {
+        ui = new Ui();
+        storage = new Storage("data/tasks.txt"); //default filepath
+        tasks = new TaskList(storage);
+        ui.setTaskList(tasks);
+    }
+
     public void run() {
         ui.awaitUserInput();
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
     }
 
     public static void main(String[] args) {
         Duke newDuke = new Duke("data/tasks.txt");
         newDuke.run();
     }
+
 }
