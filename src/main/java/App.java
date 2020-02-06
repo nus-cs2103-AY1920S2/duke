9d865da6647c8e25483af4e11ec42663dfe6552b
Aloysius Chan <alloystory@gmail.com>
2020-02-06 17:45:42 +0800

    add application launcher

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,29 +1,9 @@
-import java.io.IOException;
-
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
 import app.Duke;
-import views.MainWindow;
+import views.Launcher;
 
-final class App extends Application {
-    @Override
-    public void start(Stage stage) {
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("/views/MainWindow.fxml"));
-            Scene scene = new Scene(fxmlLoader.load());
-            stage.setScene(scene);
-            fxmlLoader.<MainWindow>getController().setDuke(new Duke());
-            stage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
+final class App {
     public static void main(String[] args) {
-        // (new Duke()).start();
-        Application.launch(App.class, args);
+        Application.launch(Launcher.class, args);
     }
 }
