c10001e79c99fd857ffe4d6dc7c45ba44e333b42
dinhnhobao <nhobao2000@gmail.com>
2020-02-06 22:44:35 +0800

    add GUI using JavaFx, Level-10, A-JavaFx

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,57 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 import java.io.FileNotFoundException;
+import java.time.format.DateTimeParseException;
 
-/**
- * Duke is our main client class. Duke allows for extensibility by allowing
- * multiple ChatBots to be run if required.
- */
 public class Duke {
+    private Storage storage;
+    private TaskList tasks;
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+
+    private Image user = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
+    private Image duke = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+    // Empty constructor required for Launcher
+    public Duke() {
+    }
+
     public static void main(String[] args) throws FileNotFoundException {
         new ChatBot().run();
+    }
+
+
+    /**
+     * Iteration 1:
+     * Creates a label with the specified text and adds it to the dialog container.
+     * @param text String containing text to add
+     * @return a label with the specified text that has word wrap enabled
+     */
+    private Label getDialogLabel(String text) {
+        // need to import javafx.scene.control.label
+        Label textToAdd = new Label(text);
+        textToAdd.setWrapText(true);
+
+        return textToAdd;
+    }
+
+    /**
+     * You should have your own function to generate a response to user input.
+     * Replace this stub with your completed method.
+     */
+    public String getResponse(String input) {
+        return "Duke heard: " + input;
     }
 }
