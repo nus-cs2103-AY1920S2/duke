67a82835fabce47513c5e17786104f9d26f99b47
grrrrnt <grantlee97@gmail.com>
2020-02-06 21:59:50 +0800

    Tweak code to comply with camelCase

--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -10,7 +10,7 @@ public class TaskTest {
     }
 
     @Test
-    public void getStatusIcon_todo_ok() {
+    public void testStatusIcon() {
         Todo eat = new Todo("eat");
         assertEquals(" X ", eat.getStatusIcon());
         eat.markAsDone();
