d548715366fe87bc7d6332934bf55a75bd0f7de0
Thng Yao Jie <yaoz1337@gmail.com>
2020-02-06 19:47:40 +0800

    Revert "Added javaFX tutorial parts 3 and 4"
    This reverts commit bce1172c0aba5d4a6fa0d1124aef72185a30eda6.

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -7,14 +7,19 @@ import java.nio.file.Paths;
 
 import duke.exceptions.IncorrectArgumentException;
 import duke.exceptions.InvalidCommandException;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Region;
+import javafx.stage.Stage;
 
 /**
  * Duke class represents the main chatbot entity; it contains classes that manage different components of the chatbot.
  */
-public class Duke {
+public class Duke extends Application {
     /** the path where the saved Tasklist file is stored. */
     private static final Path filePath = Paths.get(System.getProperty("user.dir"), "data.duke");
 
+    private View view;
     private Storage storage;
     private TaskList tasks = new TaskList();
     private Ui ui;
@@ -25,9 +30,25 @@ public class Duke {
     }
 
     /**
+     * Adapted from JavaFX tutorial.
+     * @param stage this is the primary stage that JavaFX provides.
+     */
+    @Override
+    public void start(Stage stage) {
+        Region mainLayout = view.initialize();
+        Scene scene = new Scene(mainLayout); // Set the scene to be the mainLayout
+    
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
+
+        view.format(stage, mainLayout);
+    }
+
+    /**
      * Initializes Duke and loads the TaskList from the files in the save directory.
      */
     public Duke() {
+        view = new View();
         ui = new Ui();
         storage = new Storage(filePath);
         parser = new Parser(storage, ui, tasks);
@@ -58,22 +79,5 @@ public class Duke {
                 e.printStackTrace();
             }
         }
-    }
-
-    /**
-     * You should have your own function to generate a response to user input.
-     * Replace this stub with your completed method.
-     */
-    public String getResponse(String input) {
-        boolean isShutdown = false;
-        try {
-            isShutdown = parser.parse(input);
-            storage.saveToFile(tasks);
-        } catch (InvalidCommandException | IncorrectArgumentException e) {
-            return e.getMessage();
-        } catch (IOException | NumberFormatException e) {
-            return e.getMessage();
-        }
-        return "";
     }
 }
