b9df159103082464c4ca452cedb570a2eb110dcb
Sarah <50527491+suwoons@users.noreply.github.com>
2020-02-07 15:25:33 +0800

    Remove unnecessary comments

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,14 +3,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import javafx.application.Application;
-//import javafx.beans.property.ObjectProperty;
-//import javafx.beans.property.SimpleObjectProperty;
-//import javafx.event.EventHandler;
-//import javafx.geometry.Point2D;
 import javafx.scene.Scene;
-//import javafx.scene.input.MouseEvent;
-//import javafx.scene.layout.Pane;
-//import javafx.scene.shape.Circle;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
@@ -151,95 +144,7 @@ public class Duke extends Application {
 
     @Override
     public void start(Stage stage) {
-//        // The container for the content of the chat to scroll.
-//        scrollPane = new ScrollPane();
-//        dialogContainer = new VBox();
-//        scrollPane.setContent(dialogContainer);
-//
-//        userInput = new TextField();
-//        sendButton = new Button("Send");
-//
-//        AnchorPane mainLayout = new AnchorPane();
-//        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-//
-//        scene = new Scene(mainLayout);
-//
-//        stage.setScene(scene);
-//        stage.show();
-//
-//        // Formatting the window
-//        stage.setTitle("Goose");
-//        stage.setResizable(true);
-//        stage.setMinHeight(600.0);
-//        stage.setMinWidth(400.0);
-//
-//        mainLayout.setPrefSize(400.0, 600.0);
-//
-//        scrollPane.setPrefSize(385, 535);
-//        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-//        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-//
-//        scrollPane.setVvalue(1.0);
-//        scrollPane.setFitToWidth(true);
-//
-//        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-//        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
-//
-//        userInput.setPrefWidth(325.0);
-//
-//        sendButton.setPrefWidth(55.0);
-//
-//        AnchorPane.setTopAnchor(scrollPane, 1.0);
-//
-//        AnchorPane.setBottomAnchor(sendButton, 1.0);
-//        AnchorPane.setRightAnchor(sendButton, 1.0);
-//
-//        AnchorPane.setLeftAnchor(userInput , 1.0);
-//        AnchorPane.setBottomAnchor(userInput, 1.0);
-//
-//        // Add functionality to handle user input.
-//        sendButton.setOnMouseClicked((event) -> {
-//            handleUserInput();
-//        });
-//
-//        userInput.setOnAction((event) -> {
-//            handleUserInput();
-//        });
     }
-//
-//    /**
-//     * Iteration 1:
-//     * Creates a label with the specified text and adds it to the dialog container.
-//     * @param text String containing text to add
-//     * @return a label with the specified text that has word wrap enabled.
-//     */
-//    private Label getDialogLabel(String text) {
-//        Label textToAdd = new Label(text);
-//        textToAdd.setWrapText(true);
-//
-//        return textToAdd;
-//    }
-//
-//    /**
-//     * Iteration 2:
-//     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
-//     * the dialog container. Clears the user input after processing.
-//     */
-//    private void handleUserInput() {
-//        Label userText = new Label(userInput.getText());
-//        Label gooseText = new Label(getResponse(userInput.getText()));
-//
-//        ImageView userImage = new ImageView(user);
-//        ImageView gooseImage = new ImageView(goose);
-////        userImage.setClip(clip);
-////        gooseImage.setClip(clip);
-//
-//        dialogContainer.getChildren().addAll(
-//                DialogBox.getUserDialog(userText, userImage),
-//                DialogBox.getGooseDialog(gooseText, gooseImage)
-//        );
-//        userInput.clear();
-//    }
 
     public static void main(String[] args) {
         new Duke("duke.txt", "mainList.txt").run();
