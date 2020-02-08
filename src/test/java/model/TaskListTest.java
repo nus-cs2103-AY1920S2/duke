1434f85db194507321b17ce2285a99993e370860
YingxuH <yingxu.he1998@gmail.com>
2020-02-08 22:10:03 +0800

    add tasklist test

---
+++ src/test/java/model/TaskListTest.java
@@ -0,0 +1,46 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+
+import static TestUtils.TypicalTasks.TODO_TOPICS;
+import static TestUtils.TypicalTasks.TODO_IP;
+import static TestUtils.TypicalTasks.EVENT_W3LECTURE;
+import static TestUtils.TypicalTasks.EVENT_W4LECTURE;
+import static TestUtils.TypicalTasks.DL_ASSIGNMENT_1;
+import static TestUtils.TypicalTasks.DL_ASSIGNMENT_2;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TaskListTest {
+
+    @Test
+    public void testAddTask() {
+        TaskList testTaskList = new TaskList();
+        testTaskList.add(TODO_TOPICS);
+        testTaskList.add(TODO_IP);
+        String lastResult = testTaskList.add(EVENT_W3LECTURE);
+
+        assertEquals(lastResult, "Got it. I've added this task:\n"
+                + "week 3 lecture at week 3 Friday\n"
+                + "Now you have 3 tasks in the list.\n");
+        assertEquals(testTaskList.internalList.size(), 3);
+    }
+
+    @Test
+    public void testRemoveTask() {
+        TaskList testTaskList = new TaskList();
+        testTaskList.add(TODO_TOPICS);
+        testTaskList.add(TODO_IP);
+        testTaskList.add(EVENT_W3LECTURE);
+        testTaskList.add(EVENT_W4LECTURE);
+        testTaskList.add(DL_ASSIGNMENT_1);
+        testTaskList.add(DL_ASSIGNMENT_2);
+
+        String deleteResult = testTaskList.remove(5);
+
+        assertEquals(deleteResult, "Noted. I have removed this task:\n"
+                + " assignment 2 by week 5\n"
+                + "Now you have 5 tasks in the list.\n");
+        assertEquals(testTaskList.internalList.size(), 5);
+    }
+}
