a9174bd20df51ed2ef2e576f3e902f3102d0c774
Mario Lorenzo <mariolorenzo213@gmail.com>
2020-02-08 11:10:24 +0800

    Add Note class

---
+++ src/main/java/duke/util/Note.java
@@ -0,0 +1,31 @@
+package duke.util;
+
+/*
+ * Note
+ *
+ * CS2103 AY19/20 Semester 2
+ * Individual Project
+ * Duke Project
+ *
+ * 08 Feb 2020
+ *
+ */
+
+/**
+ * <p>Note class describes the behavior of the notes
+ * to be entered by the client.</p>
+ * @author Mario Lorenzo
+ */
+
+public class Note {
+    private String text;
+
+    /**
+     * Constructs a Note instance.
+     * @param text The contain of the note.
+     */
+
+    public Note(String text) {
+        this.text = text;
+    }
+}
