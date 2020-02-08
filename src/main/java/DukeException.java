7f9672547e0cd4478575c791e2e964fab83636f6
Sarah Chua <chuaxinhuisarah97@gmail.com>
2020-02-08 15:07:09 +0800

    A-JavaDoc

--- src/main/java/DukeException.java
+++ src/main/java/DukeException.java
@@ -1,5 +1,9 @@
 public class DukeException extends Exception{
 
+    /**
+     * Duke Exception constructor method
+     * @param message Exception message to be printed out to the user
+     */
     public DukeException(String message) {
         super(message);
     }
