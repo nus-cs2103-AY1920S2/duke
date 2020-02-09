f150c53052f66d6d4272fd586018df4d371a6e73
HemanshuGandhi <hemanshu.gandhi@gmail.com>
2020-02-09 22:47:11 +0800

    Add meaningful message when user list is empty

--- src/main/java/duke/command/listCommand.java
+++ src/main/java/duke/command/listCommand.java
@@ -10,12 +10,21 @@ public class listCommand extends Command {
     public String execute(Ui ui, TaskStorage storage) {
         int counter = 1;
         String text = "";
-
         for (Task task : storage.getTaskList()) {
             text += "    " + counter + ". " + task + System.lineSeparator();
             counter++;
         }
 
-        return text.trim();
+        String prependedString = getPrependedString(storage);
+
+        return prependedString + text.trim();
+    }
+
+    private String getPrependedString(TaskStorage storage) {
+        if (storage.getTaskList().size() != 0) {
+            return "    Here are the tasks in your list:" + System.lineSeparator();
+        } else {
+            return "Your task list is currently empty.";
+        }
     }
 }
