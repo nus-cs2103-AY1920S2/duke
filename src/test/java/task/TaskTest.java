4c5243b53653bb38f1833d3d35dcfcd38f9f72cb
Branson <branson.ng@u.nus.edu>
2020-02-09 15:40:13 +0800

    Add assertions for Exceptions

--- src/test/java/task/TaskTest.java
+++ src/test/java/task/TaskTest.java
@@ -1,6 +1,7 @@
 package task;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import exception.DukeException;
 import org.junit.jupiter.api.Test;
@@ -15,8 +16,38 @@ public class TaskTest {
             assertEquals("[D][N] return book (by: 19 Dec 2019 16:00)", deadline.toString());
             Task event = Task.newTask("event project meeting /at 02/11/2020 2359");
             assertEquals("[E][N] project meeting (at: 02 Nov 2020 23:59)", event.toString());
+            Task period =
+                    Task.newTask(
+                            "period TOTO 4D BIG HUAT /start 10/02/2020 0000 /end 12/02/2020 2359");
+            assertEquals(
+                    "[P][N] TOTO 4D BIG HUAT (start: 10 Feb 2020 00:00, end: 12 Feb 2020 23:59)",
+                    period.toString());
         } catch (DukeException err) {
+
         }
+    }
+
+    @Test
+    public void taskCreationExceptions() {
+        Exception periodException =
+                assertThrows(
+                        DukeException.class,
+                        () -> {
+                            Task.newTask(
+                                    "period TOTO 4D BIG HUAT /start 14/02/2020 0000 /end 12/02/2020 2359");
+                        });
+        assertEquals(
+                "WARNING!! End dates and time must be after start dates and time",
+                periodException.getMessage());
+
+        Exception noTypesException =
+                assertThrows(
+                        DukeException.class,
+                        () -> {
+                            Task.newTask(
+                                    "TOTO 4D BIG HUAT /start 14/02/2020 0000 /end 12/02/2020 2359");
+                        });
+        assertEquals("WARNING!! No accepted types present", noTypesException.getMessage());
     }
 
     @Test
