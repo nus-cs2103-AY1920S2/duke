f5455c986ba1ad29bdb43100149dc34168c6c6eb
Sanchari Bandyopadhyay <sancharizxc@gmail.com>
2020-02-09 15:09:00 +0800

    resolved merge conflicts:

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -28,44 +28,33 @@ public class Ui {
         return (this.parseInput())[1];
     }
 
-<<<<<<< HEAD
+
     public String showList() {
         return ("Here are the tasks in your list: ");
-=======
-    public  String showList() {
-        return "Here are the tasks in your list: ";
->>>>>>> 23b7f1d0e01940f845a562962edf14088c038c89
     }
+
+
 
     public String errmessage() {
         return "Invalid Input, Please check instructions";
     }
 
     public String doneTask(Task task) {
-<<<<<<< HEAD
-        return ("-------------------------------------------------------------\nNice I have marked this task as done\n" +
-        " " + task.getDescription() + "\n-------------------------------------------------------------");
+
+        return ("Nice I have marked this task as done\n" +
+        " " + task.getDescription() + "\n");
     }
 
-    public String addedTask(Task todo) {
-        return ("-------------------------------------------------------------\n" + todo.toString() +
-        "\n-------------------------------------------------------------\n");
-    }
+
 
     public String deleteTask(String task) {
         return ("Deleted task: " + task);
-=======
-        return("Nice I have marked this task as done\n" +
-        " " + task.getDescription() + "\n");
     }
 
     public String addedTask(Task todo) {
         return (todo.toString() + "\n");
     }
 
-    public String deleteTask(String task) {
-        return("Deleted task: " + task);
->>>>>>> 23b7f1d0e01940f845a562962edf14088c038c89
-    }
 
-}
+
+} 
