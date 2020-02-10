669fca016a84a3cecc4b9156e6703571f158f4ea
Joshua <joshuaftw000@gmail.com>
2020-02-10 21:25:44 +0800

    Added GUI interface

---
+++ src/main/java/duke/Duke.java
@@ -0,0 +1,53 @@
+package duke;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * The type Main.
+ */
+public class Duke {
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        MyList taskList = new MyList();
+        Storage storage = new Storage();
+        storage.loadFile(taskList);
+        Ui ui = new Ui();
+        Parser parser = new Parser(ui);
+        boolean isExit = false;
+
+        ui.showStartMessage();
+
+        while(!isExit) {
+
+            try{
+                String word = sc.nextLine();
+                Command command = parser.parseCommand(word);
+                command.execute(word, taskList, ui, storage);
+                isExit = command.isExit();
+            } catch(DukeException | IOException e) {
+                System.out.println(e.getMessage());
+            }
+
+        }
+
+        sc.close();
+
+    }
+
+    /**
+     * You should have your own function to generate a response to user input.
+     * Replace this stub with your completed method.
+     */
+    String getResponse(String input) {
+        return "Duke heard: " + input;
+    }
+
+}
