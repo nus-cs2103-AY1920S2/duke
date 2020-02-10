d6511fa9ae0982dc4ec78948ba2f2ea4beefb317
samanthaseahpl <e0319110@u.nus.edu>
2020-02-10 12:48:28 +0800

    branch-A-JavaFX-Tut2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,10 +3,22 @@ import java.util.Scanner;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+
 public class Duke extends Application {
+
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
 
     private Store lib;
     private Ui ui = new Ui();
@@ -14,31 +26,31 @@ public class Duke extends Application {
     private DukeException DE;
     String[] CheckInput;
 
-    /**
-     * This method uses filepath to locate the file.
-     * If file exist, load the file. Otherwise create new file.
-     * @param filepath the absolute location of the .txt file.
-     */
-    public Duke(String filepath){
-        DE = new DukeException();
-        File file = new File(filepath); //create a file obj with the given filepath.
-        this.lib = new Store(file); //create store from absolute filepath
-        try {
-            boolean result = file.exists();
-            if(!result){
-                boolean ans = file.createNewFile();
-            } else {
-                    Scanner newSN = new Scanner(file);
-                    while(newSN.hasNextLine()){
-                        String nxtLine = newSN.nextLine();
-                        lib.load(nxtLine);
-                    } //end while: for reading existing file
-                    System.out.print(ui.line());
-            }
-        } catch (IOException e){
-            e.printStackTrace();
-        }
-    } //end Duke
+//    /**
+//     * This method uses filepath to locate the file.
+//     * If file exist, load the file. Otherwise create new file.
+//     * @param filepath the absolute location of the .txt file.
+//     */
+//    public Duke(String filepath){
+//        DE = new DukeException();
+//        File file = new File(filepath); //create a file obj with the given filepath.
+//        this.lib = new Store(file); //create store from absolute filepath
+//        try {
+//            boolean result = file.exists();
+//            if(!result){
+//                boolean ans = file.createNewFile();
+//            } else {
+//                    Scanner newSN = new Scanner(file);
+//                    while(newSN.hasNextLine()){
+//                        String nxtLine = newSN.nextLine();
+//                        lib.load(nxtLine);
+//                    } //end while: for reading existing file
+//                    System.out.print(ui.line());
+//            }
+//        } catch (IOException e){
+//            e.printStackTrace();
+//        }
+//    } //end Duke
 
     /**
      * This method takes input from scanner and redirect it to the respective action.
@@ -103,13 +115,65 @@ public class Duke extends Application {
         } // end sn.hasNext()
     }
 
-    /**
-     * This is the main method that uses DUke method.
-     * @param args Unused.
-     */
-    public static void main(String[] args) {
-        new Duke("D:/duke/data/d.txt").run();
+//    /**
+//     * This is the main method that uses DUke method.
+//     * @param args Unused.
+//     */
+//    public static void main(String[] args) {
+//        new Duke("D:/duke/data/d.txt").run();
+//    }
+
+    @Override
+    public void start(Stage stage) {
+        //Step 1. Setting up required components
+
+        //The container for the content of the chat to scroll.
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();                //create a textfield
+        sendButton = new Button("Send");       //set button with "send" label
+
+        AnchorPane mainLayout = new AnchorPane();   //the background
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);
+
+        stage.setScene(scene);
+        stage.show();
+
+        //Step 2. Formatting the window to look as expected
+        stage.setTitle("Duke");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        // You will need to import `javafx.scene.layout.Region` for this.
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(325.0);
+
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput , 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+
+        // more code to be added here later
     }
-
-
 }
+
