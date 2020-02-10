e6ea48045a86db885d7fdd79cccea1648bf3efef
Zhang Yuanyu <e0322803@u.nus.edu>
2020-02-10 16:44:47 +0800

    Task: Removed redundant method.

--- src/main/java/duke/task/Task.java
+++ src/main/java/duke/task/Task.java
@@ -33,27 +33,6 @@ public abstract class Task {
         this.isDone = true;
     }
     /**
-     * Returns the text representation of the task list.
-     * @param tasks The task list.
-     * @return The text representation of the task list.
-     */
-    public static String toStringDukeTasks(ArrayList<Task> tasks) {
-        String taskString = "";
-        for (Task task : tasks) {
-            if (task instanceof Todo) {
-                taskString += "T/" + task.getStatusIconInBin() + "/";
-                taskString += task.description + "\n";
-            } else if (task instanceof Deadline) {
-                taskString += "D/" + task.getStatusIconInBin() + "/";
-                taskString += task.description + "/" + ((Deadline) task).date.toString() + "\n";
-            } else {               //instance of event
-                taskString += "E/" + task.getStatusIconInBin() + "/";
-                taskString += task.description + "/" + ((Event) task).date.toString() + "\n";
-            }
-        }
-        return taskString;
-    }
-    /**
      * Returns the text representation of the task.
      * @return The text representation of the task.
      */
