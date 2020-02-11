2f39f5918118f2c89475847aa7ee09215233a6fe
jiachen <jiachen247@gmail.com>
2020-02-11 17:14:59 +0800

    when tests fail, what do we do?

--- src/test/java/jiachen/duke/StorageTest.java
+++ src/test/java/jiachen/duke/StorageTest.java
@@ -10,39 +10,6 @@ class StorageTest {
 
     private String path = "./data/test.txt";
 
-//    @Test
-//    void load() {
-//        try {
-//            if (Files.exists(Paths.get(path))) {
-//                Files.delete(Paths.get(path));
-//            }
-//            Files.write(
-//                Paths.get(path),
-//                ("D | 1 | do ma homework | 01/12/2020 0700\n"
-//                    + "E | 0 | take a nap? | 01/12/2020 0700\n"
-//                    + "E | 0 | take a nap | 01/12/2020 0800\n"
-//                    + "T | 0 | test task\n")
-//                    .getBytes());
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        Storage storage = new Storage(path);
-//        TaskList tasks = null;
-//        try {
-//            tasks = storage.load();
-//        } catch (DukeException e) {
-//            e.printStackTrace();
-//            tasks = new TaskList();
-//        }
-//
-//        assertEquals(tasks.size(), 4);
-//        try {
-//            Files.delete(Paths.get(path));
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-
     @Test
     void save() {
 
