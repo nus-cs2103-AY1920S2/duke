4baecfeebc04841d5240e30c09692696548d5a06
LE TRUNG HIEU <lth08091998@gmail.com>
2020-02-11 13:30:01 +0800

    Refactor GUI

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,8 +1,9 @@
+import duke.Main;
 import javafx.application.Application;
 
 public class Launcher {
     public static void main(String[] args) {
-        Application.launch(Duke.class, args);
+        Application.launch(Main.class, args);
     }
 
 }
