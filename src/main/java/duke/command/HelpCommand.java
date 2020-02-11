768542764c5d5196cd405573bfd04c61e4010307
Kevin Putera <kevintiosanna@gmail.com>
2020-02-11 19:50:02 +0800

    Tweak help command output

--- src/main/java/duke/command/HelpCommand.java
+++ src/main/java/duke/command/HelpCommand.java
@@ -8,21 +8,21 @@ public class HelpCommand extends Command {
     public ExecuteResult execute(List<Task> tasks) {
         return new ExecuteResult(
                 tasks,
-                "Usage: [command] [args...]\n"
-                + "Where command includes:\n"
-                + "  todo <title> : Adds a new todo to the list of tasks.\n"
-                + "  deadline <title> /by <YYYY-MM-DD> : Adds a new deadline\n"
-                + "      to the list of tasks.\n"
-                + "  event <title> /at <YYYY-MM-DD> : Adds a new event to\n"
-                + "      the list of tasks.\n"
-                + "  done <index> : Marks the task at the specified index as\n"
-                + "      done.\n"
-                + "  find <keyword> : Finds tasks containing the specified\n"
-                + "      keyword.\n"
-                + "  delete <index> : Removes the task at the specified\n"
-                + "      index.\n"
-                + "  list : Lists out all the tasks available.\n"
-                + "  help : Prints help.",
+                "Usage: [command] <args...>\n\n"
+                + "Command includes:\n"
+                + "    1. todo <title> : Adds a new todo to the list of tasks.\n"
+                + "    2. deadline <title> /by <YYYY-MM-DD> : Adds a new\n"
+                + "              deadline to the list of tasks.\n"
+                + "    3. event <title> /at <YYYY-MM-DD> : Adds a new event\n"
+                + "              to the list of tasks.\n"
+                + "    4. done <index> : Marks the task at the specified index\n"
+                + "              as done.\n"
+                + "    5. find <keyword> : Finds tasks containing the specified\n"
+                + "              keyword.\n"
+                + "    6. delete <index> : Removes the task at the specified\n"
+                + "              index.\n"
+                + "    7. list : Lists out all the tasks available.\n"
+                + "    8. help : Prints help.",
                 true
         );
     }
