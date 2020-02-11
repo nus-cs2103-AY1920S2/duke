0867752ea3aa2c00dc963ab2f765bb2c4f03c78c
johan.kok <johan.kok@grabtaxi.com>
2020-02-11 23:09:40 +0800

    Feature: Delete tasks from list - Added equals method in Tasks - Added delete feature in user cmd input

--- src/main/java/com/nus/duke/Duke.java
+++ src/main/java/com/nus/duke/Duke.java
@@ -67,7 +67,7 @@ public class Duke {
                 break;
 
             case "delete":
-                if (!controller.checkTask(taskName)) {
+                if (controller.checkTask(taskName)) {
                     controller.removeTask(taskName);
                 }
                 Greetings.prettyPrint(String.format("Deleted task: %s", taskName));
