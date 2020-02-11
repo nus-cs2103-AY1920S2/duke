7fd89909d56c01a7e16b8391635684bc575d804c
Christian James Welly <39089453+chrisjwelly@users.noreply.github.com>
2020-02-11 14:47:52 +0800

    Abstract processing into a single class

--- src/main/java/duke/main/Duke.java
+++ src/main/java/duke/main/Duke.java
@@ -4,6 +4,7 @@ import duke.exception.DukeException;
 import duke.exception.EmptyDescriptionException;
 import duke.exception.TooManyTasksException;
 import duke.exception.UnknownCommandException;
+import duke.processor.Processor;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
@@ -47,63 +48,19 @@ public class Duke {
 
     public void run() {
         Ui.greet();
-        while (Parser.stillHasInputs()) {
-            try {
-                String input = Parser.readLine();
-                if (Parser.commandEquals("bye", input)) {
-                    Ui.sayGoodbye();
-                    break;
-                } else if (Parser.commandEquals("list", input)) {
-                    Ui.displayList(tasks);
-                } else if (Parser.commandEquals("done", input)) {
-                    int taskNo = Parser.getTaskNo(input);
-                    tasks.markTaskAsDone(taskNo);
-                } else if (Parser.commandEquals("delete", input)) {
-                    int taskNo = Parser.getTaskNo(input);
-                    tasks.deleteTask(taskNo);
-                } else if (Parser.commandEquals("deadline", input)) {
-                    if (Parser.hasNoArgs(input)) throw new EmptyDescriptionException();
-                    if (tasks.isFull()) throw new TooManyTasksException();
-
-                    tasks.addDeadline(Parser.getArgs(input));
-                } else if (Parser.commandEquals("event", input)) {
-                    if (Parser.hasNoArgs(input)) throw new EmptyDescriptionException();
-                    if (tasks.isFull()) throw new TooManyTasksException();
-
-                    tasks.addEvent(Parser.getArgs(input));
-                } else if (Parser.commandEquals("todo", input)) {
-                    if (Parser.hasNoArgs(input)) throw new EmptyDescriptionException();
-                    if (tasks.isFull()) throw new TooManyTasksException();
-
-                    tasks.addTodo(Parser.getArgs(input));
-                } else if (Parser.commandEquals("find", input)) {
-                    if (Parser.hasNoArgs(input)) throw new EmptyDescriptionException();
-
-                    tasks.find(Parser.getArgs(input));
-                } else {
-                    throw new UnknownCommandException();
-                }
-
-                // Rewrites the entire file for every update you make here
-                // Probably O(n^2) time where n is the number of tasks but this is the simplest change we can make
-                storage.writeTasks(tasks);
-            } catch (DukeException e) {
-                Ui.printLine();
-                Ui.indent(e.toString());
-                Ui.printLine();
-            } catch (DateTimeParseException e) {
-                Ui.printLine();
-                Ui.indent("It seems that you have entered a format we don't understand. ");
-                Ui.indent("Please use the YYYY-MM-DD format.");
-                Ui.printLine();
-            }
+        try {
+            Processor.process(tasks, storage);
+        } catch (DukeException e) {
+            Ui.printLine();
+            Ui.indent(e.toString());
+            Ui.printLine();
+        } catch (DateTimeParseException e) {
+            Ui.printLine();
+            Ui.indent("It seems that you have entered a format we don't understand. ");
+            Ui.indent("Please use the YYYY-MM-DD format.");
+            Ui.printLine();
         }
     }
-
-    public static void handleCommand(String input) {
-
-    }
-
 
     public static void main(String[] args) {
         new Duke("data/duke.txt").run();
