41fc2326d208e3b799b7dd1a132aa019092ebead
helloimhai <1503.hai.nguyen@gmail.com>
2020-02-11 14:29:01 +0800

    Refactor package: Create separate GUI package

--- src/main/java/duke/gui/Main.java
+++ src/main/java/duke/gui/Main.java
@@ -1,8 +1,9 @@
-package duke;
+package duke.gui;
 
 
 import java.io.IOException;
 
+import duke.Duke;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
