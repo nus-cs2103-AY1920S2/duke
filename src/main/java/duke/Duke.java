c7975e680953cc2e8a4076f89f78e0100ab7f727
PangJiaDa <pangjiada52@gmail.com>
2020-02-12 03:12:42 +0800

    follow javaFX tut4 - use FXML for ui and controller classes for logic
    bridge layout/controllers with logic using Main.java.
    change Launcher to launch Main instead since it had the start override from Application

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -19,29 +19,20 @@ import java.io.IOException;
 /**
  * Duke provides a todo list functionality.
  */
-public class Duke extends Application {
+public class Duke /*extends Application*/ {
     private Storage storage;
     private TaskList tasks;
     private Ui ui;
 
-    private Image user = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image duke = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
-
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Scene scene;
-
     /**
-     * No params constructor for Launcher class to initialize Duke
+     * No params constructor for Launcher class to initialize Duke.
      */
     public Duke() {
 
     }
 
     /**
-     * Constructor for Duke
+     * Constructor for Duke.
      * @param filePath provide a hardcoded directory path to the text file to be used as a database
      */
     public Duke(String filePath) {
@@ -87,93 +78,8 @@ public class Duke extends Application {
         }
     }
 
-//    /**
-//     * Entry point for the JVM.
-//     * @param args default main method signature
-//     */
-//    public static void main(String[] args) {
-//        new Duke("C:\\Users\\Pang Jia Da\\Desktop\\CS2103\\duke\\data\\duke.txt").run();
-//    }
-
-    @Override
-    public void start(Stage stage) throws Exception {
-        //Step 1. Setting up required components
-
-        //The container for the content of the chat to scroll.
-        scrollPane = new ScrollPane();
-        dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
-
-        userInput = new TextField();
-        sendButton = new Button("Send");
-
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-
-        scene = new Scene(mainLayout);
-
-        stage.setScene(scene);
-        stage.show();
-
-        //Step 2. Formatting the window to look as expected
-        stage.setTitle("Duke");
-        stage.setResizable(false);
-        stage.setMinHeight(600.0);
-        stage.setMinWidth(400.0);
-
-        mainLayout.setPrefSize(400.0, 600.0);
-
-        scrollPane.setPrefSize(385, 535);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-
-        userInput.setPrefWidth(325.0);
-
-        sendButton.setPrefWidth(55.0);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-
-        AnchorPane.setLeftAnchor(userInput , 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-
-        //Step 3. Add functionality to handle user input.
-        sendButton.setOnMouseClicked((event) -> {
-            handleUserInput();
-        });
-
-        userInput.setOnAction((event) -> {
-            handleUserInput();
-        });
-
-        dialogContainer.heightProperty().addListener((observable -> scrollPane.setVvalue(1.0)));
-    }
-
-    private Label getDialogLabel(String text) {
-        Label textToAdd = new Label(text);
-        textToAdd.setWrapText(true);
-
-        return textToAdd;
-    }
-
-    private void handleUserInput() {
-        Label userText = new Label(userInput.getText());
-        Label dukeText = new Label(getResponse(userInput.getText()));
-        dialogContainer.getChildren().addAll(
-                DialogBox.getUserDialog(userText, new ImageView(user)),
-                DialogBox.getDukeDialog(dukeText, new ImageView(duke))
-        );
-        userInput.clear();
-    }
-
-    private String getResponse(String input) {
+    //package-private
+    String getResponse(String input) {
         return "Duke's response: " + input;
     }
 }
