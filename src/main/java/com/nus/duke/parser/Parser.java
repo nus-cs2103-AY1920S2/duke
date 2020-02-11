26475b0ae8958e0c2c1f2f6b187737dca5a65e50
johan.kok <johan.kok@grabtaxi.com>
2020-02-11 22:11:02 +0800

    Refractor tokenize method in Parser - Allows reusing of code in TaskController module

--- src/main/java/com/nus/duke/parser/Parser.java
+++ src/main/java/com/nus/duke/parser/Parser.java
@@ -5,14 +5,21 @@ import javafx.util.Pair;
 public class Parser {
     public static Pair<String, String> tokenize(String input) {
         /**
+         By default splits the string by the first whitespace character
+         */
+        return tokenize(input, " ");
+    }
+
+    public static Pair<String, String> tokenize(String input, String expr) {
+        /**
          Splits a string into a (command, condition) pair
 
          @param input
-                input text string, such as "list books"
+         input text string, such as "list books"
 
          @return Pair of (command, condition)
          */
-        String[] res        = input.split(" ", 2);
+        String[] res        = input.split(expr, 2);
         String command      = res[0];
         String condition    = res.length > 1 ? res[1] : null;
         return new Pair<>(command, condition);
