e91c80fd4bd8b302bd33e38b738f09153e846dc6
gb3h <gabriel_yfy@hotmail.com>
2020-02-11 20:46:28 +0800

    Level-10

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -20,7 +20,7 @@ import javafx.scene.image.ImageView;
 /**
  * Duke program.
  */
-public class Duke extends Application {
+public class Duke  {
     private ScrollPane scrollPane;
     private VBox dialogContainer;
     private TextField userInput;
@@ -30,8 +30,6 @@ public class Duke extends Application {
     private Storage storage;
     private TaskList tasks;
 
-    private Image user = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image duke = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
     /**
      * Constructor.
      *
@@ -42,7 +40,7 @@ public class Duke extends Application {
         try {
             tasks = new TaskList(storage.load());
         } catch (DukeException | FileNotFoundException e) {
-            UI.showLoadingError();
+            System.out.println(UI.showLoadingError());
             tasks = new TaskList();
         }
     }
@@ -62,123 +60,111 @@ public class Duke extends Application {
      */
     public void run() throws IOException {
         TaskList arr = tasks;
-        UI.say("Hello I am [AKSHAY]!\nHow may I help you?");
+        System.out.println(UI.say("Hello I am [AKSHAY]!\nHow may I help you?"));
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         while (!input.equals("bye")) {
             String[] c = input.split(" ", 2);
             switch (c[0]) {
-            case ("list"):
-                UI.list(arr);
-                break;
-            case ("done"):
-                Task curr = arr.get(Integer.parseInt(c[1]) - 1);
-                curr.mark();
-                UI.done(curr);
-                break;
-            case ("todo"):
-                try {
-                    Task todo = new Todo(c[1]);
-                    arr.add(todo);
-                    UI.added(todo);
-                } catch (ArrayIndexOutOfBoundsException e) {
-                    UI.say("OOPS!!! The description of a todo cannot be empty.");
-                }
-                break;
-            case ("deadline"):
-                String[] dl = c[1].split("/by", 2);
-                Task d = new Deadline(dl[0], dl[1].trim());
-                arr.add(d);
-                UI.added(d);
-                break;
-            case ("event"):
-                String[] ev = c[1].split("/at", 2);
-                Task e = new Event(ev[0], ev[1].trim());
-                arr.add(e);
-                UI.added(e);
-                break;
-            case ("delete"):
-                try {
-                    Task del = arr.get(Integer.parseInt(c[1]) - 1);
-                    arr.remove(Integer.parseInt(c[1]) - 1);
-                    UI.delete(del);
-                } catch (Exception i) {
-                    UI.say("Failed to delete item!!!");
-                }
-                break;
-            case ("find"):
-                UI.results(tasks.search(c[1]));
-                break;
-            default:
-                try {
-                    throw new DukeException();
-                } catch (DukeException de) {
-                    UI.say("OOPS!!! I'm sorry, but I don't know what that means :-(");
-                }
+                case ("list"):
+                    System.out.println(UI.list(arr));
+                    break;
+                case ("done"):
+                    Task curr = arr.get(Integer.parseInt(c[1]) - 1);
+                    curr.mark();
+                    System.out.println(UI.done(curr));
+                    break;
+                case ("todo"):
+                    try {
+                        Task todo = new Todo(c[1]);
+                        arr.add(todo);
+                        System.out.println(UI.added(todo));
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        System.out.println(UI.say("OOPS!!! The description of a todo cannot be empty."));
+                    }
+                    break;
+                case ("deadline"):
+                    String[] dl = c[1].split("/by", 2);
+                    Task d = new Deadline(dl[0], dl[1].trim());
+                    arr.add(d);
+                    System.out.println(UI.added(d));
+                    break;
+                case ("event"):
+                    String[] ev = c[1].split("/at", 2);
+                    Task e = new Event(ev[0], ev[1].trim());
+                    arr.add(e);
+                    System.out.println(UI.added(e));
+                    break;
+                case ("delete"):
+                    try {
+                        Task del = arr.get(Integer.parseInt(c[1]) - 1);
+                        arr.remove(Integer.parseInt(c[1]) - 1);
+                        System.out.println(UI.delete(del));
+                    } catch (Exception i) {
+                        System.out.println(UI.say("Failed to delete item!!!"));
+                    }
+                    break;
+                case ("find"):
+                    System.out.println(UI.results(tasks.search(c[1])));
+                    break;
+                default:
+                    try {
+                        throw new DukeException();
+                    } catch (DukeException de) {
+                        System.out.println(UI.say("OOPS!!! I'm sorry, but I don't know what that means :-("));
+                    }
             }
             storage.save(arr.toArr());
             input = sc.nextLine();
         }
-        UI.goodbye();
+        System.out.println(UI.goodbye());
     }
 
-    @Override
-    public void start(Stage stage) throws Exception {
-        scrollPane = new ScrollPane();
-        dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
-
-        userInput = new TextField();
-        sendButton = new Button("Send");
-
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-
-        scene = new Scene(mainLayout);
-
-        stage.setScene(scene);
-        stage.show();
-
-
-        stage.setTitle("Duke");
-        stage.setResizable(false);
-        stage.setMinHeight(600.0);
-        stage.setMinWidth(400.0);
-
-        mainLayout.setPrefSize(400.0, 600.0);
-
-        scrollPane.setPrefSize(385, 535);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        // You will need to import `javafx.scene.layout.Region` for this.
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-
-        userInput.setPrefWidth(325.0);
-
-        sendButton.setPrefWidth(55.0);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-
-        AnchorPane.setLeftAnchor(userInput , 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-        sendButton.setOnMouseClicked((event) -> {
-            handleUserInput();
-        });
-
-        userInput.setOnAction((event) -> {
-            handleUserInput();
-        });
-
-        //Scroll down to the end every time dialogContainer's height changes.
-        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
-
+    public String getResponse(String input) {
+        TaskList arr = tasks;
+        String[] c = input.split(" ", 2);
+        switch (c[0]) {
+            case ("list"):
+                return(UI.list(arr));
+            case ("done"):
+                Task curr = arr.get(Integer.parseInt(c[1]) - 1);
+                curr.mark();
+                return(UI.done(curr));
+            case ("todo"):
+                try {
+                    Task todo = new Todo(c[1]);
+                    arr.add(todo);
+                    return(UI.added(todo));
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    return(UI.say("OOPS!!! The description of a todo cannot be empty."));
+                }
+            case ("deadline"):
+                String[] dl = c[1].split("/by", 2);
+                Task d = new Deadline(dl[0], dl[1].trim());
+                arr.add(d);
+                return(UI.added(d));
+            case ("event"):
+                String[] ev = c[1].split("/at", 2);
+                Task e = new Event(ev[0], ev[1].trim());
+                arr.add(e);
+                return(UI.added(e));
+            case ("delete"):
+                try {
+                    Task del = arr.get(Integer.parseInt(c[1]) - 1);
+                    arr.remove(Integer.parseInt(c[1]) - 1);
+                    return(UI.delete(del));
+                } catch (Exception i) {
+                    return(UI.say("Failed to delete item!!!"));
+                }
+            case ("find"):
+                return(UI.results(tasks.search(c[1])));
+            default:
+                try {
+                    throw new DukeException();
+                } catch (DukeException de) {
+                    return(UI.say("OOPS!!! I'm sorry, but I don't know what that means :-("));
+                }
+        }
     }
 
     /**
@@ -195,28 +181,4 @@ public class Duke extends Application {
         return textToAdd;
     }
 
-
-    /**
-     * Iteration 2:
-     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
-     * the dialog container. Clears the user input after processing.
-     */
-    private void handleUserInput() {
-        Label userText = new Label(userInput.getText());
-        Label dukeText = new Label(getResponse(userInput.getText()));
-        dialogContainer.getChildren().addAll(
-                DialogBox.getUserDialog(userText, new ImageView(user)),
-                DialogBox.getDukeDialog(dukeText, new ImageView(duke))
-        );
-        userInput.clear();
-    }
-
-
-    /**
-     * You should have your own function to generate a response to user input.
-     * Replace this stub with your completed method.
-     */
-    private String getResponse(String input) {
-        return "Duke heard: " + input;
-    }
 }
--- src/main/java/duke/UI.java
+++ src/main/java/duke/UI.java
@@ -8,13 +8,13 @@ public class UI {
     /**
      * Commonly used beautifying line.
      */
-    public static String line = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
+    public static String LINE = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
 
     /**
      * Prints a single line.
      */
     static void println() {
-        System.out.println(line);
+        System.out.println(LINE);
     }
 
     /**
@@ -22,10 +22,9 @@ public class UI {
      *
      * @param s s
      */
-    static void say(String s) {
-        System.out.println(line);
-        System.out.println(s);
-        System.out.println(line);
+    static String say(String s) {
+//        String returnString = LINE + "\n" + s + "\n" + LINE;
+        return s;
     }
 
     /**
@@ -33,18 +32,18 @@ public class UI {
      *
      * @param arr arr
      */
-    static void listHelper(TaskList arr) {
+    static String listHelper(TaskList arr) {
+        String returnString = "";
         for (var i = 0; i < arr.size(); i++) {
             Task t = arr.get(i);
-            System.out.println(i + 1 + ": " + t.toString());
+            returnString += "\n" + (i + 1) + ": " + t.toString();
         }
+        return returnString;
     }
 
-    static void list(TaskList arr) {
-        println();
-        System.out.println("Here are the items in your list:");
-        UI.listHelper(arr);
-        println();
+    static String list(TaskList arr) {
+        String returnString = "Here are the items in your list:" + UI.listHelper(arr);
+        return say(returnString);
     }
 
     /**
@@ -52,8 +51,8 @@ public class UI {
      *
      * @param curr curr
      */
-    static void done(Task curr) {
-        say("Marked as done:\n" + curr.toString());
+    static String done(Task curr) {
+        return say("Marked as done:\n" + curr.toString());
     }
 
     /**
@@ -61,8 +60,8 @@ public class UI {
      *
      * @param curr curr
      */
-    static void added(Task curr) {
-        UI.say("Added: " + curr.toString());
+    static String added(Task curr) {
+        return say("Added: " + curr.toString());
     }
 
     /**
@@ -70,32 +69,30 @@ public class UI {
      *
      * @param curr curr
      */
-    static void delete(Task curr) {
-        UI.say("Deleted item:\n" + curr.toString());
+    static String delete(Task curr) {
+        return say("Deleted item:\n" + curr.toString());
     }
 
     /**
      * Feedback failure.
      */
-    static void failure() {
-        UI.say("OOPS!!! I'm sorry, but I don't know what that means :-(");
+    static String failure() {
+        return say("OOPS!!! I'm sorry, but I don't know what that means :-(");
     }
 
     /**
      * Says goodbye.
      */
-    static void goodbye() {
-        UI.say("Bye! Hope to see you again!");
+    static String goodbye() {
+        return say("Bye! Hope to see you again!");
     }
 
-    static void showLoadingError() {
-        UI.say("Failed to load from file. Are you sure the path is correct?");
+    static String showLoadingError() {
+        return say("Failed to load from file. Are you sure the path is correct?");
     }
 
-    static void results(TaskList tl) {
-        println();
-        System.out.println("Here are your search results:");
-        UI.listHelper(tl);
-        println();
+    static String results(TaskList tl) {
+        String returnString = "Here are your search results:\n" + UI.listHelper(tl);
+        return say(returnString);
     }
 }
