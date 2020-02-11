7f04dd5ac4ef0f5742d307902a4f601509cbdc81
Arthur Lee <ykarthurlee@gmail.com>
2020-02-11 20:36:29 +0800

    Added reminder functionality via javaFX

--- src/main/java/duke/DukeException.java
+++ src/main/java/duke/DukeException.java
@@ -15,24 +15,27 @@ public class DukeException extends Exception {
   public String getMessage() {
     String message = "";
     switch (errorType) {
-    case "empty":
-      message = "OOPS!!! The description of a " + inputType + " cannot be empty.";
-      break;
-    case "invalid":
-      message = "OOPS!!! I'm sorry, but I don't know what that means :-(";
-      break;
-    case "date":
-      message = "OOPS!!! Your date needs to be in yyyy-mm-dd format!";
-      break;
-    case "file":
-      message = "OOPS!!! The save file cannot be found!";
-      break;
-    case "read":
-      message = "OOPS!!! The save file cannot be read. A new task list will be created!";
-      break;
-    case "emptyLoad":
-      message = "OOPS!!! The save file is empty. A new task list will be created!";
-      break;
+      case "empty":
+        message = "OOPS!!! The description of a " + inputType + " cannot be empty.";
+        break;
+      case "invalid":
+        message = "OOPS!!! I'm sorry, but I don't know what that means :-(";
+        break;
+      case "date":
+        message = "OOPS!!! Your date needs to be in yyyy-mm-dd format!";
+        break;
+      case "dateTime":
+        message = "OOPS!!! Your date and time needs to be in yyyy-mm-ddThh:mm:ss";
+        break;
+      case "file":
+        message = "OOPS!!! The save file cannot be found!";
+        break;
+      case "read":
+        message = "OOPS!!! The save file cannot be read. A new task list will be created!";
+        break;
+      case "emptyLoad":
+        message = "OOPS!!! The save file is empty. A new task list will be created!";
+        break;
     }
     return message;
   }
