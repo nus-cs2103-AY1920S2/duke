e5f1510d5570300ca853f9957c60a76c3659e867
PangJiaDa <pangjiada52@gmail.com>
2020-02-12 01:24:25 +0800

    do javaFX tut3 iteration 3
    complete exercise 1 subparts: put padding around DialogBox and between image and text

--- src/main/java/duke/DialogBox.java
+++ src/main/java/duke/DialogBox.java
@@ -1,9 +1,14 @@
 package duke;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.shape.Circle;
 
 
 public class DialogBox extends HBox {
@@ -20,5 +25,27 @@ public class DialogBox extends HBox {
 
         this.setAlignment(Pos.TOP_RIGHT);
         this.getChildren().addAll(text, displayPicture);
+
+        //padding between DialogBoxes
+        this.setPadding(new Insets(10.0, 10.0, 10.0, 10.0));
+        //spacing between image and text
+        this.setSpacing(10.0);
+    }
+
+    private void flip() {
+        this.setAlignment(Pos.TOP_LEFT);
+        ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
+        FXCollections.reverse(tmp);
+        this.getChildren().setAll(tmp);
+    }
+
+    public static DialogBox getUserDialog(Label l, ImageView iv) {
+        return new DialogBox(l, iv);
+    }
+
+    public static DialogBox getDukeDialog(Label l, ImageView iv) {
+        DialogBox db = new DialogBox(l, iv);
+        db.flip();
+        return db;
     }
 }
