fefc22b9b9a150b51400ba41d2a7c98022f78aa7
Alden <49450743+alcen@users.noreply.github.com>
2020-02-13 00:39:06 +0800

    Convert Command to class

--- src/main/java/bot/command/Command.java
+++ src/main/java/bot/command/Command.java
@@ -1,26 +1,28 @@
 package bot.command;
 
 /**
- * Enum to store the various Commands
- * that may be used in the bot
+ * A class representing the various Commands
+ * that may be used in the bot. Also has
+ * the original commands that cannot be
+ * changed by the user
  */
-public enum Command {
-    BYE("bye"),
-    DELETE("delete"),
-    DONE("done"),
-    EX("ex"),
-    EXI("exi"),
-    EXIT("exit"),
-    FIND("find"),
-    HELLO("hello"),
-    HELP("help"),
-    LIST("list"),
-    NOT_DONE("notdone"),
-    DEADLINE("deadline"),
-    EVENT("event"),
-    TODO("todo"),
-    THANKS("thanks"),
-    SEARCH("search");
+public class Command {
+    public static final Command BYE = new Command("bye");
+    public static final Command DELETE = new Command("delete");
+    public static final Command DONE = new Command("done");
+    public static final Command EX = new Command("ex");
+    public static final Command EXI = new Command("exi");
+    public static final Command EXIT = new Command("exit");
+    public static final Command FIND = new Command("find");
+    public static final Command HELLO = new Command("hello");
+    public static final Command HELP = new Command("help");
+    public static final Command LIST = new Command("list");
+    public static final Command NOT_DONE = new Command("notdone");
+    public static final Command DEADLINE = new Command("deadline");
+    public static final Command EVENT = new Command("event");
+    public static final Command TODO = new Command("todo");
+    public static final Command THANKS = new Command("thanks");
+    public static final Command SEARCH = new Command("search");
 
     private final String word;
 
@@ -31,7 +33,7 @@ public enum Command {
      * @param w A String consisting of one word
      *          that represents this Command
      */
-    Command(String w) {
+    public Command(String w) {
         this.word = w;
     }
 
