4bc043348f6e9f91b12810b7a32356b4f8978285
maxiepangz <mpang45456@gmail.com>
2020-02-12 09:38:05 +0800

    Level-10

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,137 +2,274 @@ import javafx.concurrent.Task;
 
 import java.io.IOException;
 import java.util.*;
-import java.io.File;
-import java.io.PrintWriter;
-import java.time.LocalDate;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Region;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
-public class Duke {
-    public static void main(String[] args) throws DukeException, IOException {
-        Scanner sc = new Scanner(System.in);
+public class Duke extends Application {
 
-        String Hello = "Hello! I'm Duke\nWhat can i do for you?";
-        System.out.println(Hello);
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
 
-        Storage storage = new Storage();
+    private Image user = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
+    private Image duke = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
 
-        int flag = 0;
-        ArrayList<task> mylist = new ArrayList<>();
-        mylist = storage.readTxtFile();
+    private int flag = 0;
+    private ArrayList<task> mylist = new ArrayList<>();
+    private Storage storage = new Storage();
 
-        while(flag == 0) {
-            String input = sc.nextLine();
-            String[] inarr = input.split(" ");
-            String Line = "_________________________________________________________________";
-
-            System.out.println(Line);
-            try {
-                if (input.equals("bye")) {
-                    System.out.println("Bye. Hope to see you again soon!");
-                    flag = 1;
-                } else if (input.equals("list")) {
-                    printlist(mylist);
-                } else if (inarr[0].equals("done")) {
-                    try {
-                        int tocheck = Integer.parseInt(inarr[1]);
-                        task temp = mylist.get(tocheck - 1);
-                        temp.markDone();
-
-                        System.out.println("Nice! I've marked this task as done:");
-                        System.out.println(mylist.get(tocheck - 1));
-                        storage.updateTxtFile(mylist);
-                    } catch (IndexOutOfBoundsException e) {
-                        throw new DukeException("☹ OOPS!!! That task doesn't exist or you failed to include a number.", e);
-                    }
-                } else if (inarr[0].equals("todo")) {
-                    try {
-                        ToDo temp = new ToDo(input.replaceFirst("todo ", ""));
-                        System.out.println("Got it. I've added this task:");
-                        System.out.println(temp);
-                        System.out.println("Now you have " + (mylist.size() + 1) + " tasks in the list.");
-                        mylist.add(temp);
-                        storage.updateTxtFile(mylist);
-                    } catch (IndexOutOfBoundsException e) {
-                        throw new DukeException("☹ OOPS!!! You're missing some descriptions for your todo.", e);
-                    }
-                } else if (inarr[0].equals("deadline")) {
-                    try {
-                        String wodl = input.replaceFirst("deadline ", "");
-                        String[] myarr = wodl.split(" /by ");
-                        DeadLine temp = new DeadLine(myarr[0], myarr[1]);
-                        System.out.println("Got it. I've added this task:");
-                        System.out.println(temp);
-                        System.out.println("Now you have " + (mylist.size() + 1) + " tasks in the list.");
-                        mylist.add(temp);
-                        storage.updateTxtFile(mylist);
-                    } catch (IndexOutOfBoundsException e) {
-                        throw new DukeException("☹ OOPS!!! You're missing some descriptions for your deadline.", e);
-                    }
-                } else if (inarr[0].equals("event")) {
-                    try {
-                        String woe = input.replaceFirst("event ", "");
-                        String[] myarr = woe.split(" /at ");
-                        Event temp = new Event(myarr[0], myarr[1]);
-                        System.out.println("Got it. I've added this task:");
-                        System.out.println(temp);
-                        System.out.println("Now you have " + (mylist.size() + 1) + " tasks in the list.");
-                        mylist.add(temp);
-                        storage.updateTxtFile(mylist);
-                    } catch (IndexOutOfBoundsException e) {
-                        throw new DukeException("☹ OOPS!!! You're missing some descriptions for your event.", e);
-                    }
-
-                } else if (inarr[0].equals("delete")) {
-                    try {
-                        int toremove = Integer.parseInt(inarr[1]);
-                        removetask(mylist, toremove);
-                        storage.updateTxtFile(mylist);
-                    } catch(IndexOutOfBoundsException e) {
-                        throw new DukeException("☹ OOPS!!! That task doesn't exist or you failed to include a number.", e);
-                    }
-                } else if (inarr[0].equals("find")) {
-                    ArrayList<Integer> searchResults = new ArrayList<Integer>();
-                    String keyword = input.replaceFirst("find ", "");
-
-                    for (int i = 0; i < mylist.size(); i++) {
-                        if (mylist.get(i).getName().contains(keyword)) {
-                            searchResults.add(i);
-                        } else {}
-                    }
-
-                    if (searchResults.size() == 0) {
-                        System.out.println("Sorry, I can't seem to find anything related to " + keyword);
-                    } else {
-                        System.out.println("Here are the matching tasks in your list");
-
-                        for (int i = 0; i < searchResults.size(); i++) {
-                            System.out.println((i + 1) + "." + mylist.get(searchResults.get(i)));
-                        }
-                    }
-                } else {
-                    throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
-                }
-            } catch (DukeException e) {
-                System.out.println(e);
-            }
-
-            System.out.println(Line);
-        }
-    }
-
-    private static void printlist(ArrayList<task> ls) {
-        System.out.println("Here are the tasks in your list:");
+    private static String printlist(ArrayList<task> ls) {
+        String output;
+        output = "Here are the tasks in your list:";
 
         for (int i = 0; i < ls.size(); i++) {
-            System.out.println((i + 1) + "." + ls.get(i));
+            output = output + "\n" + (i + 1) + "." + ls.get(i);
+        }
+
+        return output;
+    }
+
+    private String removetask (ArrayList<task> ls, int ind) throws IOException {
+        String output;
+        task temp = ls.remove(ind - 1);
+        output = "Noted. I've removed this task:";
+        output = output + "\n" + temp;
+        output = output + "\nNow you have " + ls.size() + " tasks on the list.";
+        storage.updateTxtFile(mylist);
+        return output;
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        //Step 1. Setting up required components
+
+        //The container for the content of the chat to scroll.
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);
+
+        stage.setScene(scene);
+        stage.show();
+
+        stage.setTitle("Duke");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        // You will need to import `javafx.scene.layout.Region` for this.
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(325.0);
+
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput , 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+
+        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
+
+        sendButton.setOnMouseClicked((event) -> {
+            handleUserInput();
+        });
+
+        userInput.setOnAction((event) -> {
+            handleUserInput();
+        });
+        // more code to be added here later
+    }
+
+    /**
+     * Iteration 1:
+     * Creates a label with the specified text and adds it to the dialog container.
+     * @param text String containing text to add
+     * @return a label with the specified text that has word wrap enabled.
+     */
+    private Label getDialogLabel(String text) {
+        Label textToAdd = new Label(text);
+        textToAdd.setWrapText(true);
+
+        return textToAdd;
+    }
+
+    public String dukeStart() throws IOException {
+        String hello = "Hello! I'm Duke\nWhat can i do for you?";
+        mylist = storage.readTxtFile();
+
+        if (mylist.size() != 0) {
+            hello = hello + "\nYou have " + mylist.size() + " tasks from the previous session";
+        }
+
+        return hello;
+    }
+
+    /**
+     * Iteration 2:
+     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
+     * the dialog container. Clears the user input after processing.
+     */
+    private void handleUserInput() {
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(userInput.getText(), user),
+                DialogBox.getDukeDialog(getResponse(userInput.getText()), duke)
+        );
+        userInput.clear();
+    }
+
+    public String handleDone(int doneIndex) {
+        try {
+            task temp = mylist.get(doneIndex - 1);
+            temp.markDone();
+
+            String output = "Nice! I've marked this task as done:";
+            output = output + "\n" + mylist.get(doneIndex - 1);
+            storage.updateTxtFile(mylist);
+            return output;
+        } catch (IndexOutOfBoundsException | IOException e) {
+            return "☹ OOPS!!! You seem to have entered a wrong index.";
         }
     }
 
-    private static void removetask (ArrayList<task> ls, int ind) {
-        task temp = ls.remove(ind - 1);
-        System.out.println("Noted. I've removed this task:");
-        System.out.println(temp);
-        System.out.println("Now you have " + ls.size() + " tasks on the list.");
+    public String handleToDo(String toDoRaw) {
+        try {
+            String output;
+            ToDo temp = new ToDo(toDoRaw.replaceFirst("todo ", ""));
+            output = "Got it. I've added this task:\n";
+            output = output + temp + "\n";
+            output = output + "Now you have " + (mylist.size() + 1) + " tasks in the list.";
+            mylist.add(temp);
+            storage.updateTxtFile(mylist);
+            return output;
+        } catch (IndexOutOfBoundsException e) {
+            return "☹ OOPS!!! You're missing some descriptions for your todo.";
+        } catch (IOException e) {
+            return "☹ OOPS!!! You're missing some descriptions for your todo.";
+        }
     }
 
+    public String handleDeadline(String deadlineRaw) {
+        try {
+            String output;
+            String wodl = deadlineRaw.replaceFirst("deadline ", "");
+            String[] myarr = wodl.split(" /by ");
+            DeadLine temp = new DeadLine(myarr[0], myarr[1]);
+            output = "Got it. I've added this task:\n";
+            output = output + temp + "\n";
+            output = output + "Now you have " + (mylist.size() + 1) + " tasks in the list.";
+            mylist.add(temp);
+            storage.updateTxtFile(mylist);
+            return output;
+        } catch (IndexOutOfBoundsException | IOException e) {
+            return "☹ OOPS!!! You're missing some descriptions for your deadline.";
+        }
+    }
 
+    public String handleEvent(String eventRaw) {
+        try {
+            String output;
+            String woe = eventRaw.replaceFirst("event ", "");
+            String[] myarr = woe.split(" /at ");
+            Event temp = new Event(myarr[0], myarr[1]);
+            output = "Got it. I've added this task:\n";
+            output = output + temp + "\n";
+            output = output + "Now you have " + (mylist.size() + 1) + " tasks in the list.";
+            mylist.add(temp);
+            storage.updateTxtFile(mylist);
+            return output;
+        } catch (IndexOutOfBoundsException | IOException e) {
+            return "☹ OOPS!!! You're missing some descriptions for your event.";
+        }
+    }
+
+    public String handleDelete(int deleteIndex) {
+        try {
+            return removetask(mylist, deleteIndex);
+        } catch(IndexOutOfBoundsException | IOException e) {
+            return "☹ OOPS!!! That index doesn't seems to be missing";
+        }
+    }
+
+    public String handleFind(String findRaw) {
+        String output;
+        ArrayList<Integer> searchResults = new ArrayList<Integer>();
+        String keyword = findRaw.replaceFirst("find ", "");
+
+        for (int i = 0; i < mylist.size(); i++) {
+            if (mylist.get(i).getName().contains(keyword)) {
+                searchResults.add(i);
+            } else {}
+        }
+
+        if (searchResults.size() == 0) {
+            output = "Sorry, I can't seem to find anything related to " + keyword;
+        } else {
+            output = "Here are the matching tasks in your list";
+
+            for (int i = 0; i < searchResults.size(); i++) {
+                output = output + "\n" + (i + 1) + "." + mylist.get(searchResults.get(i));
+            }
+        }
+        return output;
+    }
+
+    public String getResponse(String input) {
+        while(flag == 0) {
+            String[] inarr = input.split(" ");
+
+            if (input.equals("bye")) {
+            flag = 1;
+            return "Bye. Hope to see you again soon!";
+            } else if (input.equals("list")) {
+                return printlist(mylist);
+            } else if (inarr[0].equals("done")) {
+                return handleDone(Integer.parseInt(inarr[1]));
+            } else if (inarr[0].equals("todo")) {
+                return handleToDo(input);
+            } else if (inarr[0].equals("deadline")) {
+                return handleDeadline(input);
+            } else if (inarr[0].equals("event")) {
+                return handleEvent(input);
+            } else if (inarr[0].equals("delete")) {
+                return handleDelete(Integer.parseInt(inarr[1]));
+            } else if (inarr[0].equals("find")) {
+                return handleFind(input);
+            } else {
+                return "☹ OOPS!!! I'm sorry, but I don't know what that means :-(";
+            }
+        }
+        return "";
+    }
 }
--- src/main/java/Ui.java
+++
@@ -1,3 +0,0 @@
-public class Ui {
-    private String intro = "Hello! I'm Duke\nWhat can i do for you?";
-}
