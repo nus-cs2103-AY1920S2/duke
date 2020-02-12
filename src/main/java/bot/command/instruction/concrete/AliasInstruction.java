93c138efedc2bb528b9f8e2c7302ba27ec143c9e
Alden <49450743+alcen@users.noreply.github.com>
2020-02-13 01:07:10 +0800

    Implement alias logic in AliasInstruction

--- src/main/java/bot/command/instruction/concrete/AliasInstruction.java
+++ src/main/java/bot/command/instruction/concrete/AliasInstruction.java
@@ -4,6 +4,7 @@ import bot.Ui;
 
 import bot.command.Command;
 import bot.command.CommandParser;
+import bot.command.instruction.Instruction;
 import bot.command.instruction.execute.StorageWriting;
 import bot.command.instruction.parse.ThreeWordsInstruction;
 
@@ -25,10 +26,30 @@ public class AliasInstruction extends ThreeWordsInstruction
         // first in pair is the original name
         // second in pair is the aliased name
         commandStore.store(nameToReplace);
-
-        String originalName = nameToReplace.getFirst();
-        String aliasedName = nameToReplace.getSecond();
-
+        AliasInstruction.addAliasedName(
+                nameToReplace.getFirst(),
+                nameToReplace.getSecond()
+        );
         ui.showAliasMessage();
+    }
+
+    /**
+     * Given the original command name and new command name,
+     * update the CommandParser maps such that the new
+     * command name will work for the same Instruction
+     * that the original command name refers to
+     *
+     * @param originalName String representing the
+     *                     original command name
+     * @param altName String representing the
+     *                new command name
+     */
+    public static void addAliasedName(String originalName, String altName) {
+        // assumes aliased name is not used already
+        Command aliased = new Command(altName);
+        Instruction inst = CommandParser.INSTR_MAP.get(originalName);
+        // update maps
+        CommandParser.INSTR_MAP.put(altName, inst);
+        CommandParser.COMMAND_MAP.put(altName, aliased);
     }
 }
