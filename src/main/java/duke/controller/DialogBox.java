1e76ab36ec91d98eea57b7446d0e68cd6a6c8730
Hanming Zhu <hanming@huawen.com>
2020-02-12 12:04:44 +0800

    fix: specify jdk version and fix dialogbox

--- src/main/java/duke/controller/DialogBox.java
+++ src/main/java/duke/controller/DialogBox.java
@@ -71,7 +71,7 @@ public class DialogBox extends HBox {
      * @return Dialog for Duke.
      */
     public static DialogBox getDukeDialog(String text, Image img) {
-        var db = new DialogBox(text, img);
+        DialogBox db = new DialogBox(text, img);
         db.flip();
         return db;
     }
