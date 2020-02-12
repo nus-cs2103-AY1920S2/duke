32f3836c71938bcdb8d0aa53bfa1d88d953bac45
Liao Meng E0148811 <lyhlm5@hotmail.com>
2020-02-12 21:09:07 +0800

    Fix method signature error for JavaFX part 4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -339,11 +339,11 @@ public class Duke extends Application {
      * the dialog container. Clears the user input after processing.
      */
     private void handleUserInput() {
-        Label userText = new Label(userInput.getText());
-        Label dukeText = new Label(getResponse(userInput.getText()));
+        String userText = userInput.getText();
+        String dukeText = getResponse(userInput.getText());
         dialogContainer.getChildren().addAll(
-                DialogBox.getUserDialog(userText, new ImageView(user)),
-                DialogBox.getDukeDialog(dukeText, new ImageView(duke))
+                DialogBox.getUserDialog(userText, user),
+                DialogBox.getDukeDialog(dukeText, duke)
         );
         userInput.clear();
     }
