105708e50053286c6c6664438775e9d8ca2e93f5
johan.kok <johan.kok@grabtaxi.com>
2020-02-13 15:15:05 +0800

    Minor edits to DAOFactory

--- src/main/java/com/nus/duke/dao/DAOFactory.java
+++ src/main/java/com/nus/duke/dao/DAOFactory.java
@@ -8,16 +8,27 @@ public class DAOFactory {
      Factory method that returns a singleton object for a DAOInterface.
      This ensures that we only have a single point of access to the data.
      This method is thread safe
+
+     @param     daoInterface
+                DAO interface that we are using to store the data
+
+     @return    DAOInterface singleton object
      */
-    private static synchronized  <T extends DAOInterface> DAOInterface getOrCreate(Class<T> DAOTypeClass) {
+    private static synchronized  <T extends DAOInterface> DAOInterface getOrCreate(Class<T> daoInterface) {
         try{
-            if (dataObj == null) dataObj = DAOTypeClass.newInstance();
+            if (dataObj == null) dataObj = daoInterface.newInstance();
         } catch (Exception ex) {
             System.out.println(ex);
         }
         return dataObj;
     }
 
+    /**
+     Method controls the dispatch of the DAO Object.
+     There should only be 1 type of the object for data consistency.
+
+     @return    DAOInterface singleton object
+     */
     public static synchronized <T extends DAOInterface> DAOInterface getOrCreate() {
         return getOrCreate(InMemDAO.class);
     }
