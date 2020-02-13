eefa3c0fc10c7241217a38d758f354ed1c5bef56
Willis Tay <27534078+tzihiang@users.noreply.github.com>
2020-02-13 15:16:18 +0800

    Implemented C-Help

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -166,7 +166,9 @@ public class Duke extends Application {
     }
 
     static void printIntro() {
-        String introText = BORDER_LINE + "Hello! I'm Duke\nWhat can I do for you?\n" + BORDER_LINE;
+        String introText = BORDER_LINE + "Hello! I'm Duke\nWhat can I do for you?\n" +
+                "If you need any assistance, type 'help' in the command line\n" + BORDER_LINE;
+
         assert(introText == null): "Intro text should not be null";
     }
 
@@ -250,6 +252,9 @@ public class Duke extends Application {
             if (input.split(" ")[0].toLowerCase().equals("delete")) {
                 deleteTask(input);
                 storage.saveFile(taskToParse(tasks));
+            } else if (!input.toLowerCase().equals("help")){
+                Helper help = new Helper();
+                System.out.println(Helper.printHelp());
             } else {
                 addTask(input);
             }
