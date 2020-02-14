67b351796ee40b46f8d9772d522e2fa0a3ccf3e3
YingxuH <yingxu.he1998@gmail.com>
2020-02-14 18:00:02 +0800

    apply fxml in replace of javafx

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,4 @@
 //package java;
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
-import javafx.stage.Stage;
 
 import parser.Command;
 import parser.ExitCommand;
@@ -34,23 +21,13 @@ import java.io.IOException;
  * An application capable of recording the tasks and events to help the users
  * manage the schedule.
  */
-public class Duke extends Application{
+public class Duke {
 
     protected String userName;
     protected Storage storage;
     protected TaskList taskList;
     protected Parser parser;
     protected Ui ui;
-
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Scene scene;
-
-    private Image user = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image duke = new Image(this.getClass().getResourceAsStream("/images/Duke_waving.svg.png"));
-
 
     public Duke() {
 
@@ -64,7 +41,7 @@ public class Duke extends Application{
         this.userName = userName;
     }
     
-    private void duke_start() {
+    private void start() {
         ui = new Ui();
         try {
             parser = new Parser();
@@ -109,99 +86,16 @@ public class Duke extends Application{
         System.exit(0);
     }
 
-    @Override
-    public void start(Stage stage) {
-
-        Image image = new Image(".\\img\\Duke_waving.svg.png", 80, 100, false, false);
-        Label duke_icon = new Label("Welcome to Duke!"); // Creating a new Label control
-
-        duke_icon.setGraphic(new ImageView(image));
-        duke_icon.setFont(new Font("Arial", 50));
-
-        scrollPane = new ScrollPane();
-        dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
-
-        userInput = new TextField();
-        sendButton = new Button("Send");
-
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-
-        scene = new Scene(mainLayout); // Setting the scene to be our Label
-
-        stage.setScene(scene); // Setting the stage to show our screen
-        stage.show(); // Render the stage.
-
-        //Step 2. Formatting the window to look as expected
-        stage.setTitle("Duke");
-        stage.setResizable(false);
-        stage.setMinHeight(600.0);
-        stage.setMinWidth(400.0);
-
-        mainLayout.setPrefSize(400.0, 600.0);
-
-        scrollPane.setPrefSize(385, 535);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        // You will need to import `javafx.scene.layout.Region` for this.
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-
-        userInput.setPrefWidth(325.0);
-
-        sendButton.setPrefWidth(55.0);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-
-        AnchorPane.setLeftAnchor(userInput , 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-
-        //Step 3. Add functionality to handle user input.
-        sendButton.setOnMouseClicked((event) -> {
-            handleUserInput();
-        });
-
-        userInput.setOnAction((event) -> {
-            handleUserInput();
-        });
-
-        //Scroll down to the end every time dialogContainer's height changes.
-        dialogContainer.heightProperty().addListener(
-                (observable) -> scrollPane.setVvalue(1.0));
-    }
-
-    /**
-     * Iteration 3:
-     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
-     * the dialog container. Clears the user input after processing.
-     */
-    private void handleUserInput() {
-        Label userText = new Label(userInput.getText());
-        Label dukeText = new Label(getResponse(userInput.getText()));
-        dialogContainer.getChildren().addAll(
-                DialogBox.getUserDialog(userText, new ImageView(user)),
-                DialogBox.getDukeDialog(dukeText, new ImageView(duke))
-        );
-        userInput.clear();
-    }
-
     /**
      * You should have your own function to generate a response to user input.
      * Replace this stub with your completed method.
      */
-    private String getResponse(String input) {
+    public String getResponse(String input) {
         return "Duke heard: " + input;
     }
 
     private void run() {
-        duke_start();
+        start();
         listen();
         exit();
     }
