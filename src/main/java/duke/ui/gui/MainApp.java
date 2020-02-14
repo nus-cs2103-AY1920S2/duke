3dfadb102bebf4b83f07ffa48b09e77c28993c66
CheyanneSim <Cheyanne@u.nus.edu>
2020-02-14 18:23:00 +0800

    add title for GUI window

--- src/main/java/duke/ui/gui/MainApp.java
+++ src/main/java/duke/ui/gui/MainApp.java
@@ -2,14 +2,11 @@ package duke.ui.gui;
 
 import duke.storage.Storage;
 import duke.task.TaskList;
-
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
 
 import java.io.IOException;
 
@@ -33,6 +30,7 @@ public class MainApp extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Duke");
             stage.setOnCloseRequest(event -> {
                 event.consume();
                 fxmlLoader.<MainWindow>getController().closeWindowButtonClicked();
