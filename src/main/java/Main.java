b9b7fcb05a4d30bcfc0168b7b97a31794fc35c72
kester-ng <kester.ng@visenze.com>
2020-02-15 15:21:59 +0800

    Added title to the chat bot

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,6 +22,7 @@ public class Main extends Application {
             Scene scene = new Scene(ap);
             stage.setScene(scene);
             stage.setResizable(false);
+            stage.setTitle("Duke Chat-Bot");
             fxmlLoader.<MainWindow>getController().setDuke(duke);
             stage.show();
         } catch (IOException e) {
