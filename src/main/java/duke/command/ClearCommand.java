3bdb590a3ebfce6f9db8a962b3572e0640f366c6
Benjamin Lau <capeguy@capeguy.net>
2020-02-16 18:01:29 +0800

    A-Assertions: Added more assertions

--- src/main/java/duke/command/ClearCommand.java
+++ src/main/java/duke/command/ClearCommand.java
@@ -6,6 +6,8 @@ import duke.main.Ui;
 public class ClearCommand extends Command {
     public static boolean run(TaskList taskList) {
         taskList.clearTasks();
+        // Ensure that the TaskList has been emptied
+        assert(taskList.size() == 0);
         Ui.tasksCleared();
         return true;
     }
