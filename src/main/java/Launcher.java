e7e20c2270c4f09efaa7dc5b95ab10ac391aee41
Raivat Shah <hiraivat@gmail.com>
2020-02-16 15:49:14 +0800

    Level-10

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,11 +1,10 @@
-import duke.Duke;
 import javafx.application.Application;
-
+import duke.Main;
 /**
  * A launcher class to workaround classpath issues.
  */
 public class Launcher {
     public static void main(String[] args) {
-        Application.launch(Duke.class, args);
+        Application.launch(Main.class, args);
     }
 }
