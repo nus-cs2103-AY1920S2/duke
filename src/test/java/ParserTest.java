81c9a7c20322828cf7615c9bc1563936078bdba4
beatricetay <beatricetayyt@gmail.com>
2020-02-19 03:30:41 +0800

    clean up code

--- src/test/java/ParserTest.java
+++ src/test/java/ParserTest.java
@@ -8,12 +8,5 @@ public class ParserTest {
 
     @Test
     public void newDeadlineTask_noDetails_DukeExceptionThrown() {
-//        try {
-//            assertEquals(0, new Parser().parse("deadline", new TaskList()));
-//        } catch (DukeException e) {
-//            assertEquals("    ____________________________________________________________\n"
-//                    + "     ☹ OOPS!!! The description of a deadline cannot be empty.\n"
-//                    + "    ____________________________________________________________\n", e.toString());
-//        }
     }
 }
--- src/test/java/ToDoTest.java
+++ src/test/java/ToDoTest.java
@@ -6,8 +6,6 @@ public class ToDoTest {
 
     @Test
     public void testStringConversion() {
-//        assertEquals("[T][✗] read book", new ToDo(" read book").toString());
-//        assertEquals("[T][✗] join club", new ToDo(" join club").toString());
     }
 }
 
