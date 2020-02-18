75b84895a5adcb882c006651ec458fc7639a482a
Aaron Choo <choozy.aaron@gmail.com>
2020-02-18 13:36:23 +0800

    Make avatars round

--- src/main/java/DialogBox.java
+++ src/main/java/DialogBox.java
@@ -11,6 +11,8 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
 
 /**
  * An example of a custom control using FXML.
@@ -21,7 +23,7 @@ public class DialogBox extends HBox {
     @FXML
     private Label dialog;
     @FXML
-    private ImageView displayPicture;
+    private Circle circle;
 
     private DialogBox(String text, Image img) {
         try {
@@ -33,8 +35,8 @@ public class DialogBox extends HBox {
             e.printStackTrace();
         }
 
+        circle.setFill(new ImagePattern(img));
         dialog.setText(text);
-        displayPicture.setImage(img);
     }
 
     /**
