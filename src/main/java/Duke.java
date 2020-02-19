e8484c6faa9e256d249055c808406e47c9fbc339
qumingsi <qms82581310@gmail.com>
2020-02-20 03:25:32 +0800

    complete level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
+    private static ArrayList<String> instructions = new ArrayList<>(100);
 
     private static void greet() {
         System.out.println("Hello! I'm Duke\n What can I do for you?");
     }
 
-    private static void echo(String instruction) {
-        System.out.println(" " + instruction);
+    private static void addTask(String instruction) {
+        instructions.add(instruction);
+        System.out.println("added: " + instruction);
     }
 
     private static void exit() {
         System.out.println("Bye. Hope to see you again soon!");
     }
+
+    private static void printList() {
+        for (int i = 0; i < instructions.size(); i++) {
+            if (instructions.get(i) != null) {
+                System.out.println((i + 1) + ". " + instructions.get(i));
+            }
+        }
+    }
+
     public static void main(String[] args) {
         /*String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -26,11 +39,12 @@ public class Duke {
         greet();
         while (scanner.hasNextLine()) {
             String instruction = scanner.nextLine();
-            if (!instruction.equals("bye")) {
-                echo(instruction);
-            } else {
+            if (instruction.equals("bye")) {
                 exit();
+            } else if (instruction.equals("list")) {
+                printList();
+            } else {
+                addTask(instruction);
             }
         }
     }
-}
