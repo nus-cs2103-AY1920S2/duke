cd10fa46071b385fb68b4fa230021df835a82167
Yujian <yujian_fu@outlook.com>
2020-02-19 14:58:47 +0800

    add task_manager

---
+++ src/main/java/tasks/TaskManager.java
@@ -0,0 +1,22 @@
+package tasks;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskManager {
+    private List<String> Tasks = new ArrayList<>();
+
+    private void Print_List(){
+
+    }
+
+
+    public void Input_Solver(String input_str){
+        if (input_str == "list"){
+
+        }
+    }
+
+
+
+}
--- src/main/java/ui/HelloDuke.java
+++ src/main/java/ui/HelloDuke.java
@@ -1,4 +1,38 @@
 package ui;
 
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class HelloDuke {
+    private static  String  WELCOME_LOGO = " ";
+
+    private static  String EXIT_LOGO = " ";
+
+    private  static String ERROR_INFORM = " ";
+
+    public static void Start(){
+        System.out.println(WELCOME_LOGO);
+    }
+
+    public static void  Exit(){
+        System.out.println(EXIT_LOGO);
+    }
+
+    public static void HandleError(){
+        System.out.println((ERROR_INFORM));
+    }
+
+    public static void Print_Out(String s){
+        String output_string = " ";
+        System.out.println(output_string);
+    }
+
+    public  static void Print_List(List<String> strs){
+        AtomicInteger ctr = new AtomicInteger();
+        StringBuilder strBldr = new StringBuilder();
+
+        System.out.println(strBldr.toString());
+    }
+
+
 }
