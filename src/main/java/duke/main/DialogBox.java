11b82b2b92b418eab65254805cc070edf3eb8ece
Dban1 <huggavamp@gmail.com>
2020-02-19 22:35:00 +0800

    -reverted font to working font (Arial, 11, caused ellipses replies from Duke)

--- src/main/java/duke/main/DialogBox.java
+++ src/main/java/duke/main/DialogBox.java
@@ -10,7 +10,6 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
-import javafx.scene.text.Font;
 
 import java.io.IOException;
 import java.util.Collections;
@@ -32,7 +31,6 @@ public class DialogBox extends HBox {
         }
 
         dialog.setText(text);
-        dialog.setFont(new Font("Arial", 11));
         displayPicture.setImage(img);
     }
 
