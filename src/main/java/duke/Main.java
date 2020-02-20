743fe88bdd8cb9a883344b6d38b41a374cbddfb8
Denise-Ng <denise.ngkl@gmail.com>
2020-02-20 23:18:00 +0800

    Fixed bugs in build.gradle

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -25,6 +25,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Ding Ding!");
             fxmlLoader.<MainWindow>getController().setDuke(duke);
             fxmlLoader.<MainWindow>getController().setStage(stage);
             stage.show();
