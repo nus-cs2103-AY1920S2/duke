2bcaef723a123bedfcf46db5cfee23d61f227ae7
Hanming Zhu <hanming@huawen.com>
2020-02-20 23:59:30 +0800

    tests: add tests for save state and attempt to debug deadline test
    SaveStateTest has been added.
    
    Will be using logging to debug DeadlineTest on Travis.

---
+++ src/test/java/duke/save/SaveStateTest.java
@@ -0,0 +1,32 @@
+package duke.save;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+import duke.task.TaskList;
+
+/**
+ * JUnit test class for {@code SaveState}
+ */
+public class SaveStateTest {
+    @Test
+    public void testConstructor() {
+        TaskList taskListMock = mock(TaskList.class);
+        when(taskListMock.getImmutableDeepCopy()).thenReturn(new ArrayList<>());
+
+        SaveState saveState = new SaveState(taskListMock, "Test command");
+        verify(taskListMock, times(1)).getImmutableDeepCopy();
+        verifyNoMoreInteractions(taskListMock);
+
+        assertEquals(saveState.getLastCommand(), "Test command");
+        assertEquals(0, saveState.getTasksFromSave().size());
+    }
+}
--- src/test/java/duke/task/DeadlineTest.java
+++ src/test/java/duke/task/DeadlineTest.java
@@ -1,12 +1,15 @@
 package duke.task;
 
-import org.junit.jupiter.api.Test;
-
-import java.time.LocalDateTime;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.junit.jupiter.api.Test;
 
 /**
  * JUnit test class for {@code Deadline}.
@@ -28,11 +31,14 @@ public class DeadlineTest {
     /**
      * Tests the overridden {@code toString()} method.
      */
-    //    @Test
-    //    public void testToString() {
-    //        assertEquals("[D][\u2718] Deadline!! (by: Dec 30 1900 6:00 pm [OVERDUE])", // cross mark
-    //                new Deadline("Deadline!!", LocalDateTime.of(1900, 12, 30, 18, 0)).toString());
-    //        assertEquals("[D][\u2713] brunch (by: Today 5:00 pm)", // tick mark
-    //                new Deadline("brunch", LocalDate.now().atTime(17, 0), true).toString());
-    //    }
+    @Test
+    public void testToString() {
+        String resultOne = new Deadline("Deadline!!", LocalDateTime.of(1900, 12, 30, 18, 0)).toString();
+        String resultTwo = new Deadline("brunch", LocalDate.now().atTime(17, 0), true).toString();
+        Logger logger = Logger.getLogger("Deadline Test");
+        logger.log(Level.WARNING, resultOne);
+        logger.log(Level.WARNING, resultTwo);
+        assertEquals("[D][\u2718] Deadline!! (by: Dec 30 1900 6:00 pm [OVERDUE])", resultOne); // cross mark
+        assertEquals("[D][\u2713] brunch (by: Today 5:00 pm)", resultTwo); // tick mark
+    }
 }
