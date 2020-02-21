2e6227d8e3822f4d001a6c1f5fb8b4aa14bb51ea
buddhavineeth <buddhavineeth@gmail.com>
2020-02-21 16:52:35 +0800

    Make JavaFX version backwards compatible

--- src/test/java/duke/task/TodoTest.java
+++ src/test/java/duke/task/TodoTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TodoTest {
     @Test
-    public void testStringConversion(){
+    public void testStringConversion() {
         //a unit test for toString() method of "T o d o" class
         assertEquals("Todo: [Pending] Level-9",
                 new Todo("Level-9").toString());
