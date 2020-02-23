218cfc58bcdbb67f533e9823146bda0f4cbab891
gyant <e0325592@u.nus.edu>
2020-02-23 18:37:31 +0800

    Prints list of available command

--- src/main/java/duke/exceptions/UnrecognizedCommandException.java
+++ src/main/java/duke/exceptions/UnrecognizedCommandException.java
@@ -1,8 +1,14 @@
 package duke.exceptions;
 
+import duke.ui.Ui;
+
 public class UnrecognizedCommandException extends DukeException {
     @Override
     public String toString() {
-        return "Your command is not recognized";
+        StringBuilder output = new StringBuilder();
+        output.append("Patrick, I don't recognize this command :(\n");
+        output.append("These are the commands you can use:\n");
+        output.append(Ui.printCommands());
+        return output.toString();
     }
 }
