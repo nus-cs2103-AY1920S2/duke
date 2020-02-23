6a2d09462ad35c8f1f698d9f2116635af7d532eb
gyant <e0325592@u.nus.edu>
2020-02-23 21:54:42 +0800

    Remove unused import

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -14,8 +14,6 @@ import duke.tasks.TaskList;
 import duke.parser.Parser;
 
 import java.io.IOException;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 public class Duke {
 
