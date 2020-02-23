0f8e4626a7365a1632fa5dc290293f062c1c6e87
ivanteejj <58815457+ivanteejj@users.noreply.github.com>
2020-02-23 17:57:06 +0800

    Completed User Guide

--- src/main/java/dukecommand/HelpCommand.java
+++ src/main/java/dukecommand/HelpCommand.java
@@ -43,9 +43,10 @@ public class HelpCommand extends DukeCommand {
             "    All available commands are: \n"
                     + "    1. 'help' to see all available commands.\n"
                     + "    2. 'list' to see all listed tasks.\n"
-                    + "    3. 'todo x' where x is an event description to note a To-Do event.\n"
-                    + "    4. 'deadline x /by y' where is x is a description and y is a date in YYYY-MM-DD format.\n"
-                    + "    5. 'event x /at y' where x is an event description and y is a date in YYYY-MM-DD format.\n"
-                    + "    6. 'delete x' where x in the task in the list you want to delete (1-indexed).\n"
-                    + "    7. 'find x where x is a 1-word long string description'";
+                    + "    3. 'done x' where x is the index of the event to mark the task as completed.\n"
+                    + "    4. 'todo x' where x is an event description to note a To-Do event.\n"
+                    + "    5. 'deadline x /by y' where is x is a description and y is a date in YYYY-MM-DD format.\n"
+                    + "    6. 'event x /at y' where x is an event description and y is a date in YYYY-MM-DD format.\n"
+                    + "    7. 'delete x' where x in the task in the list you want to delete (1-indexed).\n"
+                    + "    8. 'find x where x is a 1-word long string description'";
 }
