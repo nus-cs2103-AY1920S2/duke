1fe699ed0f5cd03d4bef549acfc40177319838b5
gyant <e0325592@u.nus.edu>
2020-02-23 17:33:32 +0800

    Change ImageView to Circle

--- src/main/java/duke/DialogBox.java
+++ src/main/java/duke/DialogBox.java
@@ -11,14 +11,15 @@ import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.*;
+import javafx.scene.shape.*;
 
 public class DialogBox extends HBox {
     @FXML
     private Label dialog;
     @FXML
-    private ImageView displayPicture;
+    private Circle displayPicture;
 
     private DialogBox(String text, Image img) {
         try {
@@ -31,7 +32,7 @@ public class DialogBox extends HBox {
         }
 
         dialog.setText(text);
-        displayPicture.setImage(img);
+        displayPicture.setFill(new ImagePattern(img));
     }
 
     /**
