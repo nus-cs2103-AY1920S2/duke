504d01807404b17eaed136debc26fafd6663fc00
gyant <e0325592@u.nus.edu>
2020-02-24 13:26:47 +0800

    Add test method

--- src/test/java/duke/ParserTest.java
+++ src/test/java/duke/ParserTest.java
@@ -10,16 +10,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ParserTest {
     @Test
-    public void parseInputTest() {
+    public void parseInputTest() throws DukeException {
         TaskList dummyList = new TaskList();
-        Storage dummyStorage = new Storage("");
-        /*
-        try {
-            assertEquals("Your command is not recognized", Parser.parse("Hello World", dummyList, dummyStorage));
-        } catch (DukeException e) {
-            e.printStackTrace();
-        }
+        Storage dummyStorage = new Storage("data/test.txt");
 
-         */
+        // Expected output
+        StringBuilder expected = new StringBuilder();
+        expected.append("You've added this Todo task!\n"
+                + "[T][\u2718] Borrow book" + "\n"
+                + "Now you have " + "1 tasks in the list\n");
+
+        assertEquals(expected.toString(),
+                Parser.parse("Todo Borrow book", dummyList, dummyStorage));
     }
 }
