e321e15af47a320dc3e779339dfbba24e735dbfe
johan.kok <johan.kok@grabtaxi.com>
2020-02-25 16:28:11 +0800

    Added javadocs for Parser class

--- src/main/java/com/nus/duke/parser/Parser.java
+++ src/main/java/com/nus/duke/parser/Parser.java
@@ -3,22 +3,30 @@ package com.nus.duke.parser;
 import javafx.util.Pair;
 
 public class Parser {
+
+    /**
+     * <p> Splits an input string based on a white space character </p>
+     *
+     * @param   input       Input string
+     * @return              (left, right) split of the string
+     * @see                 com.nus.duke.parser.Parser#tokenize(String, String)
+     */
     public static Pair<String, String> tokenize(String input) {
-        /**
-         By default splits the string by the first whitespace character
-         */
         return tokenize(input, " ");
     }
 
+    /**
+     * <p>  Splits a string into a (left, right) pair based on a supplied expression
+     *      For this application, the left is deemed as the command issued by the user
+     *      and the right is deemed as the condition
+     * </p>
+     *
+     * @param    input          text string, such as "list books"
+     * @param    expr           Split-by expression
+     * @return                  Pair of (command, condition)
+     */
     public static Pair<String, String> tokenize(String input, String expr) {
-        /**
-         Splits a string into a (command, condition) pair
 
-         @param input
-         input text string, such as "list books"
-
-         @return Pair of (command, condition)
-         */
         String[] res        = input.split(expr, 2);
         String command      = res[0];
         String condition    = res.length > 1 ? res[1] : null;
