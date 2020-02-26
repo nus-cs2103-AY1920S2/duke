65cc2b3d0acfa98f45557f8fd88f1aabaef32bd6
Mikhail Faiz <mikhailfaiz@hotmail.com>
2020-02-26 16:43:03 +0800

    changed code to adheret to coding standards

--- src/main/java/duke/util/Parser.java
+++ src/main/java/duke/util/Parser.java
@@ -19,18 +19,20 @@ public class Parser {
             taskDateTime = taskArr[1].trim();
         }
         switch (taskType) {
-            case "T":
-                Todo t = new Todo(taskDesc);
-                parsedTask = t;
-                break;
-            case "D":
-                Deadline d = new Deadline(taskDesc, taskDateTime);
-                parsedTask = d;
-                break;
-            case "E":
-                Event e = new Event(taskDesc, taskDateTime);
-                parsedTask = e;
-                break;
+        case "T":
+            Todo t = new Todo(taskDesc);
+            parsedTask = t;
+            break;
+        case "D":
+            Deadline d = new Deadline(taskDesc, taskDateTime);
+            parsedTask = d;
+            break;
+        case "E":
+            Event e = new Event(taskDesc, taskDateTime);
+            parsedTask = e;
+            break;
+        default:
+            break;
         }
         return parsedTask;
     }
