f82b68ee7ac67d2d017ddaea78cb53b2138e1ef9
watsheldon <chinaempire.cjw@gmail.com>
2020-02-27 19:13:04 +0800

    adjust greetings for gui

--- src/main/java/duke/command/HeyCommand.java
+++ src/main/java/duke/command/HeyCommand.java
@@ -12,13 +12,7 @@ public class HeyCommand extends Command {
 
     @Override
     public String execute(TaskList tasklist, Ui ui, Storage storage) {
-        String logo = " ______ _____  _____ _____ \n"
-                + "|  ____|  __ \\|_   _/ ____|\n"
-                + "| |__  | |__) | | || | \n"
-                + "|  __| |  _  /  | || | \n"
-                + "| |____| | \\ \\ _| || |__\n"
-                + "|______|_|  \\_\\_____\\_____|\n";
-        String greetings = "Hi Stan! This is\n" + logo + "\nGive me some things to do so I can lose some weight!\n";
+        String greetings = "Hi Stan! This is Eric! Give me some things to do so I can lose some weight!\n";
         return greetings;
     }
 }
