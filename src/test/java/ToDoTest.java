d5472422734641afe8437185c53c5a790e8959f2
WillySeahh <willy.seah@hotmail.com>
2020-02-29 17:51:39 +0800

    Reccess Week updateV2!

---
+++ src/test/java/ToDoTest.java
@@ -0,0 +1,20 @@
+import dukeclasses.ToDo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+/**
+ * Tests the ToDo Class
+ */
+public class ToDoTest {
+
+    /**
+     * Tests that the descriptions are correct.
+     */
+    @Test
+    public void testToDoDescription() {
+        ToDo task = new ToDo("testing");
+        assertEquals("testing", task.getDescription());
+    }
+}
