c9c1f5cffd52dee13db41798198cdbf5fbe6b652
grrrrnt <grantlee97@gmail.com>
2020-03-01 06:14:29 +0800

    Get final release version ready

--- src/main/java/DialogBox.java
+++ src/main/java/DialogBox.java
@@ -1,4 +1,5 @@
-import javafx.geometry.Pos;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
@@ -7,10 +8,8 @@ import javafx.collections.ObservableList;
 import javafx.scene.Node;
 import java.io.IOException;
 import java.util.Collections;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.image.Image;
-
+import javafx.geometry.Pos;
 
 /**
  * An example of a custom control using FXML.
