6036f51a49322d05d0f43c6456f4186f9ed1ac2a
Aloysius Chan <alloystory@gmail.com>
2020-02-29 12:32:56 +0800

    change ByeCommand output

--- src/main/java/app/core/commands/ByeCommand.java
+++ src/main/java/app/core/commands/ByeCommand.java
@@ -1,11 +1,12 @@
 package app.core.commands;
 
+import app.core.Messages;
 import app.core.tasks.TaskManager;
 import app.util.Pair;
 
 final class ByeCommand extends Command {
     @Override
     public Pair execute(TaskManager taskManager) {
-        return new Pair("", true);
+        return new Pair(Messages.GOODBYE_MESSAGE, true);
     }
 }
