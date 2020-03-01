b2ac06cb5bf9c8d2603399d45cca577e9a36c165
Hanming Zhu <hanming@huawen.com>
2020-03-01 16:46:15 +0800

    tests(SaveStateTest): update the test to test getters
    Previously it only tested the constructor. Now it tests the getters as well.

--- src/test/java/duke/save/SaveStateTest.java
+++ src/test/java/duke/save/SaveStateTest.java
@@ -1,6 +1,7 @@
 package duke.save;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -17,16 +18,30 @@ import duke.task.TaskList;
  * JUnit test class for {@code SaveState}.
  */
 public class SaveStateTest {
+    /**
+     * Tests the constructor of {@code SaveState}.
+     */
     @Test
     public void testConstructor() {
         TaskList taskListMock = mock(TaskList.class);
         when(taskListMock.getImmutableDeepCopy()).thenReturn(new ArrayList<>());
 
-        SaveState saveState = new SaveState(taskListMock, "Test command");
+        new SaveState(taskListMock, "Test command");
         verify(taskListMock, times(1)).getImmutableDeepCopy();
         verifyNoMoreInteractions(taskListMock);
+    }
 
+    /**
+     * Tests the getters of {@code SaveState}.
+     */
+    @Test
+    public void testGetters() {
+        TaskList taskListMock = mock(TaskList.class);
+        when(taskListMock.getImmutableDeepCopy()).thenReturn(new ArrayList<>());
+
+        SaveState saveState = new SaveState(taskListMock, "Test command");
         assertEquals(saveState.getLastCommand(), "Test command");
         assertEquals(0, saveState.getTasksFromSave().size());
+        assertNotNull(saveState.getTasksFromSave());
     }
 }
