5b36ab90fa1c0cd680892924e7964b74b1c562f4
HemanshuGandhi <hemanshu.gandhi@gmail.com>
2020-03-02 23:18:51 +0800

    Update JavaDoc for command/Command class

--- src/main/java/duke/command/Command.java
+++ src/main/java/duke/command/Command.java
@@ -4,6 +4,18 @@ import duke.Ui.Ui;
 import duke.storage.TaskStorage;
 import exception.IllegalTextException;
 
+/**
+ * Represents the command to be executed, in response to user inputs.
+ */
 public abstract class Command {
+
+    /**
+     * Returns a response string through the Ui after carrying out the business logic
+     * corresponding to the command type.
+     * @param ui
+     * @param storage
+     * @return A string corresponding to the response message after executing the command.
+     * @throws IllegalTextException
+     */
     public abstract String execute(Ui ui, TaskStorage storage) throws IllegalTextException;
 }
