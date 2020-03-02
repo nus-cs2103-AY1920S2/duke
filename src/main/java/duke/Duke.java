8fe920509b2c8d3956d80646ff404cf4cd2e2a16
HemanshuGandhi <hemanshu.gandhi@gmail.com>
2020-03-02 22:22:32 +0800

    Update JavaDoc for Duke class

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -4,15 +4,28 @@ import duke.Ui.Ui;
 import duke.command.Command;
 import duke.parser.Parser;
 import duke.storage.TaskStorage;
-import duke.task.Task;
 
 import java.io.FileNotFoundException;
 
+/**
+ * Represents the Chat Bot feature of the application.
+ * Comprises of a User Interface, and Storage for app data.
+ */
 public class Duke {
-    // attributes
+    /**
+     * The Storage object with in-memory and persistently stored data.
+     */
     private TaskStorage taskStorage;
+
+    /**
+     * The Ui object that gives feedback to the user.
+     */
     private Ui ui;
 
+    /**
+     * Initialises a Duke object.
+     * The Duke constructor takes in no parameters.
+     */
     public Duke() {
         try {
             this.ui = new Ui();
@@ -38,47 +51,5 @@ public class Duke {
         } catch (Exception e) {
             return this.ui.showErrorMessage(e);
         }
-
-    }
-
-    // Print a closing message before stopping duke.Duke
-    private static String stopDuke() {
-        String closingMessage = "Bye. Hope to see you again soon!";
-        return "    " + closingMessage;
-    }
-
-    // duke.task.Todo: abstract away the following logic from main via new interface for command handlers
-
-    private void handleCommandList() {
-        System.out.println("    Here are the tasks in your list:");
-        int counter = 1;
-        for (Task task : this.taskStorage.getTaskList()) {
-            System.out.println("    " + counter + ". " + task);
-            counter++;
-        }
-    }
-
-    private void handleCommandAdd(Task newTask) {
-        this.taskStorage.addToTaskList(newTask);
-        System.out.println("    Got it. I've added this task:\n"
-                + "      " + newTask);
-        System.out.println("\n    Now you have " + this.taskStorage.getTaskList().size() + " tasks in the list.");
-    }
-
-    private void handleCommandDone(int taskNumber) {
-        this.taskStorage.markTaskAsDone(taskNumber);
-        System.out.println("Nice! I've marked this task as done:\n"
-                + "    " + this.taskStorage.getTaskList().get(taskNumber - 1));
-    }
-
-    private void handleCommandDelete(int taskNumber) {
-        System.out.println("    Noted. I've removed this task:\n"
-                + "    " + this.taskStorage.getTaskList().get(taskNumber - 1));
-        this.taskStorage.deleteFromTaskList(taskNumber);
-        System.out.println("    Now you have " + this.taskStorage.getTaskList().size() + " tasks in the list.");
-    }
-
-    public static void main(String[] args) {
-        //new Duke().runDuke();
     }
 }
