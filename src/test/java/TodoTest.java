7514ca0d4c351cfcabcaddc8fcb986dbff45f489
duongphammmm <46336643+duongphammmm@users.noreply.github.com>
2020-03-02 23:11:00 +0800

    Added more tests

--- src/test/java/TodoTest.java
+++ src/test/java/TodoTest.java
@@ -1,11 +1,12 @@
-import org.junit.jupiter.api.DynamicTest;
+import duke.commons.Todo;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TodoTest {
     @Test
-    public void dummyTest() {
-        assertEquals(2, 2);
+    public void constructorTest() {
+        Todo todo = new Todo("todo", false, "Revise CS3230");
+        assertEquals(todo.toString(), "[ T ] [ ✗ ] Revise CS3230");
     }
 }
