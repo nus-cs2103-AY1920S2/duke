2713c3ead261e954d935458acacabb0c29c009a1
HemanshuGandhi <hemanshu.gandhi@gmail.com>
2020-03-02 22:35:23 +0800

    Remove unused import from Ui/DialogBox

--- src/main/java/duke/Ui/MainWindow.java
+++ src/main/java/duke/Ui/MainWindow.java
@@ -3,12 +3,12 @@ package duke.Ui;
 import duke.Duke;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Dialog;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+
 /**
  * Controller for MainWindow. Provides the layout for the other controls.
  */
