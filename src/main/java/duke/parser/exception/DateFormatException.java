8fcb59d849810f384ac2deb4b47fc8915f584bb7
PotatoCombat <39809238+PotatoCombat@users.noreply.github.com>
2020-03-02 10:25:13 +0800

    Add date and time utilities

--- src/main/java/duke/parser/exception/DateFormatException.java
+++ src/main/java/duke/parser/exception/DateFormatException.java
@@ -8,6 +8,6 @@ public class DateFormatException extends ParseException {
      * Constructs a new {@code DateFormatException} when parsing a date with an invalid format.
      */
     public DateFormatException() {
-        super("I need the date in this format:\n yyyy-mm-dd.");
+        super("I need the date in this format:\n yyyy-MM-dd.");
     }
 }
