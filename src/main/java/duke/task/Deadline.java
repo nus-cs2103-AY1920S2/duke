de63c93c97b298698569a3896052719747f68f42
HemanshuGandhi <hemanshu.gandhi@gmail.com>
2020-03-02 22:54:22 +0800

    Update JavaDoc for task/Deadline class

--- src/main/java/duke/task/Deadline.java
+++ src/main/java/duke/task/Deadline.java
@@ -1,14 +1,32 @@
 package duke.task;
 
+/**
+ * Represents a deadline to meet that contains both a description and a deadline time.
+ */
 public class Deadline extends Task {
+    /**
+     * The time of the Deadline, as input by the user.
+     */
     protected String by;
 
+    /**
+     * Constructor to create a Deadline task..
+     * @param description The description of the Deadline task, as input by the user.
+     * @param by the time of the Deadline task, as input by the user.
+     */
     public Deadline(String description, String by) {
         super(description);
         this.by = by;
     }
 
-    // duke.task.Todo: Handle exceptional cases
+    /**
+     * Takes in the arguments after the command keyword as input by the user.
+     * Returns the Deadline task object, if successful, based on the input.
+     * If incorrect arguments were specified, throws an IllegalArgumentException.
+     * @param commandArgs the arguments after the command keyword as input by the user.
+     * @return DeadlineTask object, if successful.
+     * @throws IllegalArgumentException if user entered incorrect arguments.
+     */
     public static Task createTask(String[] commandArgs) throws IllegalArgumentException {
         String description = "";
         String by = "";
